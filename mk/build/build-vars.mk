fddb4c13ff87d21d907a7d0879f2f29c9c6e8b51
Anthony Mallet <mallet@jiminy.laas.fr>
2006-12-02 00:44:41 +0100

    import build from pkgsrc

---
+++ mk/build/build-vars.mk
@@ -0,0 +1,79 @@
+# $NetBSD: bsd.build-vars.mk,v 1.3 2006/09/09 02:35:13 obache Exp $
+#
+# BUILD_DIRS is the list of directories in which to perform the build
+#	process.  If the directories are relative paths, then they
+#	are assumed to be relative to ${WRKSRC}.
+#
+# MAKE_PROGRAM is the path to the make executable that is run to
+#	process the source makefiles.  This is always overridden by
+#	the tools framework in pkgsrc/mk/tools/make.mk, but we provide
+#	a default here for documentation purposes.
+#
+# MAKE_ENV is the shell environment that is exported to the make
+#	process.
+#
+# MAKE_FLAGS is a list of arguments that is pass to the make process.
+#
+# MAKE_FILE is the path to the makefile that is processed by the make
+#	executable.  If the path is relative, then it is assumed to
+#	be relative to each directory listed in BUILD_DIRS.
+#
+BUILD_DIRS?=	${CONFIGURE_DIRS}
+MAKE_PROGRAM?=	${MAKE}
+MAKE_ENV?=	# empty
+MAKE_FLAGS?=	# empty
+MAKE_FILE?=	Makefile
+
+MAKE_ENV+=	${ALL_ENV}
+ifndef NO_EXPORT_CPP
+MAKE_ENV+=	CPP=${CPP}
+endif
+MAKE_ENV+=	LOCALBASE=${LOCALBASE}
+MAKE_ENV+=	NO_WHOLE_ARCHIVE_FLAG=${NO_WHOLE_ARCHIVE_FLAG}
+MAKE_ENV+=	WHOLE_ARCHIVE_FLAG=${WHOLE_ARCHIVE_FLAG}
+MAKE_ENV+=	PKGMANDIR=${PKGMANDIR}
+
+# Add these bits to the environment used when invoking the recursive make
+# processes for build-related phases.
+#
+PKGSRC_MAKE_ENV+=	PATH=${PATH}
+
+
+# The following are the "public" targets provided by this module:
+#
+#    build
+#
+# The following targets may be overridden in a package Makefile:
+#
+#    pre-build, do-build, post-build
+#
+
+_COOKIE.build=  ${WRKDIR}/.build_done
+
+
+# --- build (PUBLIC) -------------------------------------------------
+#
+# build is a public target to build the sources for the package.
+#
+.PHONY: build
+ifndef NO_BUILD
+  include ${PKGSRCDIR}/mk/build/build.mk
+else
+  ifeq (yes,$(call exists,${_COOKIE.build}))
+build:
+	@${DO_NADA}
+  else
+build: configure build-cookie pkginstall
+  endif
+endif
+
+
+# --- build-cookie (PRIVATE) -----------------------------------------
+#
+# build-cookie creates the "build" cookie file.
+#
+.PHONY: build-cookie
+build-cookie:
+	${_PKG_SILENT}${_PKG_DEBUG}${TEST} ! -f ${_COOKIE.build} || ${FALSE}
+	${_PKG_SILENT}${_PKG_DEBUG}${MKDIR} $(dir ${_COOKIE.build})
+	${_PKG_SILENT}${_PKG_DEBUG}${ECHO} ${PKGNAME} > ${_COOKIE.build}
---
+++ mk/build/build.mk
@@ -0,0 +1,100 @@
+# $NetBSD: build.mk,v 1.9 2006/11/09 02:53:15 rillig Exp $
+#
+# This file defines what happens in the build phase, excluding the
+# self-test, which is defined in test.mk.
+#
+# Package-settable variables:
+#
+# BUILD_MAKE_FLAGS is the list of arguments that is passed to the make
+#	process, in addition to the usual MAKE_FLAGS.
+#
+# BUILD_TARGET is the target from ${MAKE_FILE} that should be invoked
+#	to build the sources.
+#
+# Variables defined in this file:
+#
+# BUILD_MAKE_CMD
+#	This command sets the proper environment for the build phase
+#	and runs make(1) on it. It takes a list of make targets and
+#	flags as argument.
+#
+
+BUILD_MAKE_FLAGS?=	# none
+BUILD_TARGET?=		all
+
+BUILD_MAKE_CMD= \
+	${SETENV} ${MAKE_ENV}						\
+		${MAKE_PROGRAM} ${_MAKE_JOBS}				\
+			${MAKE_FLAGS} ${BUILD_MAKE_FLAGS}		\
+			-f ${MAKE_FILE}
+
+
+# --- build (PUBLIC) -------------------------------------------------
+#
+# build is a public target to build the sources from the package.
+#
+_BUILD_TARGETS+=	configure
+_BUILD_TARGETS+=	acquire-build-lock
+_BUILD_TARGETS+=	${_COOKIE.build}
+_BUILD_TARGETS+=	release-build-lock
+#_BUILD_TARGETS+=	pkginstall
+
+.PHONY: build
+ifeq (yes,$(call exists,${_COOKIE.build}))
+build:
+	@${DO_NADA}
+else
+build: ${_BUILD_TARGETS}
+endif
+
+.PHONY: acquire-build-lock release-build-lock
+acquire-build-lock: acquire-lock
+release-build-lock: release-lock
+
+ifeq (yes,$(call exists,${_COOKIE.build}))
+${_COOKIE.build}:
+	@${DO_NADA}
+else
+${_COOKIE.build}: real-build
+endif
+
+# --- real-build (PRIVATE) -------------------------------------------
+#
+# real-build is a helper target onto which one can hook all of the
+# targets that do the actual building of the sources.
+#
+_REAL_BUILD_TARGETS+=	build-message
+#_REAL_BUILD_TARGETS+=	build-vars
+#_REAL_BUILD_TARGETS+=	pre-build-checks-hook
+_REAL_BUILD_TARGETS+=	pre-build
+_REAL_BUILD_TARGETS+=	do-build
+_REAL_BUILD_TARGETS+=	post-build
+_REAL_BUILD_TARGETS+=	build-cookie
+_REAL_BUILD_TARGETS+=	error-check
+
+.PHONY: real-build
+real-build: ${_REAL_BUILD_TARGETS}
+
+.PHONY: build-message
+build-message:
+	@${PHASE_MSG} "Building for ${PKGNAME}"
+
+
+# --- pre-build, do-build, post-build (PUBLIC, override) -------------
+#
+# {pre,do,post}-build are the heart of the package-customizable
+# build targets, and may be overridden within a package Makefile.
+#
+.PHONY: pre-build do-build post-build
+
+do%build:
+	${_OVERRIDE_TARGET}
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+$(foreach _dir_,${BUILD_DIRS},						\
+	cd ${WRKSRC} && cd ${_dir_} &&					\
+	${BUILD_MAKE_CMD} ${BUILD_TARGET};				\
+)
+
+pre-build:
+
+post-build:
