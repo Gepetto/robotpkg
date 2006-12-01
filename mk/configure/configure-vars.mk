8d66d7415ae9bab382d877ac3810adb8f0afdb3f
Anthony Mallet <mallet@jiminy.laas.fr>
2006-12-02 00:44:23 +0100

    import configure from pkgsrc

---
+++ mk/configure/configure-vars.mk
@@ -0,0 +1,64 @@
+# $NetBSD: bsd.configure-vars.mk,v 1.1 2006/07/05 06:09:15 jlam Exp $
+#
+# CONFIGURE_DIRS is the list of directories in which to run the
+#	configure process.  If the directories are relative paths,
+#	then they are assumed to be relative to ${WRKSRC}.
+#
+# SCRIPTS_ENV is the shell environment passed to scripts invoked
+#	by pkgsrc.
+#
+CONFIGURE_DIRS?=	${WRKSRC}
+SCRIPTS_ENV?=		# empty
+
+SCRIPTS_ENV+=	${ALL_ENV}
+SCRIPTS_ENV+=	_PKGSRCDIR=${_PKGSRCDIR}
+SCRIPTS_ENV+=	CURDIR=${CURDIR}
+SCRIPTS_ENV+=	DEPENDS=$(call quote,${DEPENDS})
+SCRIPTS_ENV+=	DISTDIR=${DISTDIR}
+SCRIPTS_ENV+=	FILESDIR=${FILESDIR}
+SCRIPTS_ENV+=	LOCALBASE=${LOCALBASE}
+SCRIPTS_ENV+=	PATCHDIR=${PATCHDIR}
+SCRIPTS_ENV+=	PKGSRCDIR=${PKGSRCDIR}
+SCRIPTS_ENV+=	SCRIPTDIR=${SCRIPTDIR}
+SCRIPTS_ENV+=	WRKDIR=${WRKDIR}
+SCRIPTS_ENV+=	WRKSRC=${WRKSRC}
+
+# The following are the "public" targets provided by this module:
+#
+#    configure
+#
+# The following targets may be overridden in a package Makefile:
+#
+#    pre-configure, do-configure, post-configure
+#
+
+_COOKIE.configure=      ${WRKDIR}/.configure_done
+
+
+# --- configure (PUBLIC) ---------------------------------------------
+#
+# configure is a public target to configure the software for building.
+#
+.PHONY: configure
+ifndef NO_CONFIGURE
+  include ${PKGSRCDIR}/mk/configure/configure.mk
+else
+  ifeq (yes,$(call exists,${_COOKIE.configure}))
+configure:
+	@${DO_NADA}
+  else
+#configure: wrapper configure-cookie
+configure: patch configure-cookie
+  endif
+endif
+
+
+# --- configure-cookie (PRIVATE) -------------------------------------
+#
+# configure-cookie creates the "configure" cookie file.
+#
+.PHONY: configure-cookie
+configure-cookie:
+	${_PKG_SILENT}${_PKG_DEBUG}${TEST} ! -f ${_COOKIE.configure} || ${FALSE}
+	${_PKG_SILENT}${_PKG_DEBUG}${MKDIR} $(dir ${_COOKIE.configure})
+	${_PKG_SILENT}${_PKG_DEBUG}${ECHO} ${PKGNAME} > ${_COOKIE.configure}
---
+++ mk/configure/configure.mk
@@ -0,0 +1,153 @@
+# $NetBSD: configure.mk,v 1.12 2006/11/09 02:53:15 rillig Exp $
+#
+# CONFIGURE_SCRIPT is the path to the script to run in order to
+#	configure the software for building.  If the path is relative,
+#	then it is assumed to be relative to each directory listed in
+#	CONFIGURE_DIRS.
+#
+# CONFIGURE_ENV is the shell environment that is exported to the
+#	configure script.
+#
+# CONFIGURE_ARGS is the list of arguments that is passed to the
+#	configure script.
+#
+CONFIGURE_SCRIPT?=	./configure
+CONFIGURE_ENV+=		${ALL_ENV}
+CONFIGURE_ARGS?=	# empty
+_BUILD_DEFS+=		CONFIGURE_ENV CONFIGURE_ARGS
+
+ifdef GNU_CONFIGURE
+  include ${PKGSRCDIR}/mk/configure/gnu-configure.mk
+endif
+#.if defined(OVERRIDE_GNU_CONFIG_SCRIPTS)
+#.  include "${PKGSRCDIR}/mk/configure/config-override.mk"
+#.endif
+#.if defined(USE_LIBTOOL)
+#.  include "${PKGSRCDIR}/mk/configure/libtool-override.mk"
+#.endif
+#.if defined(PKGCONFIG_OVERRIDE)
+#.  include "${PKGSRCDIR}/mk/configure/pkg-config-override.mk"
+#.endif
+#.include "${PKGSRCDIR}/mk/configure/replace-interpreter.mk"
+#.if defined(USE_PKGLOCALEDIR)
+#.  include "${PKGSRCDIR}/mk/configure/replace-localedir.mk"
+#.endif
+
+
+# --- configure (PUBLIC) ---------------------------------------------
+#
+# configure is a public target to configure the sources for building.
+#
+_CONFIGURE_TARGETS+=	patch
+_CONFIGURE_TARGETS+=	acquire-configure-lock
+_CONFIGURE_TARGETS+=	${_COOKIE.configure}
+_CONFIGURE_TARGETS+=	release-configure-lock
+
+.PHONY: configure
+ifeq (yes,$(call exists,${_COOKIE.configure}))
+configure:
+	@${DO_NADA}
+else
+configure: ${_CONFIGURE_TARGETS}
+endif
+
+.PHONY: acquire-configure-lock release-configure-lock
+acquire-configure-lock: acquire-lock
+release-configure-lock: release-lock
+
+ifeq (yes,$(call exists,${_COOKIE.configure}))
+${_COOKIE.configure}:
+	@${DO_NADA}
+else
+${_COOKIE.configure}: real-configure
+endif
+
+
+# --- real-configure (PRIVATE) ---------------------------------------
+#
+# real-configure is a helper target onto which one can hook all of the
+# targets that do the actual configuration of the sources.
+#
+# Note: pre-configure-checks-hook comes after pre-configure to allow
+# packages for fixing bad files with SUBST_STAGE.* = pre-configure.
+#
+_REAL_CONFIGURE_TARGETS+=	configure-message
+#_REAL_CONFIGURE_TARGETS+=	configure-vars
+_REAL_CONFIGURE_TARGETS+=	pre-configure
+#_REAL_CONFIGURE_TARGETS+=	pre-configure-checks-hook
+_REAL_CONFIGURE_TARGETS+=	do-configure-pre-hook
+_REAL_CONFIGURE_TARGETS+=	do-configure
+_REAL_CONFIGURE_TARGETS+=	do-configure-post-hook
+_REAL_CONFIGURE_TARGETS+=	post-configure
+_REAL_CONFIGURE_TARGETS+=	configure-cookie
+#_REAL_CONFIGURE_TARGETS+=	error-check
+
+.PHONY: real-configure
+real-configure: ${_REAL_CONFIGURE_TARGETS}
+
+.PHONY: configure-message
+configure-message:
+	@${PHASE_MSG} "Configuring for ${PKGNAME}"
+
+
+# --- do-configure-pre-hook (PRIVATE) --------------------------------
+#
+# do-configure-pre-hook is a helper target onto which one can hook
+# all of the targets that should be run after pre-configure but before
+# do-configure.  These targets typically edit the files used by the
+# do-configure target.
+#
+.PHONY: do-configure-pre-hook
+do-configure-pre-hook:
+
+
+# --- do-configure-post-hook (PRIVATE) -------------------------------
+#
+# do-configure-post-hook is a helper target onto which one can hook
+# all of the targets that should be run after do-configure but before
+# post-configure.  These targets typically edit the files generated
+# by the do-configure target that are used during the build phase.
+#
+.PHONY: do-configure-post-hook
+do-configure-post-hook:
+
+
+# --- do-configure-script (PRIVATE) ----------------------------------
+#
+# do-configure-script runs the configure script to configure the
+# software for building.
+#
+_CONFIGURE_SCRIPT_ENV+=	INSTALL=${INSTALL}\ -c\ -o\ ${BINOWN}\ -g\ ${BINGRP}
+_CONFIGURE_SCRIPT_ENV+=	INSTALL_PROGRAM=${INSTALL_PROGRAM}
+_CONFIGURE_SCRIPT_ENV+=	INSTALL_SCRIPT=${INSTALL_SCRIPT}
+_CONFIGURE_SCRIPT_ENV+=	INSTALL_DATA=${INSTALL_DATA}
+_CONFIGURE_SCRIPT_ENV+=	${CONFIGURE_ENV}
+
+.PHONY: do-configure-script
+do-configure-script:
+	${_PKG_SILENT}${_PKG_DEBUG}					\
+$(foreach _dir_,${CONFIGURE_DIRS},					\
+	cd ${WRKSRC} && cd ${_dir_} &&					\
+	${SETENV} ${_CONFIGURE_SCRIPT_ENV}				\
+		${CONFIG_SHELL} ${CONFIGURE_SCRIPT} ${CONFIGURE_ARGS};	\
+)
+
+
+# --- pre-configure, do-configure, post-configure (PUBLIC, override) -
+#
+# {pre,do,post}-configure are the heart of the package-customizable
+# configure targets, and may be overridden within a package Makefile.
+#
+.PHONY: pre-configure do-configure post-configure
+
+ifdef HAS_CONFIGURE
+_DO_CONFIGURE_TARGETS+=	do-configure-script
+endif
+
+do%configure: ${_DO_CONFIGURE_TARGETS}
+	${_OVERRIDE_TARGET}
+	@${DO_NADA}
+
+pre-configure:
+
+post-configure:
