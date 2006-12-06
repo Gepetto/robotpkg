ac3978b66fc62220f9801a3f7a3bac171c92af40
Anthony Mallet <mallet@jiminy.laas.fr>
2006-12-06 22:08:28 +0100

    finish porting to gmake

--- mk/plist/plist.mk
+++ mk/plist/plist.mk
@@ -24,7 +24,7 @@
 
 PLIST_TYPE?=	static
 
-######################################################################
+# --------------------------------------------------------------------
 
 # PLIST_SRC is the source file for the generated PLIST file.  If PLIST_SRC
 # is not explicitly defined, then build one up from various PLIST.* files
@@ -47,93 +47,92 @@ endif # !PLIST_SRC
 # This is the path to the generated PLIST file.
 PLIST=		${WRKDIR}/.PLIST
 
-######################################################################
+# --------------------------------------------------------------------
 
 _LIBTOOL_EXPAND=							\
-	${SETENV} ECHO=${TOOLS_ECHO:Q} GREP=${TOOLS_GREP:Q}		\
-		SORT=${TOOLS_SORT:Q} TEST=${TOOLS_TEST:Q}		\
-	${SH} ${.CURDIR}/../../mk/plist/libtool-expand
+	${SETENV} ECHO=$(call quote,${TOOLS_ECHO})			\
+		GREP=$(call quote,${TOOLS_GREP})			\
+		SORT=$(call quote,${TOOLS_SORT})			\
+		TEST=$(call quote,${TOOLS_TEST})			\
+	${SH} ${CURDIR}/../../mk/plist/libtool-expand
 
 # _PLIST_AWK_ENV holds the shell environment passed to the awk script
 # that does post-processing of the PLIST.  See the individual *.awk
 # scripts for information on each of the variable set in the environment.
 #
-_PLIST_AWK_ENV+=	PKGLOCALEDIR=${PKGLOCALEDIR:Q}
+_PLIST_AWK_ENV+=	PKGLOCALEDIR=$(call quote,${PKGLOCALEDIR})
 _PLIST_AWK_ENV+=	USE_PKGLOCALEDIR=${USE_PKGLOCALEDIR:Dyes:Uno}
-_PLIST_AWK_ENV+=	IMAKE_MANINSTALL=${_IMAKE_MANINSTALL:Q}
-_PLIST_AWK_ENV+=	IGNORE_INFO_PATH=${_IGNORE_INFO_PATH:Q}
-_PLIST_AWK_ENV+=	PKGINFODIR=${PKGINFODIR:Q}
-_PLIST_AWK_ENV+=	IGNORE_LIBTOOLIZE=${IGNORE_LIBTOOLIZE:Q}
-_PLIST_AWK_ENV+=	LIBTOOLIZE_PLIST=${LIBTOOLIZE_PLIST:Q}
-_PLIST_AWK_ENV+=	LIBTOOL_EXPAND=${_LIBTOOL_EXPAND:Q}
-_PLIST_AWK_ENV+=	LS=${TOOLS_LS:Q}
-_PLIST_AWK_ENV+=	MANINSTALL=${MANINSTALL:Q}
-_PLIST_AWK_ENV+=	MANZ=${_MANZ:Q}
-_PLIST_AWK_ENV+=	PKGMANDIR=${PKGMANDIR:Q}
-_PLIST_AWK_ENV+=	PREFIX=${DESTDIR:Q}${PREFIX:Q}
-_PLIST_AWK_ENV+=	TEST=${TOOLS_TEST:Q}
+_PLIST_AWK_ENV+=	IMAKE_MANINSTALL=$(call quote,${_IMAKE_MANINSTALL})
+_PLIST_AWK_ENV+=	IGNORE_INFO_PATH=$(call quote,${_IGNORE_INFO_PATH})
+_PLIST_AWK_ENV+=	PKGINFODIR=$(call quote,${PKGINFODIR})
+_PLIST_AWK_ENV+=	IGNORE_LIBTOOLIZE=$(call quote,${IGNORE_LIBTOOLIZE})
+_PLIST_AWK_ENV+=	LIBTOOLIZE_PLIST=$(call quote,${LIBTOOLIZE_PLIST})
+_PLIST_AWK_ENV+=	LIBTOOL_EXPAND=$(call quote,${_LIBTOOL_EXPAND})
+_PLIST_AWK_ENV+=	LS=$(call quote,${TOOLS_LS})
+_PLIST_AWK_ENV+=	MANINSTALL=$(call quote,${MANINSTALL})
+_PLIST_AWK_ENV+=	MANZ=$(call quote,${_MANZ})
+_PLIST_AWK_ENV+=	PKGMANDIR=$(call quote,${PKGMANDIR})
+_PLIST_AWK_ENV+=	PREFIX=$(call quote,${PREFIX})
+_PLIST_AWK_ENV+=	TEST=$(call quote,${TOOLS_TEST})
 
 # PLIST_SUBST contains package-settable "${variable}" to "value"
 # substitutions for PLISTs
 #
-PLIST_SUBST+=	OPSYS=${OPSYS:Q}					\
-		OS_VERSION=${OS_VERSION:Q}				\
-		MACHINE_ARCH=${MACHINE_ARCH:Q}				\
-		MACHINE_GNU_ARCH=${MACHINE_GNU_ARCH:Q}			\
-		MACHINE_GNU_PLATFORM=${MACHINE_GNU_PLATFORM:Q}		\
-		LN=${LN:Q}						\
-		LOWER_VENDOR=${LOWER_VENDOR:Q}				\
-		LOWER_OPSYS=${LOWER_OPSYS:Q}				\
-		LOWER_OS_VERSION=${LOWER_OS_VERSION:Q}			\
-		PKGBASE=${PKGBASE:Q}					\
-		PKGNAME=${PKGNAME_NOREV:Q}				\
-		PKGLOCALEDIR=${PKGLOCALEDIR:Q}				\
-		PKGVERSION=${PKGVERSION:C/nb[0-9]*$//}			\
-		LOCALBASE=${LOCALBASE:Q}				\
-		VIEWBASE=${VIEWBASE:Q}					\
-		X11BASE=${X11BASE:Q}					\
-		X11PREFIX=${X11PREFIX:Q}				\
-		SVR4_PKGNAME=${SVR4_PKGNAME:Q}				\
-		CHGRP=${CHGRP:Q}					\
-		CHMOD=${CHMOD:Q}					\
-		CHOWN=${CHOWN:Q}					\
-		MKDIR=${MKDIR:Q}					\
-		RMDIR=${RMDIR:Q}					\
-		RM=${RM:Q}						\
-		TRUE=${TRUE:Q}						\
-		PKGMANDIR=${PKGMANDIR:Q}
+PLIST_SUBST+=	\
+	PLIST_OPSYS=$(call quote,${OPSYS})					\
+	PLIST_OS_VERSION=$(call quote,${OS_VERSION})				\
+	PLIST_MACHINE_ARCH=$(call quote,${MACHINE_ARCH})			\
+	PLIST_MACHINE_GNU_ARCH=$(call quote,${MACHINE_GNU_ARCH})		\
+	PLIST_MACHINE_GNU_PLATFORM=$(call quote,${MACHINE_GNU_PLATFORM})	\
+	PLIST_LN=$(call quote,${LN})						\
+	PLIST_LOWER_VENDOR=$(call quote,${LOWER_VENDOR})			\
+	PLIST_LOWER_OPSYS=$(call quote,${LOWER_OPSYS})			\
+	PLIST_LOWER_OS_VERSION=$(call quote,${LOWER_OS_VERSION})		\
+	PLIST_PKGBASE=$(call quote,${PKGBASE})				\
+	PLIST_PKGNAME=$(call quote,${PKGNAME_NOREV})				\
+	PLIST_PKGLOCALEDIR=$(call quote,${PKGLOCALEDIR})			\
+	PLIST_PKGVERSION=$(shell echo ${PKGVERSION} | ${SED} -e 's/r[0-9]*$$//')\
+	PLIST_LOCALBASE=$(call quote,${LOCALBASE})				\
+	PLIST_CHGRP=$(call quote,${CHGRP})					\
+	PLIST_CHMOD=$(call quote,${CHMOD})					\
+	PLIST_CHOWN=$(call quote,${CHOWN})					\
+	PLIST_MKDIR=$(call quote,${MKDIR})					\
+	PLIST_RMDIR=$(call quote,${RMDIR})					\
+	PLIST_RM=$(call quote,${RM})						\
+	PLIST_TRUE=$(call quote,${TRUE})					\
+	PLIST_PKGMANDIR=$(call quote,${PKGMANDIR})
 
 # Pass the PLIST_SUBST substitutions to the subst.awk script by prepending
 # PLIST_" to all of the variable names and adding them into the environment.
 #
-_PLIST_AWK_ENV+=	${PLIST_SUBST:S/^/PLIST_/}
-_PLIST_AWK_ENV+=	PLIST_SUBST_VARS=${PLIST_SUBST:S/^/PLIST_/:C/=.*//:M*:Q}
+_PLIST_AWK_ENV+=	${PLIST_SUBST}
+_PLIST_AWK_ENV+=	PLIST_SUBST_VARS=$(call quote,$(foreach _s_,${PLIST_SUBST},$(firstword $(subst =, ,${_s_}))))
 
-_PLIST_AWK+=		-f ${.CURDIR}/../../mk/plist/plist-functions.awk
-_PLIST_AWK+=		-f ${.CURDIR}/../../mk/plist/plist-subst.awk
-_PLIST_AWK+=		-f ${.CURDIR}/../../mk/plist/plist-locale.awk
-_PLIST_AWK+=		-f ${.CURDIR}/../../mk/plist/plist-info.awk
-_PLIST_AWK+=		-f ${.CURDIR}/../../mk/plist/plist-man.awk
-_PLIST_AWK+=		-f ${.CURDIR}/../../mk/plist/plist-libtool.awk
-_PLIST_AWK+=		-f ${.CURDIR}/../../mk/plist/plist-default.awk
+_PLIST_AWK+=		-f ${CURDIR}/../../mk/plist/plist-functions.awk
+_PLIST_AWK+=		-f ${CURDIR}/../../mk/plist/plist-subst.awk
+_PLIST_AWK+=		-f ${CURDIR}/../../mk/plist/plist-locale.awk
+_PLIST_AWK+=		-f ${CURDIR}/../../mk/plist/plist-info.awk
+_PLIST_AWK+=		-f ${CURDIR}/../../mk/plist/plist-man.awk
+_PLIST_AWK+=		-f ${CURDIR}/../../mk/plist/plist-libtool.awk
+_PLIST_AWK+=		-f ${CURDIR}/../../mk/plist/plist-default.awk
 
-_PLIST_INFO_AWK+=	-f ${.CURDIR}/../../mk/plist/plist-functions.awk
-_PLIST_INFO_AWK+=	-f ${.CURDIR}/../../mk/plist/plist-info.awk
+_PLIST_INFO_AWK+=	-f ${CURDIR}/../../mk/plist/plist-functions.awk
+_PLIST_INFO_AWK+=	-f ${CURDIR}/../../mk/plist/plist-info.awk
 
-_PLIST_SHLIB_AWK=	-f ${_SHLIB_AWKFILE.${SHLIB_TYPE}}
-_SHLIB_AWKFILE.COFF=	${.CURDIR}/../../mk/plist/shlib-none.awk
-_SHLIB_AWKFILE.ELF=	${.CURDIR}/../../mk/plist/shlib-elf.awk
-_SHLIB_AWKFILE.aixlib=	${.CURDIR}/../../mk/plist/shlib-none.awk
-_SHLIB_AWKFILE.a.out=	${.CURDIR}/../../mk/plist/shlib-aout.awk
-_SHLIB_AWKFILE.dylib=	${.CURDIR}/../../mk/plist/shlib-dylib.awk
-_SHLIB_AWKFILE.none=	${.CURDIR}/../../mk/plist/shlib-none.awk
+#_PLIST_SHLIB_AWK=	-f ${_SHLIB_AWKFILE.${SHLIB_TYPE}}
+#_SHLIB_AWKFILE.COFF=	${CURDIR}/../../mk/plist/shlib-none.awk
+#_SHLIB_AWKFILE.ELF=	${CURDIR}/../../mk/plist/shlib-elf.awk
+#_SHLIB_AWKFILE.aixlib=	${CURDIR}/../../mk/plist/shlib-none.awk
+#_SHLIB_AWKFILE.a.out=	${CURDIR}/../../mk/plist/shlib-aout.awk
+#_SHLIB_AWKFILE.dylib=	${CURDIR}/../../mk/plist/shlib-dylib.awk
+#_SHLIB_AWKFILE.none=	${CURDIR}/../../mk/plist/shlib-none.awk
 
 # SHLIB_TYPE is the type of shared library supported by the platform.
-SHLIB_TYPE=		${_SHLIB_TYPE_cmd:sh}
-_SHLIB_TYPE_cmd=							\
-	${SETENV} ECHO=${TOOLS_ECHO:Q} FILE_CMD=${TOOLS_FILE_CMD:Q}	\
-		TEST=${TOOLS_TEST:Q} PKG_INFO_CMD=${PKG_INFO_CMD:Q}	\
-	${SH} ${.CURDIR}/../../mk/plist/shlib-type ${_OPSYS_SHLIB_TYPE:Q}
+#SHLIB_TYPE=		${_SHLIB_TYPE_cmd:sh}
+#_SHLIB_TYPE_cmd=							\
+#	${SETENV} ECHO=${TOOLS_ECHO:Q} FILE_CMD=${TOOLS_FILE_CMD:Q}	\
+#		TEST=${TOOLS_TEST:Q} PKG_INFO_CMD=${PKG_INFO_CMD:Q}	\
+#	${SH} ${CURDIR}/../../mk/plist/shlib-type ${_OPSYS_SHLIB_TYPE:Q}
 
 ######################################################################
 
@@ -153,8 +152,8 @@ plist: ${PLIST}
 ${PLIST}: ${PLIST_SRC}
 	${_PKG_SILENT}${_PKG_DEBUG}${MKDIR} $(dir $@)
 	${_PKG_SILENT}${_PKG_DEBUG}					\
-	{ ${_GENERATE_PLIST} } > $@
+	{ ${_GENERATE_PLIST} } |					\
+	${SETENV} ${_PLIST_AWK_ENV} ${AWK} ${_PLIST_AWK} > $@
 
-#	${SETENV} ${_PLIST_AWK_ENV} ${AWK} ${_PLIST_AWK} |		\
 #	${SETENV} ${_PLIST_AWK_ENV} ${AWK} ${_PLIST_SHLIB_AWK}		\
 #		> $@
