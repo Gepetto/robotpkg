a7d370f272021a233fcee6b838ec98add2e8ca43
tho@useless-ficus.net <tho@citrus.home>
2006-12-08 01:02:44 +0100

    cleanup

--- mk/update/update-vars.mk
+++ mk/update/update-vars.mk
@@ -1,11 +1,3 @@
-# $NetBSD: bsd.pkg.update.mk,v 1.7 2006/10/05 12:56:27 rillig Exp $
+# From  $NetBSD: bsd.pkg.update.mk,v 1.7 2006/10/05 12:56:27 rillig Exp $
 #
-# This Makefile fragment is included by bsd.pkg.mk and contains the targets
-# and variables for "make update".
-#
-# There is no documentation on what "update" actually does.  This is merely
-# an attempt to separate the magic into a separate module that can be
-# reimplemented later.
-#
-
 include ${PKGSRCDIR}/mk/update/update.mk
--- mk/update/update.mk
+++ mk/update/update.mk
@@ -1,7 +1,50 @@
-# $NetBSD: bsd.pkg.update.mk,v 1.7 2006/10/05 12:56:27 rillig Exp $
 #
-# This Makefile fragment is included by bsd.pkg.mk and contains the targets
-# and variables for "make update".
+# Copyright (c) 2006 LAAS/CNRS                        --  Thu Dec  7 2006
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice,  this list of  conditions and  the following disclaimer in
+#      the  documentation   and/or  other  materials   provided with  the
+#      distribution.
+#
+# This project includes software developed by the NetBSD Foundation, Inc.
+# and its contributors. It is derived from the 'pkgsrc' project
+# (http://www.pkgsrc.org).
+#
+# From  $NetBSD: bsd.pkg.update.mk,v 1.7 2006/10/05 12:56:27 rillig Exp $
+# Copyright (c) 1994-2006 The NetBSD Foundation, Inc.
+#
+#   3. All advertising materials mentioning   features or use of this
+#      software must display the following acknowledgement:
+#        This product includes software developed by the NetBSD
+#        Foundation, Inc. and its contributors.
+#   4. Neither the  name  of The NetBSD Foundation  nor the names  of its
+#      contributors  may be  used to endorse or promote  products derived
+#      from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+# ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES   OF MERCHANTABILITY AND  FITNESS  FOR  A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO  EVENT SHALL THE AUTHOR OR  CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING,  BUT  NOT LIMITED TO, PROCUREMENT  OF
+# SUBSTITUTE  GOODS OR SERVICES;  LOSS   OF  USE,  DATA, OR PROFITS;   OR
+# BUSINESS  INTERRUPTION) HOWEVER CAUSED AND  ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE  USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+
+#
+# This Makefile fragment is included by robotpkg.pkg.mk and contains the
+# targets and variables for "make update".
 #
 # There is no documentation on what "update" actually does.  This is merely
 # an attempt to separate the magic into a separate module that can be
@@ -12,7 +55,7 @@ NOCLEAN?=	NO	# don't clean up after update
 REINSTALL?=	NO	# reinstall upon update
 
 # UPDATE_TARGET is the target that is invoked when updating packages during
-# a "make update".  This variable is user-settable within /etc/mk.conf.
+# a "make update".  This variable is user-settable within etc/robotpkg.conf.
 #
 ifndef UPDATE_TARGET
   ifeq (update,${DEPENDS_TARGET})
@@ -75,7 +118,7 @@ endif
 				${RECURSIVE_MAKE} deinstall _UPDATE_RUNNING=YES; \
 			fi &&						\
 			${RECURSIVE_MAKE} ${UPDATE_TARGET}	\
-				DEPENDS_TARGET=${DEPENDS_TARGET:Q} ;	\
+				DEPENDS_TARGET=${DEPENDS_TARGET} ;	\
 		else							\
 			${PHASE_MSG} "Skipping removed directory $${dep}"; \
 		fi) ;							\
