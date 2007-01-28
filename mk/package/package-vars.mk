6807b2d0f9ac06df25d5eaeabca2a8393a4b9697
Anthony Mallet <mallet@jiminy.laas.fr>
2007-01-28 19:14:19 +0100

    license

--- mk/package/package-vars.mk
+++ mk/package/package-vars.mk
@@ -1,6 +1,48 @@
-# $NetBSD: bsd.package.mk,v 1.9 2006/10/09 11:44:07 joerg Exp $
 #
-# This Makefile fragment is included by bsd.pkg.mk and provides all
+# Copyright (c) 2006 LAAS/CNRS                        --  Thu Dec  7 2006
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice,  this list of  conditions and  the following disclaimer in
+#      the  documentation   and/or  other  materials   provided with  the
+#      distribution.
+#
+# This project includes software developed by the NetBSD Foundation, Inc.
+# and its contributors. It is derived from the 'pkgsrc' project
+# (http://www.pkgsrc.org).
+#
+# From $NetBSD: bsd.package.mk,v 1.9 2006/10/09 11:44:07 joerg Exp $
+# Copyright (c) 1994-2006 The NetBSD Foundation, Inc.
+#
+#   3. All advertising materials mentioning   features or use of this
+#      software must display the following acknowledgement:
+#        This product includes software developed by the NetBSD
+#        Foundation, Inc. and its contributors.
+#   4. Neither the  name  of The NetBSD Foundation  nor the names  of its
+#      contributors  may be  used to endorse or promote  products derived
+#      from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+# ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES   OF MERCHANTABILITY AND  FITNESS  FOR  A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO  EVENT SHALL THE AUTHOR OR  CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING,  BUT  NOT LIMITED TO, PROCUREMENT  OF
+# SUBSTITUTE  GOODS OR SERVICES;  LOSS   OF  USE,  DATA, OR PROFITS;   OR
+# BUSINESS  INTERRUPTION) HOWEVER CAUSED AND  ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE  USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#
+# This Makefile fragment is included by robotpkg.mk and provides all
 # variables and targets related to binary packages.
 #
 # The following are the "public" targets provided by this module:
@@ -49,4 +91,4 @@ repackage: package-clean package
 # phase so that the "package" target may be re-invoked.
 #
 package-clean:
-	${_PKG_SILENT}${_PKG_DEBUG}${RM} -f ${_COOKIE.package}
+	${RUN}${RM} -f ${_COOKIE.package}
