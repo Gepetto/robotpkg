25bc1eb4b6d6b0af8da61d7c21ab808e0b6696ea
Anthony Mallet <anthony.mallet@laas.fr>
2007-06-11 11:21:10 +0200

    Add pkgtools as a build prerequisite.

--- mk/bulk/bulk.mk
+++ mk/bulk/bulk.mk
@@ -184,9 +184,11 @@ LINTPKGSRC_DB?=		${BULKFILESDIR}/.lintpkgsrc.db${BULK_ID}
 BULK_DBFILE?=		${BULKFILESDIR}/.bulk_db${BULK_ID}
 
 # A list of pkgs which we should _never_ delete during a build.  The primary
-# use is for digest and also for xpkgwedge.  Add pkgtools/xpkgwedge in
-# /etc/mk.conf to do an xpkgwedged bulk build.
+# use is for pkgtools and also for digest.
+BULK_PREREQ+=		pkgtools/pkg_install
 BULK_PREREQ+=		pkgtools/digest
+BULK_PREREQ+=		pkgtools/pax
+BULK_PREREQ+=		pkgtools/tnftp
 
 # Commands for printing informational messages from the bulk build.
 # BULK_MSG_CONT is used for continuing a message started with BULK_MSG
