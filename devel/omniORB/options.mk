405c2d3c4e2f9cc9575d7cdf12740f3cde2aeeb6
tho <anthony.mallet@laas.fr>
2008-01-27 20:18:33 +0100

    [devel/omniORB] Add ipv6 build option.
    Use the options framework to define ipv6 build option. I recommend no to
    build omniORB with IPv6 enabled as it doesn't interoperate correctly with
    Orbix/E. The option is disabled by default.

--- devel/omniORB/options.mk
+++ devel/omniORB/options.mk
@@ -1,11 +1,15 @@
-# $NetBSD$
+#	$Id: options.mk de6c97cd51ea60c42e9cc9201526fe77 2008/01/26 16:41:04 tho $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.omniORB
 PKG_SUPPORTED_OPTIONS=	ipv6
-PKG_SUGGESTED_OPTIONS=	ipv6
+PKG_SUGGESTED_OPTIONS=#empty
 
-.include "../../mk/bsd.options.mk"
+PKG_OPTION.ipv6=	Enable support for IPv6
 
-.if empty(PKG_OPTIONS:Mipv6)
-CONFIGURE_ARGS+=--without-ipv6
-.endif
+include ../../mk/robotpkg.options.mk
+
+ifneq (,$(findstring ipv6,$(PKG_OPTIONS)))
+CONFIGURE_ARGS+=--enable-ipv6
+else
+CONFIGURE_ARGS+=--disable-ipv6
+endif
