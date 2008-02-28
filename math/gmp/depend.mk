ddf98c699cbccc5c715fdf598f3f4e824020a0ce
Anthony Mallet <anthony.mallet@laas.fr>
2008-02-28 22:22:11 +0100

    [math/gmp] import gmp 4.2.2
    GNU MP is a library for arbitrary precision arithmetic, operating on signed
    integers, rational numbers, and floating point numbers.  It has a rich set
    of functions, and the functions have a regular interface.

---
+++ math/gmp/depend.mk
@@ -0,0 +1,25 @@
+# $Id: $
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GMP_DEPEND_MK:=		${GMP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		gmp
+endif
+
+ifeq (+,$(GMP_DEPEND_MK))
+PREFER.gmp?=		system
+
+DEPEND_USE+=		gmp
+
+DEPEND_ABI.gmp?=	gmp>=4.2.2
+DEPEND_DIR.gmp?=	../../math/gmp
+
+DEPEND_LIBS.gmp+=	-lgmp
+
+SYSTEM_SEARCH.gmp=	\
+	include/gmp.h	\
+	lib/libgmp.so*
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
