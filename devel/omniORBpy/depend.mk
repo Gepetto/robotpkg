f8d424d73d2acef394b3a3ec2c84158846638195
Anthony Mallet <anthony.mallet@laas.fr>
2008-03-13 19:58:54 +0900

    [devel/omniORBpy] Convert to depend.mk mechanism

---
+++ devel/omniORBpy/depend.mk
@@ -0,0 +1,24 @@
+# $Id: $
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+OMNIORBPY_DEPEND_MK:=	${OMNIORBPY_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		omniORBpy
+endif
+
+ifeq (+,$(OMNIORBPY_DEPEND_MK))
+PREFER.omniORBpy?=	robotpkg
+
+DEPEND_USE+=		omniORBpy
+
+DEPEND_ABI.omniORBpy?=	omniORBpy>=3.1
+DEPEND_DIR.omniORBpy?=	../../devel/omniORBpy
+
+SYSTEM_SEARCH.omniORBpy=\
+	include/omniORBpy.h
+endif
+
+include ../../devel/omniORB/depend.mk
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
