9669fa73536b8828619bdf480640fcf7722103b6
Anthony Mallet <anthony.mallet@laas.fr>
2008-03-14 16:56:53 +0900

    [image/viam-libs] Add depend.mk

---
+++ image/viam-libs/depend.mk
@@ -0,0 +1,28 @@
+# $Id: $
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+VIAMLIBS_DEPEND_MK:=	${VIAMLIBS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		viam-libs
+endif
+
+ifeq (+,$(VIAMLIBS_DEPEND_MK))
+PREFER.viam-libs?=	robotpkg
+
+DEPEND_USE+=		viam-libs
+
+DEPEND_ABI.viam-libs?=	viam-libs>=0.99
+DEPEND_DIR.viam-libs?=	../../image/viam-libs
+
+DEPEND_PKG_CONFIG.viam-libs+=lib/pkgconfig
+
+SYSTEM_SEARCH.viam-libs=\
+	include/viamlib.h		\
+	lib/pkgconfig/viam-libs.pc
+endif
+
+include ../../image/libdc1394/depend.mk
+include ../../image/opencv/depend.mk
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
