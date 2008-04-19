22f1b2086062de2e28cec15c5848f81576c9d1ca
Anthony Mallet <anthony.mallet@laas.fr>
2008-04-20 01:36:57 +0200

    [mk] Replace 8 spaces by a tab. While here fix copyright.

--- mk/checksum/checksum-vars.mk
+++ mk/checksum/checksum-vars.mk
@@ -1,9 +1,48 @@
-# $NetBSD: bsd.checksum-vars.mk,v 1.1 2006/07/13 14:02:34 jlam Exp $
 #
-# This Makefile fragment is included separately by bsd.pkg.mk and
-# defines some variables which must be defined earlier than where
-# bsd.checksum.mk is included.
+# Copyright (c) 2006-2008 LAAS/CNRS
+# All rights reserved.
 #
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice,  this list of  conditions and  the following disclaimer in
+#      the  documentation   and/or  other  materials   provided with  the
+#      distribution.
+#
+# This project includes software developed by the NetBSD Foundation, Inc.
+# and its contributors. It is derived from the 'pkgsrc' project
+# (http://www.pkgsrc.org).
+#
+# From $NetBSD: bsd.checksum-vars.mk,v 1.1 2006/07/13 14:02:34 jlam Exp $
+# Copyright (c) 1994-2006 The NetBSD Foundation, Inc.
+#
+#   3. All advertising materials mentioning   features or use of this
+#      software must display the following acknowledgement:
+#        This product includes software developed by the NetBSD
+#        Foundation, Inc. and its contributors.
+#   4. Neither the  name  of The NetBSD Foundation  nor the names  of its
+#      contributors  may be  used to endorse or promote  products derived
+#      from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+# ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES   OF MERCHANTABILITY AND  FITNESS  FOR  A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO  EVENT SHALL THE AUTHOR OR  CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING,  BUT  NOT LIMITED TO, PROCUREMENT  OF
+# SUBSTITUTE  GOODS OR SERVICES;  LOSS   OF  USE,  DATA, OR PROFITS;   OR
+# BUSINESS  INTERRUPTION) HOWEVER CAUSED AND  ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE  USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# Authored by Anthony Mallet on Thu Dec  7 2006
+
 # The following variables may be set in a package Makefile:
 #
 #    DISTINFO_FILE is the path to file containing the checksums.
@@ -30,10 +69,10 @@ DISTINFO_FILE?=		${PKGDIR}/distinfo
 ifdef NO_CHECKSUM
 .PHONY: checksum makesum makepatchsum
 checksum: fetch
-        @${DO_NADA}
+	@${DO_NADA}
 
 makesum makepatchsum:
-        @${DO_NADA}
+	@${DO_NADA}
 else
   include ${PKGSRCDIR}/mk/checksum/checksum.mk
 endif
@@ -45,7 +84,7 @@ endif
 #
 .PHONY: distinfo
 distinfo: makepatchsum makesum
-        @${DO_NADA}
+	@${DO_NADA}
 
 # Some short aliases for "makepatchsum" and "distinfo".
 .PHONY: mps mdi makedistinfo
