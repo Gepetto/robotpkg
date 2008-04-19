8a1d21995ffbe7ac3e17312c68d9d073fc89d9d7
Anthony Mallet <anthony.mallet@laas.fr>
2008-04-19 22:34:43 +0200

    [pkgtools/libnbcompat] Rename files dir to dist dir.
    While here, create a regular package for libnbcompat and allow it to be
    replaced by a system package, just in case.
    Default to the robotpkg implementation, as it is very unlikely that any
    system has a libnbcompat package...

--- pkgtools/libnbcompat/depend.mk
+++ pkgtools/libnbcompat/depend.mk
@@ -2,19 +2,30 @@
 # Copyright (C) 2008 LAAS/CNRS
 #
 # Authored by Anthony Mallet on Fri Apr 18 2008
-#
-# This depend.mk file is special: it builds a working copy of libnbcompat
-# inside ${WRKDIR} (of the calling package) and adds the appropriate
-# paths to CPPFLAGS and LDFLAGS. No independant libnbcompat package is
-# installed.
-#
 # From $NetBSD: inplace.mk,v 1.1 2004/08/13 22:34:28 jlam Exp $
+#
 
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 LIBNBCOMPAT_DEPEND_MK:=	${LIBNBCOMPAT_DEPEND_MK}+
 
-ifeq (+,$(LIBNBCOMPAT_DEPEND_MK))
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libnbcompat
+endif
 
-LIBNBCOMPAT_FILESDIR=	${PKGSRCDIR}/pkgtools/libnbcompat/files
+ifeq (+,$(LIBNBCOMPAT_DEPEND_MK))
+PREFER.libnbcompat?=		robotpkg
+
+SYSTEM_SEARCH.libnbcompat=	\
+	include/nbcompat.h	\
+	lib/libnbcompat.a
+
+  # pull-in the user preferences for libnbcompat now
+  include ../../mk/robotpkg.prefs.mk
+
+  ifeq (inplace+robotpkg,$(strip $(LIBNBCOMPAT_STYLE)+$(PREFER.libnbcompat)))
+  # This is the "inplace" version of libnbcompat package, for bootstrap process
+  #
+LIBNBCOMPAT_FILESDIR=	${PKGSRCDIR}/pkgtools/libnbcompat/dist
 LIBNBCOMPAT_SRCDIR=	${WRKDIR}/libnbcompat
 
 CPPFLAGS+=	-I${LIBNBCOMPAT_SRCDIR}
@@ -22,14 +33,28 @@ LDFLAGS+=	-L${LIBNBCOMPAT_SRCDIR} -lnbcompat
 
 post-extract: libnbcompat-extract
 libnbcompat-extract:
+	@${STEP_MSG} "Extracting libnbcompat in place"
 	${CP} -Rp ${LIBNBCOMPAT_FILESDIR} ${LIBNBCOMPAT_SRCDIR}
 
 pre-configure: libnbcompat-build
 libnbcompat-build:
+	@${STEP_MSG} "Building libnbcompat in place"
 	${RUN}								\
 	cd ${LIBNBCOMPAT_SRCDIR} && 					\
 	${SETENV} AWK="${AWK}" CC="${CC}" CFLAGS="${CFLAGS}"		\
 		CPPFLAGS="${CPPFLAGS}" ${CONFIG_SHELL} configure &&	\
 	${MAKE_PROGRAM}
+  else
+  # This is the regular version of libnbcompat package, for normal install
+  #
+DEPEND_USE+=		libnbcompat
+
+DEPEND_ABI.libnbcompat?=libnbcompat>=20080416
+DEPEND_DIR.libnbcompat?=../../pkgtools/libnbcompat
+
+DEPEND_LIBS.libnbcompat+=-lnbcompat
+  endif
 
 endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
