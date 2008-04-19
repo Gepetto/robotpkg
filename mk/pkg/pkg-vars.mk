b137bf2d6a30e603a4429cbaee5a72818a888141
Anthony Mallet <anthony.mallet@laas.fr>
2008-04-19 15:49:01 +0200

    [mk] Require the robotpkg_* admin tools for all packages.
    While here, add copyright notice.

--- mk/pkg/pkg-vars.mk
+++ mk/pkg/pkg-vars.mk
@@ -1,11 +1,55 @@
-# $NetBSD: flavor-vars.mk,v 1.2 2006/10/06 14:51:36 joerg Exp $
 #
-# This Makefile fragment is included indirectly by bsd.prefs.mk and
-# defines some variables which must be defined earlier than where
-# flavor.mk is included.
+# Copyright (c) 2006-2008 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice,  this list of  conditions and  the following disclaimer in
+#      the  documentation   and/or  other  materials   provided with  the
+#      distribution.
+#
+# This project includes software developed by the NetBSD Foundation, Inc.
+# and its contributors. It is derived from the 'pkgsrc' project
+# (http://www.pkgsrc.org).
+#
+# From $NetBSD: flavor-vars.mk,v 1.2 2006/10/06 14:51:36 joerg Exp $
+# Copyright (c) 1994-2006 The NetBSD Foundation, Inc.
+#
+#   3. All advertising materials mentioning   features or use of this
+#      software must display the following acknowledgement:
+#        This product includes software developed by the NetBSD
+#        Foundation, Inc. and its contributors.
+#   4. Neither the  name  of The NetBSD Foundation  nor the names  of its
+#      contributors  may be  used to endorse or promote  products derived
+#      from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+# ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES   OF MERCHANTABILITY AND  FITNESS  FOR  A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO  EVENT SHALL THE AUTHOR OR  CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING,  BUT  NOT LIMITED TO, PROCUREMENT  OF
+# SUBSTITUTE  GOODS OR SERVICES;  LOSS   OF  USE,  DATA, OR PROFITS;   OR
+# BUSINESS  INTERRUPTION) HOWEVER CAUSED AND  ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE  USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-# This is the package database directory for the default view.
+# Authored by Anthony Mallet on Thu Dec  7 2006
+
+# This Makefile fragment is included indirectly by robotpkg.mk and
+# defines some variables which must be defined early defined
+
+# Every package needs the robotpkg_* admin tools
+include ${PKGSRCDIR}/pkgtools/pkg_install/depend.mk
+
+# This is the default package database directory
 PKG_DBDIR?=		/usr/openrobots/var/db/pkg
 
 _PKG_DBDIR=		${PKG_DBDIR}
