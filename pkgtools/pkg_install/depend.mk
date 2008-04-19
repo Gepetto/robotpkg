32864be9a90bc435cb0b23a0ed82b1524efa772f
Anthony Mallet <anthony.mallet@laas.fr>
2008-04-19 15:41:28 +0200

    [pkgtools/pkg_install] Add depend.mk.
    The dependency is added as a "bootstrap" dependency.

---
+++ pkgtools/pkg_install/depend.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (c) 2008 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice,  this list of  conditions and  the following disclaimer in
+#      the  documentation   and/or  other  materials   provided with  the
+#      distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE  AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES   OF MERCHANTABILITY AND  FITNESS  FOR  A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO  EVENT SHALL THE AUTHOR OR  CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING,  BUT  NOT LIMITED TO, PROCUREMENT  OF
+# SUBSTITUTE  GOODS OR SERVICES;  LOSS   OF  USE,  DATA, OR PROFITS;   OR
+# BUSINESS  INTERRUPTION) HOWEVER CAUSED AND  ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE  USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# Authored by Anthony Mallet on Sat Apr 19 2008
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PKG_INSTALL_DEPEND_MK:= ${PKG_INSTALL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		pkg_install
+endif
+
+ifeq (+,$(PKG_INSTALL_DEPEND_MK))
+# it is mandatory to use the robotpkg version of these tools
+PREFER.pkg_install=		robotpkg
+
+DEPEND_USE+=			pkg_install
+
+DEPEND_METHOD.pkg_install?=	bootstrap
+DEPEND_ABI.pkg_install?=	pkg_install>=20080415
+DEPEND_DIR.pkg_install?=	../../pkgtools/pkg_install
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
