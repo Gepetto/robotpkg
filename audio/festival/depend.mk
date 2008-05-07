0e01a25e65f82f413e178c87ad41bc664ef45f05
Anthony Mallet <anthony.mallet@laas.fr>
2008-05-07 11:19:48 +0200

    [audio/festival] Import festival-1.96beta
    Festival offers a general framework for building speech synthesis
    systems. As a whole it offers full text to speech through a number APIs:
    from shell level, though a Scheme command interpreter, as a C++ library,
    from Java, and an Emacs interface.
    
    Festival is imported into robotpkg because the source distribution is a
    little bit messy and systems packages might exhibit big differences
    (e.g. the NetBSD package does not provide the C++ headers and the client
    library). However, we still default to the system package in robotpkg.
    
    This source distribution is the one of the festvox project from CMU.

---
+++ audio/festival/depend.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (c) 2008 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice,  this list of  conditions and  the following disclaimer in
+#      the  documentation   and/or  other  materials   provided with  the
+#      distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE  AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES   OF MERCHANTABILITY AND  FITNESS  FOR  A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO  EVENT SHALL THE AUTHOR OR  CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING,  BUT  NOT LIMITED TO, PROCUREMENT  OF
+# SUBSTITUTE  GOODS OR SERVICES;  LOSS   OF  USE,  DATA, OR PROFITS;   OR
+# BUSINESS  INTERRUPTION) HOWEVER CAUSED AND  ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE  USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# Authored by Anthony Mallet on Tue May  6 2008
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+FESTIVAL_DEPEND_MK:=	${FESTIVAL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		festival
+endif
+
+ifeq (+,$(FESTIVAL_DEPEND_MK)) # -------------------------------------
+
+PREFER.festival?=	system
+
+SYSTEM_SEARCH.festival=\
+	bin/festival			\
+	include/festival/festival.h	\
+	lib/libFestival.a
+
+DEPEND_USE+=		festival
+
+DEPEND_ABI.festival?=	festival>=1.96
+DEPEND_DIR.festival?=	../../audio/festival
+
+DEPEND_LIBS.festival+=	-lFestival
+
+endif # FESTIVAL_DEPEND_MK -------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
