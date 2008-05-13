a1e44d2f18e93d13552cd90a050e9f864a84d39f
Anthony Mallet <anthony.mallet@laas.fr>
2008-05-13 15:36:13 +0200

    [path/jrl-walkgen] Add depend.mk file

---
+++ path/jrl-walkgen/depend.mk
@@ -0,0 +1,19 @@
+# $Id: $
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+JRL_WALKGEN_DEPEND_MK:=	${JRL_WALKGEN_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		jrl-walkgen
+endif
+
+ifeq (+,$(JRL_WALKGEN_DEPEND_MK))
+PREFER.jrl-walkgen?=	robotpkg
+
+DEPEND_USE+=		jrl-walkgen
+
+DEPEND_ABI.jrl-walkgen?=jrl-walkgen>=2.4.1
+DEPEND_DIR.jrl-walkgen?=../../path/jrl-walkgen
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
