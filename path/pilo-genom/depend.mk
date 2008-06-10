c9a4d932a31d7a1f526b0093854b299077f5c81a
Arnaud Degroote <arnaud.degroote@laas.fr>
2008-06-10 10:46:00 +0200

    [path/pilo-genom] Import pilo-genom-0.1
    The module Pilo allows to execute lines, circle, broken lines and Reeds and
    Shepp trajectories. Its main functionnalities are the following:
    
    - Geometrical smoothing of broken lines or Reeds and Shepp path using clothoid
      or anti-clothoid in order to avoid curvature discontinuities.
    - Computation of speed profils (v and w) according to given maximum velocities
      and accelerations.
    - Execution of the trajectory thanks to a periodical production of the reference
      (speed and position). The speed can be dynamically modified.

---
+++ path/pilo-genom/depend.mk
@@ -0,0 +1,39 @@
+# $LAAS: depend.mk 2008/05/25 15:21:40 tho $
+#
+# Copyright (c) 2008 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice  and this list of  conditions in the documentation   and/or
+#      other materials provided with the distribution.
+#
+#                                      Arnaud Degroote on Tue Jun 10 2008
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PILOGENOM_DEPEND_MK:=	${PILOGENOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		pilo-genom
+endif
+
+ifeq (+,$(PILOGENOM_DEPEND_MK))
+PREFER.pilo-genom?=	robotpkg
+
+DEPEND_USE+=		pilo-genom
+
+DEPEND_ABI.pilo-genom?=	pilo-genom>=0.1
+DEPEND_DIR.pilo-genom?=	../../path/pilo-genom
+
+SYSTEM_SEARCH.pilo-genom=\
+	include/pilo/piloStruct.h		\
+	lib/pkgconfig/pilo.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
