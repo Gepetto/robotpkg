3274115a03114ca664338d31408d991851e9d6ae
Anthony Mallet <anthony.mallet@laas.fr>
2008-06-17 16:51:34 +0200

    [robots/hrp2-14] Upgrade to 1.4
    Changes since 1.3:
    	4. Add pre-computed trajectories to turn in place.

--- robots/hrp2-14/depend.mk
+++ robots/hrp2-14/depend.mk
@@ -1,32 +1,20 @@
+# $LAAS: depend.mk 2008/06/17 16:47:11 mallet $
 #
 # Copyright (c) 2008 LAAS/CNRS
 # All rights reserved.
 #
-# Redistribution  and  use in source   and binary forms,  with or without
-# modification, are permitted provided that  the following conditions are
+# Redistribution and use  in source  and binary  forms,  with or without
+# modification, are permitted provided that the following conditions are
 # met:
 #
-#   1. Redistributions  of  source code must  retain  the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#   2. Redistributions in binary form must  reproduce the above copyright
-#      notice,  this list of  conditions and  the following disclaimer in
-#      the  documentation   and/or  other  materials   provided with  the
-#      distribution.
+#   1. Redistributions of  source  code must retain the  above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice and  this list of  conditions in the  documentation and/or
+#      other materials provided with the distribution.
 #
-# THIS SOFTWARE IS PROVIDED BY THE  AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-# ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES   OF MERCHANTABILITY AND  FITNESS  FOR  A PARTICULAR
-# PURPOSE ARE DISCLAIMED.  IN NO  EVENT SHALL THE AUTHOR OR  CONTRIBUTORS
-# BE LIABLE FOR ANY DIRECT, INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING,  BUT  NOT LIMITED TO, PROCUREMENT  OF
-# SUBSTITUTE  GOODS OR SERVICES;  LOSS   OF  USE,  DATA, OR PROFITS;   OR
-# BUSINESS  INTERRUPTION) HOWEVER CAUSED AND  ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE  USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#                                      Anthony Mallet on Wed May 14 2008
 #
-
-# Authored by Anthony Mallet on Wed May 14 2008
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 HRP2_14_DEPEND_MK:=	${HRP2_14_DEPEND_MK}+
@@ -44,7 +32,7 @@ SYSTEM_SEARCH.hrp2_14=\
 
 DEPEND_USE+=		hrp2_14
 
-DEPEND_ABI.hrp2_14?=	hrp2_14>=1.3
+DEPEND_ABI.hrp2_14?=	hrp2_14>=1.4
 DEPEND_DIR.hrp2_14?=	../../robots/hrp2-14
 
 endif # HRP2_14_DEPEND_MK --------------------------------------------
