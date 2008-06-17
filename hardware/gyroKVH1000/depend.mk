d30eb3cccb63ff1c967be8a98b87d5ed02bbc5ca
Anthony Mallet <anthony.mallet@laas.fr>
2008-06-17 18:12:33 +0200

    [hardware/gyroKVH1000] Fix missing comment in front of RCS Id tag.
    And while here, update the copyright notice so that it looks like the
    other ones.

--- hardware/gyroKVH1000/depend.mk
+++ hardware/gyroKVH1000/depend.mk
@@ -1,4 +1,20 @@
- $Id: depend.mk 2008/06/17 18:03:07 mallet $
+# $LAAS: depend.mk 2008/06/17 18:12:25 mallet $
+#
+# Copyright (c) 2008 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice  and this list of  conditions in the documentation   and/or
+#      other materials provided with the distribution.
+#
+#                                      Arnaud Degroote on Tue May 20 2008
+#
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 GYROKVH1000_DEPEND_MK:=${GYROKVH1000_DEPEND_MK}+
