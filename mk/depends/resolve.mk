33e750412c79667fa8e9530d50aa4a7a687650cf
Anthony Mallet <anthony.mallet@laas.fr>
2008-07-15 12:39:19 +0200

    [mk] Don't define PREFIX.<pkg> after the barrier.
    The definition of PREFIX.<pkg> for each dependency of a package was
    defined after the barrier but this seems not required.
    Also allow users to override the prefix of individual packages.

--- mk/depends/resolve.mk
+++ mk/depends/resolve.mk
@@ -1,4 +1,4 @@
-# $LAAS: resolve.mk 2008/06/01 14:03:29 tho $
+# $LAAS: resolve.mk 2008/07/15 12:28:39 mallet $
 #
 # Copyright (c) 2008
 #      IS/AIST-ST2I/CNRS Joint Japanese-French Robotics Laboratory (JRL).
@@ -142,13 +142,6 @@ override define _dpd_sysprefix
 PREFIX.${1}=$$(firstword $$(call prefixsearch,$${SYSTEM_SEARCH.${1}},${SYSTEM_PREFIX}))
 MAKEOVERRIDES+=		PREFIX.${1}=$$(call quote,$${PREFIX.${1}})
     endif
-  else
-    ifneq (,$$(filter robotpkg,$${PREFER.${1}}))
-PKG_FAIL_REASON+= "The prefix for ${1} is defined, but the variable"
-PKG_FAIL_REASON+= "PREFER.${1} says to use the robotpkg package."
-PKG_FAIL_REASON+= "Please unset PREFIX.${1} in your"
-PKG_FAIL_REASON+= ${_MAKECONF}
-    endif
   endif
   ifeq (,$$(filter robotpkg,$${PREFER.${1}}))
 _list:=$$(call pathsearch,$${SYSTEM_SEARCH.${1}},$${PREFIX.${1}})
@@ -185,11 +178,6 @@ PKG_FAIL_REASON+= "*** ${1} package not found. (see above)"
 endef
 $(foreach _pkg_,${DEPEND_USE},$(eval $(call _dpd_sysprefix,${_pkg_})))
 
-
-# --- Begin after the barrier ----------------------------------------
-
-ifdef _PKGSRC_BARRIER
-
 # Generate default value for PREFIX.<pkg> variable for the robotpkg
 # packages.
 #
@@ -204,6 +192,11 @@ MAKEOVERRIDES+=		PREFIX.${1}=$$(call quote,$${PREFIX.${1}})
   endif
 endef
 $(foreach _pkg_,${DEPEND_USE},$(eval $(call _dpd_pkgprefix,${_pkg_})))
+
+
+# --- Begin after the barrier ----------------------------------------
+
+ifdef _PKGSRC_BARRIER
 
 # Generate default values for:
 #
