3f32fdfeb3c24de167503ff9ead4b4357c437a6a
Anthony Mallet <anthony.mallet@laas.fr>
2008-09-17 12:45:41 +0200

    [math/jrl-mathtools] Create package jrl-mathtools-1.0
    This package implements basic mathematical tools used in the JRL software.
    Main classes include:
    * jrlMathTools::Cangle for simple angle computations,
    * jrlMathTools::Matrix4x4 for 4x4 matrices, mainly for homogeneous matrices,
    * jrlMathTools::Matrix3x3 for 3x3 matrices, mainly for rotation matrices,
    * jrlMathTools::Vector4D for 4 dimensional vectors,
    * jrlMathTools::Vector3D for 3 dimensional vectors.

---
+++ math/jrl-mathtools/depend.mk
@@ -0,0 +1,40 @@
+# $LAAS: depend.mk 2008/09/17 12:43:33 mallet $
+#
+# Copyright (c) 2008 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution and use  in source  and binary  forms,  with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   1. Redistributions of  source  code must retain the  above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice and  this list of  conditions in the  documentation and/or
+#      other materials provided with the distribution.
+#
+#                                      Anthony Mallet on Wed Sep 17 2008
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+JRLMATHTOOLS_DEPEND_MK:=${JRLMATHTOOLS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		jrlmathtools
+endif
+
+ifeq (+,$(JRLMATHTOOLS_DEPEND_MK)) # ---------------------------------
+
+PREFER.jrl-mathtools?=	robotpkg
+
+DEPEND_USE+=		jrl-mathtools
+
+DEPEND_ABI.jrl-mathtools?=jrl-mathtools>=1.0
+DEPEND_DIR.jrl-mathtools?=../../math/jrl-mathtools
+
+SYSTEM_SEARCH.jrl-mathtools=\
+	include/jrlMathTools/vector4.h
+
+endif # JRLMATHTOOLS_DEPEND_MK ---------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
