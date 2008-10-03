94d27854f1c0830c8aedde93a2fa7feab7c630c6
Anthony Mallet <anthony.mallet@laas.fr>
2008-10-03 21:36:47 +0200

    [lang/sun-jre6] Import sun JRE6
    Sun JRE6 is the Linux port of the Sun Java(tm) Runtime Environment (J2SE 6.0).

---
+++ lang/sun-jre6/depend.mk
@@ -0,0 +1,41 @@
+# $LAAS: depend.mk 2008/10/03 18:17:31 mallet $
+#
+# Copyright (c) 2008 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution and use  in source  and binary  forms,  with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   1. Redistributions of  source  code must retain the  above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice and  this list of  conditions in the  documentation and/or
+#      other materials provided with the distribution.
+#
+#                                      Anthony Mallet on Fri Oct  3 2008
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+SUN_JRE_DEPEND_MK:=	${SUN_JRE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		sun-jre6
+endif
+
+ifeq (+,$(SUN_JRE_DEPEND_MK))
+PREFER.sun-jre6?=	robotpkg
+
+DEPEND_USE+=		sun-jre6
+
+DEPEND_ABI.sun-jre6?=	sun-jre6>=6.0.0
+DEPEND_DIR.sun-jre6?=	../../lang/sun-jre6
+
+SYSTEM_SEARCH.sun-jre6=\
+	bin/java
+
+JAVA_HOME=		${LOCALBASE}/java/sun6
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
+
