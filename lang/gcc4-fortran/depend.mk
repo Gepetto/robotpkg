13371c8b5765289d91dd72bfeca1fa30306b0307
Anthony Mallet <anthony.mallet@laas.fr>
2008-10-20 15:26:26 +0200

    [lang/gcc4] Update to gcc 4.2.4
    This update concerns gcc4-c, gcc4-c++ and gcc4-fortran.

--- lang/gcc4-fortran/depend.mk
+++ lang/gcc4-fortran/depend.mk
@@ -1,4 +1,4 @@
-# $Id: $
+# $Id: depend.mk 2008/10/19 20:41:52 tho $
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 GCC4_FORTRAN_DEPEND_MK:=	${GCC4_FORTRAN_DEPEND_MK}+
@@ -12,14 +12,14 @@ PREFER.gcc4-fortran?=		system
 
 DEPEND_USE+=			gcc4-fortran
 
-DEPEND_ABI.gcc4-fortran?=	gcc4-fortran>=4.2.3
+DEPEND_ABI.gcc4-fortran?=	gcc4-fortran>=4.2
 DEPEND_DIR.gcc4-fortran?=	../../lang/gcc4-fortran
 
 DEPEND_LIBS.gcc4-fortran+=	-lgfortran
 
 SYSTEM_SEARCH.gcc4-fortran=	\
-	bin/gfortran		\
-	lib/libgfortran.so*
+	'bin/gfortran:/[0-9.]/s/[^0-9.]//gp:% -dumpversion'	\
+	'lib/libgfortran.so*'
 endif
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
