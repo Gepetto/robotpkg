d13ee4c986cbabe0579473d55adc5347fad22806
Anthony Mallet <anthony.mallet@laas.fr>
2008-10-23 14:31:51 +0200

    [mk] Cosmetic changes, while here fix copyright boilerplate

--- mk/compiler/compiler-vars.mk
+++ mk/compiler/compiler-vars.mk
@@ -1,5 +1,41 @@
-# $NetBSD: compiler.mk,v 1.58 2006/12/03 08:34:45 seb Exp $
+# $LAAS: compiler-vars.mk 2008/10/23 12:13:49 mallet $
 #
+# Copyright (c) 2006-2008 LAAS/CNRS
+# All rights reserved.
+#
+# This project includes software developed by the NetBSD Foundation, Inc.
+# and its contributors. It is derived from the 'pkgsrc' project
+# (http://www.pkgsrc.org).
+#
+# Redistribution and use  in source  and binary  forms,  with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   1. Redistributions of  source  code must retain the  above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice,  this list of  conditions and the following disclaimer in
+#      the  documentation  and/or  other   materials provided  with  the
+#      distribution.
+#
+# THIS  SOFTWARE IS PROVIDED BY  THE  COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND  ANY  EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING,  BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT SHALL THE COPYRIGHT
+# HOLDERS OR      CONTRIBUTORS  BE LIABLE FOR   ANY    DIRECT, INDIRECT,
+# INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE   OF THIS SOFTWARE, EVEN   IF ADVISED OF   THE POSSIBILITY OF SUCH
+# DAMAGE.
+#
+# From $NetBSD: compiler.mk,v 1.58 2006/12/03 08:34:45 seb Exp $
+#
+#                                      Anthony Mallet on Wed Dec  6 2006
+#
+
 # This Makefile fragment implements handling for supported C/C++/Fortran
 # compilers.
 #
@@ -62,8 +98,8 @@
 #	.endif
 #
 
-ifndef BSD_COMPILER_MK
-BSD_COMPILER_MK=	defined
+ifndef ROBOTPKG_COMPILER_MK
+ROBOTPKG_COMPILER_MK=	defined
 
 include ../../mk/robotpkg.prefs.mk
 
@@ -100,4 +136,4 @@ _PKGSRC_COMPILER=	$(filter ${_PSEUDO_COMPILERS},${PKGSRC_COMPILER}) ${_COMPILER}
 
 include $(patsubst %,../../mk/compiler/%.mk,${_PKGSRC_COMPILER})
 
-endif	# BSD_COMPILER_MK
+endif	# ROBOTPKG_COMPILER_MK
