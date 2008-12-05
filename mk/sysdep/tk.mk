094d84c025db6399b1a9913b4f23dfe8f7ecccf6
Anthony Mallet <anthony.mallet@laas.fr>
2008-12-05 16:06:54 +0100

    [sysdep/tk] Fix tk headers location for ubuntu 7.10

--- mk/sysdep/tk.mk
+++ mk/sysdep/tk.mk
@@ -1,4 +1,4 @@
-# $LAAS: tk.mk 2008/11/04 22:30:19 tho $
+# $LAAS: tk.mk 2008/12/05 15:59:35 mallet $
 #
 # Copyright (c) 2008 LAAS/CNRS
 # All rights reserved.
@@ -33,8 +33,8 @@ DEPEND_ABI.tk?=		tk>=8.0
 
 SYSTEM_SEARCH.tk=	\
 	'bin/wish'							\
-	'lib/{,tk{,[0-9]*}/}tkConfig.sh:/TK_VERSION/s/[^.0-9]*//gp'	\
-	'include/{,tk{,[0-9]*}/}tk.h:/TK_VERSION/s/[^.0-9]*//gp'
+	'lib/{,tk{,[0-9.]*}/}tkConfig.sh:/TK_VERSION/s/[^.0-9]*//gp'	\
+	'include/{,t{k,cl}{,[0-9.]*}/}tk.h:/TK_VERSION/s/[^.0-9]*//gp'
 
 TK_CONFIG_SH=		$(word 2,${SYSTEM_FILES.tk})
 
