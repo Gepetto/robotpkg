415e7acd3e5111ddc3c20367bcdca180d86f65d5
Anthony Mallet <anthony.mallet@laas.fr>
2008-12-10 13:55:16 +0100

    [path/hpp-walkplanner] Update to 1.5
    Changes since 1.4.1:
    	59. Don't hardcode VRML model path.
    	58. Set same penetration for bounding box as for humanoid robot.
    	57. Removed reference to private field ChppPlanner::hppProblemVector.
    	56. Added a lateral tolerance parameter.

--- path/hpp-walkplanner/depend.mk
+++ path/hpp-walkplanner/depend.mk
@@ -1,4 +1,4 @@
-# $LAAS: depend.mk 2008/09/17 15:25:15 mallet $
+# $LAAS: depend.mk 2008/12/09 20:11:24 mallet $
 #
 # Copyright (c) 2008 LAAS/CNRS
 # All rights reserved.
@@ -33,7 +33,7 @@ SYSTEM_SEARCH.hpp-walkplanner=\
 
 DEPEND_USE+=		hpp-walkplanner
 
-DEPEND_ABI.hpp-walkplanner?=hpp-walkplanner>=1.4.1
+DEPEND_ABI.hpp-walkplanner?=hpp-walkplanner>=1.5
 DEPEND_DIR.hpp-walkplanner?=../../path/hpp-walkplanner
 
 endif # HPP_WALKPLANNER_DEPEND_MK ------------------------------------
