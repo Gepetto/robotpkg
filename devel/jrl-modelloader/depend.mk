28cabf1c4dbde38407e8088d6253eff91c6652ff
Anthony Mallet <anthony.mallet@laas.fr>
2008-12-10 13:45:45 +0100

    [devel/jrl-modelloader] Update to 1.4
    Changes since 1.3:
    	Adapt to OpenHRP3, include OrbixE runtime libraries.

---
+++ devel/jrl-modelloader/depend.mk
@@ -0,0 +1,41 @@
+# $LAAS: depend.mk 2008/12/09 16:08:50 mallet $
+#
+# Copyright (c) 2008 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice  and this list of  conditions in the documentation   and/or
+#      other materials provided with the distribution.
+#
+#                                       Anthony Mallet on Tue Dec  9 2008
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+JRL_MODELLOADER_DEPEND_MK:=${JRL_MODELLOADER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		jrl-modelloader
+endif
+
+ifeq (+,$(JRL_MODELLOADER_DEPEND_MK)) # ------------------------------
+
+PREFER.jrl-modelloader?=robotpkg
+
+DEPEND_USE+=		jrl-modelloader
+
+DEPEND_ABI.jrl-modelloader?=jrl-modelloader>=1.4
+DEPEND_DIR.jrl-modelloader?=../../devel/jrl-modelloader
+
+SYSTEM_SEARCH.jrl-modelloader=\
+	bin/ModelLoaderJRL		\
+	'lib/pkgconfig/jrl-modelloader.pc:/Version:/s/[^0-9.]*//p'
+
+endif # JRL_MODELLOADER_DEPEND_MK ------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
