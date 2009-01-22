52953409e6c7ac16619e8aa7e673dc273878242b
Anthony Mallet <anthony.mallet@laas.fr>
2009-01-22 19:11:37 +0100

    [architecture/yarp] Import yarp-2.2.2
    YARP stands for Yet Another Robot Platform. What is it? If data is the
    bloodstream of your robot, then YARP is the circulatory system. It isn't the
    brain. It isn't the heart. But it is pretty useful to have around all the
    same.
    
    More specifically, YARP supports building a robot control system as a
    collection of programs communicating in a peer-to-peer way, with a family of
    connection types that meet the diverse, sometimes contradictory, and always
    changing needs of advanced robotics.

---
+++ architecture/yarp/depend.mk
@@ -0,0 +1,43 @@
+# $LAAS: depend.mk 2009/01/22 18:54:42 mallet $
+#
+# Copyright (c) 2009 LAAS/CNRS
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software for any purpose
+# with or without   fee is hereby granted, provided   that the above  copyright
+# notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+#                                             Anthony Mallet on Thu Jan 22 2009
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+YARP_DEPEND_MK:=	${YARP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		yarp
+endif
+
+ifeq (+,$(YARP_DEPEND_MK)) # -----------------------------------------------
+
+PREFER.yarp?=		robotpkg
+
+DEPEND_USE+=		yarp
+DEPEND_ABI.yarp?=	yarp>=2.2.2
+
+SYSTEM_SEARCH.yarp=\
+	'bin/yarp:s/[^.0-9]//gp:% version'
+
+USE_YARP=		yes
+export YARP=		${PREFIX.yarp}/bin/yarp
+
+endif # YARP_DEPEND_MK ----------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
