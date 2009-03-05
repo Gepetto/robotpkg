3235c5cc008982569d69e74e695d63d2ecaffec9
Anthony Mallet <anthony.mallet@laas.fr>
2009-03-05 18:54:32 +0100

    [mk] Remove outdated comments in the subdir mk template

--- mk/robotpkg.subdir.mk
+++ mk/robotpkg.subdir.mk
@@ -1,4 +1,4 @@
-# $LAAS: robotpkg.subdir.mk 2009/03/05 00:10:40 tho $
+# $LAAS: robotpkg.subdir.mk 2009/03/05 12:40:17 mallet $
 #
 # Copyright (c) 2007,2009 LAAS/CNRS
 # All rights reserved.
@@ -27,25 +27,9 @@
 # The include file <robotpkg.subdir.mk> contains the default targets
 # for building ports subdirectories.
 #
-#
-# +++ variables +++
-#
-# OPSYS		Get the operating system type [`uname -s`]
-#
 # SUBDIR	A list of subdirectories that should be built as well.
 #		Each of the targets will execute the same target in the
 #		subdirectories.
-#
-#
-# +++ targets +++
-#
-#	index.html:
-#		Creating index.html for package.
-#
-#	afterinstall, all, beforeinstall, build, checksum, clean,
-#	configure, deinstall, depend, describe, extract, fetch, fetch-list,
-#	install, package, readmes, realinstall, reinstall, tags,
-#	mirror-distfiles, bulk-install, bulk-package, ${PKG_MISC_TARGETS}
 #
 
 .DEFAULT_GOAL:=all
@@ -68,8 +52,8 @@ endif
 #
 __targets=\
 	all fetch package extract configure build install clean		\
-	cleandir distclean depend describe reinstall tags checksum	\
-	makedistinfo makepatchsum makesum mirror-distfiles deinstall	\
+	cleandir distclean depend reinstall tags checksum makedistinfo	\
+	makepatchsum makesum mirror-distfiles deinstall			\
 	update clean-update show-var show-vars lint
 
 .PHONY: ${__targets}
