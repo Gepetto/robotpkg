838b96e44e8b3f43f2902d832958593190f233ac
Séverin Lemaignan <severin.lemaignan@laas.fr>
2009-07-06 18:55:45 +0200

    [knowledge/jena] Corrected small error in depend.mk

--- knowledge/jena/depend.mk
+++ knowledge/jena/depend.mk
@@ -31,7 +31,7 @@ PREFER.jena?=		robotpkg
 
 DEPEND_USE+=		jena
 DEPEND_ABI.jena?=	jena>=2.6.0
-DEPEND_DIR.jena?=	../../devel/jena
+DEPEND_DIR.jena?=	../../knowledge/jena
 
 SYSTEM_SEARCH.jena=\
 	java/jena/lib/jena.jar
