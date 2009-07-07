7d43e0380b07196e94fbd3249b646e00db58700a
Séverin Lemaignan <severin.lemaignan@laas.fr>
2009-07-07 14:26:20 +0200

    [knowledge/pellet]Initial import of package pellet. pellet is a reasonner for OWL ontologies

---
+++ knowledge/pellet/depend.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (c) 2009 LAAS/CNRS
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software for any purpose
+# with or without   fee is hereby granted, provided   that the above  copyright
+# notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+#                                          Séverin Lemaignan on Tue Jul 7 2009
+#
+
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PELLET_DEPEND_MK:=	${PELLET_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		pellet
+endif
+
+ifeq (+,$(PELLET_DEPEND_MK)) # -----------------------------------------------
+
+PREFER.pellet?=		robotpkg
+
+DEPEND_USE+=		pellet
+DEPEND_ABI.pellet?=	pellet>=2.0.0rc7
+DEPEND_DIR.pellet?=	../../knowledge/pellet
+
+SYSTEM_SEARCH.pellet=\
+	java/pellet/lib/pellet-core.jar
+
+endif # PELLET_DEPEND_MK -----------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
