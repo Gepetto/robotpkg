75bb8c2ddf6f6a0f391b5c17186175d65b221b99
Anthony Mallet <anthony.mallet@laas.fr>
2009-11-18 14:58:43 +0100

    [optimization/roboptim-core-cfsqp-plugin] Cosmetics changes in depend.mk
    Wrap long lines, homogenize style.

--- optimization/roboptim-core-cfsqp-plugin/depend.mk
+++ optimization/roboptim-core-cfsqp-plugin/depend.mk
@@ -1,4 +1,4 @@
-# $LAAS: depend.mk 2009/02/02 16:36:09 mallet $
+# $LAAS: depend.mk 2009/11/18 14:54:13 mallet $
 #
 # Copyright (c) 2008-2009 LAAS/CNRS
 # All rights reserved.
@@ -13,17 +13,17 @@
 #      notice and  this list of  conditions in the  documentation and/or
 #      other materials provided with the distribution.
 #
-#                                      Florent Lamiraux on Oct 14, 2009
+#                                          Thomas Moulard on Nov 17 2009
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-ROBOPTIM-CORE-CFSQP-PLUGIN_DEPEND_MK:=${ROBOPTIM-CORE-CFSQP-PLUGIN_DEPEND_MK}+
+ROBOPTIM_CORE_CFSQP_PLUGIN_DEPEND_MK:=${ROBOPTIM_CORE_CFSQP_PLUGIN_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		roboptim-core-cfsqp-plugin
 endif
 
-ifeq (+,$(ROBOPTIM-CORE-CFSQP-PLUGIN_DEPEND_MK)) # --------------------------------
+ifeq (+,$(ROBOPTIM_CORE_CFSQP_PLUGIN_DEPEND_MK)) # -------------------------
 
 PREFER.roboptim-core-cfsqp-plugin?=	robotpkg
 
@@ -33,8 +33,9 @@ SYSTEM_SEARCH.roboptim-core-cfsqp-plugin=\
 	include/roboptim/core/plugin/cfsqp.hh
 
 DEPEND_ABI.roboptim-core-cfsqp-plugin?=	roboptim-core-cfsqp-plugin>=0.4
-DEPEND_DIR.roboptim-core-cfsqp-plugin?=	../../optimization/roboptim-core-cfsqp-plugin
+DEPEND_DIR.roboptim-core-cfsqp-plugin?=\
+	../../optimization/roboptim-core-cfsqp-plugin
 
-endif # ROBOPTIM-CORE-CFSQP-PLUGIN_DEPEND_MK --------------------------------------
+endif # ROBOPTIM_CORE_CFSQP_PLUGIN_DEPEND_MK -------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
