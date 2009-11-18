1ba513f6cddcad7a945b4f24a09b2aa861b95dd9
Anthony Mallet <anthony.mallet@laas.fr>
2009-11-19 00:25:36 +0100

    [robots/hrp2-builder] Fix PKGNAME not matching its PKGPATH
    While here, fix error in the copyright year and file author.

--- robots/hrp2-builder/depend.mk
+++ robots/hrp2-builder/depend.mk
@@ -1,6 +1,5 @@
-# $LAAS: depend.mk 2009/03/09 15:19:44 mallet $
 #
-# Copyright (c) 2008-2009 LAAS/CNRS
+# Copyright (c) 2009 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution and use  in source  and binary  forms,  with or without
@@ -13,29 +12,29 @@
 #      notice and  this list of  conditions in the  documentation and/or
 #      other materials provided with the distribution.
 #
-#                                      Anthony Mallet on Wed May 14 2008
+#                                    Florent Lamiraux on Fri Oct 30 2009
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-HRP2_BUILDER_DEPEND_MK:=	${HRP2_BUILDER_DEPEND_MK}+
+HRP2_BUILDER_DEPEND_MK:=${HRP2_BUILDER_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		robotbuilder
+DEPEND_PKG+=			hrp2-builder
 endif
 
-ifeq (+,$(HRP2_BUILDER_DEPEND_MK)) # --------------------------------------
+ifeq (+,$(HRP2_BUILDER_DEPEND_MK)) # ---------------------------------------
 
-PREFER.robotbuilder?=	robotpkg
+PREFER.hrp2-builder?=		robotpkg
 
-SYSTEM_SEARCH.robotbuilder=\
+SYSTEM_SEARCH.hrp2-builder=\
 	include/robotbuilder/robotbuilder.hh \
 	lib/pkgconfig/robotbuilder.pc
 
-DEPEND_USE+=		robotbuilder
+DEPEND_USE+=			hrp2-builder
 
-DEPEND_ABI.robotbuilder?=	robotbuilder>=1.0
-DEPEND_DIR.robotbuilder?=	../../robots/hrp2-builder
+DEPEND_ABI.hrp2-builder?=	hrp2-builder>=1.0
+DEPEND_DIR.hrp2-builder?=	../../robots/hrp2-builder
 
-endif # HRP2_BUILDER_DEPEND_MK --------------------------------------------
+endif # HRP2_BUILDER_DEPEND_MK ---------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
