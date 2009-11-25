3bea22f7ed924b51dc9f84b5905fb8e2e6d2b6ac
Anthony Mallet <anthony.mallet@laas.fr>
2009-11-25 17:53:01 +0100

    [image/libraw1394] Update to 2.0.4
    Now prefer the system package by default, since recent fedora (>=11)
    seem to have working juju support for multiple cameras.
    
    Also update the MASTER_SITE to point on sourceforge as the
    www.linux1394.org seem to be dead.
    
    Visible changes since 1.3:
    . Match only /dev/fw[0-9]* as firewire-core device files
    . Fix memory leaks with async requests on firewire-core
    . Use new async stream ioctl on juju
    . Use new iso resource allocation ioctls. This allows
      raw1394_bandwidth_modify() and raw1394_channel_modify() to work
    . Use new broadcast request ioctl (juju)
    . Change the license of the "juju" fw*.[hc] files to LGPL v2.1
    . Work without permission to access local node's /dev/fw*
    . Fix stack corruption during juju lock transactions
    . Fix iso_shutdown with juju firewire stack
    . Retry read when failed with EINTR. This can occur when libraw1394
      caller receives a signal while in this read and the caller is not
      using a signal handler set.
    . Bugfix size of mmap'ed isochronous buffer (juju).
    . Wrap receive tail pointer correctly in iso mode.
    . add support for RAW1394DEV environment variable to override
      default /dev/raw1394
    . remove memory allocations for isochronous operations from the
      libraw1394 event loop to make them more respectful of realtime
      applications
    
    And maybe more since the ChangeLog is missing release timestamps.
    A lot of fixes anyway...

--- image/libraw1394/depend.mk
+++ image/libraw1394/depend.mk
@@ -1,6 +1,6 @@
-# $LAAS: depend.mk 2008/05/25 14:02:53 tho $
+# $LAAS: depend.mk 2009/11/24 16:28:04 mallet $
 #
-# Copyright (c) 2008 LAAS/CNRS
+# Copyright (c) 2008-2009 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use in source   and binary forms,  with or without
@@ -23,17 +23,23 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		libraw1394
 endif
 
-ifeq (+,$(LIBRAW1394_DEPEND_MK))
-PREFER.libraw1394?=	robotpkg
+ifeq (+,$(LIBRAW1394_DEPEND_MK)) # -----------------------------------------
+
+PREFER.libraw1394?=	system
 
 DEPEND_USE+=		libraw1394
 
 DEPEND_ABI.libraw1394?=	libraw1394>=1.3.0
 DEPEND_DIR.libraw1394?=	../../image/libraw1394
 
+SYSTEM_PKG.Linux-fedora.libraw1394=libraw1394-devel
+SYSTEM_PKG.Linux-ubuntu.libraw1394=libraw1394-dev
+SYSTEM_PKG.Linux-debian.libraw1394=libraw1394-dev
+
 SYSTEM_SEARCH.libraw1394=\
-	include/libraw1394/raw1394.h	\
-	lib/pkgconfig/libraw1394.pc
-endif
+	include/libraw1394/raw1394.h				\
+	'lib/pkgconfig/libraw1394.pc:/Version/s/[^0-9.]//gp'
+
+endif # LIBRAW1394_DEPEND_MK -----------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
