8d94fef8a1eeb2427165089f3b71673cab73d59b
Anthony Mallet <anthony.mallet@laas.fr>
2009-11-25 18:06:15 +0100

    [image/libdc1394] Update to 2.1.2
    Now default to system package since recent fedora (>=11) seems to have
    working support for juju with multiple cameras.
    
    Visible changes since 2.0.2:
    . fixed wrong returned values for dc1394_trigger_get_polarity.
    . Fix alignment problem during reads on some platforms with juju.
    . Fix image size bug in dc1394_deinterlace_stereo_frames.
    . Choose platform at run-time, not compile-time. This eliminates the
      --with-juju-dir configure option.
    . Juju: Retry busy transactions
    . Add the "usb" platform, for support of devices that do IIDC-over-USB.
    . Juju: Fix missing frames when host controller is OHCI 1.0 and
      packets-per-frame is not a multiple of 8.
    . Added automatic iso allocation to juju using the new support in
      kernel 2.6.30
    . Added timestamp support to the juju platform.
    . Match only /dev/fw[0-9]* as device names on Juju.

--- image/libdc1394/depend.mk
+++ image/libdc1394/depend.mk
@@ -1,6 +1,5 @@
-# $LAAS: depend.mk 2008/05/25 14:02:11 tho $
 #
-# Copyright (c) 2008 LAAS/CNRS
+# Copyright (c) 2008-2009 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use in source   and binary forms,  with or without
@@ -23,18 +22,24 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		libdc1394
 endif
 
-ifeq (+,$(LIBDC1394_DEPEND_MK))
-PREFER.libdc1394?=	robotpkg
+ifeq (+,$(LIBDC1394_DEPEND_MK)) # ------------------------------------------
+
+PREFER.libdc1394?=	system
 
 DEPEND_USE+=		libdc1394
 
 DEPEND_ABI.libdc1394?=	libdc1394>=2.0.1
 DEPEND_DIR.libdc1394?=	../../image/libdc1394
 
+SYSTEM_PKG.Linux-fedora.libdc1394=libdc1394-devel
+SYSTEM_PKG.Linux-ubuntu.libdc1394=libdc1394-dev
+SYSTEM_PKG.Linux-debian.libdc1394=libdc1394-dev
+
 SYSTEM_SEARCH.libdc1394=\
 	include/dc1394/dc1394.h		\
-	lib/pkgconfig/libdc1394-2.pc
-endif
+	'lib/pkgconfig/libdc1394-2.pc:/Version/s/[^0-9.]//gp'
+
+endif # LIBDC1394_DEPEND_MK ------------------------------------------------
 
 include ../../image/libraw1394/depend.mk
 
