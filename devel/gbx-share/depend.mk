dcc7c73ca0293e0e69698819d041690d5bffd479
Anthony Mallet <anthony.mallet@laas.fr>
2010-03-17 17:31:12 +0100

    [devel/gbx-share] Import gbx-share-9.11
    Gearbox is a collection of software tools which could be integrated into
    complete robotic systems with one of many software integration
    frameworks. GearBox is not a framework and thus does not compete with any
    existing or future frameworks. It does not contain any "glue" code to help you
    assemble a complete system.
    
    This package contains the cmake files installed by Gearbox that are common
    to all Gearbox packages.

---
+++ devel/gbx-share/depend.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (c) 2010 LAAS/CNRS
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software for any purpose
+# with or without   fee is hereby granted, provided   that the above  copyright
+# notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+#                                            Anthony Mallet on Wed Mar 17 2010
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GBX_SHARE_DEPEND_MK:=	${GBX_SHARE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		gbx-share
+endif
+
+ifeq (+,$(GBX_SHARE_DEPEND_MK)) # ------------------------------------------
+
+PREFER.gearbox?=	robotpkg
+PREFER.gbx-share?=	${PREFER.gearbox}
+
+SYSTEM_SEARCH.gbx-share=\
+  'lib/gearbox/gearbox-config-version.cmake:/PACKAGE_VERSION[ \t]/s/[^.0-9]//gp'
+
+DEPEND_USE+=		gbx-share
+
+DEPEND_ABI.gbx-share?=	gbx-share>=9.11
+DEPEND_DIR.gbx-share?=	../../devel/gbx-share
+
+DEPEND_METHOD.gbx-share?=build
+
+endif # GBX_SHARE_DEPEND_MK -----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
