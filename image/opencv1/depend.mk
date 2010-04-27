8396adc5f8579e12cf1a23c5483c064cdc5343ae
Anthony Mallet <anthony.mallet@laas.fr>
2010-04-27 23:28:31 +0200

    [image/opencv] Rename opencv package to opencv1 - no functional change
    Note: when updating a package depending on the previous opencv package, you
    will have to manually robotpkg_delete opencv first and reinstall opencv1 (the
    reinstall will be automatically pulled by the dependencies, though).

--- image/opencv1/depend.mk
+++ image/opencv1/depend.mk
@@ -1,6 +1,5 @@
-# $LAAS: depend.mk 2009/04/30 16:07:53 mallet $
 #
-# Copyright (c) 2008-2009 LAAS/CNRS
+# Copyright (c) 2008-2010 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use in source   and binary forms,  with or without
@@ -17,25 +16,25 @@
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-OPENCV_DEPEND_MK:=	${OPENCV_DEPEND_MK}+
+OPENCV1_DEPEND_MK:=	${OPENCV1_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		opencv
+DEPEND_PKG+=		opencv1
 endif
 
-ifeq (+,$(OPENCV_DEPEND_MK)) # ---------------------------------------------
+ifeq (+,$(OPENCV1_DEPEND_MK)) # --------------------------------------------
 
-PREFER.opencv?=		robotpkg
+PREFER.opencv1?=		robotpkg
 
-DEPEND_USE+=		opencv
+DEPEND_USE+=		opencv1
 
-DEPEND_ABI.opencv?=	opencv>=1.1pre1
-DEPEND_DIR.opencv?=	../../image/opencv
+DEPEND_ABI.opencv1?=	opencv1>=1.1pre1
+DEPEND_DIR.opencv1?=	../../image/opencv1
 
-SYSTEM_SEARCH.opencv=\
+SYSTEM_SEARCH.opencv1=\
 	include/opencv/cv.h		\
 	'lib/pkgconfig/opencv.pc:/Version/s/[^.0-9]//gp'
 
-endif # OPENCV_DEPEND_MK ---------------------------------------------------
+endif # OPENCV1_DEPEND_MK --------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
