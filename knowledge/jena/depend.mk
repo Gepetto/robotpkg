3346aadc967be861f0a3327bfd1134f8de804ee9
Séverin Lemaignan <severin.lemaignan@laas.fr>
2010-07-06 19:49:34 +0200

    [knowledge/jena] Bumped to version 2.6.3
    GraphBase:
       o Added limits to the number of triples thatr GraphBase.toString() will
         serialise.
    
    Model/Resource API:
       o Took advantage of covariant return types to allow Resource.inModel() and
         Property.inModel() to have Resource resp. Property return types, allowing
         casts of calls to inModel() to (mostly) be eliminated.
    
       o As per request on mailing list, ModelGraphInterface now has a method
         `Resource wrapAsResource(Node)` [name changed to fit better] which
         wraps a node as a Resource and throws if the node is a Literal.
    
        o As per request on mailing list, added Resource method
            x.getPropertyResourceValue( Property p )
          which answers some R such that x p R if any exists, otherwise null
    
        o As per request on mailing list, there's now an OWL2 vocabulary class
          with constants for the terms of OWL2. NOTE: there is no OWL2 ontology
          support present or implied by this vocabulary class.
    
        o getModel() has been pulled up from Resource to RDFNode. This means that
          Literal now supports getModel() and inModel().
    
        o RDFNode has .asLiteral() and .asResource() methods, which deliver
          the Literal or Resource respectively if the RDF Node has that type,
          and otherwise throw a *RequiredException.
    
    Misc:
       o Patched AnonId using UID when running in environments like GAE
       o Fixed problem with comparison on XSDDurations with fractional seconds
    
    Reasoners:
       o added makeSkolem builtin
       o fixed deadlock problem with hybrid rule systems
       o changed interpretation of Functors so they are neither literals nor objects,
         this allows validation of nested reasoners to cope with the introduction of
         functors representing inferred individuals
       o improved RETE initialization so that fewer triples are injected
         into the rule network in cases such as the RDFS no-resource rules
    
    Ontology API:
       o Fixed NPE bug when an ObjectProperty was not the inverseOf another property
    
    Schemagen:
       o Internal refactoring, which should not affect the external behaviour but should
         make it easier to extend the schemagen class
       o Individuals in an included namespace now appear in the output, even if their
         rdf:type is not included.

--- knowledge/jena/depend.mk
+++ knowledge/jena/depend.mk
@@ -15,7 +15,7 @@ ifeq (+,$(JENA_DEPEND_MK)) # -----------------------------------------------
 PREFER.jena?=		robotpkg
 
 DEPEND_USE+=		jena
-DEPEND_ABI.jena?=	jena>=2.6.2
+DEPEND_ABI.jena?=	jena>=2.6.3
 DEPEND_DIR.jena?=	../../knowledge/jena
 
 SYSTEM_SEARCH.jena=\
