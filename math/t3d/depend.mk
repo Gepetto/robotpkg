504c7437d05e10d57b1ed1be1b55cb625ed626f3
Anthony Mallet <anthony.mallet@laas.fr>
2010-07-15 14:43:04 +0200

    [math/t3d] Bump minimum required version to 2.6 in depend.mk
    While here, make cosmetic changes to clean the file

--- math/t3d/depend.mk
+++ math/t3d/depend.mk
@@ -1,6 +1,5 @@
-# $LAAS: depend.mk 2008/05/25 14:31:35 tho $
 #
-# Copyright (c) 2008 LAAS/CNRS
+# Copyright (c) 2008,2010 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use in source   and binary forms,  with or without
@@ -15,7 +14,6 @@
 #
 #                                      Arnaud Degroote on Thu May 15 2008
 #
-
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 LIBT3D_DEPEND_MK:=	${LIBT3D_DEPEND_MK}+
 
@@ -23,17 +21,19 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		libt3d
 endif
 
-ifeq (+,$(LIBT3D_DEPEND_MK))
-PREFER.libt3d?=	robotpkg
+ifeq (+,$(LIBT3D_DEPEND_MK)) # ---------------------------------------------
+
+PREFER.libt3d?=		robotpkg
 
 DEPEND_USE+=		libt3d
 
-DEPEND_ABI.libt3d?=libt3d>=2.5
-DEPEND_DIR.libt3d?=../../math/t3d
+DEPEND_ABI.libt3d?=	libt3d>=2.6
+DEPEND_DIR.libt3d?=	../../math/t3d
 
 SYSTEM_SEARCH.libt3d=\
 	include/t3d/t3d.h	\
-	lib/pkgconfig/t3d.pc
-endif
+	'lib/pkgconfig/t3d.pc:/Version/s/[^0-9.]//gp'
+
+endif # LIBT3D_DEPEND_MK ---------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
