2347e0d658a937846232fbd1e2b0a824b4e58fc9
florent <florent@laas.fr>
2010-08-18 12:31:37 +0200

    [path/hpp-core] New release 1.12
    	Replaces the previous release 1.12.

--- path/hpp-core/depend.mk
+++ path/hpp-core/depend.mk
@@ -1,5 +1,18 @@
-# robotpkg depend.mk for:	path/hpp-core
-# Created:			Anthony Mallet on Wed, 17 Sep 2008
+#
+# Copyright (c) 2008,2010 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution and use  in source  and binary  forms,  with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   1. Redistributions of  source  code must retain the  above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice and  this list of  conditions in the  documentation and/or
+#      other materials provided with the distribution.
+#
+#                                      Anthony Mallet on Wed Sep 17 2008
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
@@ -15,12 +28,12 @@ PREFER.hpp-core?=	robotpkg
 
 DEPEND_USE+=		hpp-core
 
-DEPEND_ABI.hpp-core?=	hpp-core>=1.11
+DEPEND_ABI.hpp-core?=	hpp-core>=1.12
 DEPEND_DIR.hpp-core?=	../../path/hpp-core
 
 SYSTEM_SEARCH.hpp-core=\
-	include/hppCore/hppProblem.h	\
-	lib/libhppCore.la
+	include/hpp/core/problem.hh	\
+	lib/pkgconfig/hppCore.pc
 
 endif # HPPCORE_DEPEND_MK --------------------------------------------
 
