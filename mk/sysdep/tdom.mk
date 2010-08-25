8ae06b29545cee3a95bd1a7aaa3790a313f17ad3
Anthony Mallet <anthony.mallet@laas.fr>
2010-08-25 14:46:48 +0200

    [mk/sysdep] Teach robotpkg about 'tdom'
    tDOM combines high performance XML data processing with easy and
    powerful Tcl scripting functionality. tDOM should be one of the
    fastest ways to manipulate XML with a scripting language and uses
    very little memory.

---
+++ mk/sysdep/tdom.mk
@@ -0,0 +1,28 @@
+# robotpkg sysdep/tdom.mk
+# Created:			Anthony Mallet on Wed, 25 Aug 2010
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+TDOM_DEPEND_MK:=	${TDOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		tdom
+endif
+
+ifeq (+,$(TDOM_DEPEND_MK)) # -----------------------------------------------
+
+PREFER.tdom?=		system
+
+DEPEND_USE+=		tdom
+DEPEND_ABI.tdom?=	tdom>=0.8
+
+SYSTEM_SEARCH.tdom=	\
+	'lib/tcl{,[0-9]*}/tdom[0-9]*/tdom.tcl'	\
+	'lib/tcl{,[0-9]*}/tdom[0-9]*/pkgIndex.tcl:/ifneeded/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Linux.tdom=	tdom
+
+include ../../mk/sysdep/tcl.mk
+
+endif # TDOM_DEPEND_MK ------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
