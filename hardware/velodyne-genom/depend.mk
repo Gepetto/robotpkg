3886bdbfb19a6bb73aa1b1992c9aeca0048817c1
Anthony Mallet <anthony.mallet@laas.fr>
2010-08-25 16:32:40 +0200

    [hardware/velodyne-genom] Import velodyne genom module 1.0
    GenoM module for Velodyne-HDL64 lidar data acquisition.
    
    This module can read the sensors packets from the velodyne-libs library and
    export a poster corresponding to one full panoramic scan (384 by roughly 720 3D
    points).
    
    The two important requests are: StartAcquistion() and OneShot().

---
+++ hardware/velodyne-genom/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	hardware/velodyne-genom
+# Created:			Anthony Mallet on Wed, 25 Aug 2010
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+VELODYNE_GENOM_DEPEND_MK:=	${VELODYNE_GENOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			velodyne-genom
+endif
+
+ifeq (+,$(VELODYNE_GENOM_DEPEND_MK)) # -------------------------------------
+
+PREFER.velodyne-genom?=	robotpkg
+
+DEPEND_USE+=			velodyne-genom
+
+DEPEND_ABI.velodyne-genom?=	velodyne-genom>=1.0
+DEPEND_DIR.velodyne-genom?=	../../hardware/velodyne-genom
+
+SYSTEM_SEARCH.velodyne-genom=\
+	include/velodyne/velodyneStruct.h	\
+	'lib/pkgconfig/velodyne.pc:/Version/s/[^0-9.]//gp'
+
+endif # VELODYNE_GENOM_DEPEND_MK -------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
