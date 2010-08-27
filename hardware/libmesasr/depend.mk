00a8aaa1689d107ae0b6b34cd8d79b72739e0eea
Matthieu Herrb <matthieu.herrb@laas.fr>
2010-08-27 16:58:17 +0200

    [hardware/libmesasr] a package of the MesaImaging SwissRanger API
    Warning: this package contains a blob (Binary Large OBject) that
    is not open-source at all. It will only run on linux/i386 for now.

---
+++ hardware/libmesasr/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	robots/libmesasr
+# Created:			Matthieu Herrb on Thu, 29 Apr 2010
+#
+
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+LIBMESASR_DEPEND_MK:=${LIBMESASR_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libmesasr
+endif
+
+ifeq (+,$(LIBMESASR_DEPEND_MK))
+PREFER.libmesasr?=	robotpkg
+
+DEPEND_USE+=		libmesasr
+
+DEPEND_ABI.libmesasr?=	libmesasr>=1.0.14
+DEPEND_DIR.libmesasr?=	../../hardware/libmesasr
+
+SYSTEM_SEARCH.libmesasr=\
+	include/libMesaSR.h \
+	lib/libmesasr.so
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
