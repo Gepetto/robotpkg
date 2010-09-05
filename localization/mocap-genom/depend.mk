91774e069c0ec9decd67572e16c5283a258b5e13
Anthony Mallet <anthony.mallet@laas.fr>
2010-09-05 19:58:27 +0200

    [localization/mocap-genom] Fix typos and improve package description
    In DESCR:
    - GenoM does not have a capital N in the middle.
    - State that the module indirectly requires proprietary software.
    - Describe what kind of data is exported.
    
    While here, fix headers of Makefile and depend.mk. (see c206dfb49ef.)

--- localization/mocap-genom/depend.mk
+++ localization/mocap-genom/depend.mk
@@ -1,18 +1,5 @@
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
-#
-# Redistribution  and  use in source   and binary forms,  with or without
-# modification, are permitted provided that  the following conditions are
-# met:
-#
-#   1. Redistributions  of  source code must  retain  the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#   2. Redistributions in binary form must  reproduce the above copyright
-#      notice,  this list of  conditions and  the following disclaimer in
-#      the  documentation   and/or  other  materials   provided with  the
-#      distribution.
-#
-#                                    Severin Lemaignan on Tue 31 Aug 2010
+# robotpkg depend.mk for:	localization/mocap-genom
+# Created:			Séverin Lemaignan on Wed, 1 Sep 2010
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
@@ -27,8 +14,8 @@ ifeq (+,$(MOCAP-GENOM_DEPEND_MK)) # ----------------------------------
 PREFER.mocap-genom?=	robotpkg
 
 SYSTEM_SEARCH.mocap-genom=\
-	include/mocap/mocapStruct.h		\
-	lib/pkgconfig/mocap.pc		\
+	include/mocap/mocapStruct.h			\
+	'lib/pkgconfig/mocap.pc:/Version/s/[^0-9.]//gp'	\
 	bin/mocap
 
 DEPEND_USE+=		mocap-genom
