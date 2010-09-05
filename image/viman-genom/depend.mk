4881634cb777e289f07a8d7ce0f5aa43eef1a13d
Anthony Mallet <anthony.mallet@laas.fr>
2010-09-05 23:25:31 +0200

    [image/viman-genom] Add a real DESCRiption and fix Makefile/depend.mk headings

--- image/viman-genom/depend.mk
+++ image/viman-genom/depend.mk
@@ -1,18 +1,5 @@
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
-#
-# Redistribution  and  use in source   and binary forms,  with or without
-# modification, are permitted provided that  the following conditions are
-# met:
-#
-#   1. Redistributions  of  source code must  retain  the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#   2. Redistributions in binary form must  reproduce the above copyright
-#      notice,  this list of  conditions and  the following disclaimer in
-#      the  documentation   and/or  other  materials   provided with  the
-#      distribution.
-#
-#                                    Severin Lemaignan on Tue 31 Aug 2010
+# robotpkg depend.mk for:	image/viman-genom
+# Created:			Séverin Lemaignan on Tue, 31 Aug 2010
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
@@ -27,8 +14,8 @@ ifeq (+,$(VIMAN-GENOM_DEPEND_MK)) # ----------------------------------
 PREFER.viman-genom?=	robotpkg
 
 SYSTEM_SEARCH.viman-genom=\
-	include/viman/vimanStruct.h		\
-	lib/pkgconfig/viman.pc		\
+	include/viman/vimanStruct.h			\
+	'lib/pkgconfig/viman.pc:/Version/s/[^0-9.]//gp'	\
 	bin/viman
 
 DEPEND_USE+=		viman-genom
