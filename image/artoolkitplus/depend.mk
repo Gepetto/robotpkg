7dfa40daf979b4de7f982c6a45b2e4a3dd0b466c
Anthony Mallet <anthony.mallet@laas.fr>
2010-09-05 23:35:52 +0200

    [image/artoolkitplus] Fix headings for depend.mk

--- image/artoolkitplus/depend.mk
+++ image/artoolkitplus/depend.mk
@@ -1,18 +1,5 @@
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
-#
-# Redistribution  and  use in source   and binary forms,  with or without
-# modification, are permitted provided that  the following conditions are
-# met:
-#
-#   1. Redistributions  of  source code must  retain  the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#   2. Redistributions in binary form must  reproduce the above copyright
-#      notice,  this list of  conditions and  the following disclaimer in
-#      the  documentation   and/or  other  materials   provided with  the
-#      distribution.
-#
-#                                    Severin Lemaignan on Tue 31 Aug 2010
+# robotpkg depend.mk for:	image/artoolkitplus
+# Created:			Séverin Lemaignan on Tue, 31 Aug 2010
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
