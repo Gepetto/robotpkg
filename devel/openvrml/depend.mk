43ae59650f8b88ecc4a331841a923571ab109c51
Anthony Mallet <anthony.mallet@laas.fr>
2010-09-09 01:29:24 +0200

    [devel/openvrml] Number of fixes in the package
    - Name this package openvrml-libs as it compiles the core libraries only.
    - Fix MAINTAINER, don't involve people not aware of the package.
    - Fix MASTER_SITES: use the MASTER_SITE_SOURCE_FORGE variable.
    - Add a gnu-lgpl-v3 LICENSE.
    - Disable javadoc as java is disabled.
    - Fix PLIST with doxygen.
    - Fix detection of ltdl and boost: configure script is looking in /usr only...
    - Fix package installing empty directories for disabled components.
    - Add most of missing dependencies: boost, libxml2, doxygen, ltdl, mesa, glu,
      png, jpeg. A lot more seem to be needed...
    - Add missing newline at end of file in DESCR.
    
    And in a subsequent commit will move this to 'graphics' where it belongs.

--- devel/openvrml/depend.mk
+++ devel/openvrml/depend.mk
@@ -1,44 +1,27 @@
-# $LAAS: Makefile 2010/07/13 11:55:00 tmoulard $
-#
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
-#
-# Permission to use, copy, modify, and distribute this software for any purpose
-# with or without   fee is hereby granted, provided   that the above  copyright
-# notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
-# PERFORMANCE OF THIS SOFTWARE.
-#
-#                                           Thomas Moulard on Tue Jul 13 2010
+# robotpkg depend.mk for:	devel/openvrml-libs
+# Created:			Thomas Moulard on Wed, 8 Sep 2010
 #
 
-
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-OPENVRML_DEPEND_MK:=	${OPENVRML_DEPEND_MK}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+OPENVRML_LIBS_DEPEND_MK:=	${OPENVRML_LIBS_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		openvrml
+DEPEND_PKG+=			openvrml-libs
 endif
 
-ifeq (+,$(OPENVRML_DEPEND_MK)) # -----------------------------------------------
+ifeq (+,$(OPENVRML_LIBS_DEPEND_MK)) # --------------------------------------
 
-PREFER.openvrml?=		robotpkg
+PREFER.openvrml-libs?=		robotpkg
 
-DEPEND_USE+=		openvrml
-DEPEND_ABI.openvrml?=	openvrml>=0.18.6
-DEPEND_DIR.openvrml?=	../../devel/openvrml
+DEPEND_USE+=			openvrml-libs
+DEPEND_ABI.openvrml-libs?=	openvrml-libs>=0.18.6
+DEPEND_DIR.openvrml-libs?=	../../devel/openvrml
 
-SYSTEM_SEARCH.openvrml=\
+SYSTEM_SEARCH.openvrml-libs=\
 	include/openvrml/openvrml-common.h	\
-	'lib/libopenvrml{a,so,dylib}'		\
+	'lib/libopenvrml.{a,so,dylib}'		\
 	'lib/pkgconfig/openvrml.pc:/Version/s/[^0-9.]//gp'
 
-endif # OPENVRML_DEPEND_MK -----------------------------------------------------
+endif # OPENVRML_LIBS_DEPEND_-----------------------------------------------
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
