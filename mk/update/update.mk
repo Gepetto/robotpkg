Gepetto/robotpkg

commit 408bf9b085e850fad425538c04970d0c02912811

[mk/install] Add missing barrier definition in install target
Before 44e95238db, the missing include file was implicitely triggered by the
'replace' target. The rewrite of 'replace' dependencies triggered the latent
bug.

While at this, add missing barrier requirement for 'replace' and 'update'
targets and re-arrange those targets dependencies so that we only test when
needed if the package is already up-to-date (avoid spawning useless processes).

mk/update/update.mk
@@ -46,6 +46,7 @@
 # "make update".
 #
 
+$(call require, ${ROBOTPKG_DIR}/mk/internal/barrier.mk)
 $(call require, ${ROBOTPKG_DIR}/mk/pkg/pkg-vars.mk)
 $(call require, ${ROBOTPKG_DIR}/mk/depends/depends-vars.mk)
 
@@ -65,13 +66,19 @@ _UPDATE_TARGETS+=	do-update
 _UPDATE_TARGETS+=	update-clean
 _UPDATE_TARGETS+=	update-done-message
 
-.PHONY: update
-ifeq (yes,$(call exists,${_UPDATE_DIRS}))
-  update: ${_UPDATE_TARGETS}
-else
-  update: $(call only-for, update, \
-		$(call for-unsafe-pkg, ${_UPDATE_TARGETS}, update-up-to-date))
+# run after the 'depends' barrier
+_UPDATE_TARGETS:=$(call barrier, depends, ${_UPDATE_TARGETS})
+
+ifeq (yes,$(call only-for,update,yes))	     # if we are asking for an update
+  ifneq (yes,$(call exists,${_UPDATE_DIRS})) # not resuming a previous one
+    ifneq (yes,$(call for-unsafe-pkg,yes))   # and the package is unsafe
+      _UPDATE_TARGETS:= update-up-to-date    # let us do nothing
+    endif
+  endif
 endif
+
+.PHONY: update
+update: ${_UPDATE_TARGETS}
 
 
 # --- do-update ------------------------------------------------------------

mk/install/install-vars.mk
@@ -1,6 +1,5 @@
-# $LAAS: install-vars.mk 2009/11/16 13:56:14 mallet $
 #
-# Copyright (c) 2006-2009 LAAS/CNRS
+# Copyright (c) 2006-2010 LAAS/CNRS
 # All rights reserved.
 #
 # This project includes software developed by the NetBSD Foundation, Inc.
@@ -72,6 +71,7 @@ else
     install:
 	@${DO_NADA}
   else
+    $(call require, ${ROBOTPKG_DIR}/mk/internal/barrier.mk)
     $(call require, ${ROBOTPKG_DIR}/mk/build/build-vars.mk)
 
     install: $(call barrier, depends, build install-cookie)

mk/install/install.mk
@@ -55,6 +55,7 @@ _INSTALL_TARGETS+=	release-install-lock
 ifeq (yes,$(call exists,${_COOKIE.install}))
   install:;
 else
+  $(call require, ${ROBOTPKG_DIR}/mk/internal/barrier.mk)
   install: $(call barrier, depends, ${_INSTALL_TARGETS})
 endif
 

mk/install/replace.mk
@@ -33,6 +33,7 @@
 # Updates a package in-place on the system.
 # It will acquire elevated privileges just-in-time.
 #
+$(call require, ${ROBOTPKG_DIR}/mk/internal/barrier.mk)
 $(call require, ${ROBOTPKG_DIR}/mk/build/build-vars.mk)
 $(call require, ${ROBOTPKG_DIR}/mk/pkg/pkg-vars.mk)
 
@@ -46,11 +47,17 @@ else
   su-replace: 		pkg-replace
 endif
 
+# run after the 'depends' barrier
+_REPLACE_TARGETS:=$(call barrier, depends, ${_REPLACE_TARGETS})
+
+ifneq (,$(filter replace,${MAKECMDGOALS})) # if we are asking for a replace
+  ifneq (yes,$(call for-unsafe-pkg,yes))   # and the package is unsafe
+    _REPLACE_TARGETS:= replace-up-to-date  # let us do nothing
+  endif
+endif
+
 .PHONY: replace
-replace: $(call only-for, replace,					\
-		$(call for-unsafe-pkg, 					\
-			$(call barrier, depends, ${_REPLACE_TARGETS}),	\
-			replace-up-to-date))
+replace: ${_REPLACE_TARGETS}
 
 
 # --- replace-message (PRIVATE) --------------------------------------------

commit 95c4b9d6c170f03eb13e127afbc44a3d50b81bf0

[mk/update] Fix copy/paste typo introduced in 408bf9b085
The 'udpate' target does not run after any barrier, since it's only a 'meta'
target. Wrongly copied/pasted from the 'replace' target.

mk/update/update.mk
@@ -46,7 +46,6 @@
 # "make update".
 #
 
-$(call require, ${ROBOTPKG_DIR}/mk/internal/barrier.mk)
 $(call require, ${ROBOTPKG_DIR}/mk/pkg/pkg-vars.mk)
 $(call require, ${ROBOTPKG_DIR}/mk/depends/depends-vars.mk)
 
@@ -65,9 +64,6 @@ endif
 _UPDATE_TARGETS+=	do-update
 _UPDATE_TARGETS+=	update-clean
 _UPDATE_TARGETS+=	update-done-message
-
-# run after the 'depends' barrier
-_UPDATE_TARGETS:=$(call barrier, depends, ${_UPDATE_TARGETS})
 
 ifeq (yes,$(call only-for,update,yes))	     # if we are asking for an update
   ifneq (yes,$(call exists,${_UPDATE_DIRS})) # not resuming a previous one