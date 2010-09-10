dbc2fc05ad23495c4faf5eb23f6f5bb6ee4b71ee
Anthony Mallet <anthony.mallet@laas.fr>
2010-09-10 11:51:18 +0200

    [manipulation/genManip-genom] Update to 0.6
    Removed 'api' option from this package as this genom module is an interface
    only (the 'api' option is mandatory and always selected).
    
    Changes since 0.4:
    - Added a GENMANIP_MOUSE3D_STR data structure (translation, rotation and two
      flags for mouse buttons)
    
    Bumped DEPEND_ABI to 0.6

--- manipulation/genManip-genom/depend.mk
+++ manipulation/genManip-genom/depend.mk
@@ -2,24 +2,26 @@
 # Created:			Xavier Broquere on Tue, 20 Apr 2010
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 GENMANIPGENOM_DEPEND_MK:=	${GENMANIPGENOM_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		genManip-genom
+DEPEND_PKG+=			genManip-genom
 endif
 
-ifeq (+,$(GENMANIPGENOM_DEPEND_MK))
-PREFER.genManip-genom?=	robotpkg
+ifeq (+,$(GENMANIPGENOM_DEPEND_MK)) # --------------------------------------
 
-DEPEND_USE+=		genManip-genom
+PREFER.genManip-genom?=		robotpkg
 
-DEPEND_ABI.genManip-genom?=	genManip-genom>=0.4
+DEPEND_USE+=			genManip-genom
+
+DEPEND_ABI.genManip-genom?=	genManip-genom>=0.6
 DEPEND_DIR.genManip-genom?=	../../manipulation/genManip-genom
 
 SYSTEM_SEARCH.genManip-genom=\
-	include/genManip/genManipStruct.h		\
-	lib/pkgconfig/genManip.pc
-endif
+	include/genManip/genManipStruct.h			\
+	'lib/pkgconfig/genManip.pc:/Version/s/[^0-9.]//gp'
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
+endif # GENMANIPGENOM_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
