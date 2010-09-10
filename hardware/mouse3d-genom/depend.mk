693679babed6c6aa6f1def1fa0c54b964fb06f96
Anthony Mallet <anthony.mallet@laas.fr>
2010-09-10 17:53:54 +0200

    [hardware/mouse3d-genom] Update to 1.2
    Changes since 1.0:
       Remove dependency with spacenav daemon and libraries. The spacenav daemon
       does nothing but reading the usb device. It's simpler to do the same without
       this extra layer.
    
       New requests:
        - Init: now takes a device path (empty for auto-detection).
        - Export: exports mouse data in the poster.
        - Stop: stops exporting data.
        - Bindings: maps raw axis numbers into x,y,z Rx,Ry,Rz b0,b1
        - Gains: apply a gain on the raw axis values.
        - Threshold: set mouse position to 0 below this threshold (after gain).
    
       Removed "ResetOffset" request as I didn't see what it could be useful for
       (the position is absolute, so it won't drift).
    
    While here, fixed PLIST and added missing dependencies (gbM, genBasic,
    genManip).

--- hardware/mouse3d-genom/depend.mk
+++ hardware/mouse3d-genom/depend.mk
@@ -3,23 +3,25 @@
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-MOUSE3DGENOM_DEPEND_MK:=	${MOUSE3DGENOM_DEPEND_MK}+
+MOUSE3DGENOM_DEPEND_MK:=${MOUSE3DGENOM_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		mouse3d-genom
 endif
 
-ifeq (+,$(MOUSE3DGENOM_DEPEND_MK))
+ifeq (+,$(MOUSE3DGENOM_DEPEND_MK)) # ---------------------------------------
+
 PREFER.mouse3d-genom?=	robotpkg
 
 DEPEND_USE+=		mouse3d-genom
 
-DEPEND_ABI.mouse3d-genom?=	mouse3d-genom>=1.0
-DEPEND_DIR.mouse3d-genom?=	../../hardware/mouse3d-genom
+DEPEND_ABI.mouse3d-genom?=mouse3d-genom>=1.2
+DEPEND_DIR.mouse3d-genom?=../../hardware/mouse3d-genom
 
 SYSTEM_SEARCH.mouse3d-genom=\
-	include/mouse3d/mouse3dStruct.h		\
-	lib/pkgconfig/mouse3d.pc
-endif
+	include/mouse3d/mouse3dStruct.h				\
+	'lib/pkgconfig/mouse3d.pc:/Version/s/[^0-9.]//gp'
+
+endif # MOUSE3DGENOM_DEPEND_MK ---------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
