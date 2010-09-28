6218106e639896ae654b57eafc587df7c3feda31
Severin Lemaigan <slemaign@laas.fr>
2010-09-28 17:46:56 +0200

    [graphics/pqp] Import pqp-1.3, a proximity query package
    PQP is a library for performing three types of proximity queries on a pair of
    geometric models composed of triangles:
    
     * Collision detection - detecting whether the two models overlap, and
       optionally, all of the triangles that overlap.
     * Distance computation - computing the minimum distance between a pair of
       models, i.e., the distance between the closest pair of points.
     * Tolerance verification - determining whether two models are closer or
       farther than a tolerance distance.
    
    Available under the pqp-license, a free-for-non-commercial-use license.

---
+++ graphics/pqp/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	graphics/pqp
+# Created:			Severin Lemaignan on Tue, 31 Aug 2010
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PQP_DEPEND_MK:=		${PQP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		pqp
+endif
+
+ifeq (+,$(PQP_DEPEND_MK)) # ------------------------------------------------
+
+PREFER.pqp?=		robotpkg
+
+SYSTEM_SEARCH.pqp=\
+	include/pqp/PQP.h	\
+	lib/libpqp.a
+
+DEPEND_USE+=		pqp
+
+DEPEND_ABI.pqp?=	pqp>=1.3
+DEPEND_DIR.pqp?=	../../graphics/pqp
+
+endif # PQP_DEPEND_MK ------------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
