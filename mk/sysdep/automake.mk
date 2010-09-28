035b5ca4c7220f6bf7c97d3f1db6a07d72f32322
Anthony Mallet <anthony.mallet@laas.fr>
2010-09-28 15:10:50 +0200

    [mk/sysdep] Teach robotpkg about automake as a system dependency.

---
+++ mk/sysdep/automake.mk
@@ -0,0 +1,28 @@
+# robotpkg sysdep/automake.mk
+# Created:			Anthony Mallet on Tue, 28 Sep 2010
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+AUTOMAKE_DEPEND_MK:=	${AUTOMAKE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		automake
+endif
+
+ifeq (+,$(AUTOMAKE_DEPEND_MK)) # -------------------------------------------
+
+PREFER.automake?=	system
+
+DEPEND_USE+=		automake
+DEPEND_ABI.automake?=	automake>=1.8
+DEPEND_METHOD.automake?=build
+SYSTEM_SEARCH.automake=\
+	'bin/aclocal:/aclocal/{s/[^0-9.]//gp;q;}:% --version'	\
+	'bin/automake:/automake/{s/[^0-9.]//gp;q;}:% --version'
+
+export AUTOMAKE=		${PREFIX.automake}/bin/automake
+
+GNU_CONFIGURE?=		yes
+
+endif # AUTOMAKE_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
