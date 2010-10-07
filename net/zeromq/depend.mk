2730328043f786e299464799ceb4eb4e12da0678
Azamat Shakhimardanov <azamat.shakhimardanov@fh-brs.de>
2010-10-07 16:00:17 +0200

    [net/zeromq] Import 0MQ-2.0.10
    0MQ looks like an embeddable networking library but acts like a concurrency
    framework. It gives you sockets that carry whole messages across various
    transports like inproc, IPC, TCP, and multicast. You can connect sockets N-to-N
    with patterns like fanout, pubsub, task distribution, and request-reply. It's
    fast and small enough to be the fabric for clustered products. Its asynchronous
    I/O model gives you scalable multicore applications, built as asynchronous
    message-processing tasks. It has over twenty language APIs and runs on most
    operating systems.
    
    Package initially contributed in the BRICS package repository by Azamat
    Shakhimardanov and Jan Paulus. Minor changes to define OpenPGM support as an
    option and added version check in depend.mk
    
    While here, upgrade to 2.0.10. Changes since 2.0.9:
    
    New functionality
    -----------------
    * Upgrade OpenPGM to 2.1.28~dfsg (Martin Lucina)
    * Added a ZMQ_VERSION macro to zmq.h for compile-time API version
      detection (Martin Sustrik, Gonzalo Diethelm, Martin Lucina)
    
    Bug fixes:
    ----------
    * Fix memory leak under Windows (Taras Shpot)
    * Makefile.am: Add missing files to distribution, improve
      maintainer-clean (Martin Lucina)
    * Add support for RHEL6 in the spec file (Sebastian Otaegui)
    * configure.in: Do not patch libtool rpath handling (Martin Lucina)
    * Fixing the Red Hat packaging (Martin Sustrik)
    * zmq_msg_move called on uninitialised message in xrep_t::xrecv -- fixed
      (Max Wolf)
    * crash when closing an ypipe -- fixed (Dhammika Pathirana)
    * REQ socket can die when reply is delivered on wrong unerlying
      connection -- fixed (Martin Sustrik)
    * if TSC jumps backwards (in case of migration to a different CPU core)
      latency peak may occur -- fixed (Martin Sustrik)
    * values of RATE, RECOVERY_IVL and SWAP options are checked for negative
      values (Martin Sustrik)

---
+++ net/zeromq/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	brics/libunicap
+# Created:	 Azamat BRSU on 18 Aug 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ZEROMQ_DEPEND_MK:= 	${ZEROMQ_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		zeromq
+endif
+
+ifeq (+,$(ZEROMQ_DEPEND_MK)) # ---------------------------------------------
+
+PREFER.zeromq?=		robotpkg
+
+DEPEND_USE+=		zeromq
+
+DEPEND_ABI.zeromq?=	zeromq>=2.0.9
+DEPEND_DIR.zeromq?=	../../net/zeromq
+
+SYSTEM_SEARCH.zeromq=\
+	include/zmq.h	\
+	liblibzmq.la	\
+	'lib/pkgconfig/libzmq.pc:/Version/s/[^0-9.]//gp'
+
+endif # ZEROMQ_DEPEND_MK ---------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
