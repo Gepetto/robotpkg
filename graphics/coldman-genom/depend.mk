97c027dbd2087f9cb9ab1a9b8e889a0b9d36d5ac
Anthony Mallet <anthony.mallet@laas.fr>
2010-10-15 17:49:58 +0200

    [graphics/coldman-genom] Import coldman-genom-1.2
    coldman-genom is a GenoM module that can be used to control the display
    facility found in coldman-libs.

---
+++ graphics/coldman-genom/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	manipulation/coldman-genom
+# Created:			Anthony Mallet on Fri, 15 Oct 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+COLDMAN-GENOM_DEPEND_MK:=	${COLDMAN-GENOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		coldman-genom
+endif
+
+ifeq (+,$(COLDMAN-GENOM_DEPEND_MK)) # ----------------------------------
+
+PREFER.coldman-genom?=	robotpkg
+
+SYSTEM_SEARCH.coldman-genom=\
+	bin/coldman-genom			\
+	include/coldman/coldmanStruct.h		\
+	'lib/pkgconfig/coldman.pc:s/[^0-9.]//gp'
+
+DEPEND_USE+=		coldman-genom
+
+DEPEND_ABI.coldman-genom?=coldman-genom>=1.0
+DEPEND_DIR.coldman-genom?=../../graphics/coldman-genom
+
+endif # COLDMAN-GENOM_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
