31acd5b10e9f9330424c50e87d8225aeb60d0439
Anthony Mallet <anthony.mallet@laas.fr>
2010-10-15 17:47:17 +0200

    [graphics/coldman-libs] Import coldman-libs 1.3
    coldmanlibs is a library for collision detection, base on PQP (see
    graphics/pqp). It includes an optional 3d display facility.

---
+++ graphics/coldman-libs/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	manipulation/coldman-libs
+# Created:			Anthony Mallet on Fri, 15 Oct 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+COLDMAN-LIBS_DEPEND_MK:=	${COLDMAN-LIBS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		coldman-libs
+endif
+
+ifeq (+,$(COLDMAN-LIBS_DEPEND_MK)) # ----------------------------------
+
+PREFER.coldman-libs?=	robotpkg
+
+SYSTEM_SEARCH.coldman-libs=\
+	include/coldman-libs/coldman.h	\
+	lib/libcoldman-libs.so
+
+DEPEND_USE+=		coldman-libs
+
+DEPEND_ABI.coldman-libs?=coldman-libs>=1.3
+DEPEND_DIR.coldman-libs?=../../graphics/coldman-libs
+
+endif # COLDMAN-LIBS_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
