09749ec458d5f3864b6b65a235d670922dd0c51b
Anthony Mallet <anthony.mallet@laas.fr>
2010-10-28 15:32:24 +0200

    [mk/sysdep] Rename libxmu.mk to libXmu.mk to match the package name.
    While here, add a check on the .pc file to retrieve libXmu version.

---
+++ mk/sysdep/libXmu.mk
@@ -0,0 +1,28 @@
+# robotpkg sysdep/libXmu.mk
+# Created:			Xavier Broquere on Thu 28 Oct 2010
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+LIBXMU_DEPEND_MK:=	${LIBXMU_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libXmu
+endif
+
+ifeq (+,$(LIBXMU_DEPEND_MK)) # ---------------------------------------------
+
+PREFER.libXmu?=		system
+DEPEND_USE+=		libXmu
+DEPEND_ABI.libXmu?=	libXmu>=1.0.5
+
+SYSTEM_SEARCH.libXmu=	\
+	include/X11/Xmu/Xmu.h	\
+	lib/libXmu.so		\
+	'lib/pkgconfig/xmu.pc:/Version/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Linux-fedora.libXmu=libXmu-devel
+SYSTEM_PKG.Linux-ubuntu.libXmu=libXmu-dev
+SYSTEM_PKG.Linux-debian.libXmu=libXmu-dev
+
+endif # LIBXMU_DEPEND_MK ---------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
