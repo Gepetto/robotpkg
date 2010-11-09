9a9480b12810554f68310252c6748128a6eae750
Xavier Broquere <xavier.broquere@laas.fr>
2010-11-09 13:41:29 +0100

    [architecture/genHum-genom] Bump to 1.0
      - add GENHUM_HUMAN_DISTANCE structure
      - add depend.mk

---
+++ architecture/genHum-genom/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	architecture/genHum-genom
+# Created:			Xavier BROQUERE on Tue, 09 Nov 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GENHUMGENOM_DEPEND_MK:=	${GENHUMGENOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		genHum-genom
+endif
+
+ifeq (+,$(GENHUMGENOM_DEPEND_MK))
+PREFER.genHum-genom?=	robotpkg
+
+DEPEND_USE+=		genHum-genom
+
+DEPEND_ABI.genHum-genom?=	genHum-genom>=1.0
+DEPEND_DIR.genHum-genom?=	../../architecture/genHum-genom
+
+SYSTEM_SEARCH.genHum-genom=\
+	include/genHum/genHumStruct.h		\
+	lib/pkgconfig/genHum.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
