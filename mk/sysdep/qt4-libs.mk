5f38a0a65cfb54149b34a904da2761976719345d
Michael Reckhaus <michael.reckhaus@h-brs.de>
2010-11-17 14:19:31 +0100

    [mk/sysdep] Teach robotpkg about qt4 libraries.
    Imported from the BRICS package repository, contributed by Michael Reckhaus.

---
+++ mk/sysdep/qt4-libs.mk
@@ -0,0 +1,30 @@
+# robotpkg sysdep/qt4-libs
+# Created: Michael Reckhaus on 07 Oct 2010
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+QT4_LIBS_DEPEND_MK:=	${QT4_LIBS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+= 		qt4-libs
+endif
+
+ifeq (+,$(QT4_LIBS_DEPEND_MK)) # -------------------------------------------
+
+PREFER.qt4-libs?= 	system
+
+DEPEND_USE+= 		qt4-libs
+DEPEND_ABI.qt4-libs?= 	qt4-libs>=4.6.2
+
+SYSTEM_SEARCH.qt4-libs=\
+	'include/{,qt4/{,include/}}Qt/qcoreevent.h'	\
+	'{,share/qt4/}lib/libQtCore.{a,so,dylib}'	\
+	'lib/pkgconfig/QtCore.pc:/Version/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Linux-fedora.qt4-libs=	qt-devel
+SYSTEM_PKG.Linux-ubuntu.qt4-libs=	libqt4-dev
+SYSTEM_PKG.Linux-debian.qt4-libs=	libqt4-dev
+SYSTEM_PKG.NetBSD.qt4-libs=		pkgsrc/x11/qt4-libs pkgsrc/x11/qt4-tools
+
+endif # QT4_LIBS_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:= ${DEPEND_DEPTH:+=}
