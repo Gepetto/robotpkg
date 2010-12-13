4b6aceb0aa30179416fe26bea16a99c1f7bfe89a
Anthony Mallet <anthony.mallet@laas.fr>
2010-12-13 18:31:55 +0100

    [image/libstereopixel] Upgrade to 1.4
    Changes since 1.3:
     - Build system fixes for better handling pthread and sse options.
     - Import patch-aa reseting some statistics on the number of correlated pixels.

--- image/libstereopixel/depend.mk
+++ image/libstereopixel/depend.mk
@@ -2,26 +2,28 @@
 # Created:			Anthony Mallet on Fri, 14 Mar 2008
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-LIBSTEREOPIXEL_DEPEND_MK:=${LIBSTEREOPIXEL_DEPEND_MK}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+LIBSTEREOPIXEL_DEPEND_MK:=	${LIBSTEREOPIXEL_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		libstereopixel
+DEPEND_PKG+=			libstereopixel
 endif
 
-ifeq (+,$(LIBSTEREOPIXEL_DEPEND_MK))
-PREFER.libstereopixel?=	robotpkg
+ifeq (+,$(LIBSTEREOPIXEL_DEPEND_MK)) # -------------------------------------
 
-DEPEND_USE+=		libstereopixel
+PREFER.libstereopixel?=		robotpkg
 
-DEPEND_ABI.libstereopixel?=	libstereopixel>=1.3
+DEPEND_USE+=			libstereopixel
+
+DEPEND_ABI.libstereopixel?=	libstereopixel>=1.4
 DEPEND_DIR.libstereopixel?=	../../image/libstereopixel
 
 SYSTEM_SEARCH.libstereopixel=\
 	include/libstereopixel.h	\
-	lib/pkgconfig/libstereopixel.pc
-endif
+	'lib/pkgconfig/libstereopixel.pc:/Version/s/[^0-9.]//gp'
+
+endif # LIBSTEREOPIXEL_DEPEND_MK -------------------------------------------
 
 include ../../image/libimages3d/depend.mk
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
