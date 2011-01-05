Gepetto/robotpkg

commit 2d2ae1704643acaeea0e34986a1ad072c407e8e6

[mk/fetch] Support fetching over cvs, git and svn.
Added FETCH_METHOD package variable, which defaults to 'archive', meaning that
a regular archive is to be fetched from MASTER_SITES.

FETCH_METHOD can be set to 'cvs', 'git' or 'svn' in a package Makefile, meaning
that the distfile is to be fetched from a source repository. The MASTER_SITES
is then interpreted as the URL of this repository, using the following format:
   MASTER_SITES= url[@revision[+module]]
The bits between squared brackets are optional, and can be used to specify a
particular commit and module to be fetched in the repository located at 'url'.
The interpretation of revision and module as well as the url is left to the
underlying fetch tool (cvs, git or svn).

The archive extracted out of the repository is stored in DISTDIR as for other
archives, and its checksum is computed according to the contents of the
archive, ommitting irrelevant metadata information (because metadata will vary
depending on the fetcher's uid, date, etc.). The size of the archive is not
either recorded in distinfo (it will vary for the same reasons).

Of course, fetching over cvs,git,svn must not be mistaken with the 'checkout'
target. Fetch still retrieves an archive from a fixed commit number, while
checkout extracts a complete repository for further manipulation or testing.

This new feature should probably be used sparingly, as making an archive
available so that robotpkg or anyone else can download it is still the
preferred (robust) method.

mk/fetch/fetch-vars.mk
@@ -1,6 +1,5 @@
-# $LAAS: fetch-vars.mk 2009/03/01 15:49:33 tho $
 #
-# Copyright (c) 2006,2008-2009 LAAS/CNRS
+# Copyright (c) 2006,2008-2010 LAAS/CNRS
 # All rights reserved.
 #
 # This project includes software developed by the NetBSD Foundation, Inc.
@@ -62,12 +61,82 @@
 # The default DISTDIR is currently set in robotpkg.prefs.mk.
 #DISTDIR?=               ${ROBOTPKG_DIR}/distfiles
 
+# The fetch method.
+FETCH_METHOD?=   	archive
+# "manual" will explicitly fail if the DISTFILES don't exist locally.
+# "custom" requires setting FETCH_CMD, FETCH_BEFORE_ARGS, FETCH_AFTER_ARGS,
+# FETCH_RESUME_ARGS and FETCH_OUTPUT_ARGS. FETCH_CMD must understand fetching
+# files located via URLs.
+# Possible: tnftp, cvs, git, svn, manual, custom
+# Default: tnftp
+ifeq (,$(filter archive cvs git svn manual custom,${FETCH_METHOD}))
+  PKG_FAIL_REASON+= "FETCH_METHOD for ${PKGNAME} must be one of"
+  PKG_FAIL_REASON+= "	archive cvs git svn manual custom"
+endif
+
+# Default archive extension
 EXTRACT_SUFX?=		.tar.gz
 
 _DISTDIR=		${DISTDIR}/${DIST_SUBDIR}
 DISTFILES?=		${DISTNAME}${EXTRACT_SUFX}
 
+# Backup site for archives
+_MASTER_SITE_BACKUP=	${MASTER_SITE_BACKUP:=${DIST_SUBDIR:=/}}
+_MASTER_SITE_OVERRIDE=	${MASTER_SITE_OVERRIDE:=${DIST_SUBDIR:=/}}
+
+# The following variables are all lists of options to pass to he command
+# used to do the actual fetching of the file.
+ifeq (archive,$(strip ${FETCH_METHOD}))
+  _FETCH_CMD=		${TNFTP}
+  _FETCH_RESUME_ARGS=	-R
+  _FETCH_OUTPUT_ARGS=	-o
+
+  _FETCH_DEPEND=	pkgtools/tnftp/depend.mk
+  DEPEND_METHOD.tnftp+=	bootstrap
+endif
+
+ifeq (cvs,$(strip ${FETCH_METHOD}))
+  _FETCH_CMD=		${CVS}
+
+  _FETCH_DEPEND=	mk/sysdep/cvs.mk pkgtools/pax/depend.mk
+  DEPEND_METHOD.cvs+=	bootstrap
+  DEPEND_METHOD.pax+=	bootstrap
+endif
+
+ifeq (git,$(strip ${FETCH_METHOD}))
+  _FETCH_CMD=		${GIT}
+
+  _FETCH_DEPEND=	mk/sysdep/git.mk
+  DEPEND_METHOD.git+=	bootstrap
+endif
+
+ifeq (svn,$(strip ${FETCH_METHOD}))
+  _FETCH_CMD=		${SVN}
+
+  _FETCH_DEPEND=	mk/sysdep/svn.mk pkgtools/pax/depend.mk
+  DEPEND_METHOD.svn+=	bootstrap
+  DEPEND_METHOD.pax+=	bootstrap
+endif
+
+ifeq (manual,$(strip ${FETCH_METHOD}))
+  _FETCH_BEFORE_ARGS=	${FALSE}
+endif
+
+ifeq (custom,$(strip ${FETCH_METHOD}))
+  # FETCH_CMD is the program used to fetch files for FETCH_USING=custom.
+  # It must understand fetching files located via URLs.
+  #
+  _FETCH_BEFORE_ARGS=	${FETCH_CMD} ${FETCH_BEFORE_ARGS}
+  _FETCH_AFTER_ARGS=	${FETCH_AFTER_ARGS}
+  _FETCH_RESUME_ARGS=	${FETCH_RESUME_ARGS}
+  _FETCH_OUTPUT_ARGS=	${FETCH_OUTPUT_ARGS}
+endif
+
+# Fetch logfile
+FETCH_LOGFILE?=		${WRKDIR}/.fetch.log
+FETCH_LOGFILTER?=\
+	${_LOGFILTER} $(if $(filter cvs git svn,${FETCH_METHOD}),,-n)	\
+	${_LOGFILTER_FLAGS} -l ${FETCH_LOGFILE} --
+
 include ${ROBOTPKG_DIR}/mk/fetch/sites.mk
 include ${ROBOTPKG_DIR}/mk/fetch/fetch.mk
-#include "${ROBOTPKG_DIR}/mk/fetch/fetch-list.mk"
-#include "${ROBOTPKG_DIR}/mk/fetch/distclean.mk"

mk/fetch/fetch
@@ -1,5 +1,4 @@
 #!/bin/sh
-# $LAAS: fetch 2010/08/25 19:05:18 mallet $
 #
 # Copyright (c) 2006 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -44,7 +43,8 @@
 #	fetch -- fetch files via URLs
 #
 # SYNOPSIS
-#	fetch [-c] [-d dir] [-f distinfo] file site ..."
+#	fetch [-m archive|cvs|git|svn] [-e extract_dir] [-c] [-d dir]
+#		[-f distinfo] file site ..."
 #
 # DESCRIPTION
 #	fetch will attempt to fetch the file from the list of specified
@@ -57,6 +57,10 @@
 #	no fetch action is taken.
 #
 # OPTIONS
+#	-m	Fetch method. Must be archive, cvs, git or svn.
+#
+#	-e	Extract directory.
+#
 #	-c	Verify the checksum for the file.  If the checksum
 #		does not match, then the fetch is determined to be
 #		not successful.
@@ -108,28 +112,40 @@
 : ${ROBOTPKG_DIR:=/usr/robotpkg}
 : ${CHECKSUM:=${ROBOTPKG_DIR}/mk/checksum/checksum}
 : ${CP:=cp}
+: ${ID:=id}
+: ${UMASK:=umask}
 : ${ECHO:=echo}
-: ${FETCH_CMD:=ftp}
+: ${FETCH_CMD:=tnftp}
 : ${MKDIR:=mkdir}
 : ${MV:=mv}
 : ${TEST:=test}
 : ${TOUCH:=touch}
 : ${WC:=wc}
+: ${FIND:=find}
+: ${SORT:=sort}
+: ${GZIP:=gzip}
+: ${PAX:=pax}
 
 self="${0##*/}"
 
 usage() {
-	${ECHO} 1>&2 "usage: $self [-c] [-f distinfo] [-r] file site ..."
+    ${ECHO} 1>&2	\
+	"usage: $self [-m archive|cvs|git|svn] [-e extract_dir] [-c]"	\
+	"[-f distinfo] [-r] file site ..."
 }
 
 # Process optional arguments
+format=archive
+extract=work
 checksum=
 distinfo=
 fetchdir=.
 resume=
 verbose=
 while ${TEST} $# -gt 0; do
-	case "$1" in
+    case "$1" in
+	-m)	format="$2"; shift 2 ;;
+	-e)	extract="$2"; shift 2 ;;
 	-c)	checksum=yes; shift ;;
 	-d)	fetchdir="$2"; shift 2 ;;
 	-f)	distinfo="$2"; shift 2 ;;
@@ -141,7 +157,7 @@ while ${TEST} $# -gt 0; do
 		exit 1
 		;;
 	*)	break ;;
-	esac
+    esac
 done
 if ${TEST} -n "$checksum" -a -z "$distinfo"; then
 	${ECHO} 1>&2 "$self: \`\`-c'' requires \`\`-f distinfo''."
@@ -259,34 +275,79 @@ ${TEST} -z "$distsize" || ${ECHO} "=> Total size: $distsize $distunits"
 while ${TEST} $# -gt 0; do
 	site="$1"; shift
 
-	( cd $fetchdir
-	  if ${TEST} -n "$resume"; then
-		fetch_cmd="${FETCH_CMD} ${FETCH_BEFORE_ARGS} ${FETCH_RESUME_ARGS} ${FETCH_OUTPUT_ARGS} $outputfile $site$file"
-	  else
-		fetch_cmd="${FETCH_CMD} ${FETCH_BEFORE_ARGS} ${FETCH_OUTPUT_ARGS} $outputfile $site$file ${FETCH_AFTER_ARGS}"
-	  fi
-	  ${TEST} -z "$verbose" || ${ECHO} "$fetch_cmd"
-	  $fetch_cmd )
+	# Build repository bits from repository+module@rev format
+	repo=${site%@*}
+	rev=${site##*@}
+	${TEST} "$rev" = "$site" && rev=
+	module=${rev##*+}
+	rev=${rev%+*}
+
+	(
+	    cd $fetchdir
+	    fetch_cmd="${FETCH_CMD} ${FETCH_BEFORE_ARGS}"
+	    if ${TEST} -n "$resume"; then
+		fetch_cmd="${fetch_cmd} ${FETCH_RESUME_ARGS}"
+	    fi
+	    if ${TEST} -n "${FETCH_OUTPUT_ARGS}"; then
+		fetch_cmd="${fetch_cmd} ${FETCH_OUTPUT_ARGS} $outputfile"
+	    fi
+	    fetch_cmd="${fetch_cmd} ${FETCH_AFTER_ARGS}"
+
+	    # Use the correct extraction procedure to create the archive based
+	    # on the repository format.
+	    ${TEST} -d "$extract" && rm -rf "$extract"
+	    case "$format" in
+		archive)
+		    ${ECHO} $fetch_cmd $site$file
+		    $fetch_cmd $site$file
+		    ;;
+
+		cvs)
+		    ${ECHO} $fetch_cmd -d $repo export -r $rev $module
+		    $fetch_cmd -d $repo export -d $extract -r $rev $module &&
+		    ${FIND} $extract \! -type d | ${SORT} | ${PAX} -wzf $file
+		    ;;
+
+		git)
+		    ${ECHO} $fetch_cmd clone $repo $extract
+		    $fetch_cmd clone -n $repo $extract &&
+		    $fetch_cmd --git-dir=$extract/.git			\
+			archive --prefix=$extract/ $rev $module |	\
+			${GZIP} -c >$file
+		    ;;
+
+		svn)
+		    ${TEST} -n "$rev" && rev="-r $rev"
+		    ${ECHO} $fetch_cmd export $rev $repo $extract
+		    $fetch_cmd --non-interactive --trust-server-cert	\
+			export $rev $repo $extract &&
+		    ${FIND} $extract \! -type d | ${SORT} | ${PAX} -wzf $file
+		    ;;
+	    esac
+	    status=$?
+	    ${TEST} -d "$extract" && rm -rf "$extract"
+	    exit $status
+	)
 	if ${TEST} $? -ne 0; then
-		${ECHO} 1>&2 "$self: Unable to fetch expected file $file"
-		continue
+	    ${ECHO} 1>&2 "$self: Unable to fetch expected file $file"
+	    continue
 	fi
 	if verify_file $outputpath; then
-		${TEST} -z "$resume" || ${MV} -f $outputpath $path
-		break
+	    ${TEST} -z "$resume" || ${MV} -f $outputpath $path
+	    break
 	else
-		${ECHO} 1>&2 "$self: Unable to verify fetched file $file"
+	    ${ECHO} 1>&2 "$self: Unable to verify fetched file $file"
 	fi
 	if ${TEST} -n "$resume"; then
-		if ${TEST} -f $path; then
-			${CP} -f $path $outputpath
-		else
-			${TOUCH} $outputpath
-		fi
+	    if ${TEST} -f $path; then
+		${CP} -f $path $outputpath
+	    else
+		${TOUCH} $outputpath
+	    fi
 	fi
 done
 if ${TEST} -f $path; then
-	exit 0
+    exit 0
 else
-	exit 1
+    exit 1
 fi

mk/fetch/fetch.mk
@@ -41,10 +41,6 @@
 #
 #					Anthony Mallet on Tue Dec  5 2006
 #
-
-_MASTER_SITE_BACKUP=	${MASTER_SITE_BACKUP:=${DIST_SUBDIR:=/}}
-_MASTER_SITE_OVERRIDE=	${MASTER_SITE_OVERRIDE:=${DIST_SUBDIR:=/}}
-
 ALLFILES?=	$(sort ${DISTFILES} ${PATCHFILES})
 CKSUMFILES?=	$(filter-out ${IGNOREFILES},${ALLFILES})
 
@@ -62,7 +58,6 @@ _PATCHFILES?=	${PATCHFILES}
 endif
 _ALLFILES=	$(filter-out ${NOFETCHFILES}, 				\
 			$(sort ${_DISTFILES} ${_PATCHFILES}))
-
 
 BUILD_DEFS+=	DISTFILES PATCHFILES
 
@@ -101,12 +96,12 @@ endef
 $(foreach fetchfile,${_PATCHFILES},$(eval ${_PATCHFILES_VAR}))
 
 
-# XXX require pkgtools/tnftp, although the fetch program is configurable
+# depend on the proper fetch tool if required
 ifeq (,$(filter fetch,${INTERACTIVE_STAGE}))
   ifneq ($(words $(wildcard $(addprefix ${DISTDIR}/,${_ALLFILES}))),\
 	 $(words ${_ALLFILES}))
-    DEPEND_METHOD.tnftp+=	bootstrap
-    include ${ROBOTPKG_DIR}/pkgtools/tnftp/depend.mk
+    $(call require,${ROBOTPKG_DIR}/mk/depends/depends-vars.mk)
+    include $(addprefix ${ROBOTPKG_DIR}/, ${_FETCH_DEPEND})
   endif
 endif
 
@@ -157,9 +152,15 @@ ifneq (,$(filter fetch,${INTERACTIVE_STAGE}))
 $(addprefix ${DISTDIR}/,${_ALLFILES}):
   ifndef FETCH_MESSAGE
 	@${TEST} ! -f $@ || exit 0;					\
-	${ERROR_MSG} "The fetch stage of this package requires user interaction to download"; \
-	${ERROR_MSG} "the distfiles.  Please fetch the distfiles manually and place them in:"; \
+	${ERROR_MSG} ${hline}; 						\
+	${ERROR_MSG} ""; 						\
+	${ERROR_MSG} "The fetch stage of this package requires user"	\
+		"interaction to download"; 				\
+	${ERROR_MSG} "the distfiles.  Please fetch the distfiles"	\
+		"manually and place them in:"; 				\
 	${ERROR_MSG} "    ${_DISTDIR}";					\
+	${ERROR_MSG} ""; 						\
+	${ERROR_MSG} ${hline}; 						\
 	if ${TEST} -n ${MASTER_SITES}""; then				\
 		${ERROR_MSG} "The distfiles are available from:";	\
 		for site in ${MASTER_SITES}; do				\
@@ -189,75 +190,39 @@ endif
 # transfer the files from the appropriate sites if needed.
 #
 #
-# FETCH_CMD is the program used to fetch files.  It must understand
-#	fetching files located via URLs, e.g. NetBSD's ftp, net/tnftp,
-#	etc.  The default value is set in pkgsrc/mk/defaults/mk.conf.
-#
-# The following variables are all lists of options to pass to he command
-# used to do the actual fetching of the file.
-#
-# FETCH_BEFORE_ARGS appear before all other options on the command line.
-#
-# FETCH_AFTER_ARGS appear after all other options on the command line.
-#
-# FETCH_RESUME_ARGS appears just after FETCH_BEFORE_ARGS and is the set
-#	of options for causing the command to resume a previous transfer.
-#
-# FETCH_OUTPUT_ARGS is a set of options for specifying the name of the
-#	local file that will hold the contents of the fetched file.
-#
-# FAILOVER_FETCH, if defined, will cause a checksum to be performed during
-#	a fetch to verify the transferred file is correct; if the checksum
-#	is incorrect, then the next site will be tried.
-#
-# PKG_RESUME_TRANSFERS is a yes/no variable that causes the fetch script
-#	to try to resume interrupted file transfers to avoid downloading
-#	the whole file.  The default is set in pkgsrc/mk/defaults/mk.conf.
-#
-#FETCH_CMD?=		ftp	# default is set by pkgsrc/mk/defaults/mk.conf
-FETCH_BEFORE_ARGS?=	${_FETCH_BEFORE_ARGS.$(notdir ${FETCH_CMD})}
-FETCH_AFTER_ARGS?=	${_FETCH_AFTER_ARGS.$(notdir ${FETCH_CMD})}
-FETCH_RESUME_ARGS?=	${_FETCH_RESUME_ARGS.$(notdir ${FETCH_CMD})}
-FETCH_OUTPUT_ARGS?=	${_FETCH_OUTPUT_ARGS.$(notdir ${FETCH_CMD})}
 
-_FETCH_BEFORE_ARGS.tnftp=	# empty
-_FETCH_AFTER_ARGS.tnftp=	# empty
-_FETCH_RESUME_ARGS.tnftp=	-R
-_FETCH_OUTPUT_ARGS.tnftp=	-o
+_FETCH_SCRIPT=${FETCH_LOGFILTER} ${SETENV}				\
+	CP=${CP} ECHO=${ECHO} MV=$(call quote,${MV})			\
+	TEST=$(call quote,${TEST}) MKDIR=$(call quote,${MKDIR})		\
+	TOUCH=$(call quote,${TOUCH}) WC=$(call quote,${WC})		\
+	FIND=$(call quote,${FIND}) SORT=$(call quote,${SORT})		\
+	CHECKSUM=$(call quote,${_CHECKSUM_CMD})				\
+	FETCH_CMD=$(call quote,${_FETCH_CMD}) 				\
+	FETCH_BEFORE_ARGS=$(call quote,${_FETCH_BEFORE_ARGS})		\
+	FETCH_AFTER_ARGS=$(call quote,${_FETCH_AFTER_ARGS})		\
+	FETCH_RESUME_ARGS=$(call quote,${_FETCH_RESUME_ARGS})		\
+	FETCH_OUTPUT_ARGS=$(call quote,${_FETCH_OUTPUT_ARGS})		\
+	${SH} ${ROBOTPKG_DIR}/mk/fetch/fetch
 
-_FETCH_CMD=\
-	${SETENV} CHECKSUM=$(call quote,${_CHECKSUM_CMD})		\
-		CP=${TOOLS_CP}						\
-		ECHO=${TOOLS_ECHO}					\
-		FETCH_CMD=${FETCH_CMD}					\
-		FETCH_BEFORE_ARGS=$(call quote,${FETCH_BEFORE_ARGS})	\
-		FETCH_AFTER_ARGS=$(call quote,${FETCH_AFTER_ARGS})	\
-		FETCH_RESUME_ARGS=$(call quote,${FETCH_RESUME_ARGS})	\
-		FETCH_OUTPUT_ARGS=$(call quote,${FETCH_OUTPUT_ARGS})	\
-		MKDIR=$(call quote,${TOOLS_MKDIR})			\
-		MV=$(call quote,${TOOLS_MV})				\
-		TEST=$(call quote,${TOOLS_TEST})			\
-		TOUCH=$(call quote,${TOOLS_TOUCH})			\
-		WC=$(call quote,${TOOLS_WC})				\
-		${SH} ${ROBOTPKG_DIR}/mk/fetch/fetch
-
+_FETCH_ARGS+=	-e $(patsubst %${EXTRACT_SUFX},%,$(notdir $@))
 ifdef PKG_VERBOSE
-_FETCH_ARGS+=	${PKG_VERBOSE}
+  _FETCH_ARGS+=	${PKG_VERBOSE}
 endif
 ifeq (yes,$(call exists,${DISTINFO_FILE}))
-_FETCH_ARGS+=	-f $(call quote,${DISTINFO_FILE})
+  ifeq (,$(filter cvs git svn,${FETCH_METHOD}))
+    _FETCH_ARGS+=-f $(call quote,${DISTINFO_FILE})
+  endif
 endif
 ifeq (,${DIST_SUBDIR})
-_FETCH_ARGS+=	-d .
+  _FETCH_ARGS+=	-d .
 else
-_FETCH_ARGS+=	-d ${DIST_SUBDIR}
+  _FETCH_ARGS+=	-d ${DIST_SUBDIR}
 endif
 
 ifeq (,$(filter fetch,${INTERACTIVE_STAGE}))
 $(addprefix ${DISTDIR}/,${_ALLFILES}):
 	@${STEP_MSG} "Fetching $(notdir $@)"
-	${_PKG_SILENT}${_PKG_DEBUG}${MKDIR} $(dir $@)
-	${_PKG_SILENT}${_PKG_DEBUG}					\
+	${RUN}${MKDIR} $(dir $@);					\
 	for d in "" $(subst :, ,${DIST_PATH}); do			\
 		case $$d in						\
 		""|${DISTDIR})	continue ;;				\
@@ -268,18 +233,20 @@ $(addprefix ${DISTDIR}/,${_ALLFILES}):
 			${RM} -f $@;					\
 			${LN} -s $$file $@;				\
 		fi;							\
-	done
-	${_PKG_SILENT}${_PKG_DEBUG}set -e;				\
+	done;								\
 	unsorted_sites="${SITES.$(subst =,--,$(notdir $@))}";		\
-	sites="${_ORDERED_SITES} ${_MASTER_SITE_BACKUP}";		\
 	cd $(patsubst %/${DIST_SUBDIR},%,$(dir $@)) &&			\
-	${_FETCH_CMD} ${_FETCH_ARGS} $(notdir $@) $$sites
-	${_PKG_SILENT}${_PKG_DEBUG}					\
+	${_FETCH_SCRIPT} -m ${FETCH_METHOD} ${_FETCH_ARGS}		\
+		$(notdir $@) ${_ORDERED_SITES};				\
+	if ${TEST} ! -f $@; then					\
+		${_FETCH_SCRIPT} -m archive ${_FETCH_ARGS}		\
+			$(notdir $@) ${_MASTER_SITE_BACKUP};		\
+	fi;								\
 	if ${TEST} ! -f $@; then					\
 		${ERROR_MSG} "Could not fetch the following file:";	\
 		${ERROR_MSG} "    $(notdir $@)";			\
 		${ERROR_MSG} "";					\
-		${ERROR_MSG} "Please retrieve this file manually into:"; \
+		${ERROR_MSG} "Please retrieve this file manually into:";\
 		${ERROR_MSG} "    $(dir $@)";				\
 		exit 1;							\
 	fi

mk/robotpkg.default.conf
@@ -236,11 +236,6 @@ DISTDIR?=	${ROBOTPKG_DIR}/distfiles
 # Possible:  any directories
 # Default: none
 
-FETCH_CMD?=             ${PKG_TOOLS_BIN}/tnftp
-# Default program to fetch the files with pkgsrc.
-# Default: ${PKG_TOOLS_BIN}/tnftp
-# Possible: any.
-
 OVERRIDE_DIRDEPTH?=     2
 # Represents the common directory depth under ${WRKSRC} up to which we
 # find the files that need to be overridden.  By default, we search two

commit e8b2f29c4ddf6f18d8a2e7e85f0da65d5df5b8c5

[mk/fetch] Fix verbosity of the regular ftp fetch target
Fix bug introduced in 2d2ae170 and preventing the ftp fetch target to display
a download progress bar.

mk/fetch/fetch-vars.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2006,2008-2010 LAAS/CNRS
+# Copyright (c) 2006,2008-2011 LAAS/CNRS
 # All rights reserved.
 #
 # This project includes software developed by the NetBSD Foundation, Inc.
@@ -134,9 +134,9 @@ endif
 
 # Fetch logfile
 FETCH_LOGFILE?=		${WRKDIR}/.fetch.log
-FETCH_LOGFILTER?=\
-	${_LOGFILTER} $(if $(filter cvs git svn,${FETCH_METHOD}),,-n)	\
-	${_LOGFILTER_FLAGS} -l ${FETCH_LOGFILE} --
+FETCH_LOGFILTER?=	${_LOGFILTER} ${_LOGFILTER_FLAGS} \
+	$(if $(filter cvs git svn,${FETCH_METHOD}), -l ${FETCH_LOGFILE},-n) \
+	--
 
 include ${ROBOTPKG_DIR}/mk/fetch/sites.mk
 include ${ROBOTPKG_DIR}/mk/fetch/fetch.mk