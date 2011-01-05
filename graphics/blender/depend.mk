677e6fd603b894d40c4e94ef093eb11a5740959c
Severin Lemaignan <severin.lemaignan@laas.fr>
2011-01-05 18:43:21 +0100

    [graphics/blender] Import blender-2.56
    Blender is a suite of tools enabling the creation of and replay of linear and
    real-time, interactive 3D content. It offers full functionality for modeling,
    rendering, animation, postproduction and game creation and playback with the
    singular benefits of cross-platform operability and a download file size of
    less than 2.5MB.
    
    Aimed at media professionals and individual creative users, Blender can be
    used to create commercials and other broadcast quality linear content, while
    the incorporation of a real-time 3D engine allows for the creation of 3D
    interactive content for stand-alone playback or integration in a web browser.
    
    Originally developed by the company 'Not a Number' (NaN), Blender now is
    continued as 'Free Software', with the sources available under GNU GPL.

---
+++ graphics/blender/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	graphics/blender
+# Created:			Séverin Lemaignan on Mon, 7 Dec 2009
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+BLENDER_DEPEND_MK:=	${BLENDER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		blender
+endif
+
+ifeq (+,$(BLENDER_DEPEND_MK)) # -----------------------------------------------
+
+PREFER.blender?=	robotpkg
+
+DEPEND_USE+=		blender
+DEPEND_ABI.blender?=	blender>=2.5
+DEPEND_DIR.blender?=	../../graphics/blender
+
+SYSTEM_SEARCH.blender=\
+	'bin/blender:1{s/sub.*)//;s/[^0-9.]//gp;}:% -v'
+
+endif # BLENDER_DEPEND_MK -----------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
