14094eac1a5b3ac4bbdb2b34e99c27b4e102ae06
Anthony Mallet <anthony.mallet@laas.fr>
2011-01-17 11:51:15 +0100

    [mk/sysdep] Teach robotpkg about py-yaml
    The sysdeps for python packages should probably be rewritten to handle in a
    generic way any python package (specified in a variable or so), but since there
    are only two python packages in mk/sysdep for now, let's postpone that...

---
+++ mk/sysdep/py-yaml.mk
@@ -0,0 +1,38 @@
+# robotpkg sysdep/py-yaml.mk
+# Created:			Anthony Mallet on Thu,  9 Sep 2010
+#
+
+# YAML is a data serialization format designed for human readability and
+# interaction with scripting languages.  PyYAML is a YAML parser and
+# emitter for Python.
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PYYAML_DEPEND_MK:=	${PYYAML_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		py-yaml
+endif
+
+ifeq (+,$(PYYAML_DEPEND_MK)) # ---------------------------------------------
+
+PREFER.py-yaml?=	system
+
+USE_LANGUAGES+=		python
+
+DEPEND_USE+=		py-yaml
+DEPEND_ABI.py-yaml?=	py-yaml
+
+DEPEND_PYTHONPATH.py-yaml=\
+	$(dir $(word 2,${SYSTEM_FILES.py-yaml}))..
+
+_pynamespec=python{2.6,2.5,2.4,[0-9].[0-9],}
+SYSTEM_SEARCH.py-yaml=\
+	'bin/${_pynamespec}:s/[^.0-9]//gp:% --version'			\
+	'lib/${_pynamespec}/site-packages/yaml/__init__.py'
+
+SYSTEM_PKG.Linux-fedora.py-yaml=	PyYAML
+SYSTEM_PKG.NetBSD.py-yaml=		pkgsrc/textproc/py-yaml
+
+endif # PYYAML_DEPEND_MK ---------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
