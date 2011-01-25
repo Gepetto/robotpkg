8ee03c3ed2359a91e1e1f40f60dd0b342011a431
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-01-25 16:34:36 +0100

    [interfaces/openrobots-pose] Created package
    openrobots-pose offers a set of data structures for 3D pose and joint states of articulated bodies.

---
+++ interfaces/openrobots-pose/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	architecture/openrobots-pose
+# Created:			Mokhtar Gharbi on Tue, 18 Jan 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+OPENROBOTSPOSE_DEPEND_MK:=	${OPENROBOTSPOSE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		openrobots-pose
+endif
+
+ifeq (+,$(OPENROBOTSPOSE_DEPEND_MK))
+PREFER.openrobots-pose?=	robotpkg
+
+DEPEND_USE+=		openrobots-pose
+
+DEPEND_ABI.openrobots-pose?=	openrobots-pose>=1.1
+DEPEND_DIR.openrobots-pose?=	../../interfaces/openrobots-pose
+
+SYSTEM_SEARCH.openrobots-pose=\
+	include/openrobots/pose.h		\
+	lib/pkgconfig/openrobots-pose.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
