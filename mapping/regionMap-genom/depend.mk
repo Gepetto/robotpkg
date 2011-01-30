f2ed157d9f322e65464824b118f359e3a7df2ded
Arnaud Degroote <arnaud.degroote@laas.fr>
2011-01-30 22:20:44 +0100

    [mapping/regionMap-genom] Add support for regionMap GeNoM part
    It is the module associated with the regionMap library
    (mapping/regionMap-lib) which permits to construct some traversability
    map on the base of 3D points (from velodyne or stereovision)

---
+++ mapping/regionMap-genom/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	mapping/regionMap-genom
+# Created:			Arnaud Degroote on Fri, 28 Jan 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+REGIONMAPGENOM_DEPEND_MK:=	${REGIONMAPGENOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		regionMap-genom
+endif
+
+ifeq (+,$(REGIONMAPGENOM_DEPEND_MK))
+PREFER.regionMap-genom?=	robotpkg
+
+DEPEND_USE+=		regionMap-genom
+
+DEPEND_ABI.regionMap-genom?=	regionMap-genom>=0.1
+DEPEND_DIR.regionMap-genom?=	../../mapping/regionMap-genom
+
+SYSTEM_SEARCH.regionMap-genom=\
+	include/regionMap/regionMapStruct.h		\
+	lib/pkgconfig/regionMap.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
