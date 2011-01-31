f8691444643774f6ebc8a1b8cc9d7754bd95062a
Matthieu Herrb <matthieu.herrb@laas.fr>
2011-01-31 15:54:07 +0100

    [image/niut-genom] add the Genom module providing user tracking with openni

---
+++ image/niut-genom/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	image/niut-genom
+# Created:			Matthieu Herrb on Wed, 26 Jan 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+NIUTGENOM_DEPEND_MK:=	${NIUTGENOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		niut-genom
+endif
+
+ifeq (+,$(NIUTGENOM_DEPEND_MK))
+PREFER.niut-genom?=	robotpkg
+
+DEPEND_USE+=		niut-genom
+
+DEPEND_ABI.niut-genom?=	niut-genom>=0.1
+DEPEND_DIR.niut-genom?=	../../image/niut-genom
+
+SYSTEM_SEARCH.niut-genom=\
+	include/niut/niutStruct.h		\
+	lib/pkgconfig/niut.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
