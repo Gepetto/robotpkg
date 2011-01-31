495505c9f52b8e87b6ad006ec7b959dd94e9c97d
Matthieu Herrb <matthieu.herrb@laas.fr>
2011-01-31 15:46:15 +0100

    [hardware/openni-sensor] add the OpenNI driver for the Kinect sensor

---
+++ hardware/openni-sensor/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	hardware/openni-sensor
+# Created:			Matthieu Herrb on Tue, 14 Dec 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+OPENNI_SENSOR_DEPEND_MK:=${OPENNI_SENSOR_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		openni-sensor
+endif
+
+ifeq (+,$(OPENNI_SENSOR_DEPEND_MK))
+PREFER.openni-sensor?=	robotpkg
+
+DEPEND_USE+=		openni-sensor
+
+DEPEND_ABI.openni-sensor?=	openni-sensor>=20101214
+DEPEND_DIR.openni-sensor?=	../../hardware/openni-sensor
+
+SYSTEM_SEARCH.openni-sensor=\
+	lib/libXnDeviceFile.so \
+	lib/libXnDeviceSensorV2.so
+
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
