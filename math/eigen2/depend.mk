76108d179782015e132eec3974be8e179c7f485f
Anthony Mallet <anthony.mallet@laas.fr>
2011-02-04 10:51:08 +0100

    [math/eigen2] Add version check when PREFER.eigen2 == system

--- math/eigen2/depend.mk
+++ math/eigen2/depend.mk
@@ -17,7 +17,15 @@ DEPEND_ABI.eigen2?=	eigen2>=2.0.12
 DEPEND_DIR.eigen2?=	../../math/eigen2
 
 SYSTEM_SEARCH.eigen2=	\
-	include/eigen2/Eigen/Array
+	include/eigen2/Eigen/Array \
+	'include/eigen2/Eigen/src/Core/util/Macros.h:${_eigen2_version_sed}'
+
+# extracting version from the .h file is challenging...
+_eigen2_version_sed=\
+ /^\#define EIGEN_\(WORLD\|MAJOR\|MINOR\)_VERSION[ \t]*/{s///;H;};
+_eigen2_version_sed+=\
+ $${x;s/\n/./g;s/^.//;p;}
+
 endif
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
