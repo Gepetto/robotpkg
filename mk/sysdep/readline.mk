5b90617376c7428a31c261608c2c6d42847e8c77
Matthieu Herrb <matthieu.herrb@laas.fr>
2011-02-09 15:03:54 +0100

    [mk/sysdeps] add Ubuntu package name for readline

--- mk/sysdep/readline.mk
+++ mk/sysdep/readline.mk
@@ -32,6 +32,7 @@ DEPEND_USE+=		readline
 DEPEND_ABI.readline?=	readline>=5
 
 SYSTEM_PKG.Linux-fedora.readline=	"readline-devel"
+SYSTEM_PKG.Linux-ubuntu.readline=	"libreadline-dev"
 SYSTEM_PKG.NetBSD.readline=		"pkgsrc/devel/readline"
 
 SYSTEM_SEARCH.readline=\
