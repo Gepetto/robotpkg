329312fbf55614f154afdd7bc752eef86f480792
Anthony Mallet <anthony.mallet@laas.fr>
2011-02-09 19:09:59 +0100

    [speech/dialogs] Regen Makefile and depend.mk headings

--- speech/dialogs/depend.mk
+++ speech/dialogs/depend.mk
@@ -1,18 +1,6 @@
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
+# robotpkg depend.mk for:	speech/dialogs
+# Created:			Séverin Lemaignan on Wed, 9 Feb 2011
 #
-# Redistribution  and  use in source   and binary forms,  with or without
-# modification, are permitted provided that  the following conditions are
-# met:
-#
-#   1. Redistributions  of  source code must  retain  the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#   2. Redistributions in binary form must  reproduce the above copyright
-#      notice,  this list of  conditions and  the following disclaimer in
-#      the  documentation   and/or  other  materials   provided with  the
-#      distribution.
-#
-#                                        Severin Lemaignan on 25 Jan 2011
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 DIALOGS_DEPEND_MK:=	${DIALOGS_DEPEND_MK}+
