7c9f55be51c6cb60f1b4025bf49c9419b83e0e38
Redouane Boumghar <redouane.boumghar@laas.fr>
2011-03-17 18:19:59 +0100

    [devel/jafar-kernel] Import jafar-kernel-0.4
    This jafar package defines common tools: exceptions, debug message facility,
    data logging facility,...

---
+++ devel/jafar-kernel/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	devel/jafar-kernel
+# Created:			Redouane Boumghar on Thu, 17 Mar 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+JAFAR_KERNEL_DEPEND_MK:=${JAFAR_KERNEL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		jafar-kernel
+endif
+
+ifeq (+,$(JAFAR_KERNEL_DEPEND_MK)) # ---------------------------------------
+
+include ../../meta-pkgs/jafar/depend.common
+PREFER.jafar-kernel?=	${PREFER.jafar}
+
+DEPEND_USE+=		jafar-kernel
+DEPEND_ABI.jafar-kernel?=jafar-kernel>=0.4
+DEPEND_DIR.jafar-kernel?=../../devel/jafar-kernel
+
+SYSTEM_SEARCH.jafar-kernel=\
+	include/jafar/kernel/kernelException.hpp	\
+	lib/libjafar-kernel.so				\
+	'lib/pkgconfig/jafar-kernel.pc:/Version/s/[^0-9.]//gp'
+
+endif # JAFAR_KERNEL_DEPEND_MK ---------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
