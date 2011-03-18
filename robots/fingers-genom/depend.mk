6a0532561cb1b15c4b0b5de2826317dd8a3f06e7
Xavier Broquere <xavier.broquere@laas.fr>
2011-03-18 14:48:01 +0100

    [robots/fingers-genom] add depend.mk

---
+++ robots/fingers-genom/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	robots/fingers-genom
+# Created:			Xavier Broquere on Fri, 18 March 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+FINGERSGENOM_DEPEND_MK:=	${FINGERSGENOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		fingers-genom
+endif
+
+ifeq (+,$(FINGERSGENOM_DEPEND_MK))
+PREFER.fingers-genom?=	robotpkg
+
+DEPEND_USE+=		fingers-genom
+
+DEPEND_ABI.fingers-genom?=	fingers-genom>=1.0
+DEPEND_DIR.fingers-genom?=	../../robots/fingers-genom
+
+SYSTEM_SEARCH.fingers-genom=\
+	include/fingers/fingersStruct.h		\
+	lib/pkgconfig/fingers.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
