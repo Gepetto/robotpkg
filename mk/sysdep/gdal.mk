6f7377e9935fb74333b75445cbf40a61d2ae3fff
Arnaud Degroote <arnaud.degroote@laas.fr>
2011-03-18 14:36:57 +0100

    [mk/sysdep] Add a depend to retrieve presence of gdal
    GDAL is a translator library for raster geospatial data formats that
    is released under an X/MIT style Open Source license. As a library, it
    presents a single abstract data model to the calling application for
    all supported formats.

---
+++ mk/sysdep/gdal.mk
@@ -0,0 +1,31 @@
+# robotpkg sysdep/gdal.mk
+# Created:			Arnaud Degroote on Fri, 18 Mar 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GDAL_DEPEND_MK:=		${GDAL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		gdal
+endif
+
+ifeq (+,$(GDAL_DEPEND_MK)) # ------------------------------------------------
+
+PREFER.gdal?=		system
+
+DEPEND_USE+=		gdal
+DEPEND_ABI.gdal?=		gdal>=1.6
+
+SYSTEM_SEARCH.gdal=	\
+	'bin/gdal-config:p:% --version' \
+	'include/gdal/gdal_version.h' \
+	'lib/libgdal[0-9\.]*.so'
+
+SYSTEM_PKG.Linux-fedora.gdal=	gdal-devel
+SYSTEM_PKG.Linux-ubuntu.gdal=	libgdal1-dev
+SYSTEM_PKG.NetBSD.gdal=		pkgsrc/geography/gdal-lib
+
+endif # GDAL_DEPEND_MK -----------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
+
