3ce7b78171cee9973104a3703c5903df6e5d47da
Anthony Mallet <anthony.mallet@laas.fr>
2011-03-31 16:41:13 +0200

    [mk/internal] Fix `make headings` for files not under git yet.
    When `make headings` is invoked on files not add to git yet, `git config
    user.name` has to access the user $HOME directory. Pass it explicitly from
    the saved HOME.env variable.

--- mk/internal/headings.mk
+++ mk/internal/headings.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2010 LAAS/CNRS
+# Copyright (c) 2010-2011 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use  in  source  and binary  forms,  with  or  without
@@ -53,7 +53,7 @@ do-headings-pkg-%: %
 	   | ${AWK} 'NR==1 { for(i=1;i<NF-2;i++) printf $$i " ";	\
 		print $$i }'`; 						\
 	${TEST} -z "$$h" && {						\
-	  h=`${GIT} config user.name`" on ";				\
+	  h=`${SETENV} HOME=${HOME.env} ${GIT} config user.name`" on ";	\
 	  h="$$h"`${_CDATE_CMD} '+%a, %e %b %Y'`;			\
 	};								\
 	printf	'# robotpkg $^ for:\t%s\n# Created:\t\t\t%s\n#\n\n'	\
