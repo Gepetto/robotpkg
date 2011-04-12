bf356121c6270bcd949871ba5b1c5607bf6f0f0d
Anthony Mallet <anthony.mallet@laas.fr>
2011-04-12 11:14:03 +0200

    [simulation/morse-yarp] Import morse-yarp-0.3
    YARP bindings for the morse simulator, see simulation/morse/DESCR.

---
+++ simulation/morse-yarp/depend.mk
@@ -0,0 +1,29 @@
+# robotpkg depend.mk for:	simulation/morse-yarp
+# Created:			Anthony Mallet on Tue, 12 Apr 2011
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+MORSE_YARP_DEPEND_MK:=		${MORSE_YARP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			morse-yarp
+endif
+
+ifeq (+,$(MORSE_YARP_DEPEND_MK)) # -----------------------------------------
+
+PREFER.morse-yarp?=		robotpkg
+
+DEPEND_USE+=			morse-yarp
+
+DEPEND_ABI.morse-yarp?=		morse-yarp>=0.2
+DEPEND_DIR.morse-yarp?=		../../simulation/morse-yarp
+
+SYSTEM_SEARCH.morse-yarp=\
+	${PYTHON_SITELIB}/morse/middleware/yarp_mw.py
+
+DEPEND_METHOD.python3+=		bootstrap
+include ../../lang/python3/depend.mk
+
+endif # MORSE_YARP_DEPEND_MK -----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
