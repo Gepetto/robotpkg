72c65c969ecfeb04b4fe7a4151f91aab8e47dfc6
Anthony Mallet <anthony.mallet@laas.fr>
2011-04-26 18:55:32 +0200

    [architecture/genom] Fix depend.mk so that it can be included in any order
    GenoM standard package options and module generation must be defined for direct
    dependencies only and even if genom was already required as an indirect
    dependency. So this code must be placed in the case DEPEND_DEPTH == + and not
    in the part with the include guards part.

--- architecture/genom/depend.mk
+++ architecture/genom/depend.mk
@@ -8,6 +8,59 @@ GENOM_DEPEND_MK:=	${GENOM_DEPEND_MK}+
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		genom
 
+# Add genom-related options for direct dependencies
+ifdef GENOM_MODULE
+  GENOM_ARGS?=#		empty
+
+  PKG_SUPPORTED_OPTIONS+=	api tcl openprs tclserv_client xenomai
+  PKG_SUGGESTED_OPTIONS+=	tcl
+
+  PKG_OPTION_DESCR.api=	Generate module API only
+  define PKG_OPTION_SET.api
+    GENOM_ARGS+=	-a
+  endef
+
+  PKG_OPTION_DESCR.tcl=	Generate TCL client code
+  define PKG_OPTION_SET.tcl
+    GENOM_ARGS+=	-t
+    CONFIGURE_ARGS+=	--with-tcl=$(dir ${TCL_CONFIG_SH})
+    include ../../net/tclserv/depend.mk
+    include ../../mk/sysdep/tcl.mk
+  endef
+
+  PKG_OPTION_DESCR.openprs=Generate OpenPRS client code
+  define PKG_OPTION_SET.openprs
+    GENOM_ARGS+=	-o
+    include ../../supervision/transgen/depend.mk
+  endef
+
+  PKG_OPTION_DESCR.tclserv_client=Generate C tclServ client code
+  define PKG_OPTION_SET.tclserv_client
+    DEPEND_ABI.genom=  genom>=2.6
+    GENOM_ARGS+= -x
+    include ../../net/libtclserv_client/depend.mk
+  endef
+
+  PKG_OPTION_DESCR.xenomai=Enable Xenomai support
+
+  define PKG_OPTION_SET.xenomai
+    CONFIGURE_ARGS+=	--with-xenomai
+    REQD_BUILD_OPTIONS.genom+= xenomai
+    include ../../mk/sysdep/xenomai.mk
+  endef
+
+  GNU_CONFIGURE=	yes
+
+  pre-configure: genom-generate
+
+  .PHONY: genom-generate
+  genom-generate:
+	@${STEP_MSG} "Generating ${GENOM_MODULE} module"
+	${RUN}cd ${WRKSRC} && ${CONFIGURE_LOGFILTER} 			\
+		${SETENV} ${CONFIGURE_ENV} 				\
+		${TOOLS.genom} ${GENOM_ARGS} ${GENOM_MODULE}
+endif # GENOM_MODULE
+
 # GenoM modules use mkdep, pocolibs, pkg-config, autoconf and libtool. Depend
 # on these at the global level so that the dependencies get registered as a
 # primary dependency for the packages including this file.
@@ -17,7 +70,7 @@ include ../../middleware/pocolibs/depend.mk
 include ../../pkgtools/libtool/depend.mk
 include ../../pkgtools/pkg-config/depend.mk
 include ../../mk/sysdep/autoconf.mk
-endif
+endif # DEPEND_DEPTH == +
 
 ifeq (+,$(GENOM_DEPEND_MK)) # ----------------------------------------
 
@@ -35,62 +88,7 @@ DEPEND_ABI.genom?=	genom>=2.5r2
 DEPEND_DIR.genom?=	../../architecture/genom
 
 TOOLS.genom?=		${PREFIX.genom}/bin/genom
-GENOM_ARGS?=#		empty
 
-# Add genom-related options for direct dependencies
-ifeq (+,$(DEPEND_DEPTH))
-
-PKG_SUPPORTED_OPTIONS+=	api tcl openprs tclserv_client xenomai
-PKG_SUGGESTED_OPTIONS+=	tcl
-
-PKG_OPTION_DESCR.api=	Generate module API only
-define PKG_OPTION_SET.api
-  GENOM_ARGS+=	-a
-endef
-
-PKG_OPTION_DESCR.tcl=	Enable the generation of the TCL client code
-define PKG_OPTION_SET.tcl
-  GENOM_ARGS+=	-t
-  CONFIGURE_ARGS+=	--with-tcl=$(dir ${TCL_CONFIG_SH})
-  include ../../net/tclserv/depend.mk
-  include ../../mk/sysdep/tcl.mk
-endef
-
-PKG_OPTION_DESCR.openprs=Enable the generation of the OpenPRS client code
-define PKG_OPTION_SET.openprs
-  GENOM_ARGS+=	-o
-  include ../../supervision/transgen/depend.mk
-endef
-
-PKG_OPTION_DESCR.tclserv_client=Enable the generation of the C tclServ client code
-define PKG_OPTION_SET.tclserv_client
-  DEPEND_ABI.genom=  genom>=2.6
-  GENOM_ARGS+= -x
-  include ../../net/libtclserv_client/depend.mk
-endef
-
-PKG_OPTION_DESCR.xenomai=Enable Xenomai support
-
-define PKG_OPTION_SET.xenomai
-  CONFIGURE_ARGS+=	--with-xenomai
-  REQD_BUILD_OPTIONS.genom+= xenomai
-  include ../../mk/sysdep/xenomai.mk
-endef
-
-ifdef GENOM_MODULE
-  GNU_CONFIGURE=	yes
-
-  pre-configure: genom-generate
-
-  .PHONY: genom-generate
-  genom-generate:
-	@${STEP_MSG} "Generating ${GENOM_MODULE} module"
-	${RUN}cd ${WRKSRC} && ${CONFIGURE_LOGFILTER} 			\
-		${SETENV} ${CONFIGURE_ENV} 				\
-		${TOOLS.genom} ${GENOM_ARGS} ${GENOM_MODULE}
-endif # GENOM_MODULE
-
-endif # DEPEND_DEPTH==+
 endif # GENOM_DEPEND_MK ----------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
