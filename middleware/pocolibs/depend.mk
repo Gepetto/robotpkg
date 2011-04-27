651a26773279cd7d52c5f2a94942939cec137251
Arnaud Degroote <arnaud.degroote@laas.fr>
2011-04-27 16:41:10 +0200

    [middleware/pocolibs] Bump DEPEND_ABI for pocolibs
    GeNoM 2.7 requires pocolibs 2.9, and as long it is the (only?) user of
    it, force everyone to use this pocolibs version.
    
    oked by Anthony

--- middleware/pocolibs/depend.mk
+++ middleware/pocolibs/depend.mk
@@ -14,7 +14,7 @@ PREFER.pocolibs?=	robotpkg
 
 DEPEND_USE+=		pocolibs
 
-DEPEND_ABI.pocolibs?=	pocolibs>=2.7
+DEPEND_ABI.pocolibs?=	pocolibs>=2.9
 DEPEND_DIR.pocolibs?=	../../middleware/pocolibs
 
 SYSTEM_SEARCH.pocolibs=\
