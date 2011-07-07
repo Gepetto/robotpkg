f789e48927c6c591f992536ecd7cc79fcc42778f
Anthony Mallet <anthony.mallet@laas.fr>
2011-07-07 22:30:37 +0200

    [devel/swig] Import swig-2.0.4
    SWIG (Simplified Wrapper and Interface Generator) is a compiler that integrates
    C and C++ with languages including Perl, Python, Tcl, Guile, Mzscheme, Java,
    Ruby, PHP, and Ocaml.
    
    This package is made available in robotpkg because version 2.0.4 is not widely
    available yet and 2.0.4 has important fixes for python as well as support for
    python-3.2.
    
    The mk/sysdep/swig.mk file was moved to devel/swig/depend.mk and packages using
    it have been fixed accordingly.

---
+++ devel/swig/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	devel/swig
+# Created:			Anthony Mallet on Thu,  7 Jul 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+SWIG_DEPEND_MK:=	${SWIG_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		swig
+endif
+
+ifeq (+,$(SWIG_DEPEND_MK)) # -----------------------------------------
+
+PREFER.swig?=		system
+
+DEPEND_USE+=		swig
+DEPEND_ABI.swig?=	swig>=1.3
+DEPEND_METHOD.swig+=	build
+
+SYSTEM_SEARCH.swig=	'bin/swig:/Version/{s/[^.0-9]//gp;q;}:% -version'
+
+export SWIG=		$(word 1,${SYSTEM_FILES.swig})
+
+endif # SWIG_DEPEND_MK -----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
