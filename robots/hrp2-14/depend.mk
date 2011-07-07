07af76f50f9b15a4a6b6e057b05231dbfe8d5ce3
Florent Lamiraux <florent@laas.fr>
2011-07-07 14:07:08 +0200

    [robots/hrp2-14] Upgrade to version 1.8
        Normalize package name.

--- robots/hrp2-14/depend.mk
+++ robots/hrp2-14/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg depend.mk for:	robots/hrp2_14
+# robotpkg depend.mk for:	robots/hrp2-14
 # Created:			Anthony Mallet on Wed, 14 May 2008
 #
 
@@ -6,20 +6,20 @@ DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 HRP2_14_DEPEND_MK:=	${HRP2_14_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		hrp2_14
+DEPEND_PKG+=		hrp2-14
 endif
 
 ifeq (+,$(HRP2_14_DEPEND_MK)) # --------------------------------------
 
-PREFER.hrp2_14?=	robotpkg
+PREFER.hrp2-14?=	robotpkg
 
-SYSTEM_SEARCH.hrp2_14=\
-	include/hrp2_14/hrp2_14.h
+SYSTEM_SEARCH.hrp2-14=\
+	include/hrp2-14/hrp2_14.h
 
-DEPEND_USE+=		hrp2_14
+DEPEND_USE+=		hrp2-14
 
-DEPEND_ABI.hrp2_14?=	hrp2_14>=1.7.5
-DEPEND_DIR.hrp2_14?=	../../robots/hrp2-14
+DEPEND_ABI.hrp2-14?=	hrp2-14>=1.7.5
+DEPEND_DIR.hrp2-14?=	../../robots/hrp2-14
 
 endif # HRP2_14_DEPEND_MK --------------------------------------------
 
