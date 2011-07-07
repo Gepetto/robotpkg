679af47d44c73fe1481ebd0c3f3a477d8116e063
Florent Lamiraux <florent@laas.fr>
2011-07-07 15:13:20 +0200

    [motion/dynamic-graph] New package: version 2.5
        Implementation of dynamic data flow.

---
+++ motion/dynamic-graph/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	motion/dynamic-graph
+# Created:			Florent Lamiraux, Thu 7 July 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HPP_GIK_DEPEND_MK:=	${HPP_GIK_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		dynamic-graph
+endif
+
+ifeq (+,$(HPP_GIK_DEPEND_MK)) # --------------------------------------
+
+PREFER.dynamic-graph?=	robotpkg
+
+SYSTEM_SEARCH.dynamic-graph=\
+	include/dynamic-graph/entity.h	\
+	lib/libdynamic-graph.so		\
+	lib/pkgconfig/dynamic-graph.pc
+
+DEPEND_USE+=		dynamic-graph
+
+DEPEND_ABI.dynamic-graph?=	dynamic-graph>=2.5
+DEPEND_DIR.dynamic-graph?=	../../motion/dynamic-graph
+
+endif # HPP_GIK_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
