a215933ffbc37b353b26367c65f95bf2a05bcb52
Florent Lamiraux <florent@laas.fr>
2011-07-08 17:31:42 +0200

    [robots/sot-openhrp] New package
        hrpsys plugin for the stack of tasks.

---
+++ robots/sot-openhrp/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	robots/sot-openhrp
+# Created:			Florent Lamiraux, Fri Jul 8, 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+SOT_OPENHRP_DEPEND_MK:=	${SOT_OPENHRP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		sot-openhrp
+endif
+
+ifeq (+,$(SOT_OPENHRP_DEPEND_MK)) # --------------------------------------
+
+PREFER.sot-openhrp?=	robotpkg
+
+SYSTEM_SEARCH.sot-openhrp=\
+
+
+DEPEND_USE+=		sot-openhrp
+
+DEPEND_ABI.sot-openhrp?=	sot-openhrp>=2.5
+DEPEND_DIR.sot-openhrp?=	../../robots/sot-openhrp
+
+endif # SOT_OPENHRP_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
