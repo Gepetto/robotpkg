3cd5cb8c0855d3f5de31c56982cec5b937f60394
Florent Lamiraux <florent@laas.fr>
2011-07-08 10:31:09 +0200

    [motion/sot-core] New package
        Hierarchical task based control of humanoid robots.

---
+++ motion/sot-core/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	motion/sot-core
+# Created:			Florent Lamiraux, Thu 7 July 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HPP_GIK_DEPEND_MK:=	${HPP_GIK_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		sot-core
+endif
+
+ifeq (+,$(HPP_GIK_DEPEND_MK)) # --------------------------------------
+
+PREFER.sot-core?=	robotpkg
+
+SYSTEM_SEARCH.sot-core=\
+
+DEPEND_USE+=		sot-core
+
+DEPEND_ABI.sot-core?=	sot-core>=2.5
+DEPEND_DIR.sot-core?=	../../motion/sot-core
+
+endif # HPP_GIK_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
