f50acfe0c45b366b55b615f25d572ff37bbec80f
Anthony Mallet <anthony.mallet@laas.fr>
2011-08-09 21:35:34 +0200

    [archivers/libarchive] Fix compilation on MacOSX > 10.5
    On MacOSX, an outdated libarchive may be found in /usr/lib. Make sure the
    linker uses our static version by passing -search_paths_first.
    
    Fix from commit $NetBSD: Makefile,v 1.178 2010/04/15 22:25:11 tron Exp $ in
    pkgsrc/pkgtools/pkg_install, reported by Simon in #right_ear

--- archivers/libarchive/depend.mk
+++ archivers/libarchive/depend.mk
@@ -38,6 +38,12 @@ CPPFLAGS+=	-I${LIBARCHIVE_SRCDIR}/libarchive
 LDFLAGS+=	-L${LIBARCHIVE_SRCDIR}/.libs
 LIBS+=		-larchive -lbz2 -lz
 
+ifeq (Darwin,${OPSYS})
+  # Make sure that the linker uses our static library instead of the
+  # (outdated) dynamic library "/usr/lib/libarchive.dylib".
+  LDFLAGS+=	-Wl,-search_paths_first
+endif
+
 post-extract: libarchive-extract
 libarchive-extract:
 	@${STEP_MSG} "Extracting libarchive in place"
