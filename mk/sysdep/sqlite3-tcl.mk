d83f978a007eaddbcd16c22911624da84811bb59
Anthony Mallet <anthony.mallet@laas.fr>
2011-09-12 15:16:08 +0200

    [mk/sysdep] Teach robotpkg about sqlite3-tcl

---
+++ mk/sysdep/sqlite3-tcl.mk
@@ -0,0 +1,32 @@
+# robotpkg sysdep/sqlite3-tcl.mk
+# Created:			Anthony Mallet on Mon, 12 Sep 2011
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+SQLITE3_TCL_DEPEND_MK:=	${SQLITE3_TCL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		sqlite3-tcl
+endif
+
+ifeq (+,$(SQLITE3_TCL_DEPEND_MK)) # ----------------------------------------
+
+PREFER.sqlite3-tcl?=	system
+
+DEPEND_USE+=		sqlite3-tcl
+
+DEPEND_ABI.sqlite3-tcl?=sqlite3-tcl>=3
+
+_vregex=/ifneeded/{s/.*ifneeded sqlite3[ \t]*//;s/[ \t]*[[].*//;p;q;}
+SYSTEM_SEARCH.sqlite3-tcl=\
+  'lib/{,tcl{,[0-9]*}}/sqlite3/pkgIndex.tcl:${_vregex}'
+
+SYSTEM_PKG.Fedora.sqlite3-tcl=	sqlite-tcl
+SYSTEM_PKG.Ubuntu.sqlite3-tcl=	sqlite3-tcl
+SYSTEM_PKG.Debian.sqlite3-tcl=	sqlite3-tcl
+SYSTEM_PKG.NetBSD.sqlite3-tcl=	databases/sqlite3-tcl
+
+include ../../mk/sysdep/tcl.mk
+
+endif # SQLITE3_TCL_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
