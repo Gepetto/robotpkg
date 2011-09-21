b75e64a06a0cd504aca5c366e2e451f79abf068a
Anthony Mallet <anthony.mallet@laas.fr>
2011-09-21 18:23:09 +0200

    [mk/sysdep] Teach robotpkg about libXpm

---
+++ mk/sysdep/libXpm.mk
@@ -0,0 +1,30 @@
+# robotpkg sysdep/libXpm.mk
+# Created:			Anthony Mallet on Wed 21 Sep 2011
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+LIBXPM_DEPEND_MK:=	${LIBXPM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libXpm
+endif
+
+ifeq (+,$(LIBXPM_DEPEND_MK)) # ---------------------------------------------
+
+PREFER.libXpm?=		system
+DEPEND_USE+=		libXpm
+
+DEPEND_ABI.libXpm?=	libXpm
+
+SYSTEM_SEARCH.libXpm=	\
+	include/X11/xpm.h						\
+	lib/libXpm.{so,a}						\
+	'lib/pkgconfig/xpm.pc:/Version/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Fedora.libXpm=libxpm-devel
+SYSTEM_PKG.Ubuntu.libXpm=libxpm-dev
+SYSTEM_PKG.Debian.libXpm=libxpm-dev
+SYSTEM_PKG.NetBSD.libXpm=pkgsrc/x11/libXpm
+
+endif # LIBXPM_DEPEND_MK ---------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
