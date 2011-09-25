7766cd75cc5373f72bf3a495beac3e108083327a
Anthony Mallet <anthony.mallet@laas.fr>
2011-09-25 16:05:22 +0200

    [mk/sysdep] Fix quoting of SYSTEM_SEARCH.libXpm

--- mk/sysdep/libXpm.mk
+++ mk/sysdep/libXpm.mk
@@ -17,7 +17,7 @@ DEPEND_ABI.libXpm?=	libXpm
 
 SYSTEM_SEARCH.libXpm=	\
 	include/X11/xpm.h						\
-	lib/libXpm.{so,a}						\
+	'lib/libXpm.{so,a}'						\
 	'lib/pkgconfig/xpm.pc:/Version/s/[^0-9.]//gp'
 
 SYSTEM_PKG.Fedora.libXpm=libXpm-devel
