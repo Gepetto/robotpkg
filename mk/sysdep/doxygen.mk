4ef65022fb4ef898c82297104468262af11c2aac
Anthony Mallet <anthony.mallet@laas.fr>
2011-10-31 16:16:22 +0100

    [mk/sysdep] Stop generating obsolete @dirrm PLIST directives for doxygen
    - DOXYGEN_PLIST_DIR does not need to generate any @dirrm anymore
    - While here, tidy the file a bit.

--- mk/sysdep/doxygen.mk
+++ mk/sysdep/doxygen.mk
@@ -1,19 +1,5 @@
-# $LAAS: doxygen.mk 2011/09/10 23:05:23 tho $
-#
-# Copyright (c) 2008-2009,2011 LAAS/CNRS
-# All rights reserved.
-#
-# Redistribution and use  in source  and binary  forms,  with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#   1. Redistributions of  source  code must retain the  above copyright
-#      notice and this list of conditions.
-#   2. Redistributions in binary form must reproduce the above copyright
-#      notice and  this list of  conditions in the  documentation and/or
-#      other materials provided with the distribution.
-#
-#                                      Anthony Mallet on Fri Dec 12 2008
+# robotpkg sysdep/tdom.mk
+# Created:			Anthony Mallet on Fri Dec 12 2008
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
@@ -33,17 +19,14 @@ DEPEND_METHOD.doxygen+=	build
 SYSTEM_SEARCH.doxygen=	'bin/doxygen:p:% --version'
 
 SYSTEM_PKG.Linux.doxygen=	doxygen
-SYSTEM_PKG.NetBSD.doxygen=	pkgsrc/devel/doxygen
+SYSTEM_PKG.NetBSD.doxygen=	devel/doxygen
 
 # Automatic PLIST generation for doxygen generated files
 ifdef DOXYGEN_PLIST_DIR
   GENERATE_PLIST+=							\
 	${FIND} $(addprefix ${PREFIX}/,${DOXYGEN_PLIST_DIR})		\
 		 \( -type f -o -type l \) -print | ${SORT}		\
-		| ${SED} -e "s,${PREFIX}/,,g";				\
-	${FIND} $(addprefix ${PREFIX}/,${DOXYGEN_PLIST_DIR})		\
-		 -type d -print 					\
-		| ${SORT} -r | ${SED} -e "s,${PREFIX}/,@dirrm ,g";
+		| ${SED} -e "s,${PREFIX}/,,g";
 
   PRINT_PLIST_IGNORE_DIRS+=${DOXYGEN_PLIST_DIR}
 endif
