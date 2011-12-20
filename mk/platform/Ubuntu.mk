9779313fd3f6c897ea62b5371dbe233de9e5616d
Anthony Mallet <anthony.mallet@laas.fr>
2011-12-20 12:15:37 +0100

    [mk] Fix SYSLIBDIR on recent Ubuntu/x86_64
    - Older ubuntu/x86_64 uses lib64 even on 64bits systems (at least
      [08.04-11.04])
    - Newer ubuntu/x86_64 seems to have dropped lib64 (at least 11.10), but I could
      not find any authoritative answer about this.

--- mk/platform/Ubuntu.mk
+++ mk/platform/Ubuntu.mk
@@ -5,7 +5,13 @@
 # System library directories
 _syslibdir=$(patsubst /usr/%,%,$(wildcard /usr/lib/${MACHINE_ARCH}-linux-gnu))
 ifeq (${MACHINE_ARCH},x86_64)	# 64bits arch
-  SYSLIBDIR?=	${_syslibdir} lib64
+  ifneq (,$(filter 08.04 10.04 10.10 11.04,${OS_VERSION}))
+    # older ubuntu uses lib64 even on 64bits systems
+    SYSLIBDIR?= ${_syslibdir} lib64
+  else
+    # newer ubuntu seems to have dropped lib64 on 64bits systems
+    SYSLIBDIR?= ${_syslibdir} lib
+  endif
 else				# 32bits arch
   SYSLIBDIR?=	${_syslibdir} lib
 endif
