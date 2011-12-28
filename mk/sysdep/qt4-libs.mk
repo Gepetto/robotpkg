925774d0c1f774a829e759303899e90896ad8e07
Anthony Mallet <anthony.mallet@laas.fr>
2011-12-28 15:40:18 +0100

    [mk/sysdep] Fix qt4-libs.mk for pkgsrc
    - Adapt SYSTEM_SEARCH.qt4-libs to pkgsrc's hierarchy (/usr/pkg/qt4 prefix)
    - While here, cleanup space-before-tabs

--- mk/sysdep/qt4-libs.mk
+++ mk/sysdep/qt4-libs.mk
@@ -5,25 +5,25 @@ DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 QT4_LIBS_DEPEND_MK:=	${QT4_LIBS_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+= 		qt4-libs
+DEPEND_PKG+=		qt4-libs
 endif
 
 ifeq (+,$(QT4_LIBS_DEPEND_MK)) # -------------------------------------------
 
-PREFER.qt4-libs?= 	system
+PREFER.qt4-libs?=	system
 
-DEPEND_USE+= 		qt4-libs
-DEPEND_ABI.qt4-libs?= 	qt4-libs>=4.6.2
+DEPEND_USE+=		qt4-libs
+DEPEND_ABI.qt4-libs?=	qt4-libs>=4.6.2
 
 SYSTEM_SEARCH.qt4-libs=\
-	'include/{,qt4/{,include/}}Qt{,Core}/qcoreevent.h'	\
-	'{,share/qt4/}lib/libQtCore.{a,so}'	\
+	'{,qt[0-9]/}include/{,qt4/{,include/}}Qt{,Core}/qcoreevent.h'	\
+	'{,share/qt4/,qt[0-9]/}lib/libQtCore.{a,so}'			\
 	'lib/pkgconfig/QtCore.pc:/Version/s/[^0-9.]//gp'
 
 SYSTEM_PKG.Fedora.qt4-libs=	qt-devel
 SYSTEM_PKG.Ubuntu.qt4-libs=	libqt4-dev
 SYSTEM_PKG.Debian.qt4-libs=	libqt4-dev
-SYSTEM_PKG.NetBSD.qt4-libs=		pkgsrc/x11/qt4-libs pkgsrc/x11/qt4-tools
+SYSTEM_PKG.NetBSD.qt4-libs=	x11/qt4-libs x11/qt4-tools
 
 endif # QT4_LIBS_DEPEND_MK -------------------------------------------------
 
