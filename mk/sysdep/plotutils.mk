949e9e6b8aa422c6d09130b38d171ecfd06f6bf8
Anthony Mallet <anthony.mallet@laas.fr>
2011-12-30 13:46:19 +0100

    [mk/sysdep] Cleanup plotutils sysdep - no functional changes

--- mk/sysdep/plotutils.mk
+++ mk/sysdep/plotutils.mk
@@ -1,20 +1,5 @@
-#
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
-#
-# Permission to use, copy, modify, and distribute this software for any purpose
-# with or without   fee is hereby granted, provided   that the above  copyright
-# notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
-# PERFORMANCE OF THIS SOFTWARE.
-#
-#                                             Thomas Moulard on Mon May 17 2010
+# robotpkg sysdep/plotutils.mk
+# Created:			Thomas Moulard on Mon May 17 2010
 #
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 PLOTUTILS_DEPEND_MK:=	${PLOTUTILS_DEPEND_MK}+
@@ -23,9 +8,9 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		plotutils
 endif
 
-ifeq (+,$(PLOTUTILS_DEPEND_MK)) # ---------------------------------------------
+ifeq (+,$(PLOTUTILS_DEPEND_MK)) # ------------------------------------------
 
-PREFER.plotutils?=		system
+PREFER.plotutils?=	system
 
 DEPEND_USE+=		plotutils
 
@@ -35,11 +20,11 @@ SYSTEM_SEARCH.plotutils=	\
 	'include/plotter.h:/PL_LIBPLOT_VER_STRING .*"/s/[^.0-9]//gp'	\
 	'lib/libplot.{a,so}'
 
+SYSTEM_PKG.Debian.plotutils=	libplot-dev
 SYSTEM_PKG.Fedora.plotutils=	plotutils-devel
+SYSTEM_PKG.NetBSD.plotutils=	graphics/plotutils
 SYSTEM_PKG.Ubuntu.plotutils=	libplot-dev
 
-# SYSTEM_PKG.Debian.plotutils=	plotutils-dev
-
-endif # PLOTUTILS_DEPEND_MK --------------------------------------------------
+endif # PLOTUTILS_DEPEND_MK ------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
