ffe1e1515780210f610ba28b3ba455a7fd8f70fb
Anthony Mallet <anthony.mallet@laas.fr>
2012-01-18 15:02:17 +0100

    [mk/sysdep] Add a check for glibconfig in glib2.mk
    Some packages want to know the location of glibconfig.h from glib-2.0, so add
    this file to SYSTEM_SEARCH.glib2.
    
    Note: those packages would better use pkg-config, since glib-2.0 has a .pc
    file...

--- mk/sysdep/glib2.mk
+++ mk/sysdep/glib2.mk
@@ -1,20 +1,6 @@
+# robotpkg sysdep/glib2.mk
+# Created:			Anthony Mallet on Thu Dec  3 2009
 #
-# Copyright (c) 2009 LAAS/CNRS
-# All rights reserved.
-#
-# Redistribution and use  in source  and binary  forms,  with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#   1. Redistributions of  source  code must retain the  above copyright
-#      notice and this list of conditions.
-#   2. Redistributions in binary form must reproduce the above copyright
-#      notice and  this list of  conditions in the  documentation and/or
-#      other materials provided with the distribution.
-#
-#                                      Anthony Mallet on Thu Dec  3 2009
-#
-
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 GLIB2_DEPEND_MK:=	${GLIB2_DEPEND_MK}+
 
@@ -33,6 +19,7 @@ SYSTEM_PKG.NetBSD.glib2=	pkgsrc/devel/glib2
 
 SYSTEM_SEARCH.glib2=	\
 	'include/{,glib/}glib-2.0/glib.h'				\
+	'lib/glib-2.0/include/glibconfig.h'				\
 	'lib/libglib-2.0.{a,so}'					\
 	'lib/pkgconfig/glib-2.0.pc:/Version/s/[^.0-9]//gp'
 
