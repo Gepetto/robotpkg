4a75e0e011f662b72478d1763fefc26b2ffc5be3
Anthony Mallet <anthony.mallet@laas.fr>
2012-02-14 10:37:56 +0100

    [motion/sot-core] Fix depend.mk: typos preventing proper dependency tracking

--- motion/sot-core/depend.mk
+++ motion/sot-core/depend.mk
@@ -3,13 +3,13 @@
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-HPP_GIK_DEPEND_MK:=	${HPP_GIK_DEPEND_MK}+
+SOT_CORE_DEPEND_MK:=	${SOT_CORE_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		sot-core
 endif
 
-ifeq (+,$(HPP_GIK_DEPEND_MK)) # --------------------------------------
+ifeq (+,$(SOT_CORE_DEPEND_MK)) # -------------------------------------------
 
 PREFER.sot-core?=	robotpkg
 
@@ -23,6 +23,6 @@ DEPEND_USE+=		sot-core
 DEPEND_ABI.sot-core?=	sot-core>=2.5
 DEPEND_DIR.sot-core?=	../../motion/sot-core
 
-endif # HPP_GIK_DEPEND_MK --------------------------------------------
+endif # SOT_CORE_DEPEND_MK -------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
