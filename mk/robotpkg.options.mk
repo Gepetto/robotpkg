dbf6ad535d2824d1c5f1ca65dc522a380b030d01
Anthony Mallet <anthony.mallet@laas.fr>
2012-02-22 16:38:02 +0100

    [mk] Add pattern options settings in robotpkg.conf: PKG_OPTIONS.%
    Any package for which the PKG_OPTIONS.<pkg> variable is undefined may take its
    options from PKG_OPTIONS.<pattern> variables.
    
    For instance:
    PKG_OPTIONS.%-genom = debug will configure the debug option for all packages
    matching the %-genom pattern (provided that those package don't define their
    own options explicitly).
    
    Note that setting PKG_OPTIONS.% is not equivalent to using PKG_DEFAULT_OPTIONS,
    because PKG_DEFAULT_OPTIONS is always added to a package options list, while
    PKG_OPTIONS.% is used only as a fallback if PKG_OPTIONS.<pkg> is not set.

--- mk/robotpkg.options.mk
+++ mk/robotpkg.options.mk
@@ -204,6 +204,15 @@ PKG_OPTIONS_SUFFIX?=	${PKGBASE}
 PKG_OPTIONS_VAR?=	PKG_OPTIONS.${PKG_OPTIONS_SUFFIX}
 
 
+# Check for pattern option settings in the absence of an explicit setting
+#
+ifndef ${PKG_OPTIONS_VAR}
+  ${PKG_OPTIONS_VAR}:=$(strip						\
+    $(foreach _,$(filter PKG_OPTIONS.%,${.VARIABLES}),$(strip		\
+      $(if $(filter $_,${PKG_OPTIONS_VAR}),$(value $_)))))
+endif
+
+
 # Derive options from a required package name
 #
 ifneq (,$(findstring ~,${PKGREQD}))
