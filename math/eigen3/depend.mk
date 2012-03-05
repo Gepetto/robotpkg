aee27437da3da4418fc799ef8edda37eb040a957
Anthony Mallet <anthony.mallet@laas.fr>
2012-03-05 16:04:00 +0100

    [math/eigen3] Fix a typo in depend.mk, pkg-config version check

--- math/eigen3/depend.mk
+++ math/eigen3/depend.mk
@@ -27,7 +27,7 @@ DEPEND_DIR.eigen3?=	../../math/eigen3
 
 SYSTEM_SEARCH.eigen3=	\
 	include/eigen3/signature_of_eigen3_matrix_library  \
-	'{lib,share}/pkgconfig/eigen3.pc:/Version/s/[^0-9]//gp'
+	'{lib,share}/pkgconfig/eigen3.pc:/Version/s/[^0-9.]//gp'
 
 endif # EIGEN3_DEPEND_MK ---------------------------------------------------
 
