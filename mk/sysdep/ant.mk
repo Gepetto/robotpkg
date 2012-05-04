e3442f0816bc9cc7d19eae306ce144e584aaedfc
Anthony Mallet <anthony.mallet@laas.fr>
2012-05-04 18:05:39 +0200

    [mk/sysdep] Fix version extraction for ant
    While here, remove copyright and add std header

--- mk/sysdep/ant.mk
+++ mk/sysdep/ant.mk
@@ -1,23 +1,6 @@
-# $LAAS: ant.mk 2009/10/13 12:13:32 mallet $
+# robotpkg sysdep/ant.mk
+# Created:			Séverin Lemaignan on Wed Jun  3 2009
 #
-# Copyright (c) 2009 LAAS/CNRS
-# All rights reserved.
-#
-# Permission to use, copy, modify, and distribute this software for any purpose
-# with or without   fee is hereby granted, provided   that the above  copyright
-# notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
-# PERFORMANCE OF THIS SOFTWARE.
-#
-#                                          Séverin Lemaignan on Wed Jun  3 2009
-#
-
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 ANT_DEPEND_MK:=		${ANT_DEPEND_MK}+
 
@@ -32,7 +15,8 @@ PREFER.ant?=		system
 DEPEND_USE+=		ant
 DEPEND_ABI.ant?=	ant>=1.7
 DEPEND_METHOD.ant+=	build
-SYSTEM_SEARCH.ant=	'bin/ant:s/[^.0-9]//gp:% -version'
+SYSTEM_SEARCH.ant=\
+	'bin/ant:/version/{s/[^0-9. ]//g;s/^ *//;s/ .*//;p;}:% -version'
 
 export ANT=		${PREFIX.ant}/bin/ant
 ANT_ARGS?=		# empty
