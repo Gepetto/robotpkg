dc0b0a6591e012ccfd5fc18d6b468aeae5563132
Anthony Mallet <anthony.mallet@laas.fr>
2012-05-04 18:07:14 +0200

    [mk/sysdep] Typo in libsoqt.mk ubuntu package name
    While here, fix headings

--- mk/sysdep/libsoqt.mk
+++ mk/sysdep/libsoqt.mk
@@ -1,20 +1,5 @@
-#
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
-#
-# Permission to use, copy, modify, and distribute this software for any purpose
-# with or without   fee is hereby granted, provided   that the above  copyright
-# notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
-# PERFORMANCE OF THIS SOFTWARE.
-#
-#                                            Anthony Mallet on Tue Feb  9 2010
+# robotpkg sysdep/libsoqt.mk
+# Created:			Anthony Mallet on Tue Feb  9 2010
 #
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 LIBSOQT_DEPEND_MK:=	${LIBSOQT_DEPEND_MK}+
@@ -36,8 +21,8 @@ SYSTEM_SEARCH.libsoqt=	\
  'lib/libSoQt.{a,so}'
 
 SYSTEM_PKG.Fedora.libsoqt=	SoQt-devel
-SYSTEM_PKG.Ubuntu.libsoqt=	liblibsoqt-dev
-SYSTEM_PKG.Debian.libsoqt=	liblibsoqt-dev
+SYSTEM_PKG.Ubuntu.libsoqt=	libsoqt-dev
+SYSTEM_PKG.Debian.libsoqt=	libsoqt-dev
 
 endif # LIBSOQT_DEPEND_MK --------------------------------------------------
 
