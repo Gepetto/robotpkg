bee03dbaee0025a2684ef3650cdd1abde27a00ce
Cyril Roussillon <cyril.roussillon@laas.fr>
2012-06-18 10:57:45 +0200

    [mk/sysdep] Add qt4-libs system search path for Gentoo systems
    qt4 is found under lib/qt4 on Getoo system (yet another custom location).

--- mk/sysdep/qt4-libs.mk
+++ mk/sysdep/qt4-libs.mk
@@ -16,9 +16,9 @@ DEPEND_USE+=		qt4-libs
 DEPEND_ABI.qt4-libs?=	qt4-libs>=4.6.2
 
 SYSTEM_SEARCH.qt4-libs=\
-	'{,qt[0-9]/}include/{,qt4/{,include/}}Qt{,Core}/qcoreevent.h'	\
-	'{,share/qt4/,qt[0-9]/}lib/libQtCore.{a,so}'			\
-	'lib/pkgconfig/QtCore.pc:/Version/s/[^0-9.]//gp'
+  '{,qt[0-9]/}include/{,qt4/{,include/}}Qt{,Core}/qcoreevent.h'		\
+  '{lib,share/qt[0-9]/lib,qt[0-9]/lib,lib/qt[0-9]}/libQtCore.{so,a}'	\
+  'lib/pkgconfig/QtCore.pc:/Version/s/[^0-9.]//gp'
 
 SYSTEM_PKG.Fedora.qt4-libs=	qt-devel
 SYSTEM_PKG.Ubuntu.qt4-libs=	libqt4-dev
