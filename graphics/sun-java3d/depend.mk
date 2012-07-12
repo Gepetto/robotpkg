6cde4d30148e640bd62ebaa7525828b0caa02d20
Anthony Mallet <anthony.mallet@laas.fr>
2012-07-12 15:54:16 +0200

    [graphics/sun-java3d] Move to graphics and use java alternatives

--- graphics/sun-java3d/depend.mk
+++ graphics/sun-java3d/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg depend.mk for:	devel/sun-java3d
+# robotpkg depend.mk for:	graphics/sun-java3d
 # Created:			Anthony Mallet on Fri, 10 Oct 2008
 #
 
@@ -15,13 +15,12 @@ PREFER.sun-java3d?=	robotpkg
 
 DEPEND_USE+=		sun-java3d
 
-DEPEND_ABI.sun-java3d?=	sun-java3d>=1.5.1
+DEPEND_ABI.sun-java3d?=	sun-java3d
 DEPEND_DIR.sun-java3d?=	../../devel/sun-java3d
 
 SYSTEM_SEARCH.sun-java3d=\
-	'{,java/sun-6/}lib/ext/j3dcore.jar'
+	'{java/lib/ext,share/java}j3dcore.jar'
 
 endif # --- SUN_JAVA3D_DEPEND_MK -------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
-
