208f829021ee9ac2c40e39b14fbf13b23e023e9a
Anthony Mallet <anthony.mallet@laas.fr>
2012-07-12 10:31:23 +0200

    [mk/sysdep] Add an openjdk sysdep

---
+++ mk/sysdep/openjdk.mk
@@ -0,0 +1,43 @@
+# robotpkg sysdep/openjdk.mk
+# Created:			Anthony Mallet on Wed, 11 Jul 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+OPENJDK_DEPEND_MK:=	${OPENJDK_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		openjdk
+endif
+
+ifeq (+,$(OPENJDK_DEPEND_MK)) # --------------------------------------------
+
+DEPEND_USE+=		openjdk
+
+include ../../mk/sysdep/java.mk
+PREFER.openjdk?=	system
+
+DEPEND_ABI.openjdk?=	openjdk>=1.6
+DEPEND_METHOD.openjdk?=	${DEPEND_METHOD.java}
+
+_openjdk_v:=	{y/_/./;s/[^0-9.]//g;h;}
+_openjdk_vv:=	/version/${_openjdk_v};/OpenJDK/{g;p;q;}
+SYSTEM_SEARCH.openjdk=\
+  'bin/{,openjdk[0-9]-}java:${_openjdk_vv}:% -version'	\
+  'bin/{,openjdk[0-9]-}javac:1${_openjdk_v}:% -version'	\
+  'bin/{,openjdk[0-9]-}jar'				\
+  'bin/{,openjdk[0-9]-}javadoc'
+
+SYSTEM_PKG.Fedora.openjdk=	java-1.[67].0-openjdk-devel
+SYSTEM_PKG.Ubuntu.openjdk=	openjdk-[67]-jdk
+SYSTEM_PKG.Debian.openjdk=	openjdk-[67]-jdk
+SYSTEM_PKG.NetBSD.openjdk=	lang/openjdk7
+
+export JAVA_HOME=${PREFIX.openjdk}
+export JAVA=	$(word 1,${SYSTEM_FILES.openjdk})
+export JAVAC=	$(word 2,${SYSTEM_FILES.openjdk})
+export JAR=	$(word 3,${SYSTEM_FILES.openjdk})
+export JAVADOC=	$(word 4,${SYSTEM_FILES.openjdk})
+
+endif # OPENJDK_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
