98a5456561c5dc678c525367526a06e9928a3921
Anthony Mallet <anthony.mallet@laas.fr>
2012-07-18 11:34:59 +0200

    [mk] Never let a package conflict with itself
    ${CONFLICTS} is meant for describing inter-packages conflicts, but when using
    wildcards, it can happen that CONFLICTS matches the current package. Since a
    package necessarily conflicts with itself, this is handled separately and
    CONFLICTS must not interfere here.

--- mk/pkg/install.mk
+++ mk/pkg/install.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2006-2011 LAAS/CNRS
+# Copyright (c) 2006-2012 LAAS/CNRS
 # All rights reserved.
 #
 # This project includes software developed by the NetBSD Foundation, Inc.
@@ -47,6 +47,7 @@ ${foreach _conflict_,${CONFLICTS},					\
 	found="`${_PKG_BEST_EXISTS} '${_conflict_}' || ${TRUE}`";	\
 	case "$$found" in						\
 	"")	;;							\
+	"${PKGNAME}")	;;						\
 	*)	${ECHO} "$$found" >> ${WRKDIR}/.CONFLICTS ;;		\
 	esac;								\
 }									\
