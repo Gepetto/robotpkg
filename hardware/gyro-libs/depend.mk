9041beb566b1169beb81c7bf610306c15f5ee7a1
Matthieu Herrb <matthieu.herrb@laas.fr>
2012-09-13 09:47:34 +0200

    [hardware/gyro-libs] add gyro-libs 2.0
    This is the merge of the gyroKVH1000 and gyroKVH5000 packages,
    providing a new API.
    
    Packages that still depend on the old libs will be updated soon.

---
+++ hardware/gyro-libs/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	hardware/gyro-libs
+# Created:			Arnaud Degroote on Mon, 14 Jul 2008
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GYRO_LIBS_DEPEND_MK:=${GYRO_LIBS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		gyro-libs
+endif
+
+ifeq (+,$(GYRO_LIBS_DEPEND_MK))
+PREFER.gyro-libs?=	robotpkg
+
+DEPEND_USE+=		gyro-libs
+
+DEPEND_ABI.gyro-libs?=	gyro-libs>=2.0
+DEPEND_DIR.gyro-libs?=	../../wip/gyro-libs
+
+SYSTEM_SEARCH.gyro-libs=\
+	bin/gyroTest\
+	include/gyroLib/gyro.h \
+	lib/pkgconfig/gyro-libs.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
