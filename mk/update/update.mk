Gepetto/robotpkg

commit fc4521a72740c5ddda572fe2bea045bd529a5f1f

[mk] Fix recovering from a failed update target in a 'checkout'ed package
Recovering from a previously failed update target in a 'checkout'ed package
implies recomputing the list of packages to update and starting all over
again. This is so because a 'checkout'ed package is always considered
outdated (there is no way to know if the package source was modified since the
last 'update' attempt).

This has no effect on the recovery of regular updates, because already
reinstalled packages will appear up-to-date (unless 'confirm' is given).

mk/update/update.mk
@@ -59,10 +59,8 @@ _UPDATE_TARGETS+=	cbbh
 _UPDATE_TARGETS+=	update-message
 _UPDATE_TARGETS+=	pre-update
 _UPDATE_TARGETS+=	update-create-dlist
-ifneq (yes,$(call exists,${_UPDATE_LIST}))
-  ifeq (,$(filter replace,${UPDATE_TARGET}))
-    _UPDATE_TARGETS+=	update-deinstall-dlist
-  endif
+ifeq (,$(filter replace,${UPDATE_TARGET}))
+  _UPDATE_TARGETS+=	$(call if-outdated-pkg,update-deinstall-dlist)
 endif
 _UPDATE_TARGETS+=	do-update
 _UPDATE_TARGETS+=	post-update
@@ -190,17 +188,21 @@ update-clean:
 .PHONY: update-create-dlist
 update-create-dlist: ${_UPDATE_LIST}
 
-${_UPDATE_LIST}:
-	${RUN} ${MKDIR} $(dir $@); >$@;					\
-	if ${PKG_INFO} -qe '${PKGWILDCARD}'; then			\
+${_UPDATE_LIST}: $(call if-outdated-pkg,.FORCE)
+	${RUN} ${MKDIR} $(dir $@);					\
+	if ${PKG_INFO} -qe '${PKGWILDCARD}' || ${TEST} -s $@; then	\
 	  ${STEP_MSG} "Building package update list";			\
 	  ${TEST} -t 1 && i="-i";					\
-	  { ${ECHO} '${PKGPATH}:${PKGREQD}';				\
-	    for pkg in `${PKG_INFO} -qr '${PKGWILDCARD}'`; do		\
-	      base=$${pkg%~*}; base=$${base%-*};			\
-	      ${ECHO} `${PKG_INFO} -Q PKGPATH $$pkg`":$$base";		\
-	    done; }							\
-	  | ${_pkgset_tsort_deps} -1 -s $$i				\
+	  ${TEST} -f $@ && ${CP} $@ ${@:=.old};				\
+	  >$@;								\
+	  {								\
+	    ${ECHO} '${PKGPATH}:${PKGREQD}';				\
+	    ${TEST} -f ${@:=.old} && ${CAT} ${@:=.old};			\
+	    for p in `${PKG_INFO} -qr '${PKGWILDCARD}' 2>/dev/null`; do	\
+	      base=$${p%~*}; base=$${base%-*};				\
+	      ${ECHO} `${PKG_INFO} -Q PKGPATH $$p`":$$base";		\
+	    done;							\
+	  } | ${_pkgset_tsort_deps} -1 -s $$i				\
 	  | while IFS=: read dir pkg; do				\
 	    if ${TEST} -z "$$dir"; then ${ECHO} "$$pkg"; continue; fi;	\
 	    if ${TEST} "$$dir" = "***"; then				\
@@ -223,7 +225,7 @@ ${_UPDATE_LIST}:
 	  done;								\
 	  ${TEST} -s $@ || { ${RM} $@; exit 2; }			\
 	else								\
-	  ${ECHO} '${PKGPATH}:${PKGREQD}' >>$@;				\
+	  ${ECHO} '${PKGPATH}:${PKGREQD}' >$@;				\
 	fi;								\
 	${CP} $@ ${@:=.clean}
 

commit d930124df9a835c035a3a1989d40a90bf3b798e4

[mk] Fix updating an up-to-date package (a.k.a. `make update confirm`)
If 'confirm' is specified on the command line, the update must unconditionally
deinstall the package and dependencies.

This regression was introduced by commit fc4521a.

mk/update/update.mk
@@ -60,7 +60,11 @@ _UPDATE_TARGETS+=	update-message
 _UPDATE_TARGETS+=	pre-update
 _UPDATE_TARGETS+=	update-create-dlist
 ifeq (,$(filter replace,${UPDATE_TARGET}))
-  _UPDATE_TARGETS+=	$(call if-outdated-pkg,update-deinstall-dlist)
+  ifeq (,$(filter confirm,${MAKECMDGOALS}))
+    _UPDATE_TARGETS+=	$(call if-outdated-pkg,update-deinstall-dlist)
+  else
+    _UPDATE_TARGETS+=	update-deinstall-dlist
+  endif
 endif
 _UPDATE_TARGETS+=	do-update
 _UPDATE_TARGETS+=	post-update