5817b98f0c16176a8da830323746f9f4d4a857d7
Anthony Mallet <anthony.mallet@laas.fr>
2012-12-09 19:24:16 +0100

    [mk] Newer ubuntu >= 11.10 have dropped lib64 on x86_64
    This fixes commit 8650c1e that introduced this regression

--- mk/platform/Ubuntu.mk
+++ mk/platform/Ubuntu.mk
@@ -4,11 +4,14 @@
 
 # System library directories
 ifeq (${MACHINE_ARCH},x86_64)	# 64bits arch
+  _syslibdir=\
+	$(patsubst /usr/%,%,$(wildcard /usr/lib/${MACHINE_ARCH}-linux-gnu))
   ifneq (,$(filter 08.04 10.04 10.10 11.04,${OS_VERSION}))
     # older ubuntu uses lib64 even on 64bits systems
-    _syslibdir=\
-	$(patsubst /usr/%,%,$(wildcard /usr/lib/${MACHINE_ARCH}-linux-gnu))
     SYSLIBDIR?=	${_syslibdir} lib64
+  else
+    # newer ubuntu seems to have dropped lib64 on 64bits systems
+    SYSLIBDIR?= ${_syslibdir} lib
   endif
 endif
 
