565986a550ab730f01fa0188a44ff869d0acdeea
Anthony Mallet <anthony.mallet@laas.fr>
2012-12-10 14:57:41 +0100

    [sysutils/py-rospkg] Import rospkg-1.0.16
    The rospkg Python module provides basic utilities for querying information
    about ROS packages, stacks, and distributions. There are several basic APIs:
    ROS environment, RosPack/RosStack, OS detection, and distributions.

---
+++ sysutils/py-rospkg/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	sysutils/py-rospkg
+# Created:			Anthony Mallet on Sun, 15 Jul 2012
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_ROSPKG_DEPEND_MK:=	${PY_ROSPKG_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		py-rospkg
+endif
+
+ifeq (+,$(PY_ROSPKG_DEPEND_MK)) # ------------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.py-rospkg?=	${PREFER.ros-base}
+
+DEPEND_USE+=		py-rospkg
+
+DEPEND_ABI.py-rospkg?=	${PKGTAG.python}-rospkg>=0.10.12
+DEPEND_DIR.py-rospkg?=	../../sysutils/py-rospkg
+
+SYSTEM_SEARCH.py-rospkg=\
+	'bin/rosversion'				\
+	'${PYTHON_SYSLIBSEARCH}/rospkg/__init__.py'
+
+SYSTEM_PKG.Ubuntu.py-rospkg=	python-rospkg
+
+include ../../mk/sysdep/python.mk
+
+endif # PY_ROSPKG_DEPEND_MK ------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
