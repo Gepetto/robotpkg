13ca7ad7d5114378ced835983f496333f58feb71
Anthony Mallet <anthony.mallet@laas.fr>
2012-12-10 16:37:24 +0100

    [interfaces/ros-std-msgs] Import ros-std-msgs-0.4.11
    std_msgs contains common message types representing primitive data types and
    other basic message constructs, such as multiarrays. For common, generic
    robot-specific message types, please see common_msgs.
    
    std_msgs contains wrappers for ROS primitive types, which are documented in the
    msg specification. It also contains the Empty type, which is useful for sending
    an empty signal.
    
    This package also contains the "MultiArray" types, which can be useful for
    storing sensor data.

---
+++ interfaces/ros-std-msgs/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	interfaces/ros-std-msgs
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ROS_STD_MSGS_DEPEND_MK:=	${ROS_STD_MSGS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		ros-std-msgs
+endif
+
+ifeq (+,$(ROS_STD_MSGS_DEPEND_MK)) # ---------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-std-msgs?=	${PREFER.ros-base}
+
+DEPEND_USE+=		ros-std-msgs
+
+DEPEND_ABI.ros-std-msgs?=	ros-std-msgs>=0.4.11
+DEPEND_DIR.ros-std-msgs?=	../../interfaces/ros-std-msgs
+
+SYSTEM_SEARCH.ros-std-msgs=\
+	include/std_msgs/Int32.h				\
+	'${PYTHON_SYSLIBSEARCH}/std_msgs/msg/_Int32.py'		\
+	share/common-lisp/ros/std_msgs/msg/Int32.lisp		\
+	share/std_msgs/cmake/std_msgs-config.cmake		\
+	'share/std_msgs/stack.xml:/<version>/s/[^0-9.]//gp'	\
+	'lib/pkgconfig/std_msgs.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_STD_MSGS_DEPEND_MK ---------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
