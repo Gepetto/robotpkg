50ddfd24d6c0d0c7f47724df4ee749fe62c42911
Anthony Mallet <anthony.mallet@laas.fr>
2012-12-10 16:52:12 +0100

    [middleware/ros-actionlib] Import ros-actionlib-1.8.7
    The actionlib package provides a standardized interface for interfacing with
    preemptible tasks. Examples of this include moving the base to a target
    location, performing a laser scan and returning the resulting point cloud,
    detecting the handle of a door, etc.
    
    In any large ROS based system, there are cases when someone would like to send
    a request to a node to perform some task, and also receive a reply to the
    request. This can currently be achieved via ROS services.
    
    In some cases, however, if the service takes a long time to execute, the user
    might want the ability to cancel the request during execution or get periodic
    feedback about how the request is progressing. The actionlib package provides
    tools to create servers that execute long-running goals that can be
    preempted. It also provides a client interface in order to send requests to the
    server.

---
+++ middleware/ros-actionlib/depend.mk
@@ -0,0 +1,30 @@
+# robotpkg depend.mk for:	middleware/ros-actionlib
+# Created:			Anthony Mallet on Wed, 10 Oct 2012
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_ACTIONLIB_DEPEND_MK:=	${ROS_ACTIONLIB_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		ros-actionlib
+endif
+
+ifeq (+,$(ROS_ACTIONLIB_DEPEND_MK)) # --------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-actionlib?=	${PREFER.ros-base}
+
+DEPEND_USE+=		ros-actionlib
+
+DEPEND_ABI.ros-actionlib?=	ros-actionlib>=1.8.7<1.9
+DEPEND_DIR.ros-actionlib?=	../../middleware/ros-actionlib
+
+SYSTEM_SEARCH.ros-actionlib=\
+	include/actionlib/server/action_server.h		\
+	lib/libactionlib.so					\
+	'share/actionlib/stack.xml:/<version>/s/[^0-9.]//gp'	\
+	'lib/pkgconfig/actionlib.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_ACTIONLIB_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
