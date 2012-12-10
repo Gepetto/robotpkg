5a1ca932cd0e27161ab83f6f3a7aba1fc279a99b
Anthony Mallet <anthony.mallet@laas.fr>
2012-12-10 15:55:23 +0100

    [lang/ros-genlisp] Import ros-genlisp-0.3.3
    Project genmsg exists in order to decouple code generation from ROS .msg & .srv
    format files from the parsing of these files and from impementation details of
    the build system (project directory layout, existence or nonexistence of
    utilities like rospack, values of environment variables such as
    ROS_PACKAGE_PATH): i.e. none of these are required to be set in any particular
    way.

---
+++ lang/ros-genlisp/depend.mk
@@ -0,0 +1,33 @@
+# robotpkg depend.mk for:	lang/ros-genlisp
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ROS_GENLISP_DEPEND_MK:=	${ROS_GENLISP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		ros-genlisp
+endif
+
+ifeq (+,$(ROS_GENLISP_DEPEND_MK)) # ----------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-genlisp?=	${PREFER.ros-base}
+
+DEPEND_USE+=		ros-genlisp
+
+DEPEND_ABI.ros-genlisp?=ros-genlisp>=0.3.3
+DEPEND_DIR.ros-genlisp?=../../lang/ros-genlisp
+
+SYSTEM_SEARCH.ros-genlisp=\
+	bin/gen_lisp.py						\
+	share/genlisp/cmake/genlisp-config.cmake		\
+	'share/genlisp/stack.xml:/<version>/s/[^0-9.]//gp'	\
+	'lib/pkgconfig/genlisp.pc:/Version/s/[^0-9.]//gp'
+
+export GENLISP_BIN=	${PREFIX.ros-genlisp}/bin/gen_lisp.py
+CMAKE_ARGS+=		-DGENLISP_BIN=${GENLISP_BIN}
+
+endif # ROS_GENLISP_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
