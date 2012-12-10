0e7b26f828b358c4d26cfb97efaad298b8e5789b
Anthony Mallet <anthony.mallet@laas.fr>
2012-12-10 15:13:34 +0100

    [devel/ros-core] Import ros-core-1.8.10
    ROS is an open-source, meta-operating system for your robot. It provides the
    services you would expect from an operating system, including hardware
    abstraction, low-level device control, implementation of commonly-used
    functionality, message-passing between processes, and package management. It
    also provides tools and libraries for obtaining, building, writing, and running
    code across multiple computers. ROS is similar in some respects to 'robot
    frameworks', such as Player, YARP, Orocos, CARMEN, Orca, MOOS, and Microsoft
    Robotics Studio.
    
    The ROS runtime "graph" is a peer-to-peer network of processes that are loosely
    coupled using the ROS communication infrastructure. ROS implements several
    different styles of communication, including synchronous RPC-style
    communication over services, asynchronous streaming of data over topics, and
    storage of data on a Parameter Server. These are explained in greater detail in
    our Conceptual Overview.
    
    ROS is not a realtime framework, though it is possible to integrate ROS with
    realtime code. The Willow Garage PR2 robot uses a system called pr2_etherCAT,
    which transports ROS messages in and out of a realtime process. ROS also has
    seamless integration with the Orocos Real-time Toolkit.

---
+++ devel/ros-core/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	devel/ros-core
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ROS_CORE_DEPEND_MK:=	${ROS_CORE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		ros-core
+endif
+
+ifeq (+,$(ROS_CORE_DEPEND_MK)) # -------------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-core?=	${PREFER.ros-base}
+
+DEPEND_USE+=		ros-core
+
+DEPEND_ABI.ros-core?=	ros-core>=1.8<1.9
+DEPEND_DIR.ros-core?=	../../devel/ros-core
+
+SYSTEM_SEARCH.ros-core=\
+	bin/rosrun					\
+	lib/libroslib.so				\
+	share/roslib/cmake/roslib-config.cmake		\
+	share/rosunit/cmake/rosunit-config.cmake	\
+	'lib/pkgconfig/roslib.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_CORE_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
