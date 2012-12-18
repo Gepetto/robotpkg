Gepetto/robotpkg

commit 79e8687b47c3b7d1cc1676a9c4fa82ee65514848

[mk] Generalize ALTERNATIVE definition
Move to sysdep/python.mk the logic selecting an alternative (this is the only
place where alternatives are used). This leads to the following changes:

- Alternatives define an optional selection macro that returns a non-empty
  value when the alternative is acceptable (for python, this is where the
  version requirements are checked).
- Other packages may define alternatives that are always valid (thus with no
  selection macro).
- Alternatives define an optional SET/UNSET script. This mimics closely what is
  used for options settings. (for python, this is where the actual
  python<nn>.mk is included).
- Alternatives define a human readable description, as for options.
- Alternatives define an optional PKGTAG.<sel> string, that is automatically
  copied to PKGTAG.<alt> when a selection has been done (this tag is meant to
  be included in the package name to allow multiple instances).

The following other changes are cosmetic only:
- DEPEND_ALTERNATIVES is renamed to PKG_ALTERNATIVES
- PKG_SELECT.<alt> is renamed to PREFER_ALTERNATIVE.<alt>
- ALTERNATIVE.<alt> is renamed to PKG_ALTERNATIVE.<alt>
- PYPKGPREFIX is generalized to PKGTAG.<alt>
- For comfort, -PKGTAG.<alt> and PKGTAG.<alt>- are defined to be PKGTAG.<alt>
  prefixed or suffixed with a dash when not empty.

mk/depends/resolve.mk
@@ -68,19 +68,24 @@
 # An alternative is defined by the following variables:
 #
 # -------------8<-------------8<-------------8<-------------8<-------------
-# DEPEND_ALTERNATIVE+=		pkg
-# ALTERNATIVES.pkg=		pkg1 .. pkgx .. pkgn # unsorted
-# PKG_SELECT.pkg?=		pkg2 pkg1 # sorted by preference
-# DEPEND_ABI.pkg=		pkg>=1<=n
+# PKG_ALTERNATIVES+=		pkg
+# PKG_ALTERNATIVES.pkg=		pkg1 .. pkgx .. pkgn # unsorted
+# PREFER_ALTERNATIVE.pkg?=	pkg2 pkg1 # sorted by preference
 #
 # for each pkgx:
+# PKG_ALTERNATIVE_DESCR.pkgx=	# human readable description
+# and optionnaly:
+# PKG_ALTERNATIVE_SELECT.pkgx=	# macro returning non-empty if pkgx is ok
+# PKGTAG.pkgx=			# copied to PKGTAG.pkg
+#
 # ALTERNATIVE_ABI.pkgx=		pkg>=x.0<y.0 (note: not 'pkgx>=...')
 # ALTERNATIVE_MK.pkgx=		../../mk/sysdep/pkgx.mk
 # -------------8<-------------8<-------------8<-------------8<-------------
 #
-# Users may set PKG_SELECT.pkg in robotpkg.conf. After dependency resolution,
-# ALTERNATIVE.pkg is set to the selected alternative (forcing ALTERNATIVE.pkg
-# in robotpkg.conf/cmdline is also allowed, but discouraged. Use PKG_SELECT).
+# Users may set PREFER_ALTERNATIVE.pkg in robotpkg.conf. After dependency
+# resolution, PKG_ALTERNATIVE.pkg is set to the selected alternative (forcing
+# PKG_ALTERNATIVE.pkg in robotpkg.conf/cmdline is also allowed, but
+# discouraged. Use PREFER_ALTERNATIVE).
 #
 
 # DEPEND_PKG contains the list of packages for which we add a direct
@@ -97,92 +102,65 @@ DEPEND_USE?=	# empty
 # --- resolve alternatives -------------------------------------------------
 
 # NO ALTERNATIVE CAN BE ADDED AFTER THIS POINT
-DEPEND_ALTERNATIVE:=$(sort ${DEPEND_ALTERNATIVE})
+PKG_ALTERNATIVES:=$(sort ${PKG_ALTERNATIVES})
 
 # list of unresolved alternatives (not in robotpkg.conf or cmdline)
-_alt_list:=$(foreach _,${DEPEND_ALTERNATIVE},$(if ${ALTERNATIVE.$_},,$_))
+_alt_list:=$(foreach _,${PKG_ALTERNATIVES},$(if ${PKG_ALTERNATIVE.$_},,$_))
 ifneq (,$(strip ${_alt_list}))
   # compute acceptable alternatives
   $(foreach _,${_alt_list},$(eval \
-    _alt_select.$_=$(filter ${ALTERNATIVES.$_},${PKG_SELECT.$_})))
+    _alt_select.$_=$(filter ${PKG_ALTERNATIVES.$_},${PREFER_ALTERNATIVE.$_})))
 
-  # choose a version: generate a list of calls to 'preduce' for each
-  # pattern in order of preference. Then pass this to 'or', so that the
-  # first match wins. This demands a bit of quoting... but generates the
-  # minimum number of calls to preduce. (the trailing [,] in the macro below is
-  # not a typo).
+  # choose a version: generate a list of test for each pattern in order of
+  # preference. Then pass this to 'or', so that the first match wins. This
+  # demands a bit of quoting... but generates the minimum number of calls to
+  # the selection macros. (the trailing [,] in the macro below is not a typo).
   override define _alt_match
-    $$$$(if $$$$(call preduce,${DEPEND_ABI.$1} ${ALTERNATIVE_ABI.$2}),$2),
+    $$$$(if $$(value PKG_ALTERNATIVE_SELECT.$1),$1),
   endef
   $(foreach _,${_alt_list},$(eval \
-    _alt_test.$_=$(foreach a,${_alt_select.$_},$(call _alt_match,$_,$a))))
+    _alt_test.$_=$(foreach a,${_alt_select.$_},$(call _alt_match,$a))))
 
   $(foreach _,${_alt_list},$(eval \
-    ALTERNATIVE.$_:=$$(strip $$(or ${_alt_test.$_}))))
+    PKG_ALTERNATIVE.$_:=$$(strip $$(or ${_alt_test.$_}))))
 endif # _alt_list
 
-# check empty or malformed ALTERNATIVE.<pkg>, and set a sane fallback for the
-# rest of the processing
+# check empty or malformed PKG_ALTERNATIVE.<pkg>, and set a sane fallback for
+# the rest of the processing
 override define _alt_error
-  ifeq (,$(filter 0 1,$(words ${ALTERNATIVE.$1})))
+  ifeq (,$(filter 0 1,$(words ${PKG_ALTERNATIVE.$1})))
     PKG_FAIL_REASON+=\
       "$${bf}The $1 alternative selection must be a single choice$${rm}"\
-      "	ALTERNATIVE.$1 = ${ALTERNATIVE.$1}"				\
-      "	(use PKG_SELECT.$1 to define an ordered list)"
-    override ALTERNATIVE.$1=$(word 1,${ALTERNATIVES.$1})
+      "	PKG_ALTERNATIVE.$1 = ${PKG_ALTERNATIVE.$1}"			\
+      "	(use PREFER_ALTERNATIVE.$1 to define an ordered list)"
+    override PKG_ALTERNATIVE.$1=$(word 1,${PKG_ALTERNATIVES.$1})
   endif
-  ifeq (,$(filter ${ALTERNATIVE.$1},${ALTERNATIVES.$1}))
+  ifeq (,$(filter ${PKG_ALTERNATIVE.$1},${PKG_ALTERNATIVES.$1}))
     PKG_FAIL_REASON+=\
       "$${bf}The $1 alternatives cannot be resolved$${rm}"		\
-      "	ALTERNATIVE.$1 = ${ALTERNATIVE.$1}"				\
-      "	PKG_SELECT.$1 = ${PKG_SELECT.$1}"				\
-      "	DEPEND_ABI.$1 = ${DEPEND_ABI.$1}"
-    override ALTERNATIVE.$1=$(word 1,${ALTERNATIVES.$1})
+      "	PKG_ALTERNATIVE.$1 = ${PKG_ALTERNATIVE.$1}"			\
+      "	PREFER_ALTERNATIVE.$1 = ${PREFER_ALTERNATIVE.$1}"
+    override PKG_ALTERNATIVE.$1=$(word 1,${PKG_ALTERNATIVES.$1})
   endif
 endef
-$(foreach _,${DEPEND_ALTERNATIVE},$(eval $(call _alt_error,$_)))
+$(foreach _,${PKG_ALTERNATIVES},$(eval $(call _alt_error,$_)))
 
-# list of unresolved alternatives ABI (normally none or all)
-_altabi_list:=$(foreach _,${DEPEND_ALTERNATIVE},$(if ${ALTERNATIVE_ABI.$_},,$_))
-ifneq (,$(strip ${_altabi_list}))
-  $(foreach _,${_altabi_list},$(eval \
-    _alt_abi.$_=	${ALTERNATIVE_ABI.${ALTERNATIVE.$_}}))
+# define PGKTAG.,-PKGTAG. and PKGTAG.-
+$(foreach _,${PKG_ALTERNATIVES},$(eval \
+  PKGTAG.$_=$(or ${PKGTAG.${PKG_ALTERNATIVE.$_}},${PKG_ALTERNATIVE.$_})))
+$(foreach _,${PKG_ALTERNATIVES},$(eval -PKGTAG.$_=$(addprefix -,${PKGTAG.$_})))
+$(foreach _,${PKG_ALTERNATIVES},$(eval PKGTAG.$_-=$(addsuffix -,${PKGTAG.$_})))
 
-  # refine the required version of the selected package
-  $(foreach _,${_altabi_list},$(eval \
-    _alt_reqd.$_:=$(call preduce,${DEPEND_ABI.$_} ${_alt_abi.$_})))
-
-  $(foreach _,${_altabi_list},$(eval \
-    ALTERNATIVE_ABI.$_=$(subst $_,${ALTERNATIVE.$_},${_alt_reqd.$_})))
-endif # _altabi_list
-
-# check failures, and set a sane fallback for the rest of the processing
-override define _altabi_error
-  ifeq (,$(strip ${ALTERNATIVE_ABI.$1}))
-    PKG_FAIL_REASON+=\
-      "The requirements on $1 alternatives cannot be satisfied:"	\
-      ""								\
-      "	ALTERNATIVE.$1 = ${ALTERNATIVE.$1}"				\
-      "	DEPEND_ABI.$1 = ${DEPEND_ABI.$1}"
-    override ALTERNATIVE_ABI.$1 =${DEPEND_ABI.$1}
-  endif
-endef
-$(foreach _,${DEPEND_ALTERNATIVE},$(eval $(call _altabi_error,$_)))
-
-# enforce computed ABI requirement on the selected packages
-$(foreach _,${DEPEND_ALTERNATIVE},$(eval \
-  override DEPEND_ABI.${ALTERNATIVE.$_} =${ALTERNATIVE_ABI.$_}))
-
-# add depend method, default to full
-$(foreach _,${DEPEND_ALTERNATIVE},$(eval DEPEND_METHOD.$_?=full))
-$(foreach _,${DEPEND_ALTERNATIVE},$(eval \
-  DEPEND_METHOD.${ALTERNATIVE.$_} +=${DEPEND_METHOD.$_}))
-
-# require the mk file corresponding to the selected alternatives, but as
-# an indirect dependency only. Alternative wrappers should have added the
-# proper entry in DEPEND_PKG/DEPEND_USE at the proper dependency level.
+# execute set/unset scripts. Do this in an indirect dependency context
+# only. Alternative wrappers should have added the proper entry in
+# DEPEND_PKG/DEPEND_USE at the proper dependency level.
 DEPEND_DEPTH:=${DEPEND_DEPTH}+
-include $(foreach _,${DEPEND_ALTERNATIVE},${ALTERNATIVE_MK.${ALTERNATIVE.$_}})
+
+$(foreach _,${PKG_ALTERNATIVES},					\
+  $(eval $(value PKG_ALTERNATIVE_SET.${PKG_ALTERNATIVE.$_}))		\
+  $(foreach 1,$(filter-out ${PKG_ALTERNATIVE.$_},${PKG_ALTERNATIVES.$_}),\
+    $(eval $(value PKG_ALTERNATIVE_UNSET.$1))))
+
 DEPEND_DEPTH:=${DEPEND_DEPTH:+=}
 
 
@@ -324,9 +302,9 @@ endif
 .PHONY: resolve-alternatives
 resolve-alternatives:
 	${RUN} >${_ALTERNATIVES_FILE}; exec 3>>${_ALTERNATIVES_FILE};	\
-  $(foreach _,${DEPEND_ALTERNATIVE},					\
+  $(foreach _,${PKG_ALTERNATIVES},					\
 	${STEP_MSG} "Required virtual ${DEPEND_ABI.$_} provided"	\
 	  "by ${ALTERNATIVE_ABI.$_}";					\
-	${ECHO} 1>&3 'ALTERNATIVE.$_=${ALTERNATIVE.$_}';		\
+	${ECHO} 1>&3 'PKG_ALTERNATIVE.$_=${PKG_ALTERNATIVE.$_}';	\
 	${ECHO} 1>&3 'ALTERNATIVE_ABI.$_=${ALTERNATIVE_ABI.$_}';	\
   )

devel/py-simpleparse/Makefile
@@ -4,7 +4,7 @@
 
 PKGVERSION=		2.1.1a2
 PKGREVISION=		1
-PKGNAME=		${PYPKGPREFIX}-simpleparse-${PKGVERSION}
+PKGNAME=		${PKGTAG.python-}simpleparse-${PKGVERSION}
 DISTNAME=		SimpleParse-${PKGVERSION_NOREV}
 MASTER_SITES=		${MASTER_SITE_SOURCEFORGE:=simpleparse/}
 

middleware/py-yarp/Makefile
@@ -5,7 +5,7 @@
 PKGVERSION=		2.3.1
 PKGREVISION=		2
 DISTNAME=		yarp-${PKGVERSION_NOREV}
-PKGNAME=		${PYPKGPREFIX}-yarp-${PKGVERSION}
+PKGNAME=		${PKGTAG.python-}yarp-${PKGVERSION}
 MASTER_SITES=		${MASTER_SITE_SOURCEFORGE:=yarp0/}
 
 CATEGORIES=		middleware
@@ -15,7 +15,7 @@ COMMENT=		Python bindings for YARP
 LICENSE=		gnu-lgpl-v2
 
 CONFLICTS+=		libpyyarp-[0-9]*
-CONFLICTS+=		${PYPKGPREFIX}-libpyyarp-[0-9]*
+CONFLICTS+=		${PKGTAG.python-}libpyyarp-[0-9]*
 
 USE_LANGUAGES+=		c c++
 

middleware/py-yarp/depend.mk
@@ -14,9 +14,9 @@ ifeq (+,$(PY_YARP_DEPEND_MK)) # --------------------------------------------
 PREFER.py-yarp?=	robotpkg
 
 DEPEND_USE+=		py-yarp
-DEPEND_ABI.py-yarp?=	${PYPKGPREFIX}-yarp>=2.3.1
+DEPEND_ABI.py-yarp?=	${PKGTAG.python-}yarp>=2.3.1
 DEPEND_DIR.py-yarp?=	../../middleware/py-yarp
-DEPEND_VARS.py-yarp+=	ALTERNATIVE.python
+DEPEND_VARS.py-yarp+=	PKG_ALTERNATIVE.python
 
 SYSTEM_SEARCH.py-yarp=\
 	'${PYTHON_SYSLIBSEARCH}/yarp.py'

mk/internal/can-be-built-here.mk
@@ -145,7 +145,7 @@ ifeq (yes,$(call exists,${_COOKIE.wrkdir}))
     _wrk_stale:=yes
   endif
 
-  _wrk_alt:=$(strip $(foreach _,${DEPEND_ALTERNATIVE},${ALTERNATIVE.$_}))
+  _wrk_alt:=$(strip $(foreach _,${PKG_ALTERNATIVES},${PKG_ALTERNATIVE.$_}))
   ifneq (,$(filter-out ${_wrk_alt},${_COOKIE.wrkdir.alternatives}))
     PKG_FAIL_REASON+= "$${bf}Alternatives for ${PKGNAME} have changed$${rm}"
     ifeq (,${_COOKIE.wrkdir.alternatives})

mk/internal/utils.mk
@@ -153,7 +153,7 @@ ${_COOKIE.wrkdir}:
 	${ECHO} "_COOKIE.wrkdir.date:=`${_CDATE_CMD}`";			\
 	${ECHO} "_COOKIE.wrkdir.pkgversion:=$$v";			\
 	${ECHO} "_COOKIE.wrkdir.pkgoptions:=${PKG_OPTIONS}";		\
-	alt='$(foreach _,${DEPEND_ALTERNATIVE},${ALTERNATIVE.$_})';	\
+	alt='$(foreach _,${PKG_ALTERNATIVES},${PKG_ALTERNATIVE.$_})';	\
 	${ECHO} "_COOKIE.wrkdir.alternatives:=$$alt"
 
 ${PKG_DBDIR}:

mk/sysdep/py-setuptools.mk
@@ -14,13 +14,13 @@ ifeq (+,$(PYSETUPTOOLS_DEPEND_MK)) # ---------------------------------------
 PREFER.py-setuptools?=		system
 
 DEPEND_USE+=			py-setuptools
-DEPEND_ABI.py-setuptools?=	${PYPKGPREFIX}-setuptools
+DEPEND_ABI.py-setuptools?=	${PKGTAG.python-}setuptools
 
 SYSTEM_SEARCH.py-setuptools=\
 	'${PYTHON_SYSLIBSEARCH}/setuptools/__init__.py'
 
 SYSTEM_PKG.Linux.py-setuptools=	python-setuptools (python-${PYTHON_VERSION})
-SYSTEM_PKG.NetBSD.py-setuptools=pkgsrc/devel/${PYPKGPREFIX}-setuptools
+SYSTEM_PKG.NetBSD.py-setuptools=pkgsrc/devel/${PKGTAG.python-}setuptools
 
 include ../../mk/sysdep/python.mk
 

mk/sysdep/py-yaml.mk
@@ -18,7 +18,7 @@ ifeq (+,$(PYYAML_DEPEND_MK)) # ---------------------------------------------
 PREFER.py-yaml?=	system
 
 DEPEND_USE+=		py-yaml
-DEPEND_ABI.py-yaml?=	${PYPKGPREFIX}-yaml
+DEPEND_ABI.py-yaml?=	${PKGTAG.python-}yaml
 
 SYSTEM_SEARCH.py-yaml=\
 	'${PYTHON_SYSLIBSEARCH}/yaml/__init__.py'
@@ -26,7 +26,7 @@ SYSTEM_SEARCH.py-yaml=\
 SYSTEM_PKG.Linux-fedora.py-yaml=	PyYAML (python-${PYTHON_VERSION})
 SYSTEM_PKG.Linux-ubuntu.py-yaml=	python-yaml (python-${PYTHON_VERSION})
 SYSTEM_PKG.Linux-debian.py-yaml=	python-yaml (python-${PYTHON_VERSION})
-SYSTEM_PKG.NetBSD.py-yaml=		pkgsrc/textproc/${PYPKGPREFIX}-yaml
+SYSTEM_PKG.NetBSD.py-yaml=		pkgsrc/textproc/${PKGTAG.python-}yaml
 
 include ../../mk/sysdep/python.mk
 

commit b53437867134b65695195626925b4fc3bbcfcddb

[mk] Fix cache file for alternative selection
This cache was broken after commit 79e8687 renaming some internal
variables, 1.5 years ago, yay! But since this is just a cache, the fact that it
did not cache anything was not blatant.

mk/depends/bootstrap.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2009-2011 LAAS/CNRS
+# Copyright (c) 2009-2012 LAAS/CNRS
 # All rights reserved.
 #
 # Permission to use, copy, modify, and distribute this software for any purpose
@@ -83,7 +83,7 @@ bootstrap-depends-cookie: makedirs
 _REAL_BSDEPENDS_TARGETS+=	makedirs
 _REAL_BSDEPENDS_TARGETS+=	acquire-depends-lock
 _REAL_BSDEPENDS_TARGETS+=	bootstrap-depends-message
-ifdef DEPEND_ALTERNATIVE
+ifdef PKG_ALTERNATIVES
   _REAL_BSDEPENDS_TARGETS+=	resolve-alternatives
 endif
 _REAL_BSDEPENDS_TARGETS+=	pkg-bootstrap-depends

mk/depends/resolve.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2008-2011 LAAS/CNRS
+# Copyright (c) 2008-2012 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use  in  source  and binary  forms,  with  or  without
@@ -78,8 +78,8 @@
 # PKG_ALTERNATIVE_SELECT.pkgx=	# macro returning non-empty if pkgx is ok
 # PKGTAG.pkgx=			# copied to PKGTAG.pkg
 #
-# ALTERNATIVE_ABI.pkgx=		pkg>=x.0<y.0 (note: not 'pkgx>=...')
-# ALTERNATIVE_MK.pkgx=		../../mk/sysdep/pkgx.mk
+# PKG_ALTERNATIVE_SET.pkgx=	# Evaluated when pkgx is selected
+# PKG_ALTERNATIVE_UNSET.pkgx=	# Evaluated when pkgx is not selected
 # -------------8<-------------8<-------------8<-------------8<-------------
 #
 # Users may set PREFER_ALTERNATIVE.pkg in robotpkg.conf. After dependency
@@ -328,8 +328,7 @@ endif
 resolve-alternatives:
 	${RUN} >${_ALTERNATIVES_FILE}; exec 3>>${_ALTERNATIVES_FILE};	\
   $(foreach _,${PKG_ALTERNATIVES},					\
-	${STEP_MSG} "Required virtual ${DEPEND_ABI.$_} provided"	\
-	  "by ${ALTERNATIVE_ABI.$_}";					\
+	${STEP_MSG} "Alternative ${DEPEND_ABI.$_} provided"		\
+	  "by ${DEPEND_ABI.${PKG_ALTERNATIVE.$_}}";			\
 	${ECHO} 1>&3 'PKG_ALTERNATIVE.$_=${PKG_ALTERNATIVE.$_}';	\
-	${ECHO} 1>&3 'ALTERNATIVE_ABI.$_=${ALTERNATIVE_ABI.$_}';	\
   )