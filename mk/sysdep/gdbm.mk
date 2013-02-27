5a8e1d1c2609e5aad149553cebf076780630a1f1
Anthony Mallet <anthony.mallet@laas.fr>
2013-02-27 11:27:54 +0100

    [mk/sysdep] Do not require a minimum version of the gdbm dependency
    There is no way to guess the installed version of gdbm from the installed
    files, so disable the abi requirement.
    
    While here, update headings style.

--- mk/sysdep/gdbm.mk
+++ mk/sysdep/gdbm.mk
@@ -1,25 +1,8 @@
-# $LAAS: gdbm.mk 2011/10/06 18:46:50 mallet $
+# robotpkg sysdep/gdbm.mk
+# Created:			Séverin Lemaignan on Mon Dec 7 2009
 #
-# Copyright (c) 2009,2011 LAAS/CNRS
-# All rights reserved.
-#
-# Permission to use, copy, modify, and distribute this software for any purpose
-# with or without   fee is hereby granted, provided   that the above  copyright
-# notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
-# PERFORMANCE OF THIS SOFTWARE.
-#
-#                                           Séverin Lemaignan on Mon Dec 7 2009
-#
-
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-GDBM_DEPEND_MK:=		${GDBM_DEPEND_MK}+
+GDBM_DEPEND_MK:=	${GDBM_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		gdbm
@@ -30,7 +13,7 @@ ifeq (+,$(GDBM_DEPEND_MK)) # ------------------------------------------------
 PREFER.gdbm?=		system
 
 DEPEND_USE+=		gdbm
-DEPEND_ABI.gdbm?=	gdbm>=1.8
+DEPEND_ABI.gdbm?=	gdbm
 
 SYSTEM_SEARCH.gdbm=	\
 	'include/gdbm.h'	\
