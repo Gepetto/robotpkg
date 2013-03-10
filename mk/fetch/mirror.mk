6c358123945ddeb0f99cd45313a3b0d13d1bae18
Anthony Mallet <anthony.mallet@laas.fr>
2013-03-10 19:07:28 +0100

    [mk] Fix a glitch in mirror-distfiles
    Use [ :\r] as the awk field separator when looking for the Content-Length for a
    HEAD request in check-master-sites. Apparently, gawk>=4 needs this.

--- mk/fetch/mirror.mk
+++ mk/fetch/mirror.mk
@@ -118,7 +118,7 @@ check-master-sites:
 	            "$$site: fatal error";				\
 	          fatal=1; continue ;;					\
 	      esac;							\
-	      size=`${ECHO} "$$hdr" | ${AWK} -F'[ :]+'			\
+	      size=`${ECHO} "$$hdr" | ${AWK} -F'[ :\r]+'		\
 	        '/[Cc]ontent-[Ll]ength: +[0-9]+/ {print $$2}'`;		\
 	      if ${TEST} -z "$$size"; then				\
 	        ${ECHO} "SKIP:  $$site: cannot determine file size";	\
