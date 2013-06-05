f4da7ed7124f3bb6bf5e4e8359308e83d95cf79b
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-05 14:22:01 +0200

    [mk/sysdep] Teach robotpkg about gzip

---
+++ mk/sysdep/gzip.mk
@@ -0,0 +1,27 @@
+# robotpkg sysdep/gzip.mk
+# Created:			Anthony Mallet on Wed,  5 Jun 2013
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GZIP_DEPEND_MK:=	${GZIP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		gzip
+endif
+
+ifeq (+,$(GZIP_DEPEND_MK)) # -----------------------------------------------
+
+PREFER.gzip?=		system
+DEPEND_USE+=		gzip
+DEPEND_ABI.gzip?=	gzip
+
+SYSTEM_SEARCH.gzip=	\
+	'bin/gzip:1s/[^0-9.]//gp:% --version'
+
+# Don't call this variable "GZIP" because the gzip program uses this
+# environment variable to define default options.
+#
+export GZIP_CMD=	$(word 1,${SYSTEM_FILES.gzip})
+
+endif # GZIP_DEPEND_MK -----------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
