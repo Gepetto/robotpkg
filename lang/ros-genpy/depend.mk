c7cdac58f9c4572193d3737d86df26e4341cb2e3
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-22 15:58:40 +0200

    [lang/ros-genpy] Update to 0.4.12
    Changes since 0.3.7:
    
    0.4.12 (2013-06-18)
    -------------------
    * fix deserialize bytes in Python3
      (`#10 <https://github.com/ros/genpy/issues/10>`_)
    
    0.4.11 (2013-03-08)
    -------------------
    * fix handling spaces in folder names
      (`ros/catkin#375 <https://github.com/ros/catkin/issues/375>`_)
    
    0.4.10 (2012-12-21)
    -------------------
    * first public release for Groovy

--- lang/ros-genpy/depend.mk
+++ lang/ros-genpy/depend.mk
@@ -6,7 +6,7 @@ DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 ROS_GENPY_DEPEND_MK:=	${ROS_GENPY_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		ros-genpy
+DEPEND_PKG+=			ros-genpy
 endif
 
 ifeq (+,$(ROS_GENPY_DEPEND_MK)) # ------------------------------------------
@@ -15,22 +15,16 @@ include ../../meta-pkgs/ros-base/depend.common
 PREFER.ros-genpy?=		${PREFER.ros-base}
 SYSTEM_PREFIX.ros-genpy?=	${SYSTEM_PREFIX.ros-base}
 
-DEPEND_USE+=		ros-genpy
+DEPEND_USE+=			ros-genpy
 
-DEPEND_ABI.ros-genpy?=	ros-genpy>=0.3.4
-DEPEND_DIR.ros-genpy?=	../../lang/ros-genpy
+DEPEND_ABI.ros-genpy?=		ros-genpy>=0.4<0.5
+DEPEND_DIR.ros-genpy?=		../../lang/ros-genpy
 
 SYSTEM_SEARCH.ros-genpy=\
-	bin/genmsg_py.py					\
-	bin/gensrv_py.py					\
-	share/genpy/cmake/genpy-config.cmake			\
-	'share/genpy/stack.xml:/<version>/s/[^0-9.]//gp'	\
+	lib/genpy/genmsg_py.py					\
+	lib/genpy/gensrv_py.py					\
+	'share/genpy/package.xml:/<version>/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/genpy.pc:/Version/s/[^0-9.]//gp'
-
-export GENMSG_PY_BIN=	${PREFIX.ros-genpy}/bin/genmsg_py.py
-export GENSRV_PY_BIN=	${PREFIX.ros-genpy}/bin/gensrv_py.py
-CMAKE_ARGS+=		-DGENMSG_PY_BIN=${GENMSG_PY_BIN}
-CMAKE_ARGS+=		-DGENSRV_PY_BIN=${GENSRV_PY_BIN}
 
 endif # ROS_GENPY_DEPEND_MK ------------------------------------------------
 
