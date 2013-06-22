86dbd9240fff82e69676641ec904b4bba93930a4
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-22 15:50:21 +0200

    [lang/ros-genlisp] Update to 0.4.11
    No change log available for changes since 0.3.3

--- lang/ros-genlisp/depend.mk
+++ lang/ros-genlisp/depend.mk
@@ -6,7 +6,7 @@ DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 ROS_GENLISP_DEPEND_MK:=	${ROS_GENLISP_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		ros-genlisp
+DEPEND_PKG+=			ros-genlisp
 endif
 
 ifeq (+,$(ROS_GENLISP_DEPEND_MK)) # ----------------------------------------
@@ -15,19 +15,15 @@ include ../../meta-pkgs/ros-base/depend.common
 PREFER.ros-genlisp?=		${PREFER.ros-base}
 SYSTEM_PREFIX.ros-genlisp?=	${SYSTEM_PREFIX.ros-base}
 
-DEPEND_USE+=		ros-genlisp
+DEPEND_USE+=			ros-genlisp
 
-DEPEND_ABI.ros-genlisp?=ros-genlisp>=0.3.3
-DEPEND_DIR.ros-genlisp?=../../lang/ros-genlisp
+DEPEND_ABI.ros-genlisp?=	ros-genlisp>=0.4<0.5
+DEPEND_DIR.ros-genlisp?=	../../lang/ros-genlisp
 
 SYSTEM_SEARCH.ros-genlisp=\
-	bin/gen_lisp.py						\
-	share/genlisp/cmake/genlisp-config.cmake		\
-	'share/genlisp/stack.xml:/<version>/s/[^0-9.]//gp'	\
+	lib/genlisp/gen_lisp.py					\
+	'share/genlisp/package.xml:/<version>/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/genlisp.pc:/Version/s/[^0-9.]//gp'
-
-export GENLISP_BIN=	${PREFIX.ros-genlisp}/bin/gen_lisp.py
-CMAKE_ARGS+=		-DGENLISP_BIN=${GENLISP_BIN}
 
 endif # ROS_GENLISP_DEPEND_MK ----------------------------------------------
 
