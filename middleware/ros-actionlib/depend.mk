681b67df06f62065515bdb1e0bfc7f44367fe0d0
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-23 12:49:40 +0200

    [middleware/ros-actionlib] Update to 1.9.12
    Local patches are retained as they've not been considered upstream yet.
    
    Changes since 1.8.7:
    
    1.9.12 (2013-06-06)
    -------------------
    * fix location of library before installation
      (`#1 <https://github.com/ros/actionlib/issues/1>`_)
    * define DEPRECATED only if not defined already
    * modified dependency type of catkin to buildtool
    
    1.9.11 (2012-12-13)
    -------------------
    * first public release for Groovy

--- middleware/ros-actionlib/depend.mk
+++ middleware/ros-actionlib/depend.mk
@@ -6,7 +6,7 @@ DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 ROS_ACTIONLIB_DEPEND_MK:=	${ROS_ACTIONLIB_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		ros-actionlib
+DEPEND_PKG+=			ros-actionlib
 endif
 
 ifeq (+,$(ROS_ACTIONLIB_DEPEND_MK)) # --------------------------------------
@@ -15,15 +15,16 @@ include ../../meta-pkgs/ros-base/depend.common
 PREFER.ros-actionlib?=		${PREFER.ros-base}
 SYSTEM_PREFIX.ros-actionlib?=	${SYSTEM_PREFIX.ros-base}
 
-DEPEND_USE+=		ros-actionlib
+DEPEND_USE+=			ros-actionlib
 
-DEPEND_ABI.ros-actionlib?=	ros-actionlib>=1.8.7<1.9
+DEPEND_ABI.ros-actionlib?=	ros-actionlib>=1.9<1.10
 DEPEND_DIR.ros-actionlib?=	../../middleware/ros-actionlib
 
 SYSTEM_SEARCH.ros-actionlib=\
 	include/actionlib/server/action_server.h		\
+	include/actionlib/client/action_client.h		\
 	lib/libactionlib.so					\
-	'share/actionlib/stack.xml:/<version>/s/[^0-9.]//gp'	\
+	'share/actionlib/package.xml:/<version>/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/actionlib.pc:/Version/s/[^0-9.]//gp'
 
 endif # ROS_ACTIONLIB_DEPEND_MK --------------------------------------------
