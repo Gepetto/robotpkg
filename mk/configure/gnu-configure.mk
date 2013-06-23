654b7dff0ed8ba4d843419421db4d53055ef4e5a
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-23 19:54:44 +0200

    [mk] Fix usage of obsolete USE_LANGUAGES variable in gnu-configure targets
    The substitution rule used to prevents errors about the C++ pre-processor (in
    gnu configure scripts) cannot test the value of USE_LANGUAGES anymore.
    
    So instead, check the value of CXXCPP itself and enable the substitution rules
    if it is equal to the default "false".

--- mk/configure/gnu-configure.mk
+++ mk/configure/gnu-configure.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2006,2008-2011 LAAS/CNRS
+# Copyright (c) 2006,2008-2011,2013 LAAS/CNRS
 # All rights reserved.
 #
 # This project includes software developed by the NetBSD Foundation, Inc.
@@ -107,13 +107,11 @@ configure-scripts-override:
 # package has not declared c++ in the USE_LANGUAGES variable and thus no C++
 # cpp is required.
 #
-ifeq (,$(filter c++,${USE_LANGUAGES}))
-  SUBST_CLASSES+=		fixcxxcpp
-  SUBST_STAGE.fixcxxcpp=	do-configure-pre-hook
-  SUBST_MESSAGE.fixcxxcpp=\
+SUBST_CLASSES+=		$(if $(filter ${FALSE},${CXXCPP}),fixcxxcpp)
+SUBST_STAGE.fixcxxcpp=	do-configure-pre-hook
+SUBST_MESSAGE.fixcxxcpp=\
 	Disabling fatal errors with C++ preprocessor in GNU configure scripts
-  SUBST_FILES.fixcxxcpp=	${CONFIGURE_SCRIPT} */${CONFIGURE_SCRIPT}
-  SUBST_SED.fixcxxcpp=\
+SUBST_FILES.fixcxxcpp=	${CONFIGURE_SCRIPT} */${CONFIGURE_SCRIPT}
+SUBST_SED.fixcxxcpp=\
 	-e '/C++ preproc.*fails sanity/,/exit 1/s/exit 1/: /g'	\
 	-e '/fn_error.*C++ preproc.*fails sanity/s/^/: /g'
-endif
