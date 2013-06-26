393591b4fcdad5bb6798fbb917cbf2d02ff16853
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-26 16:55:54 +0200

    [graphics/ros-rqt-common-plugins] Import ros-rqt-common-plugins-0.2.16
    ros-rqt_common_plugins metapackage provides ROS backend graphical tools suite
    that can be used on/off of robot runtime.
    
    Note: this commit also deletes the obsolete graphics/ros-rx.

---
+++ graphics/ros-rqt-common-plugins/depend.mk
@@ -0,0 +1,34 @@
+# robotpkg depend.mk for:	devel/ros-rqt-common-plugins
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+ROS_RQT_COMMON_PLUGINS_DEPEND_MK:=	${ROS_RQT_COMMON_PLUGINS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=				ros-rqt-common-plugins
+endif
+
+ifeq (+,$(ROS_RQT_COMMON_PLUGINS_DEPEND_MK)) # -----------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-rqt-common-plugins?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-rqt-common-plugins?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=				ros-rqt-common-plugins
+
+DEPEND_ABI.ros-rqt-common-plugins?=	ros-rqt-common-plugins>=0.2
+DEPEND_DIR.ros-rqt-common-plugins?=	../../graphics/ros-rqt-common-plugins
+
+SYSTEM_SEARCH.ros-rqt-common-plugins=\
+  'bin/rqt_console'						\
+  'lib/libqt_gui_cpp.so'					\
+  'share/rqt_console/package.xml:/<version>/s/[^0-9.]//gp'	\
+  '${PYTHON_SYSLIBSEARCH}/rqt_console/__init__.py'		\
+  'lib/pkgconfig/rqt_console.pc:/Version/s/[^0-9.]//gp'	
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS_RQT_COMMON_PLUGINS_DEPEND_MK -----------------------------------
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH:+=}
