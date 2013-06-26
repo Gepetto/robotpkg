90b58d29ded503254fbd7e492f0be5bc8734c1b5
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-27 01:20:08 +0200

    [devel/ros-class-loader] Import ros-class-loader-0.1.29
    The class_loader package is a ROS-independent package for loading plugins
    during runtime and the foundation of the higher level ROS "pluginlib"
    library. class_loader utilizes the host operating system's runtime loader to
    open runtime libraries (e.g. .so/.dll files), introspect the library for
    exported plugin classes, and allows users to instantiate objects of said
    exported classes without the explicit declaration (i.e. header file) for those
    classes.

---
+++ devel/ros-class-loader/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	devel/ros-class-loader
+# Created:			Anthony Mallet on Thu, 27 Jun 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_CLASS_LOADER_DEPEND_MK:=	${ROS_CLASS_LOADER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-class-loader
+endif
+
+ifeq (+,$(ROS_CLASS_LOADER_DEPEND_MK)) # -----------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-class-loader?=	${PREFER.ros-base}
+SYSTEM_PREFIX.ros-class-loader?=${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-class-loader
+
+DEPEND_ABI.ros-class-loader?=	ros-class-loader>=0.1
+DEPEND_DIR.ros-class-loader?=	../../devel/ros-class-loader
+
+SYSTEM_SEARCH.ros-class-loader=\
+  'include/class_loader/class_loader.h'				\
+  'lib/libclass_loader.so'					\
+  'share/class_loader/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/class_loader.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_CLASS_LOADER_DEPEND_MK -----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
