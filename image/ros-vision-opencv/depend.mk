9a78f456f3c1bc3ebf78fac8a7a71e6ee1de70c5
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-26 16:54:39 +0200

    [image/ros-vision-opencv] Import ros-vision-opencv-1.10.6
    The vision_opencv stack provides a ROS interface with the popular OpenCV
    library:
        cv_bridge: Bridge between ROS messages and OpenCV.
        image_geometry: Collection of methods for dealing with image and pixel
        geometry

---
+++ image/ros-vision-opencv/depend.mk
@@ -0,0 +1,39 @@
+# robotpkg depend.mk for:	image/ros-vision-opencv
+# Created:			Anthony Mallet on Wed, 26 Jun 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_VISION_OPENCV_DEPEND_MK:=	${ROS_VISION_OPENCV_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-vision-opencv
+endif
+
+ifeq (+,$(ROS_VISION_OPENCV_DEPEND_MK)) # ----------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-vision-opencv?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-vision-opencv?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-vision-opencv
+
+DEPEND_ABI.ros-vision-opencv?=	ros-vision-opencv>=1.10
+DEPEND_DIR.ros-vision-opencv?=	../../image/ros-vision-opencv
+
+SYSTEM_SEARCH.ros-vision-opencv=\
+	include/cv_bridge/cv_bridge.h					\
+	include/image_geometry/pinhole_camera_model.h			\
+	lib/libcv_bridge.so						\
+	lib/libimage_geometry.so					\
+	'share/cv_bridge/package.xml:/<version>/s/[^0-9.]//gp'		\
+	'share/image_geometry/package.xml:/<version>/s/[^0-9.]//gp'	\
+	'${PYTHON_SYSLIBSEARCH}/cv_bridge/__init__.py'			\
+	'${PYTHON_SYSLIBSEARCH}/image_geometry/__init__.py'		\
+	'lib/pkgconfig/cv_bridge.pc:/Version/s/[^0-9.]//gp'		\
+	'lib/pkgconfig/image_geometry.pc:/Version/s/[^0-9.]//gp'
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS_VISION_OPENCV_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
