b85bcc48344d5289e2929f15e7db174e9e87b5c3
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-27 01:37:13 +0200

    [devel/ros-pluginlib] Import ros-pluginlib-1.9.20
    The pluginlib package provides tools for writing and dynamically loading
    plugins using the ROS build infrastructure. To work, these tools require plugin
    providers to register their plugins in the package.xml of their package.

---
+++ devel/ros-pluginlib/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	devel/ros-pluginlib
+# Created:			Anthony Mallet on Thu, 27 Jun 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_PLUGINLIB_DEPEND_MK:=	${ROS_PLUGINLIB_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-pluginlib
+endif
+
+ifeq (+,$(ROS_PLUGINLIB_DEPEND_MK)) # --------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-pluginlib?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-pluginlib?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-pluginlib
+
+DEPEND_ABI.ros-pluginlib?=	ros-pluginlib>=1.9<1.10
+DEPEND_DIR.ros-pluginlib?=	../../devel/ros-pluginlib
+
+SYSTEM_SEARCH.ros-pluginlib=\
+  'bin/plugin_macro_update'					\
+  'include/pluginlib/class_loader.h'				\
+  'share/pluginlib/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/pluginlib.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_PLUGINLIB_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
