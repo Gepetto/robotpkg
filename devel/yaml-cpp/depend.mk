4a38e28eb680f0b0f00858b87d8899bafec670f7
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-27 23:58:25 +0200

    [devel/yaml-cpp] Import yaml-cpp-0.3.0
    yaml-cpp is a YAML parser and emitter in C++ matching the YAML 1.2 spec.
    
    It is not available for Ubuntu<=12.04 and NetBSD, so easier to deal with in a
    robotpkg package than a system one.

---
+++ devel/yaml-cpp/depend.mk
@@ -0,0 +1,37 @@
+# robotpkg depend.mk for:	devel/yaml-cpp
+# Created:			Anthony Mallet on Tue, 18 Oct 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+YAML_CPP_DEPEND_MK:=	${YAML_CPP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		yaml-cpp
+endif
+
+ifeq (+,$(YAML_CPP_DEPEND_MK)) # -------------------------------------------
+
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+
+ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 10.04 12.04,${OS_VERSION}))
+    PREFER.yaml-cpp?=	robotpkg
+  endif
+else ifeq (NetBSD,${OPSYS})
+  PREFER.yaml-cpp?=	robotpkg
+endif
+PREFER.yaml-cpp?=	system
+
+DEPEND_USE+=		yaml-cpp
+
+DEPEND_ABI.yaml-cpp?=	yaml-cpp>=0.2
+DEPEND_DIR.yaml-cpp?=	../../devel/yaml-cpp
+
+SYSTEM_SEARCH.yaml-cpp=\
+	include/yaml-cpp/yaml.h					\
+	'lib/libyaml-cpp.{so,a}'				\
+	'lib/pkgconfig/yaml-cpp.pc:/Version/s/[^0-9.]//gp'
+
+endif # YAML_CPP_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
