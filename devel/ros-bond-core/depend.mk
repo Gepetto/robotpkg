7526a05ae66648aa831109ee28afd54ac8796694
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-28 21:29:16 +0200

    [devel/ros-bond-core] Import ros-bond-core-1.7.12
    A bond allows two processes, A and B, to know when the other has terminated,
    either cleanly or by crashing. The bond remains connected until it is either
    broken explicitly or until a heartbeat times out.
    
    This package provides C++ and python implementations.

---
+++ devel/ros-bond-core/depend.mk
@@ -0,0 +1,33 @@
+# robotpkg depend.mk for:	devel/ros-bond-core
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_BONDCPP_CORE_DEPEND_MK:=	${ROS_BONDCPP_CORE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-bond-core
+endif
+
+ifeq (+,$(ROS_BONDCPP_CORE_DEPEND_MK)) # -----------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-bond-core?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-bond-core?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-bond-core
+
+DEPEND_ABI.ros-bond-core?=	ros-bond-core>=1.7
+DEPEND_DIR.ros-bond-core?=	../../devel/ros-bond-core
+
+SYSTEM_SEARCH.ros-bond-core=\
+  'include/bondcpp/bond.h'						\
+  'lib/libbondcpp.so'							\
+  '${PYTHON_SYSLIBSEARCH}/bondpy/__init__.py'				\
+  'share/bondcpp/package.xml:/<version>/s/[^0-9.]//gp'			\
+  'lib/pkgconfig/bond.pc:/Version/s/[^0-9.]//gp'			\
+  'lib/pkgconfig/bondcpp.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_BONDCPP_CORE_DEPEND_MK ------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
