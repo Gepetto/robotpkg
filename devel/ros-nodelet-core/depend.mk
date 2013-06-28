2de6a2c01401b77812ce3e42838a0fdd393fe1a2
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-28 22:30:28 +0200

    [devel/ros-nodelet-core] Import ros-nodelet-core-1.7.15
    The nodelet package is designed to provide a way to run multiple algorithms in
    the same process with zero copy transport between algorithms. This package
    provides both the nodelet base class needed for implementing a nodelet, as well
    as the NodeletLoader class used for instantiating nodelets.

---
+++ devel/ros-nodelet-core/depend.mk
@@ -0,0 +1,36 @@
+# robotpkg depend.mk for:	devel/ros-nodelet-core
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_NODELET_CORE_DEPEND_MK:=	${ROS_NODELET_CORE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-nodelet-core
+endif
+
+ifeq (+,$(ROS_NODELET_CORE_DEPEND_MK)) # -----------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-nodelet-core?=	${PREFER.ros-base}
+SYSTEM_PREFIX.ros-nodelet-core?=${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-nodelet-core
+
+DEPEND_ABI.ros-nodelet-core?=	ros-nodelet-core>=1.7
+DEPEND_DIR.ros-nodelet-core?=	../../devel/ros-nodelet-core
+
+SYSTEM_SEARCH.ros-nodelet-core=\
+  'include/nodelet/nodelet.h'						\
+  'lib/libnodeletlib.so'						\
+  '${PYTHON_SYSLIBSEARCH}/nodelet/__init__.py'				\
+  'share/nodelet_topic_tools/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'share/nodelet/package.xml:/<version>/s/[^0-9.]//gp'			\
+  'lib/pkgconfig/nodelet_topic_tools.pc:/Version/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/nodelet.pc:/Version/s/[^0-9.]//gp'
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS_NODELET_CORE_DEPEND_MK ------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
