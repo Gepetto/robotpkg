00663e9b994ef564d6739436e1d05b72485b36ee
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-29 13:35:54 +0200

    [graphics/ros-qt-gui-core] Import ros-qt-gui-core-0.2.16
    ROS-specific plugins for a Qt-based GUI.

---
+++ graphics/ros-qt-gui-core/depend.mk
@@ -0,0 +1,45 @@
+# robotpkg depend.mk for:	devel/ros-qt-gui-core
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_QT_GUI_CORE_DEPEND_MK:=	${ROS_QT_GUI_CORE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-qt-gui-core
+endif
+
+ifeq (+,$(ROS_QT_GUI_CORE_DEPEND_MK)) # ------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-qt-gui-core?=	${PREFER.ros-base}
+SYSTEM_PREFIX.ros-qt-gui-core?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-qt-gui-core
+
+DEPEND_ABI.ros-qt-gui-core?=	ros-qt-gui-core>=0.2
+DEPEND_DIR.ros-qt-gui-core?=	../../graphics/ros-qt-gui-core
+
+SYSTEM_SEARCH.ros-qt-gui-core=\
+  'include/qt_gui_cpp/settings.h'				\
+  'lib/libqt_gui_cpp.so'					\
+  'share/qt_dotgraph/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'share/qt_gui/package.xml:/<version>/s/[^0-9.]//gp'		\
+  'share/qt_gui_cpp/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'share/qt_gui_py_common/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'share/qt_gui_app/package.xml:/<version>/s/[^0-9.]//gp'	\
+  '${PYTHON_SYSLIBSEARCH}/qt_dotgraph/__init__.py'		\
+  '${PYTHON_SYSLIBSEARCH}/qt_gui/__init__.py'			\
+  '${PYTHON_SYSLIBSEARCH}/qt_gui_cpp/__init__.py'		\
+  '${PYTHON_SYSLIBSEARCH}/qt_gui_py_common/__init__.py'		\
+  'lib/pkgconfig/qt_dotgraph.pc:/Version/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/qt_gui.pc:/Version/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/qt_gui_cpp.pc:/Version/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/qt_gui_py_common.pc:/Version/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/qt_gui_app.pc:/Version/s/[^0-9.]//gp'
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS_QT_GUI_CORE_DEPEND_MK ------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
