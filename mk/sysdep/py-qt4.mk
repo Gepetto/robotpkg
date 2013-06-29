b2a9300d743eab1e17d8e1f33cc06a87e9544264
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-29 12:02:53 +0200

    [mk/sysdep] Teach robotpkg about PyQt4 (py-qt4)

---
+++ mk/sysdep/py-qt4.mk
@@ -0,0 +1,33 @@
+# robotpkg sysdep/py-qt4.mk
+# Created:			Anthony Mallet on Fri Jun 28 2013
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PY_QT4_DEPEND_MK:=	${PY_QT4_DEPEND_MK}+
+
+include ../../mk/sysdep/python.mk
+
+ifeq (+,$(DEPEND_DEPTH))
+  DEPEND_PKG+=		py-qt4
+endif
+
+ifeq (+,$(PY_QT4_DEPEND_MK)) # ---------------------------------------------
+
+PREFER.py-qt4?=		system
+
+DEPEND_USE+=		py-qt4
+
+DEPEND_ABI.py-qt4?=	${PKGTAG.python}-qt4>=4
+
+SYSTEM_SEARCH.py-qt4=\
+  '${PYTHON_SYSLIBSEARCH}/PyQt4/Qt.so'					\
+  '${PYTHON_SYSLIBSEARCH}/PyQt4/__init__.py'				\
+  '${PYTHON_SYSLIBSEARCH}/PyQt4/pyqtconfig.py:/pyqt_version_str/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Debian.py-qt4=	python-qt4 (python-${PYTHON_VERSION})
+SYSTEM_PKG.Fedora.py-qt4=	python-qt4 (python-${PYTHON_VERSION})
+SYSTEM_PKG.NetBSD.py-qt4=	x11/${PKGTAG.python}-qt4
+SYSTEM_PKG.Ubuntu.py-qt4=	python-qt4 (python-${PYTHON_VERSION})
+
+endif # PY_QT4_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
