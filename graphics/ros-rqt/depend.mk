83af7f4fdeeeb87178b219d9f555dc7e06263c17
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-29 16:35:29 +0200

    [graphics/ros-rqt] Import ros-rqt-0.2.9
    rqt is a Qt-based framework for GUI development for ROS. It consists of three
    parts:
    
    rqt provides a widget rqt_gui that enables multiple `rqt` widgets to be
    docked in a single window.
    
    rqt_common_plugins - ROS backend tools suite that can be used on/off of
    robot runtime.
    
    rqt_robot_plugins - Tools for interacting with robots during their runtime.

---
+++ graphics/ros-rqt/depend.mk
@@ -0,0 +1,40 @@
+# robotpkg depend.mk for:	devel/ros-rqt
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ROS_RQT_DEPEND_MK:=	${ROS_RQT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		ros-rqt
+endif
+
+ifeq (+,$(ROS_RQT_DEPEND_MK)) # --------------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-rqt?=	${PREFER.ros-base}
+SYSTEM_PREFIX.ros-rqt?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=		ros-rqt
+
+DEPEND_ABI.ros-rqt?=	ros-rqt>=0.2
+DEPEND_DIR.ros-rqt?=	../../graphics/ros-rqt
+
+SYSTEM_SEARCH.ros-rqt=\
+  'bin/rqt'							\
+  'include/rqt_gui_cpp/plugin.h'				\
+  'lib/librqt_gui_cpp.so'					\
+  'share/rqt_gui/package.xml:/<version>/s/[^0-9.]//gp'		\
+  'share/rqt_gui_cpp/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'share/rqt_gui_py/package.xml:/<version>/s/[^0-9.]//gp'	\
+  '${PYTHON_SYSLIBSEARCH}/rqt_gui/__init__.py'			\
+  '${PYTHON_SYSLIBSEARCH}/rqt_gui_py/__init__.py'		\
+  'lib/pkgconfig/rqt_gui.pc:/Version/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/rqt_gui_cpp.pc:/Version/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/rqt_gui_py.pc:/Version/s/[^0-9.]//gp'
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS_RQT_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
