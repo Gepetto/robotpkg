0f0be8011ca79edc62ba616c3dd2bd205ae440dc
Anthony Mallet <anthony.mallet@laas.fr>
2013-06-29 11:59:53 +0200

    [graphics/ros-python-qt-binding] Import ros-python-qt-binding-0.2.10
    This stack provides Python bindings for Qt.  There are two providers: pyside
    and pyqt.  PySide is released under the LGPL.  PyQt is released under the GPL.
    
    Also provided is adapter code to make the user's Python code independent of
    which binding provider was actually used which makes it very easy to switch
    between these.
    
    Currently, robotpkg disables PySide. Enabling it simply requires some more
    mk/sysdep files and the definition of an alternative for py-qt bindings.
    Later...

---
+++ graphics/ros-python-qt-binding/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	devel/ros-python-qt-binding
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+ROS_PYTHON_QT_BINDING_DEPEND_MK:=	${ROS_PYTHON_QT_BINDING_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=				ros-python-qt-binding
+endif
+
+ifeq (+,$(ROS_PYTHON_QT_BINDING_DEPEND_MK)) # ------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-python-qt-binding?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-python-qt-binding?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=				ros-python-qt-binding
+
+DEPEND_ABI.ros-python-qt-binding?=	ros-python-qt-binding>=0.2.10
+DEPEND_DIR.ros-python-qt-binding?=	../../graphics/ros-python-qt-binding
+
+SYSTEM_SEARCH.ros-python-qt-binding=\
+  'share/python_qt_binding/package.xml:/<version>/s/[^0-9.]//gp'	\
+  '${PYTHON_SYSLIBSEARCH}/python_qt_binding/__init__.py'		\
+  'lib/pkgconfig/python_qt_binding.pc:/Version/s/[^0-9.]//gp'
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS_PYTHON_QT_BINDING_DEPEND_MK ------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
