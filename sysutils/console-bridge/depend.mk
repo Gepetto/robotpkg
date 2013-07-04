a2e885bf7578fd2935ce5b85e61abfa329af1a55
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-04 22:42:21 +0200

    [sysutils/console-bridge] Rename ros-console-bridge to console-bridge
    This package was wrongly imported as ros-console-bridge; it should have been
    console-bridge. Although it is provided by ros, this is a ros-independant
    package... And there is actually a different ROS package called
    rosconsole-bridge that do depend on ROS.
    
    Beware, the distfile has also changed. The former one was the ros-ified version
    of console-bridge, available on ros-gbp (that defeats the purpose of having a
    ros-independant package) and the new one is the ros-free one, available on ros.

---
+++ sysutils/console-bridge/depend.mk
@@ -0,0 +1,30 @@
+# robotpkg depend.mk for:	sysutils/console-bridge
+# Created:			Anthony Mallet on Thu,  4 Jul 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+CONSOLE_BRIDGE_DEPEND_MK:=	${CONSOLE_BRIDGE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			console-bridge
+endif
+
+ifeq (+,$(CONSOLE_BRIDGE_DEPEND_MK)) # -------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.console-bridge?=		${PREFER.ros-base}
+SYSTEM_PREFIX.console-bridge?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			console-bridge
+
+DEPEND_ABI.console-bridge?=	console-bridge>=0.2
+DEPEND_DIR.console-bridge?=	../../sysutils/console-bridge
+
+SYSTEM_SEARCH.console-bridge=\
+  'include/console_bridge/console.h'				\
+  'lib/libconsole_bridge.so'					\
+  'lib/pkgconfig/console_bridge.pc:/Version/s/[^0-9.]//gp'
+
+endif # CONSOLE_BRIDGE_DEPEND_MK -------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
