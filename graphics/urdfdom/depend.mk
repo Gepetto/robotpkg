0b1cd95d0ef42cb3da56d54b0fb153d6092a8a8f
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-04 18:38:59 +0200

    [graphics/urdfdom] Import urdfdom-0.2.8
    The URDF (U-Robot Description Format) library
      provides core data structures and a simple XML parsers
      for populating the class data structures from an URDF file.
    
    For now, the details of the URDF specifications reside on
      http://ros.org/wiki/urdf

---
+++ graphics/urdfdom/depend.mk
@@ -0,0 +1,30 @@
+# robotpkg depend.mk for:	graphics/urdfdom
+# Created:			Anthony Mallet on Thu,  4 Jul 2013
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+URDFDOM_DEPEND_MK:=	${URDFDOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		urdfdom
+endif
+
+ifeq (+,$(URDFDOM_DEPEND_MK)) # --------------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.urdfdom?=	${PREFER.ros-base}
+SYSTEM_PREFIX.urdfdom?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=		urdfdom
+
+DEPEND_ABI.urdfdom?=	urdfdom>=0.2.8
+DEPEND_DIR.urdfdom?=	../../graphics/urdfdom
+
+SYSTEM_SEARCH.urdfdom=\
+  'include/urdf_parser/urdf_parser.h'				\
+  'lib/liburdfdom_model.so'					\
+  'lib/pkgconfig/urdfdom.pc:/Version/s/[^0-9.]//gp'
+
+endif # URDFDOM_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
