19d7934d5ee57621e8399263f90ccf1a31faaba9
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-04 17:51:10 +0200

    [math/ros-angles] Import ros-angles-1.9.9
    This package provides a set of simple math utilities to work with angles. The
    utilities cover simple things like normalizing an angle and conversion between
    degrees and radians. But even if you're trying to calculate things like the
    shortest angular distance between two joint space positions of your robot, but
    the joint motion is constrained by joint limits, this package is what you
    need.

---
+++ math/ros-angles/depend.mk
@@ -0,0 +1,30 @@
+# robotpkg depend.mk for:	math/ros-angles
+# Created:			Anthony Mallet on Thu,  4 Jul 2013
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ROS_ANGLES_DEPEND_MK:=	${ROS_ANGLES_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		ros-angles
+endif
+
+ifeq (+,$(ROS_ANGLES_DEPEND_MK)) # -------------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-angles?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-angles?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=		ros-angles
+
+DEPEND_ABI.ros-angles?=	ros-angles>=1.9
+DEPEND_DIR.ros-angles?=	../../math/ros-angles
+
+SYSTEM_SEARCH.ros-angles=\
+  'include/angles/angles.h'					\
+  'share/angles/package.xml:/<version>/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/angles.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_ANGLES_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
