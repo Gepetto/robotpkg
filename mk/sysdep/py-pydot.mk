2bfc2402c57e3a75a4236851763c8011539d82a7
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-04 14:32:40 +0200

    [mk/sysdep] Fix Fedora package name for py-pydot

--- mk/sysdep/py-pydot.mk
+++ mk/sysdep/py-pydot.mk
@@ -18,7 +18,7 @@ DEPEND_ABI.py-pydot?=	${PKGTAG.python-}pydot>=1
 SYSTEM_SEARCH.py-pydot=\
   '${PYTHON_SYSLIBSEARCH}/{,python-pydot/}pydot.py:/__version__/s/[^0-9.]//gp'
 
-SYSTEM_PKG.Fedora.py-pydot=	python-pydot (python-${PYTHON_VERSION})
+SYSTEM_PKG.Fedora.py-pydot=	pydot (python-${PYTHON_VERSION})
 SYSTEM_PKG.Ubuntu.py-pydot=	python-pydot (python-${PYTHON_VERSION})
 SYSTEM_PKG.Debian.py-pydot=	python-pydot (python-${PYTHON_VERSION})
 SYSTEM_PKG.NetBSD.py-pydot=	wip/${PKGTAG.python-}pydot
