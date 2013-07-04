f1ff208410a647f8da973607eb887e40f074d9c7
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-04 13:40:29 +0200

    [simulation/bullet] Impor bullet-2.81
    Bullet is a 3D Collision Detection and Rigid Body Dynamics Library
    for games and animation.  Free for commercial use, including
    Playstation 3, open source under the ZLib License.  Discrete and
    continuous collision detection, integrated into Blender 3D, and
    COLLADA 1.4 Physics import.

---
+++ simulation/bullet/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	simulation/bullet
+# Created:			Anthony Mallet on Thu,  4 Jul 2013
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+BULLET_DEPEND_MK:=	${BULLET_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		bullet
+endif
+
+ifeq (+,$(BULLET_DEPEND_MK)) # ---------------------------------------------
+
+PREFER.bullet?=		robotpkg
+
+SYSTEM_SEARCH.bullet=\
+  'include/bullet/Bullet-C-Api.h'			\
+  'lib/libBulletDynamics.{so,a}'			\
+  'lib/pkgconfig/bullet.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_USE+=		bullet
+
+DEPEND_ABI.bullet?=	bullet>=2.75
+DEPEND_DIR.bullet?=	../../simulation/bullet
+
+endif # BULLET_DEPEND_MK ---------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
