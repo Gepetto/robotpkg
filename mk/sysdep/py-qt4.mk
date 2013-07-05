03da8414c3ea277298295d5353adb988fc27fc83
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-05 11:11:57 +0200

    [mk/sysdep] Improve py-qt4 sysdep
    - Check one more file provided by -dev or -devel packages.
    - Debians need the -dev version of the package.

--- mk/sysdep/py-qt4.mk
+++ mk/sysdep/py-qt4.mk
@@ -19,14 +19,15 @@ DEPEND_USE+=		py-qt4
 DEPEND_ABI.py-qt4?=	${PKGTAG.python}-qt4>=4
 
 SYSTEM_SEARCH.py-qt4=\
+  'share/sip{,${PYTHON_VERSION}}/{,PyQt4}/QtCore/QtCoremod.sip'		\
   '${PYTHON_SYSLIBSEARCH}/PyQt4/Qt.so'					\
   '${PYTHON_SYSLIBSEARCH}/PyQt4/__init__.py'				\
   '${PYTHON_SYSLIBSEARCH}/PyQt4/pyqtconfig.py:/pyqt_version_str/s/[^0-9.]//gp'
 
-SYSTEM_PKG.Debian.py-qt4=	python-qt4 (python-${PYTHON_VERSION})
+SYSTEM_PKG.Debian.py-qt4=	python-qt4-dev (python-${PYTHON_VERSION})
 SYSTEM_PKG.Fedora.py-qt4=	PyQt4-devel (python-${PYTHON_VERSION})
 SYSTEM_PKG.NetBSD.py-qt4=	x11/${PKGTAG.python}-qt4
-SYSTEM_PKG.Ubuntu.py-qt4=	python-qt4 (python-${PYTHON_VERSION})
+SYSTEM_PKG.Ubuntu.py-qt4=	python-qt4-dev (python-${PYTHON_VERSION})
 
 endif # PY_QT4_DEPEND_MK ----------------------------------------------
 
