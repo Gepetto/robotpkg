5a31ac08ba5452a63e3be1eaad40a3e7a712d4ec
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-16 11:31:34 +0200

    [pkgtools/pkgrepo2deb] Update to 0.9.20130715
    Changes since 0.9.20130712:
     - Add incremental update of Packages{,.gz}
     - Fix temporary directories sometimes not removed
     - Add dependency on gnupg.
    
    Also converted the package to a NO_CHECKSUM/NO_DISTFILES one to avoid circular
    dependencies with base packages pkg_install/digest/tnftp. pkg_install
    also needs a special depend.mk with an 'inplace' build strategy. Too bad that
    all these hacks are required only because of 3 packages.

--- pkgtools/pkgrepo2deb/depend.mk
+++ pkgtools/pkgrepo2deb/depend.mk
@@ -5,22 +5,44 @@
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 PKGREPO2DEB_DEPEND_MK:=	${PKGREPO2DEB_DEPEND_MK}+
 
-ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		pkgrepo2deb
-endif
-
 ifeq (+,$(PKGREPO2DEB_DEPEND_MK)) # ----------------------------------------
+  PREFER.pkgrepo2deb?=		robotpkg
 
-DEPEND_USE+=		pkgrepo2deb
+  DEPEND_ABI.pkgrepo2deb?=	pkgrepo2deb>=0.9.20130712
+  DEPEND_DIR.pkgrepo2deb?=	../../pkgtools/pkgrepo2deb
 
-PREFER.pkgrepo2deb?=	robotpkg
-
-DEPEND_ABI.pkgrepo2deb?=pkgrepo2deb>=0.9.20130712
-DEPEND_DIR.pkgrepo2deb?=../../pkgtools/pkgrepo2deb
-
-SYSTEM_SEARCH.pkgrepo2deb=	\
-  'sbin/pkgrepo2deb:p:% --version'
-
+  SYSTEM_SEARCH.pkgrepo2deb=	\
+    'sbin/pkgrepo2deb:p:% --version'
 endif # PKGREPO2DEB_DEPEND_MK ----------------------------------------------
+
+# pull-in the user preferences for inplace+robotpkg check
+include ../../mk/robotpkg.prefs.mk
+_pkgrepo2deb_style=$(strip ${PKGREPO2DEB_STYLE})+$(strip ${PREFER.pkgrepo2deb})
+
+ifneq (inplace+robotpkg,${_pkgrepo2deb_style})
+  # This is the regular version, for normal install
+  #
+  ifeq (+,$(DEPEND_DEPTH))
+    DEPEND_PKG+=		pkgrepo2deb
+  endif
+
+  ifeq (+,$(PKGREPO2DEB_DEPEND_MK))
+    DEPEND_USE+=		pkgrepo2deb
+  endif
+
+  export PKGREPO2DEB?=	${PREFIX.pkgrepo2deb}/sbin/pkgrepo2deb
+else
+  # This is the "inplace" version, for bootstrap process
+  #
+  pre-package: pkgrepo2deb-inplace
+
+  .PHONY: pkgrepo2deb-inplace
+  pkgrepo2deb-inplace:
+	@${STEP_MSG} "Building pkgrepo2deb in place"
+	${RUN} cd ${ROBOTPKG_DIR}/${PKGPATH}/${DEPEND_DIR.pkgrepo2deb};	\
+	${RECURSIVE_MAKE} WRKDIR=${WRKDIR}/pkgrepo2deb build
+
+  export PKGREPO2DEB=	${SH} ${WRKDIR}/pkgrepo2deb/pkgrepo2deb/pkgrepo2deb
+endif
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
