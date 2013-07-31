b94cb4c6140381ec68c26bafe96fe304f5087730
Florent Lamiraux <florent@laas.fr>
2013-07-31 08:27:05 +0200

    [path/hpp-model-urdf] New package
      This package implements a library that allows users to load an hpp-model
      robot by parsing an urdf robot model file.

---
+++ path/hpp-model-urdf/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	path/hpp-model-urdf
+# Created:			Florent Lamiraux on Tue, 30 Jul 2013
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HPP_MODEL_URDF_DEPEND_MK:=	${HPP_MODEL_URDF_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		hpp-model-urdf
+endif
+
+ifeq (+,$(HPP_MODEL_URDF_DEPEND_MK)) # --------------------------------------
+
+PREFER.hpp-model-urdf?=	robotpkg
+
+DEPEND_USE+=		hpp-model-urdf
+
+DEPEND_ABI.hpp-model-urdf?=	hpp-model-urdf>=0.2
+DEPEND_DIR.hpp-model-urdf?=	../../path/hpp-model-urdf
+
+SYSTEM_SEARCH.hpp-model-urdf=			\
+	lib/libhpp-model-urdf.so		\
+	include/hpp/model/urdf/parser.hh	\
+	'lib/pkgconfig/hpp-model-urdf.pc:/Version/s/[^0-9.]//gp'
+
+endif # HPP_MODEL_URDF_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
