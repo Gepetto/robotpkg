1a3a712f8021f2358f4b8135711e94910025f8cd
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-31 17:41:27 +0200

    [archivers/pax] Provide an "inplace" build of pax, for bootstrap
    The inplace version is only used if pax is configured with PREFER.pax =
    robotpkg. This is useful to avoid a circular dependency between pax and
    pkg_install (reported by Raphael Lallement, thanks!).

--- archivers/pax/depend.mk
+++ archivers/pax/depend.mk
@@ -5,10 +5,6 @@
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 PAX_DEPEND_MK:=		${PAX_DEPEND_MK}+
 
-ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		pax
-endif
-
 ifeq (+,$(PAX_DEPEND_MK)) # ------------------------------------------------
 
 PREFER.pax?=		system
@@ -22,15 +18,45 @@ SYSTEM_PKG.Fedora.pax=	pax tar
 SYSTEM_PKG.Ubuntu.pax=	pax tar
 SYSTEM_PKG.Debian.pax=	pax tar
 
-DEPEND_USE+=		pax
-
 DEPEND_METHOD.pax?=	build
 DEPEND_ABI.pax?=	pax
 DEPEND_DIR.pax?=	../../archivers/pax
 
-export PAX=		$(word 1,${SYSTEM_FILES.pax})
-export TAR=		$(word 2,${SYSTEM_FILES.pax})
-
 endif # PAX_DEPEND_MK ------------------------------------------------------
+
+# pull-in the user preferences for inplace+robotpkg check
+include ../../mk/robotpkg.prefs.mk
+_pax_style=$(strip ${PAX_STYLE})+$(strip ${PREFER.pax})
+
+ifneq (inplace+robotpkg,${_pax_style})
+  # This is the regular version, for normal install
+  #
+  ifeq (+,$(DEPEND_DEPTH))
+    DEPEND_PKG+=		pax
+  endif
+
+  ifeq (+,$(PKGREPO2DEB_DEPEND_MK))
+    DEPEND_USE+=		pax
+  endif
+
+  export PAX=		$(word 1,${SYSTEM_FILES.pax})
+  export TAR=		$(word 2,${SYSTEM_FILES.pax})
+else
+  # This is the "inplace" version, for bootstrap process
+  #
+  pre-configure: pax-inplace
+
+  .PHONY: pax-inplace
+  pax-inplace:
+	@${STEP_MSG} "Building pax in place"
+	${RUN} cd ${ROBOTPKG_DIR}/${PKGPATH}/${DEPEND_DIR.pax};		\
+	${RECURSIVE_MAKE} build						\
+	  WRKDIR=${WRKDIR}/pax						\
+	  NO_PKGTOOLS_REQD_CHECK=yes;					\
+	${LN} -s pax ${WRKDIR}/pax/pax-20080110/tar
+
+  export PAX=		${WRKDIR}/pax/pax-20080110/pax
+  export TAR=		${WRKDIR}/pax/pax-20080110/tar
+endif
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
