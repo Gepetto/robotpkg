0e54375af14268cbc06c976af53250676c6e8504
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-08-06 15:05:02 +0200

    [supervision/ros-executive-smach] Import smach-1.3.0
    A procedural python-based task execution framework with ROS integration.
    
    Note that this version is *not* the official ROS Groovy one (which was
    1.2.2).
    The main diff (besides one or two bugfixes) is the catkinization. Hence
    we include it here.

---
+++ supervision/ros-executive-smach/depend.mk
@@ -0,0 +1,38 @@
+# robotpkg depend.mk for:	supervision/ros-executive-smach
+# Created:			Séverin Lemaignan on Tue, 06 Aug 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_EXECUTIVE_SMACH_DEPEND_MK:=	${ROS_EXECUTIVE_SMACH_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-executive-smach
+endif
+
+ifeq (+,$(ROS_EXECUTIVE_SMACH_DEPEND_MK)) # --------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+include ../../mk/sysdep/python.mk
+
+PREFER.ros-executive-smach?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-executive-smach?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-executive-smach
+ROS_DEPEND_USE+=		ros-executive-smach
+
+DEPEND_ABI.ros-executive-smach?=	ros-executive-smach>=1.2
+DEPEND_DIR.ros-executive-smach=	../../supervision/ros-executive-smach
+
+DEPEND_ABI.ros-executive-smach.fuerte+=	ros-executive-smach>=1.2<1.2.2
+DEPEND_ABI.ros-executive-smach.groovy+=	ros-executive-smach>=1.2.2<=1.3.0
+DEPEND_ABI.ros-executive-smach.hydro+=	ros-executive-smach>=1.3.1<1.4
+
+SYSTEM_SEARCH.ros-executive-smach=\
+	${PYTHON_SITELIB}/smach/__init__.py		\
+	'share/executive_smach/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
+	'share/smach_msgs/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
+	'lib/pkgconfig/smach_msgs.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_EXECUTIVE_SMACH_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
