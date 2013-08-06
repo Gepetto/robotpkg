48b6baf5abca5e72c8e4ad63fc94f3cc5f502082
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-08-06 11:58:50 +0200

    [graphics] Import ros-convex-decomposition-0.1.9
    convex_decomposition contains the Convex Mesh Generation Library, by
    John Ratcliff. This library is used to auto-generate convex decomposed
    meshes for collision and visualization of robot links.
    
    ROS package that wraps (with some patches) the original project:
    http://www.amillionpixels.us/ConvexDecomposition.zip

---
+++ graphics/ros-convex-decomposition/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	graphics/ros-convex-decomposition
+# Created:			Séverin Lemaignan on Tue, 06 Aug 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_CONVEX_DECOMPOSITION_DEPEND_MK:=	${ROS_CONVEX_DECOMPOSITION_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-convex-decomposition
+endif
+
+ifeq (+,$(ROS_CONVEX_DECOMPOSITION_DEPEND_MK)) # --------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-convex-decomposition?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-convex-decomposition?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-convex-decomposition
+ROS_DEPEND_USE+=		ros-convex-decomposition
+
+DEPEND_ABI.ros+= ros>=groovy
+DEPEND_ABI.ros-convex-decomposition?=	ros-convex-decomposition>=0.1
+DEPEND_DIR.ros-convex-decomposition=	../../graphics/ros-convex-decomposition
+
+SYSTEM_SEARCH.ros-convex-decomposition=\
+	bin/convex_decomposition		\
+	'share/convex_decomposition/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
+
+endif # ROS_CONVEX_DECOMPOSITION_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
