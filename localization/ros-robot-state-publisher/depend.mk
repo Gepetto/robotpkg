def8f14dbd1339e997725a3e852e39406b8df7df
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-08-06 10:17:00 +0200

    [localization] Import ros-robot-state-publisher-1.9.9
    This ROS package allows to publish the state of a robot to tf. Once the
    state gets published, it is available to all components in the system
    that also use tf. The package takes the joint angles of the robot, as
    well as a kinematic description (URDF file), as input and publishes the
    3D poses of the robot links, using a kinematic tree model of the robot.
    The package can both be used as a library and as a ROS node.

---
+++ localization/ros-robot-state-publisher/depend.mk
@@ -0,0 +1,34 @@
+# robotpkg depend.mk for:	localization/ros-robot-state-publisher
+# Created:			Séverin Lemaignan on Tue, 06 Aug 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_ROBOT_STATE_PUBLISHER_DEPEND_MK:=	${ROS_ROBOT_STATE_PUBLISHER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-robot-state-publisher
+endif
+
+ifeq (+,$(ROS_ROBOT_STATE_PUBLISHER_DEPEND_MK)) # --------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-robot-state-publisher?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-robot-state-publisher?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-robot-state-publisher
+ROS_DEPEND_USE+=		ros-robot-state-publisher
+
+DEPEND_ABI.ros-robot-state-publisher?=	ros-robot-state-publisher>=1.9
+DEPEND_DIR.ros-robot-state-publisher=	../../localization/ros-robot-state-publisher
+
+DEPEND_ABI.ros += ros>=groovy
+
+SYSTEM_SEARCH.ros-robot-state-publisher=\
+	include/robot_state_publisher/robot_state_publisher.h		\
+	lib/robot_state_publisher/robot_state_publisher					\
+	'share/robot_state_publisher/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
+	'lib/pkgconfig/robot_state_publisher.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_ROBOT_STATE_PUBLISHER_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
