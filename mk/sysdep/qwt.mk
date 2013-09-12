ce7bd39d5b4cb2ac38d43305a479ec6002425b8b
Anthony Mallet <anthony.mallet@laas.fr>
2013-09-12 17:14:54 +0200

    [mk/sysdep] Teach robotpkg about qwt
    Qwt is an extension to the Qt GUI library from Troll Tech AS.
    The Qwt library contains widgets and components which are
    primarily useful for technical and scientific purposes.
    It includes a 2-D plotting widget, different kinds of sliders,
    and much more.

---
+++ mk/sysdep/qwt.mk
@@ -0,0 +1,30 @@
+# robotpkg sysdep/qwt.mk
+# Created:			Anthony Mallet on Thu Sep 12, 2013
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+QWT_DEPEND_MK:=		${QWT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		qwt
+endif
+
+ifeq (+,$(QWT_DEPEND_MK)) # ------------------------------------------------
+
+PREFER.qwt?=		system
+
+DEPEND_USE+=		qwt
+DEPEND_ABI.qwt?=	qwt>=5
+
+SYSTEM_SEARCH.qwt=\
+  'include/{,qwt/}qwt_global.h:/QWT_VERSION_STR/s/[^0-9.]//gp'		\
+  'lib/libqwt.so'
+
+SYSTEM_PKG.Fedora.qwt=	qwt-devel
+SYSTEM_PKG.Debian.qwt=	libqwt-dev
+SYSTEM_PKG.NetBSD.qwt=	x11/qwt-qt4
+
+include ../../mk/sysdep/qt4-libs.mk
+
+endif # QWT_DEPEND_MK ------------------------------------------------------
+
+DEPEND_DEPTH:= ${DEPEND_DEPTH:+=}
