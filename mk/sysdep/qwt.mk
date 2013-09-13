2579efaccfc58a54954c69203cd7c8ce15682f11
Anthony Mallet <anthony.mallet@laas.fr>
2013-09-13 10:44:14 +0200

    [mk/sysdep] Make qwt.mk compatible with debian qwt5 package

--- mk/sysdep/qwt.mk
+++ mk/sysdep/qwt.mk
@@ -16,11 +16,11 @@ DEPEND_USE+=		qwt
 DEPEND_ABI.qwt?=	qwt>=5
 
 SYSTEM_SEARCH.qwt=\
-  'include/{,qwt/}qwt_global.h:/QWT_VERSION_STR/s/[^0-9.]//gp'		\
-  'lib/libqwt.so'
+  'include/{,qwt{,-qt[0-9]}/}qwt_global.h:/QWT_VERSION_STR/s/[^0-9.]//gp' \
+  'lib/libqwt{,-qt[0-9]}.so{.[0-9]*,}:s/.*[.]so[.]*//p:$${ECHO} %'
 
-SYSTEM_PKG.Fedora.qwt=	qwt-devel
 SYSTEM_PKG.Debian.qwt=	libqwt-dev
+SYSTEM_PKG.Fedora.qwt=	qwt-devel
 SYSTEM_PKG.NetBSD.qwt=	x11/qwt-qt4
 
 include ../../mk/sysdep/qt4-libs.mk
