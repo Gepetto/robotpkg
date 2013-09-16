e725084d9ee8057918865917d61a5df8b9bd1493
Anthony Mallet <anthony.mallet@laas.fr>
2013-09-16 13:27:25 +0200

    Revert "[mk/sysdep] Updating qwt.mk for last version of qwt"
    This reverts commit dd811c4ab5d13841581d88591c6524979ea46403.

--- mk/sysdep/qwt.mk
+++ mk/sysdep/qwt.mk
@@ -13,17 +13,17 @@ ifeq (+,$(QWT_DEPEND_MK)) # ------------------------------------------------
 PREFER.qwt?=		system
 
 DEPEND_USE+=		qwt
-DEPEND_ABI.qwt?=	qwt>=6
+DEPEND_ABI.qwt?=	qwt>=5
 
 SYSTEM_SEARCH.qwt=\
-  include/qwt/qwt.h\
-  lib/libqwt.so\
+  'include/{,qwt{,-qt[0-9]}/}qwt_global.h:/QWT_VERSION_STR/s/[^0-9.]//gp' \
+  'lib/libqwt{,-qt[0-9]}.so{.[0-9]*,}:s/.*[.]so[.]*//p:$${ECHO} %'
 
-SYSTEM_PKG.Fedora.qwt=qwt
-SYSTEM_PKG.Ubuntu.qwt=libqwt-dev
-SYSTEM_PKG.Debian.qwt=libqwt-dev
-SYSTEM_PKG.NetBSD.qwt=x11/qwt
+SYSTEM_PKG.Debian.qwt=	libqwt-dev
+SYSTEM_PKG.Fedora.qwt=	qwt-devel
+SYSTEM_PKG.NetBSD.qwt=	x11/qwt-qt4
 
+include ../../mk/sysdep/qt4-libs.mk
 
 endif # QWT_DEPEND_MK ------------------------------------------------------
 
