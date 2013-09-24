a6d0dfea9871d0f88f32064d62445ee52f2c6f70
Anthony Mallet <anthony.mallet@laas.fr>
2013-09-24 17:44:13 +0200

    [image/opencv] Fix depend.mk for PREFER.opencv = system
    Commit f67e0cc was wrong: old opencv headers are still installed by modern
    opencv. This is just the debian package that is split in a weird manner and
    both libopencv-dev and libcv-dev must be installed. Both are needed since most
    of the packages using opencv never bothered with updating their includes path
    to opencv2.
    
    So re-add include/opencv in the SYSTEM_SEARCH, and while here mention the
    SYSTEM_PKG names for supported platforms.

--- image/opencv/depend.mk
+++ image/opencv/depend.mk
@@ -18,8 +18,14 @@ DEPEND_ABI.opencv?=	opencv>=2.2.0
 DEPEND_DIR.opencv?=	../../image/opencv
 
 SYSTEM_SEARCH.opencv=\
-	include/opencv2/core/version.hpp		\
-	'lib/pkgconfig/opencv.pc:/Version/s/[^.0-9]//gp'
+  'include/opencv/cv.h'					\
+  'include/opencv2/core/version.hpp'			\
+  'lib/pkgconfig/opencv.pc:/Version/s/[^.0-9]//gp'
+
+SYSTEM_PKG.Debian=	libopencv-dev libcv-dev
+SYSTEM_PKG.Fedora=	opencv-devel
+SYSTEM_PKG.Gentoo=	media-libs/opencv
+SYSTEM_PKG.NetBSD=	graphics/opencv
 
 endif # OPENCV_DEPEND_MK ---------------------------------------------------
 
