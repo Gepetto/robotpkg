609ec4425ad92cb24fb15503345a58e6bbcb2f20
Anthony Mallet <anthony.mallet@laas.fr>
2013-09-25 17:09:02 +0200

    [image/viam-libs] Update to 1.12
    Changes since 1.11:
    . Fix signed vs. unsigned char and opencv IplImage vs. private IplImage
    . Add missing IPL_DEPTH_32F definition in case of disabled opencv support
    . Split the .pc file into opencv-dependant and -independant parts
    . Fix a %d format for size_t (via a cast to unsigned)
    . Fix OPENCV_LIBS for some opencv distributions (with broken .pc)

--- image/viam-libs/depend.mk
+++ image/viam-libs/depend.mk
@@ -9,17 +9,20 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		viam-libs
 endif
 
-ifeq (+,$(VIAMLIBS_DEPEND_MK)) # -------------------------------------
+ifeq (+,$(VIAMLIBS_DEPEND_MK)) # -------------------------------------------
+
 PREFER.viam-libs?=	robotpkg
 
 DEPEND_USE+=		viam-libs
 
-DEPEND_ABI.viam-libs?=	viam-libs>=1.9
+DEPEND_ABI.viam-libs?=	viam-libs>=1.12
 DEPEND_DIR.viam-libs?=	../../image/viam-libs
 
 SYSTEM_SEARCH.viam-libs=\
-	include/viam/viamlib.h		\
-	lib/pkgconfig/viam-libs.pc
-endif # VIAMLIBS_DEPEND_MK -------------------------------------------
+  'include/viam/viamlib.h'						\
+  'lib/pkgconfig/viam-libs.pc:/Version/s/[^0-9.]//gp'			\
+  'lib/pkgconfig/viam-libs-cv.pc:/Version/s/[^0-9.]//gp::opencv'
+
+endif # VIAMLIBS_DEPEND_MK -------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
