037e36f894ab85bc2b5ee741922b8df75ff6b564
Anthony Mallet <anthony.mallet@laas.fr>
2013-09-25 17:27:09 +0200

    [image/viam-genom] Update to 1.10
    Changes since 1.9:
            41. Drop useless EXTRA_INCLUDES/LIBS in codels Makefile
            40. Fix require: and codels-require: fields
    
    Changes in the package:
     . Fix dependencies: the codels require viam-libs~opencv, which in turn
       requires opencv (the latter cannot be tracked by viam-libs/depend.mk because
       of the optional nature of the option)
    
     . Properly track interface dependencies in the depend.mk (namely: viam-libs +
       pom-genom).
    
     . Add a version check against the viam.pc file in the depend.mk

--- image/viam-genom/depend.mk
+++ image/viam-genom/depend.mk
@@ -9,7 +9,8 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		viam-genom
 endif
 
-ifeq (+,$(VIAMGENOM_DEPEND_MK))
+ifeq (+,$(VIAMGENOM_DEPEND_MK)) # ------------------------------------------
+
 PREFER.viam-genom?=	robotpkg
 
 DEPEND_USE+=		viam-genom
@@ -18,11 +19,13 @@ DEPEND_ABI.viam-genom?=	viam-genom>=1.6
 DEPEND_DIR.viam-genom?=	../../image/viam-genom
 
 SYSTEM_SEARCH.viam-genom=\
-	include/viam/viamStruct.h		\
-	lib/pkgconfig/viam.pc
+ 'include/viam/viamStruct.h'			\
+ 'lib/pkgconfig/viam.pc:/Version/s/[^0-9]//gp'
 
 include ../../architecture/genom/depend.mk
+include ../../image/viam-libs/depend.mk
+include ../../localization/pom-genom/depend.mk
 
-endif
+endif # --------------------------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
