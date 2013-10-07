4ec29c9b90e0a36dc16e7a8176b6170ae515c31d
Anthony Mallet <anthony.mallet@laas.fr>
2013-10-07 15:45:53 +0200

    [archivers/pax] Drop useless 'inplace' depend strategy
    This was required only when building a binary package for pkgtools/pkg_install
    with PREFER.pax = robotpkg, and this has just been fixed.

--- archivers/pax/depend.mk
+++ archivers/pax/depend.mk
@@ -5,7 +5,13 @@
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 PAX_DEPEND_MK:=		${PAX_DEPEND_MK}+
 
+ifeq (+,$(DEPEND_DEPTH))
+  DEPEND_PKG+=		pax
+endif
+
 ifeq (+,$(PAX_DEPEND_MK)) # ------------------------------------------------
+
+DEPEND_USE+=		pax
 
 PREFER.pax?=		system
 
@@ -22,41 +28,9 @@ DEPEND_METHOD.pax?=	build
 DEPEND_ABI.pax?=	pax
 DEPEND_DIR.pax?=	../../archivers/pax
 
+export PAX=		$(word 1,${SYSTEM_FILES.pax})
+export TAR=		$(word 2,${SYSTEM_FILES.pax})
+
 endif # PAX_DEPEND_MK ------------------------------------------------------
-
-# pull-in the user preferences for inplace+robotpkg check
-include ../../mk/robotpkg.prefs.mk
-_pax_style=$(strip ${PAX_STYLE})+$(strip ${PREFER.pax})
-
-ifneq (inplace+robotpkg,${_pax_style})
-  # This is the regular version, for normal install
-  #
-  ifeq (+,$(DEPEND_DEPTH))
-    DEPEND_PKG+=		pax
-  endif
-
-  ifeq (+,$(PAX_DEPEND_MK))
-    DEPEND_USE+=		pax
-
-    export PAX=		$(word 1,${SYSTEM_FILES.pax})
-    export TAR=		$(word 2,${SYSTEM_FILES.pax})
-  endif
-else
-  # This is the "inplace" version, for bootstrap process
-  #
-  pre-configure: pax-inplace
-
-  .PHONY: pax-inplace
-  pax-inplace:
-	@${STEP_MSG} "Building pax in place"
-	${RUN} cd ${ROBOTPKG_DIR}/${PKGPATH}/${DEPEND_DIR.pax};		\
-	${RECURSIVE_MAKE} build						\
-	  WRKDIR=${WRKDIR}/pax						\
-	  NO_PKGTOOLS_REQD_CHECK=yes;					\
-	${LN} -s pax ${WRKDIR}/pax/pax-20080110/tar
-
-  export PAX=		${WRKDIR}/pax/pax-20080110/pax
-  export TAR=		${WRKDIR}/pax/pax-20080110/tar
-endif
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
