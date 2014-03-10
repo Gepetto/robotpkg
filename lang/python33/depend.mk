fdbc1cb48f63f68760bbd3c358c5329ea7fbf348
Anthony Mallet <anthony.mallet@laas.fr>
2014-03-10 22:40:31 +0100

    [lang/python33] Import python-3.3.5
    Python is an interpreted, interactive, object-oriented programming language
    that combines remarkable power with very clear syntax.
    This package provides Python version 3.3 series.
    
    Thanks to adegroot@ for the final fixes in wip/

--- lang/python33/depend.mk
+++ lang/python33/depend.mk
@@ -1,5 +1,5 @@
-# robotpkg sysdep/python33.mk
-# Created:			Anthony Mallet on Tue Apr  9 2013
+# robotpkg depend.mk for:	lang/python33
+# Created:			Anthony Mallet on Tue, 10 Mar 2014
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
@@ -14,15 +14,21 @@ ifeq (+,$(PYTHON33_DEPEND_MK)) # -------------------------------------------
 DEPEND_USE+=		python33
 
 include ../../mk/sysdep/python.mk
+ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 12.04,${OS_VERSION}))
+    PREFER.python33?=	robotpkg
+  endif
+endif
 PREFER.python33?=	${PREFER.python}
 
 DEPEND_ABI.python33?=	python33>=3.3<3.4
+DEPEND_DIR.python33?=	../../lang/python33
 
 # see sysdep/python.mk for the definition of SYSTEM_SEARCH.python
 _py_search33=		{3.3,3,}{,d}{,m}{,u}
 SYSTEM_SEARCH.python33=	$(call _py_syssearch,${_py_search33})
 
-SYSTEM_PKG.Fedora.python33=	python3-devel
+SYSTEM_PKG.Fedora.python33=	python3.3-devel
 SYSTEM_PKG.Debian.python33=	python3.3-dev
 SYSTEM_PKG.NetBSD.python33=	lang/python33
 SYSTEM_PKG.Gentoo.python33=	'=dev-lang/python-3.3*'
