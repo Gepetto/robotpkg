74ecf795d83f811d20e897aec043396f85d15dd3
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-13 16:52:35 +0200

    [devel/ros-xacro] Update to 1.9.2
    Changes since 1.7.3:
    1.9.2 (2014-07-11)
    ------------------
    * add a few more tests to exercise the symbol table a bit more
    * allow for recursive evaluation of properties in expressions
    * add useful debugging information when parameters are not set
    * stop test from failing the second time it is run
    * unified if/unless handling, correctly handle floating point expressions
    * floating point expressions not equal zero are now evaluated as True
    * changed quotes to omit cmake warning
    * Contributors: Robert Haschke, Mike Ferguson
    
    1.9.1 (2014-06-21)
    ------------------
    * fixup tests so they run
    * export architecture_independent flag in package.xml
    * installed relocatable fix
    * Contributors: Michael Ferguson, Mike Purvis, Scott K Logan
    
    1.9.0 (2014-03-28)
    ------------------
    * Remove the roslint_python glob, use the default one.
    * Add roslint target to xacro; two whitespace fixes so that it passes.
    * fix evaluation of integers in if statements
      also added a unit test, fixes `#15 <https://github.com/ros/xacro/issues/15>`_
    * fix setting of _xacro_py CMake var, fixes `#16
    <https://github.com/ros/xacro/issues/16>`_
    * Add support for globbing multiple files in a single <xacro:include>
    * code cleanup and python3 support
    * check for CATKIN_ENABLE_TESTING
    
    1.8.4 (2013-08-06)
    ------------------
    * Merge pull request `#9 <https://github.com/ros/xacro/issues/9>`_ from
    davetcoleman/hydro-devel
      Xacro should not use plain 'include' tags but only namespaced ones.
    * Fix for the fact that minidom creates text nodes which count as child nodes
    * Removed <uri> checking and made it more general for any child element of an
    <include> tag
    * Removed Groovy reference, only being applied to Hydro
    * Created check for Gazebo's <uri> tabs only only shows deprecated warnings if
    not present.
    * Small spelling fix
    * Xacro should not use plain 'include' tags but only namespaced ones.
    * Merge pull request `#8 <https://github.com/ros/xacro/issues/8>`_ from
    piyushk/hydro-devel-conditional
      xacro conditional blocks
    * using refined arguments instead of sys.argv for xml file location
    * adding conditional blocks to xacro
    
    1.8.3 (2013-04-22)
    ------------------
    * bumped version to 1.8.3 for hydro release
    * backwards compatilibity with rosbuild
    * adding unit test for substitution args
    * Adding supoprt for substitution_args 'arg' fields
    * Remove bin copy of xacro.py

--- devel/ros-xacro/depend.mk
+++ devel/ros-xacro/depend.mk
@@ -18,16 +18,16 @@ SYSTEM_PREFIX.ros-xacro?=	${SYSTEM_PREFIX.ros-base}
 DEPEND_USE+=			ros-xacro
 ROS_DEPEND_USE+=		ros-xacro
 
-DEPEND_ABI.ros-xacro?=	ros-xacro>=1.6
-DEPEND_DIR.ros-xacro=	../../devel/ros-xacro
+DEPEND_ABI.ros-xacro?=		ros-xacro>=1.7
+DEPEND_DIR.ros-xacro=		../../devel/ros-xacro
 
-DEPEND_ABI.ros-xacro.fuerte += ros-xacro>=1.6<1.7
-DEPEND_ABI.ros-xacro.groovy += ros-xacro>=1.7<1.8
-DEPEND_ABI.ros-xacro.hydro += ros-xacro>=1.8<1.9
+DEPEND_ABI.ros-xacro.groovy+=	ros-xacro>=1.7<1.8
+DEPEND_ABI.ros-xacro.hydro+=	ros-xacro>=1.8<1.9
+DEPEND_ABI.ros-xacro.indigo+=	ros-xacro>=1.9<1.10
 
 SYSTEM_SEARCH.ros-xacro=\
-	bin/xacro.py		\
-	'share/xacro/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
+	'lib/xacro/xacro'					\
+	'share/xacro/package.xml:/<version>/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/xacro.pc:/Version/s/[^0-9.]//gp'
 
 endif # ROS_XACRO_DEPEND_MK --------------------------------------------
