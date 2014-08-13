3118f65bc5e68d6cfbd1cfca152fe1c2d95bd47d
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-13 16:14:17 +0200

    [devel/ros-filters] Update to 1.7.4
    Changes since 1.7.2 unspecified.

--- devel/ros-filters/depend.mk
+++ devel/ros-filters/depend.mk
@@ -19,16 +19,16 @@ DEPEND_USE+=			ros-filters
 ROS_DEPEND_USE+=		ros-filters
 
 DEPEND_ABI.ros-filters?=	ros-filters>=1.6
-DEPEND_DIR.ros-filters=	../../devel/ros-filters
+DEPEND_DIR.ros-filters=		../../devel/ros-filters
 
-DEPEND_ABI.ros-filters.fuerte += ros-filters>=1.6<1.7
-DEPEND_ABI.ros-filters.groovy += ros-filters>=1.6<=1.7.2
-DEPEND_ABI.ros-filters.hydro += ros-filters>=1.7.3<1.8
+DEPEND_ABI.ros-filters.groovy+=	ros-filters>=1.6<=1.7.2
+DEPEND_ABI.ros-filters.hydro+=	ros-filters>=1.7.3<1.8
+DEPEND_ABI.ros-filters.indigo+=	ros-filters>=1.7.3<1.8
 
 SYSTEM_SEARCH.ros-filters=\
-	include/filters/filter_base.h		\
-	lib/libmean.so		\
-	'share/filters/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
+	include/filters/filter_base.h				\
+	lib/libmean.so						\
+	'share/filters/package.xml:/<version>/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/filters.pc:/Version/s/[^0-9.]//gp'
 
 endif # ROS_FILTERS_DEPEND_MK --------------------------------------------
