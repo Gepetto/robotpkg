68bcc3bbbee3d0dd3b7475bbb9e4204d2fc7d5c4
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-13 15:57:58 +0200

    [devel/ros-bond-core] Update to 1.7.14
    Changes since 1.7.12: only WIN32 related changes.

--- devel/ros-bond-core/depend.mk
+++ devel/ros-bond-core/depend.mk
@@ -18,18 +18,18 @@ SYSTEM_PREFIX.ros-bond-core?=	${SYSTEM_PREFIX.ros-base}
 DEPEND_USE+=			ros-bond-core
 ROS_DEPEND_USE+=		ros-bond-core
 
-DEPEND_ABI.ros-bond-core?=	ros-bond-core>=1.6
+DEPEND_ABI.ros-bond-core?=	ros-bond-core>=1.7
 DEPEND_DIR.ros-bond-core?=	../../devel/ros-bond-core
 
-DEPEND_ABI.ros-bond-core.fuerte?=	ros-bond-core>=1.6<1.7
 DEPEND_ABI.ros-bond-core.groovy?=	ros-bond-core>=1.7<1.8
 DEPEND_ABI.ros-bond-core.hydro?=	ros-bond-core>=1.7<1.8
+DEPEND_ABI.ros-bond-core.indigo?=	ros-bond-core>=1.7<1.8
 
 SYSTEM_SEARCH.ros-bond-core=\
   'include/bondcpp/bond.h'						\
   'lib/libbondcpp.so'							\
   '${PYTHON_SYSLIBSEARCH}/bondpy/__init__.py'				\
-  'share/bondcpp/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'			\
+  'share/bondcpp/package.xml:/<version>/s/[^0-9.]//gp'			\
   'lib/pkgconfig/bond.pc:/Version/s/[^0-9.]//gp'			\
   'lib/pkgconfig/bondcpp.pc:/Version/s/[^0-9.]//gp'
 
