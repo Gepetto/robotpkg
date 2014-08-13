1a954d8d0c6142ced257b7bc4f40da4d52a4112d
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-13 16:45:54 +0200

    [devel/ros-lint] Improt ros-lint-0.9.2
    CMake lint commands for ROS packages.
    
    The lint commands perform static checking of Python or C++ source code for
    errors and standards compliance.

---
+++ devel/ros-lint/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	devel/ros-lint
+# Created:			Anthony Mallet on Mon, 10 Dec 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ROS_LINT_DEPEND_MK:=	${ROS_LINT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		ros-lint
+endif
+
+ifeq (+,$(ROS_LINT_DEPEND_MK)) # -------------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-lint?=	${PREFER.ros-base}
+SYSTEM_PREFIX.ros-lint?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=		ros-lint
+ROS_DEPEND_USE+=	ros-lint
+
+DEPEND_ABI.ros-lint?=	ros-lint>=0.9
+DEPEND_DIR.ros-lint?=	../../devel/ros-lint
+
+SYSTEM_SEARCH.ros-lint=\
+	'lib/roslint/cpplint'					\
+	'share/roslint/package.xml:/<version>/s/[^0-9.]//gp'	\
+	'${PYTHON_SYSLIBSEARCH}/roslint/__init__.py'		\
+	'lib/pkgconfig/roslint.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_LINT_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
