d2141a0a475afc6fb403eba2b80ec2d54ffc6172
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-13 16:06:30 +0200

    [devel/ros-class-loader] Update to 0.3.0
    Changes since 0.1.29:
    0.3.0 (2014-06-25)
    ------------------
    * Use system-provided console-bridge
    * Contributors: Esteve Fernandez
    
    0.2.5 (2014-03-04)
    ------------------
    * Changed format of debug messages so that rosconsole_bridge can correctly
    parse the prefix
    * Improved debug output
    
    0.2.4 (2014-02-12)
    ------------------
    * fix race condition with multi threaded library loading (`#16
    <https://github.com/ros/class_loader/issues/16>`_)
    
    0.2.3 (2013-08-21)
    ------------------
    * fix missing class name in logWarn output
    
    0.2.2 (2013-07-14)
    ------------------
    * check for CATKIN_ENABLE_TESTING (`#10
    <https://github.com/ros/class_loader/issues/10>`_)
    * fix find Poco to return full lib path (`#8
    <https://github.com/ros/class_loader/issues/8>`_)
    * add missing runtime destination for library under Windows
    * add Boosst component system
    
    0.2.1 (2013-06-06)
    ------------------
    * improve check for Poco foundation and headers (`#7
    <https://github.com/ros/class_loader/issues/7>`_)
    
    0.2.0 (2013-03-13)
    ------------------
    * use find_package for Poco/dl instead to make it work on other platforms
    * update Poco cmake file to include libdl on non-windows systems
    * No longer CATKIN_DEPEND on console_bridge
    
    Remove useless patches that have been fixed upstream

--- devel/ros-class-loader/depend.mk
+++ devel/ros-class-loader/depend.mk
@@ -18,18 +18,17 @@ SYSTEM_PREFIX.ros-class-loader?=${SYSTEM_PREFIX.ros-base}
 DEPEND_USE+=			ros-class-loader
 ROS_DEPEND_USE+=		ros-class-loader
 
-DEPEND_ABI.ros+=		ros>=groovy
-
 DEPEND_ABI.ros-class-loader?=	ros-class-loader>=0.1
 DEPEND_DIR.ros-class-loader?=	../../devel/ros-class-loader
 
 DEPEND_ABI.ros-class-loader.groovy?=	ros-class-loader>=0.1<0.2
 DEPEND_ABI.ros-class-loader.hydro?=	ros-class-loader>=0.2<0.3
+DEPEND_ABI.ros-class-loader.indigo?=	ros-class-loader>=0.3<0.4
 
 SYSTEM_SEARCH.ros-class-loader=\
   'include/class_loader/class_loader.h'				\
   'lib/libclass_loader.so'					\
-  'share/class_loader/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
+  'share/class_loader/package.xml:/<version>/s/[^0-9.]//gp'	\
   'lib/pkgconfig/class_loader.pc:/Version/s/[^0-9.]//gp'
 
 endif # ROS_CLASS_LOADER_DEPEND_MK -----------------------------------------
