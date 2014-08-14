284d7c19130f329c97f6af4b9d1980df4707146c
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-14 15:26:55 +0200

    [hardware/ros-driver-common] Update to 1.6.8
    No functional change since 1.6.6 (only maintainer names updates)

--- hardware/ros-driver-common/depend.mk
+++ hardware/ros-driver-common/depend.mk
@@ -9,7 +9,7 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=			ros-driver-common
 endif
 
-ifeq (+,$(ROS_DRIVER_COMMON_DEPEND_MK)) # ---------------------------------------
+ifeq (+,$(ROS_DRIVER_COMMON_DEPEND_MK)) # ----------------------------------
 
 include ../../meta-pkgs/ros-base/depend.common
 PREFER.ros-driver-common?=		${PREFER.ros-base}
@@ -18,22 +18,22 @@ SYSTEM_PREFIX.ros-driver-common?=	${SYSTEM_PREFIX.ros-base}
 DEPEND_USE+=			ros-driver-common
 ROS_DEPEND_USE+=		ros-driver-common
 
-DEPEND_ABI.ros-driver-common?=	ros-driver-common>=1.4
+DEPEND_ABI.ros-driver-common?=	ros-driver-common>=1.6
 DEPEND_DIR.ros-driver-common=	../../hardware/ros-driver-common
 
-DEPEND_ABI.ros-driver-common.fuerte += ros-driver-common>=1.4<1.5
 DEPEND_ABI.ros-driver-common.groovy += ros-driver-common>=1.6<1.7
 DEPEND_ABI.ros-driver-common.hydro += ros-driver-common>=1.6<1.7
+DEPEND_ABI.ros-driver-common.indigo += ros-driver-common>=1.6<1.7
 
 SYSTEM_SEARCH.ros-driver-common=\
-  'include/driver_base/driver.h'				\
-  'include/timestamp_tools/trigger_matcher.h'					\
-  'share/driver_base/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
-  'share/driver_common/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'			\
+  'include/driver_base/driver.h'					\
+  'include/timestamp_tools/trigger_matcher.h'				\
+  'share/driver_base/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'		\
+  'share/driver_common/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
   'share/timestamp_tools/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/driver_base.pc:/Version/s/[^0-9.]//gp'		\
-  'lib/pkgconfig/timestamp_tools.pc:/Version/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/driver_base.pc:/Version/s/[^0-9.]//gp'			\
+  'lib/pkgconfig/timestamp_tools.pc:/Version/s/[^0-9.]//gp'
 
-endif # ROS_DRIVER_COMMON_DEPEND_MK ---------------------------------------------
+endif # ROS_DRIVER_COMMON_DEPEND_MK ----------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
