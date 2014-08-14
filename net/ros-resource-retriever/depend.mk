6f357b06553ce25a5fba2acd70e86fcf75c81dfe
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-14 10:44:44 +0200

    [net/ros-resource-retriever] Import ros-resource-retriever-1.11.5
    This package retrieves data from url-format files such as http://, ftp://,
    package:// file://, etc., and loads the data into memory.  The package:// url
    for ros packages is translated into a local file:// url.  The resourse
    retriever was initially designed to load mesh files into memory, but it can be
    used for any type of data. The resource retriever is based on the the libcurl
    library.

---
+++ net/ros-resource-retriever/depend.mk
@@ -0,0 +1,36 @@
+# robotpkg depend.mk for:	net/ros-resource_retriever
+# Created:			Anthony Mallet on Thu, 14 Aug 2014
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+ROS_RESOURCE_RETRIEVER_DEPEND_MK:=	${ROS_RESOURCE_RETRIEVER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=				ros-resource-retriever
+endif
+
+ifeq (+,$(ROS_RESOURCE_RETRIEVER_DEPEND_MK)) # -----------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-resource-retriever?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-resource-retriever?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=				ros-resource-retriever
+ROS_DEPEND_USE+=			ros-resource-retriever
+
+DEPEND_ABI.ros-resource-retriever?=	ros-resource-retriever>=1.9
+DEPEND_DIR.ros-resource-retriever?=	../../net/ros-resource-retriever
+
+DEPEND_ABI.ros-resource-retriever.groovy?=ros-resource-retriever>=1.9<1.10
+DEPEND_ABI.ros-resource-retriever.hydro?= ros-resource-retriever>=1.10<1.11
+DEPEND_ABI.ros-resource-retriever.indigo?=ros-resource-retriever>=1.11<1.12
+
+SYSTEM_SEARCH.ros-resource-retriever=\
+  'include/resource_retriever/retriever.h'				\
+  'lib/libresource_retriever.so'					\
+  'lib/pkgconfig/resource_retriever.pc:/Version/s/[^0-9.]//gp'		\
+  'share/resource_retriever/package.xml:/<version>/s/[^0-9.]//gp'
+
+endif # ROS_RESOURCE_RETRIEVER_DEPEND_MK -----------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
