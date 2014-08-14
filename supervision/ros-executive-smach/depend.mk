863dab6ccd01e646d1227ebee702186c9d8b23e0
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-14 16:59:57 +0200

    [supervision/ros-executive-smach] Update to 2.0.0
    Changes since 1.3.0:
    
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    Changelog for package smach_msgs
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    2.0.0 (2014-04-17)
    ------------------
    * cleaning up and removing rosbuild support
    * merging groovy and hydro
    * Add explanations within message definitions
    * Contributors: Felix Kolbe, Jonathan Bohren
    
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    Changelog for package smach
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    2.0.0 (2014-04-17)
    ------------------
    * Merging changes, resolving conflicts, from strands-project (@cburbridge)
    * cleaning up and removing rosbuild support
    * merging groovy and hydro
    * Fix get_internal_edges returning list of tuples, not list of lists
    * Remove old methods set_userdata
    * Remove superfluous parent class declaration 'UserDatá from 'Remapper'
    * Add local error base class 'SmachError', extending Exception
    * Fix syntax errors, doc typos and indentations glitches
    * Fixed invalid exception type in concurrence.py
    * Checking threads have fully terminated before cleanup of outcomes dict
      This commit uses thread.isAlive() on each concurrent state runner to check
      for termination of all the threads before continuing. This is necessary as
      only checking that the outcome has been filled in does not mean the thread
      has completed; if the thread has not completed it may not yet have called the
      termination callback. If this loop exits before the termination callback of
      the last thread is called, then the callback will occasionally be sent an
      empty dictionary (when the main thread has got to line 305).
    * cope with missed state termination notifications
      Concurrent states could terminate and notify _ready_event without the
      concurrence container realising, as it could be busy checking the outcome
      values. This makes the concurrency container get stuck on line 250. This
      commit adds a timeout to the wait to safely cope with missing notifications.
    * Adding event for thread synchronization in concurrence and using event not
    condition in monitor state
    * Contributors: Felix Kolbe, Jonathan Bohren, Piotr Orzechowski, cburbridge
    
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    Changelog for package smach_ros
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    2.0.0 (2014-04-17)
    ------------------
    * smach_ros: Adding rostests to cmakelists
    * Merging changes, resolving conflicts, from strands-project (@cburbridge)
    * cleaning up and removing rosbuild support
    * merging groovy and hydro
    * Listing available goal slots in case of specifying wrong ones
    * Fix syntax errors, doc typos and indentations glitches
    * if monitor state prempted before executing, return.
    * Adding event for thread synchronization in concurrence and using event not
    condition in monitor state
    * Listing available goal slots in case of specifying wrong ones
    * [MonitorState] Make exception handler more verbose
    * edited monitor state to allow input and output keys
    * Contributors: Boris Gromov, Bruno Lacerda, Felix Kolbe, Hendrik Wiese,
    Jonathan Bohren, cburbridge

--- supervision/ros-executive-smach/depend.mk
+++ supervision/ros-executive-smach/depend.mk
@@ -9,7 +9,7 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=			ros-executive-smach
 endif
 
-ifeq (+,$(ROS_EXECUTIVE_SMACH_DEPEND_MK)) # --------------------------------------
+ifeq (+,$(ROS_EXECUTIVE_SMACH_DEPEND_MK)) # --------------------------------
 
 include ../../meta-pkgs/ros-base/depend.common
 include ../../mk/sysdep/python.mk
@@ -17,22 +17,22 @@ include ../../mk/sysdep/python.mk
 PREFER.ros-executive-smach?=		${PREFER.ros-base}
 SYSTEM_PREFIX.ros-executive-smach?=	${SYSTEM_PREFIX.ros-base}
 
-DEPEND_USE+=			ros-executive-smach
-ROS_DEPEND_USE+=		ros-executive-smach
+DEPEND_USE+=				ros-executive-smach
+ROS_DEPEND_USE+=			ros-executive-smach
 
 DEPEND_ABI.ros-executive-smach?=	ros-executive-smach>=1.2
-DEPEND_DIR.ros-executive-smach=	../../supervision/ros-executive-smach
+DEPEND_DIR.ros-executive-smach=		../../supervision/ros-executive-smach
 
-DEPEND_ABI.ros-executive-smach.fuerte+=	ros-executive-smach>=1.2<1.2.2
 DEPEND_ABI.ros-executive-smach.groovy+=	ros-executive-smach>=1.2.2<=1.3.0
 DEPEND_ABI.ros-executive-smach.hydro+=	ros-executive-smach>=1.3.1<1.4
+DEPEND_ABI.ros-executive-smach.indigo+=	ros-executive-smach>=2.0<2.1
 
 SYSTEM_SEARCH.ros-executive-smach=\
-	${PYTHON_SITELIB}/smach/__init__.py		\
-	'share/executive_smach/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
-	'share/smach_msgs/${ROS_STACKAGE}:/<version>/s/[^0-9.]//gp'	\
+	'${PYTHON_SITELIB}/smach/__init__.py'				\
+	'share/executive_smach/package.xml:/<version>/s/[^0-9.]//gp'	\
+	'share/smach_msgs/package.xml:/<version>/s/[^0-9.]//gp'		\
 	'lib/pkgconfig/smach_msgs.pc:/Version/s/[^0-9.]//gp'
 
-endif # ROS_EXECUTIVE_SMACH_DEPEND_MK --------------------------------------------
+endif # ROS_EXECUTIVE_SMACH_DEPEND_MK --------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
