551ac2d7240ed5c7fe0b861764c232f566cee322
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-19 15:51:36 +0200

    [image/opencv] Fix depend.mk for the location of OpenCVconfig.cmake on fedora20

--- image/opencv/depend.mk
+++ image/opencv/depend.mk
@@ -23,7 +23,7 @@ SYSTEM_SEARCH.opencv=\
   'include{,/opencv-*}/opencv2/core/version.hpp'			\
   'include{,/opencv-*}/opencv2/highgui/highgui.hpp'			\
   'lib/pkgconfig/opencv{,-[0-9]*}.pc:/Version/s/[^.0-9]//gp'		\
-  '{lib/cmake,share}/{OpenCV,opencv}/OpenCVConfig.cmake'
+  '{lib/cmake,share,lib}/{OpenCV,opencv}/OpenCVConfig.cmake'
 
 SYSTEM_PKG.Debian.opencv=	libopencv-dev libcv-dev libhighgui-dev
 SYSTEM_PKG.Fedora.opencv=	opencv-devel
