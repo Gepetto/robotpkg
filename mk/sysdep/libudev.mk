85ca1e750e1cb95a6c7e4d115abac4ef73b347ae
Anthony Mallet <anthony.mallet@laas.fr>
2014-08-22 11:31:51 +0200

    [mk/sysdep] Teach robotpkg about libudev

---
+++ mk/sysdep/libudev.mk
@@ -0,0 +1,28 @@
+# robotpkg sysdep/libudev.mk
+# Created:			Anthony Mallet on Aug 22 2014
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+LIBUDEV_DEPEND_MK:=	${LIBUDEV_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libudev
+endif
+
+ifeq (+,$(LIBUDEV_DEPEND_MK)) # --------------------------------------------
+
+PREFER.libudev?=	system
+DEPEND_USE+=		libudev
+DEPEND_ABI.libudev?=	libudev>=0
+
+SYSTEM_SEARCH.libudev=\
+  'include/libudev.h'					\
+  'lib/libudev.so'					\
+  'lib/pkgconfig/libudev.pc:/^Version/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Fedora.libudev=	systemd-devel
+SYSTEM_PKG.Debian.libudev=	libudev-dev
+SYSTEM_PKG.Gentoo.libudev=	sysfs-udev
+
+endif # LIBUDEV_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
