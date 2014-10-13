dba87f0b33bc2c374370e93162dd39dbf6d487c7
Aurelie Clodic <aurelie.clodic@gmail.com>
2014-10-13 10:24:33 +0200

    [math/jrl-dynamics-urdf] From wip to robotpkg

---
+++ math/jrl-dynamics-urdf/depend.mk
@@ -0,0 +1,33 @@
+# robotpkg depend.mk for:	math/jrl-dynamics-urdf
+# Created:			Olivier Stasse on Fri, Jun 28
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+JRL_DYNAMICS_URDF_DEPEND_MK:=	${JRL_DYNAMICS_URDF_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			jrl-dynamics-urdf
+endif
+
+ifeq (+,$(JRL_DYNAMICS_URDF_DEPEND_MK)) # ---------------------------------
+
+include ../../interfaces/jrl-mal/depend.mk
+include ../../math/jrl-dynamics/depend.mk
+include ../../graphics/urdfdom/depend.mk
+include ../../graphics/urdfdom-headers/depend.mk
+
+PREFER.jrl-dynamics-urdf?=	robotpkg
+
+DEPEND_USE+=			jrl-dynamics-urdf
+
+DEPEND_ABI.jrl-dynamics-urdf?=	jrl-dynamics-urdf>=1.0.0
+DEPEND_DIR.jrl-dynamics-urdf?=	../../math/jrl-dynamics-urdf
+
+SYSTEM_SEARCH.jrl-dynamics-urdf=\
+  'bin/display-robot'			\
+  'include/jrl/dynamics/urdf/parser.hh'	\
+  'lib/libjrl-dynamics-urdf.so'
+
+endif # JRL_DYNAMICS_URDF_DEPEND_MK ---------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
