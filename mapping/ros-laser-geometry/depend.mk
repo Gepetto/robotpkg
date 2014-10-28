06ada1c943de827d98ff235bcee1e8feaf4e9661
Charles Lesire <charles.lesire@onera.fr>
2014-10-28 11:41:07 +0100

    [mapping/ros-laser-geometry] Import ros-laser-geometry-1.6.2
    This package contains a class for converting from a 2D laser scan as defined by
    sensor_msgs/LaserScan into a point cloud as defined by sensor_msgs/PointCloud
    or sensor_msgs/PointCloud2. In particular, it contains functionality to account
    for the skew resulting from moving robots or tilting laser scanners.

---
+++ mapping/ros-laser-geometry/depend.mk
@@ -0,0 +1,38 @@
+# robotpkg depend.mk for:	perception/ros-laser-geometry
+# Created:			Charles Lesire on Tue, 8 Apr 2014
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_LASER_GEOMETRY_DEPEND_MK:=	${ROS_LASER_GEOMETRY_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-laser-geometry
+endif
+
+ifeq (+,$(ROS_LASER_GEOMETRY_DEPEND_MK)) # ------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-laser-geometry?=	${PREFER.ros-base}
+SYSTEM_PREFIX.ros-laser-geometry?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-laser-geometry
+ROS_DEPEND_USE+=		ros-laser-geometry
+
+DEPEND_ABI.ros+=		ros>=groovy
+DEPEND_ABI.ros-laser-geometry?=	ros-laser-geometry>=1.5
+DEPEND_DIR.ros-laser-geometry?=	../../mapping/ros-laser-geometry
+
+DEPEND_ABI.ros-laser-geometry.groovy?=	ros-laser-geometry>=1.5<1.6
+DEPEND_ABI.ros-laser-geometry.hydro?=	ros-laser-geometry>=1.6<1.7
+DEPEND_ABI.ros-laser-geometry.indigo?=	ros-laser-geometry>=1.6<1.7
+
+SYSTEM_SEARCH.ros-laser-geometry=\
+  'include/laser_geometry/laser_geometry.h'			\
+  'lib/liblaser_geometry.so'					\
+  '${PYTHON_SYSLIBSEARCH}/laser_geometry/__init__.py'		\
+  'lib/pkgconfig/laser_geometry.pc:/Version/s/[^0-9.]//gp'	\
+  'share/laser_geometry/package.xml:/<version>/s/[^0-9.]//gp'
+
+endif # ROS_LASER_GEOMETRY_DEPEND_MK ------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
