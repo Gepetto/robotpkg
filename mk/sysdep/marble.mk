e520d105f467169dd79e1fb8867739012a6d8897
Anthony Mallet <anthony.mallet@laas.fr>
2015-01-26 18:32:27 +0100

    [mk/sydep] Fix SYSTEM_SEARCH for marble on Gentoo
    Reported by Cyril Roussillon, thanks!

--- mk/sysdep/marble.mk
+++ mk/sysdep/marble.mk
@@ -19,7 +19,7 @@ SYSTEM_SEARCH.marble=	\
   'bin/marble:/Marble/s/[^0-9.]//gp:% --version'	\
   'include/marble/Planet.h'				\
   'lib/libmarblewidget.so'				\
-  'share/kde{4,}/apps/cmake/modules/FindMarble.cmake'
+  'share/{kde4/,kde/,}apps/cmake/modules/FindMarble.cmake'
 
 SYSTEM_PKG.Linux.marble=	marble libmarble-dev
 SYSTEM_PKG.NetBSD.marble=	pkgsrc/misc/marble
