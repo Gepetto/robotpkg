f992f973628b52754901279c06acef2d579610d6
Anthony Mallet <anthony.mallet@laas.fr>
2015-01-27 15:40:20 +0100

    [sysutils/tk3-flash] Import tk3-flash-1.0
    tk3-flash is a command-line utility used to update the program stored
    in permanent memory of microcontrollers. Currently, it supports only
    programming the AVR microcontrollers with the Mikrokopter bootloader.
    
    tk3-flash supports flashing the EEPROM or the flash memory. It also
    allows to set parameters specific to the flashed program, without the
    need for changing the program itself.

---
+++ sysutils/tk3-flash/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	sysutils/tk3-flash
+# Created:			Matthieu Herrb on Thu,  9 Dec 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+TK3_FLASH_DEPEND_MK:=	${TK3_FLASH_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		tk3-flash
+endif
+
+ifeq (+,$(TK3_FLASH_DEPEND_MK)) # ------------------------------------------
+
+PREFER.tk3-flash?=	robotpkg
+
+DEPEND_USE+=		tk3-flash
+
+DEPEND_ABI.tk3-flash?=	tk3-flash>=1.0
+DEPEND_DIR.tk3-flash?=	../../sysutils/tk3-flash
+
+SYSTEM_SEARCH.tk3-flash=\
+  'bin/tk3-flash:p:% -v'
+
+endif # TK3_FLASH_DEPEND_MK ------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
