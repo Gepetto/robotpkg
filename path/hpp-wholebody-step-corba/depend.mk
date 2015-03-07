949e422d1d3ae1565c0270aff46284a8f3add942
Florent Lamiraux <florent@laas.fr>
2015-03-07 21:24:46 +0100

    [path/hpp-wholebody-step-corba] Upgrade to version 3.1.0
      - Package does not depend anymore on Kitelab.

--- path/hpp-wholebody-step-corba/depend.mk
+++ path/hpp-wholebody-step-corba/depend.mk
@@ -15,7 +15,7 @@ PREFER.hpp-wholebody-step-corba?=	robotpkg
 
 DEPEND_USE+=		hpp-wholebody-step-corba
 
-DEPEND_ABI.hpp-wholebody-step-corba?=	hpp-wholebody-step-corba>=0.1
+DEPEND_ABI.hpp-wholebody-step-corba?=	hpp-wholebody-step-corba>=3.1.0
 DEPEND_DIR.hpp-wholebody-step-corba?=	../../path/hpp-wholebody-step-corba
 
 SYSTEM_SEARCH.hpp-wholebody-step-corba=			\
