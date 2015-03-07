e9d3fe87f860f1a7c4a4fd89cb124d7ddf2f25ec
Florent Lamiraux <florent@laas.fr>
2015-03-07 18:28:24 +0100

    [path/hpp-wholebody-step] Upgrade to version 3.1.0
      - Renamed package from hpp-wholebody-step-planner,
      - package does not depend anymore on kitelab.

---
+++ path/hpp-wholebody-step/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	path/hpp-wholebody-step
+# Created:			Antonio El Khoury on Thu, 26 Sep 2013
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HPP_WHOLEBODY_STEP_DEPEND_MK:=	${HPP_WHOLEBODY_STEP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		hpp-wholebody-step
+endif
+
+ifeq (+,$(HPP_WHOLEBODY_STEP_DEPEND_MK)) # ----------------------------------
+
+PREFER.hpp-wholebody-step?=	robotpkg
+
+DEPEND_USE+=		hpp-wholebody-step
+
+DEPEND_ABI.hpp-wholebody-step?=	hpp-wholebody-step>=3.1.0
+DEPEND_DIR.hpp-wholebody-step?=	../../path/hpp-wholebody-step
+
+SYSTEM_SEARCH.hpp-wholebody-step=		\
+	lib/libhpp-wholebody-step.so		\
+	include/hpp/wholebody-step/static-stability-constraint.hh	\
+	'lib/pkgconfig/hpp-wholebody-step.pc:/Version/s/[^0-9.]//gp'
+
+endif # HPP_WHOLEBODY_STEP_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
