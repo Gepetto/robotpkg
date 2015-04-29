dee47132fade1d05d40dd0c2a23c6184939256c5
Anthony Mallet <anthony.mallet@laas.fr>
2015-04-29 14:49:34 +0200

    [graphics/assimp] Accept assimp version 2 in the depend.mk
    Assimp2 and 3 have only minor differences (the most important being some
    headers renaned, but this happened in the middle of the assim2 releases, so...)
    
    And the packages using assimp in robotpkg are compatible with both versions.

--- graphics/assimp/depend.mk
+++ graphics/assimp/depend.mk
@@ -12,15 +12,7 @@ endif
 ifeq (+,$(ASSIMP_DEPEND_MK)) # ---------------------------------------------
 
 include ../../mk/robotpkg.prefs.mk # for OPSYS
-ifeq (Ubuntu,${OPSYS})
-  ifneq (,$(filter 12.04 12.10 13.04,${OS_VERSION}))
-    PREFER.assimp?=	robotpkg
-  endif
-else ifeq (Fedora,${OPSYS})
-  ifneq (,$(filter 19,${OS_VERSION}))
-    PREFER.assimp?=	robotpkg
-  endif
-else ifeq (Gentoo,${OS_FAMILY})
+ifeq (Gentoo,${OS_FAMILY})
   PREFER.assimp?=	robotpkg
 else ifeq (NetBSD,${OPSYS})
   PREFER.assimp?=	robotpkg
@@ -29,12 +21,12 @@ PREFER.assimp?=		system
 
 DEPEND_USE+=		assimp
 
-DEPEND_ABI.assimp?=	assimp>=3.0
+DEPEND_ABI.assimp?=	assimp>=2.0
 DEPEND_DIR.assimp?=	../../graphics/assimp
 
 SYSTEM_SEARCH.assimp=\
 	bin/assimp						\
-	include/assimp/version.h				\
+	include/assimp/{aiV,v}ersion.h				\
 	'lib/pkgconfig/assimp.pc:/Version/s/[^0-9.]//gp'	\
 	lib/libassimp.so
 
