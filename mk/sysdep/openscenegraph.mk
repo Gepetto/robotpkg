efaab62ae730da83dd10f3881ed7229475e89ef4
Anthony Mallet <anthony.mallet@laas.fr>
2015-04-29 17:10:29 +0200

    [mk/sysdep] Teach robotpkg about openscenegraph

---
+++ mk/sysdep/openscenegraph.mk
@@ -0,0 +1,26 @@
+# robotpkg sysdep/openscenegraph.mk
+# Created:			Florent Lamiraux on Sun, 8 Mar 2015
+#
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+OPENSCENEGRAPH_DEPEND_MK:=	${OPENSCENEGRAPH_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			openscenegraph
+endif
+
+ifeq (+,$(OPENSCENEGRAPH_DEPEND_MK)) # -------------------------------------
+
+PREFER.openscenegraph?=		system
+
+DEPEND_USE+=			openscenegraph
+
+DEPEND_ABI.openscenegraph?=	openscenegraph>=3
+
+SYSTEM_SEARCH.openscenegraph=\
+  'include/osg/Object'						\
+  'lib/libosg.so'						\
+  'lib/pkgconfig/openscenegraph.pc:/Version/s/[^0-9.]//gp'
+
+endif # OPENSCENEGRAPH_DEPEND_MK -------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
