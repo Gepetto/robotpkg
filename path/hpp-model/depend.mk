fa3f2bd34552ae556c16fd69e48041ea8f32f778
Anthony Mallet <anthony.mallet@laas.fr>
2015-04-30 12:04:05 +0200

    [hpp-model] Export an eigen3 >= 3.2 dependency for dependant packages.
    This package leaks eigen3 in its public headers.
    And it requires eigen3 >= 3.2: override PREFER defaults where needed.
    
    This will work only if path/hpp-model/depend.mk is included before
    math/eigen3/depend.mk.

--- path/hpp-model/depend.mk
+++ path/hpp-model/depend.mk
@@ -24,4 +24,21 @@ DEPEND_DIR.hpp-model?=	../../path/hpp-model
 
 endif # HPP_MODEL_DEPEND_MK ------------------------------------------
 
+# This package leaks eigen3 in its public headers.
+# And it requires eigen3 >= 3.2: override PREFER defaults where needed.
+# This works only if this file is included before math/eigen3/depend.mk
+ifneq (,$(filter eigen3,${DEPEND_USE}))
+  PKG_FAIL_REASON +=\
+    "You have to include path/hpp-model/depend.mk before math/eigen3/depend.mk"
+endif
+
+DEPEND_ABI.eigen3 += eigen3>=3.2.0
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 12.04,${OS_VERSION}))
+    PREFER.eigen3?=	robotpkg
+  endif
+endif
+include ../../math/eigen3/depend.mk
+
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
