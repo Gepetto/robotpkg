9c1f607497d6626131a67da997f367eca727a04e
Anthony Mallet <anthony.mallet@laas.fr>
2015-06-16 15:40:52 +0200

    [net/genomix] Import genomix-1.5
    The genomix HTTP server is a generic interface between clients and genom
    components (using the generic genom C client template). Control is done be the
    mean of specific HTTP GET requests (not yet documented...).
    
    Moved from wip/.

---
+++ net/genomix/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	net/genomix
+# Created:			Anthony Mallet on Fri, 19 Oct 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GENOMIX_DEPEND_MK:=	${GENOMIX_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		genomix
+endif
+
+ifeq (+,$(GENOMIX_DEPEND_MK)) # --------------------------------------------
+
+DEPEND_USE+=		genomix
+PREFER.genomix?=	robotpkg
+
+SYSTEM_SEARCH.genomix=\
+  'bin/genomixd:p:% --version'				\
+  'lib/pkgconfig/genomix.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_ABI.genomix?=	genomix>=1.5
+DEPEND_DIR.genomix?=	../../net/genomix
+
+endif # GENOMIX_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
