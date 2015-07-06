de41b1985e240c44c61f76d61bd7cff1b0fc0c4a
Anthony Mallet <anthony.mallet@laas.fr>
2015-07-06 17:41:11 +0200

    [robots/tk3-matlab] Import tk3-matlab-1.0
    This is a matlab / simulink interface for controlling the Mikrokopter hardware
    running tk3-mikrokopter.

---
+++ robots/tk3-matlab/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	supervision/tk3-matlab
+# Created:			Anthony Mallet on Tue, 16 Jun 2015
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+MATLAB_GENOMIX_DEPEND_MK:=	${MATLAB_GENOMIX_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		tk3-matlab
+endif
+
+ifeq (+,$(MATLAB_GENOMIX_DEPEND_MK)) # -------------------------------------
+
+DEPEND_USE+=		tk3-matlab
+PREFER.tk3-matlab?=	robotpkg
+
+SYSTEM_SEARCH.tk3-matlab=\
+  'lib/matlab/+tk3/Contents.m'
+
+DEPEND_ABI.tk3-matlab?=	tk3-matlab
+DEPEND_DIR.tk3-matlab?=	../../robots/tk3-matlab
+
+endif # MATLAB_GENOMIX_DEPEND_MK -------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
