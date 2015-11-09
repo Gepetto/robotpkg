53e59dd3dec0c28452feca933c0ee8a6c69bb8a7
Anthony Mallet <anthony.mallet@laas.fr>
2015-11-09 19:44:15 +0100

    [interfaces/openrobots-idl] Import openrobots-idl-1.1 (from wip)
    Changes since 1.0:
     . Drop unused fields in the pose_estimator state and add an 'intrinsic'
       flag.
     . Add a rotorcraft interface with wrench control input

---
+++ interfaces/openrobots-idl/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	interfaces/openrobots-idl
+# Created:			Anthony Mallet on Mon,  9 Nov 2015
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+OPENROBOTS_IDL_DEPEND_MK:=	${OPENROBOTS_IDL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		openrobots-idl
+endif
+
+ifeq (+,$(OPENROBOTS_IDL_DEPEND_MK)) # -------------------------------------
+
+DEPEND_USE+=		openrobots-idl
+PREFER.openrobots-idl?=	robotpkg
+
+SYSTEM_SEARCH.openrobots-idl=\
+	'share/idl/or/time/time.idl'				\
+	'lib/pkgconfig/openrobots-idl.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_ABI.openrobots-idl?=	openrobots-idl>=1.1
+DEPEND_DIR.openrobots-idl?=	../../interfaces/openrobots-idl
+
+endif # OPENROBOTS_IDL_DEPEND_MK -------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
