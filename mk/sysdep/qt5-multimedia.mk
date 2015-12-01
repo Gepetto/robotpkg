85708ab3368c6de09a94630c262f415542602479
Arnaud Degroote <arnaud.degroote@isae-supaero.fr>
2015-12-01 12:20:24 +0100

    [mk/sysdep] Teach robotpkg about qt5-multimedia
    Qt5 Multimedia module provides a rich feature set that enables you to
    easily take advantage of a platform's multimedia capabilities such as
    media playback and the use of camera and radio devices.

---
+++ mk/sysdep/qt5-multimedia.mk
@@ -0,0 +1,28 @@
+# robotpkg sysdep/qt5-multimedia
+# Created: Arnaud Degroote on 01 Dec 2015
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+QT5_MULTIMEDIA_DEPEND_MK:=	${QT5_MULTIMEDIA_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		qt5-multimedia
+endif
+
+ifeq (+,$(QT5_MULTIMEDIA_DEPEND_MK)) # -------------------------------------------
+
+PREFER.qt5-multimedia?=	system
+
+DEPEND_USE+=		qt5-multimedia
+DEPEND_ABI.qt5-multimedia?=	qt5-multimedia>=5.0.0
+
+SYSTEM_SEARCH.qt5-multimedia=\
+  '{,qt5/}include/{,qt5/{,include/}}Qt{,Multimedia}/qcamera.h'		\
+  '{lib,share/qt5/lib,qt5/lib,lib/qt5}/libQt5Multimedia.{so,a}'	\
+  'lib/pkgconfig/Qt5Multimedia.pc:/Version/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Debian.qt5-multimedia=	qtmultimedia5-dev
+SYSTEM_PKG.NetBSD.qt5-multimedia=	x11/qt5-qtmultimedia 
+
+endif # QT5_MULTIMEDIA_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:= ${DEPEND_DEPTH:+=}
