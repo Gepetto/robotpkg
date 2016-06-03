26abe6aa8f23e51e564b097a1fda76a603efe9b1
Anthony Mallet <anthony.mallet@laas.fr>
2016-06-03 13:45:48 +0200

    [motion/dynamic-graph-bridge] Import 2.0.6 (from wip)
    This ROS package binds together the ROS framework with the dynamic-graph
    real-time control architecture.

---
+++ motion/dynamic-graph-bridge/depend.mk
@@ -0,0 +1,39 @@
+# robotpkg depend.mk for:	motion/dynamic-graph-bridge
+# Created:			Aurelie Clodic on Mon, 16 Dec 2013
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+ROS_DYNAMICGRAPHBRIDGE_DEPEND_MK:=	${ROS_DYNAMICGRAPHBRIDGE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			dynamic-graph-bridge
+endif
+
+ifeq (+,$(ROS_DYNAMICGRAPHBRIDGE_DEPEND_MK)) # -----------------------------
+
+DEPEND_USE+=			dynamic-graph-bridge
+ROS_DEPEND_USE+=		dynamic-graph-bridge
+
+DEPEND_ABI.ros+=		ros>=groovy
+
+DEPEND_ABI.dynamic-graph-bridge?=	dynamic-graph-bridge>=1.0.0
+DEPEND_DIR.dynamic-graph-bridge?=	../../motion/dynamic-graph-bridge
+
+DEPEND_ABI.dynamic-graph-bridge.groovy?=dynamic-graph-bridge>=1.0.0
+DEPEND_ABI.dynamic-graph-bridge.hydro?=	dynamic-graph-bridge>=1.0.0
+
+SYSTEM_SEARCH.dynamic-graph-bridge=\
+  'include/dynamic_graph_bridge/config.h'				\
+  'lib/pkgconfig/dynamic_graph_bridge.pc:/Version/s/[^0-9.]//gp'	\
+  'lib/librobot_model.so'						\
+  'share/dynamic_graph_bridge/robot_pose_publisher'			\
+  '${PYTHON_SITELIB}/dynamic_graph/ros/ros.py'
+
+# headers leak the ros dependency ...
+include ../../middleware/ros-comm/depend.mk
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS_DYNAMICGRAPHBRIDGE_DEPEND_MK -----------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
