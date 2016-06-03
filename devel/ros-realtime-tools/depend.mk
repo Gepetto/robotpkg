badc0fa84198e2be4da7fd24fed7d47f9fd184b2
Anthony Mallet <anthony.mallet@laas.fr>
2016-06-03 11:30:55 +0200

    [devel/ros-realtime-tools] Import 1.9.1 (from wip)
    This package contains a set of tools that can be used from a hard realtime
    thread, without breaking the realtime behavior. The tools currently only
    provides the realtime publisher, which makes it possible to publish messages to
    a ROS topic from a realtime thread.

---
+++ devel/ros-realtime-tools/depend.mk
@@ -0,0 +1,33 @@
+# robotpkg depend.mk for:	devel/ros-realtime-tools
+# Created:			Aurelie Clodic on Thu, 28 Nov 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_REALTIMETOOLS_DEPEND_MK:=	${ROS_REALTIMETOOLS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-realtime-tools
+endif
+
+ifeq (+,$(ROS_REALTIMETOOLS_DEPEND_MK)) # ----------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-realtime-tools?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-realtime-tools?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-realtime-tools
+ROS_DEPEND_USE+=		ros-realtime-tools
+
+DEPEND_ABI.ros+=ros>=hydro
+
+DEPEND_ABI.ros-realtime-tools?=	ros-realtime-tools>=1.8.2
+DEPEND_DIR.ros-realtime-tools?=	../../devel/ros-realtime-tools
+
+SYSTEM_SEARCH.ros-realtime-tools=\
+  'include/realtime_tools/realtime_box.h'			\
+  'share/realtime_tools/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/realtime_tools.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROS_REALTIMETOOLS_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
