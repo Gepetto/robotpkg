e340da1fbcf8760d7a29c269854265a83a3c4c5a
Anthony Mallet <anthony.mallet@laas.fr>
2016-11-02 13:54:32 +0100

    [interfaces/ros-comm-msgs] Update to 1.11.2
    Require > 1.10.3 for the upcoming ros-comm update.
    
    Changes since 1.10.2:
     * add SetBool service
     * add Trigger service
     * add delivered_msgs attribute to TopicStatistics message
     * add TopicStatistics message

--- interfaces/ros-comm-msgs/depend.mk
+++ interfaces/ros-comm-msgs/depend.mk
@@ -20,7 +20,7 @@ ROS_DEPEND_USE+=		ros-comm-msgs
 
 DEPEND_ABI.ros+=		ros>=indigo
 
-DEPEND_ABI.ros-comm-msgs?=	ros-comm-msgs>=1.10
+DEPEND_ABI.ros-comm-msgs?=	ros-comm-msgs>=1.10.3
 DEPEND_DIR.ros-comm-msgs?=	../../interfaces/ros-comm-msgs
 
 SYSTEM_SEARCH.ros-comm-msgs=\
