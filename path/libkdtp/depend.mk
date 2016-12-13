f223314b6960eb9d16312dc606d1eccc1f6d628a
Anthony Mallet <anthony.mallet@laas.fr>
2016-12-13 17:50:01 +0100

    [path/libkdtp] Import libkdtp-1.0
    Kinodynamic Trajectory Planner
    
    Spline-based, minimum time, constant snap trajectory generator. It applies to
    any robot with decoupled dynamics (or differencially flat).
    
    Whitepaper: Alexandre Boeuf, Juan Cortes, Rachid Alami, Thierry
    Simeon. Enhancing sampling-based kinodynamic motion planning for
    quadrotors. IEEE/RSJ International Conference on Intelligent Robots and
    Systems, Sep 2015, Hamburg, Germany. 2015.

---
+++ path/libkdtp/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	path/libkdtp
+# Created:			Anthony Mallet on Tue, 13 Dec 2016
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+LIBKDTP_DEPEND_MK:=	${LIBKDTP_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libkdtp
+endif
+
+ifeq (+,$(LIBKDTP_DEPEND_MK)) # --------------------------------------------
+
+PREFER.libkdtp?=	robotpkg
+
+DEPEND_USE+=		libkdtp
+
+DEPEND_ABI.libkdtp?=	libkdtp>=1.0
+DEPEND_DIR.libkdtp?=	../../path/libkdtp
+
+SYSTEM_SEARCH.libkdtp=\
+  'include/libkdtp.h'					\
+  'lib/pkgconfig/libkdtp.pc:/Version/s/[^0-9.]//gp'
+
+endif # LIBKDTP_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
