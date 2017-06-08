b8062cd351b56f77037301ef0b01acd8402edd69
Anthony Mallet <anthony.mallet@laas.fr>
2017-06-08 17:20:15 +0200

    [mk/sysdep] Teach robotpkg about gazebo

---
+++ mk/sysdep/gazebo.mk
@@ -0,0 +1,28 @@
+# robotpkg sysdep/gazebo.mk
+# Created:			Anthony Mallet on Thu  8 Jun 2017
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+GAZEBO_DEPEND_MK:=	${GAZEBO_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		gazebo
+endif
+
+ifeq (+,$(GAZEBO_DEPEND_MK)) # ---------------------------------------------
+
+PREFER.gazebo?=		system
+DEPEND_USE+=		gazebo
+DEPEND_ABI.gazebo?=	gazebo>=5
+
+SYSTEM_SEARCH.gazebo=	\
+  'bin/gazebo{-[0-9]*,}:1s/[^.0-9]//gp:% -v | cat'			\
+  'include/gazebo{-[0-9]*,}/gazebo/gazebo_config.h:/VERSION_FULL/s/[^.0-9]//gp'	\
+  'lib/libgazebo.so'							\
+  'lib/cmake/gazebo/gazebo-config.cmake'				\
+  'lib/pkgconfig/gazebo.pc:/Version/s/[^.0-9]//gp'
+
+SYSTEM_PKG.Fedora.gazebo=gazebo-devel
+
+endif # GAZEBO_DEPEND_MK ---------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
