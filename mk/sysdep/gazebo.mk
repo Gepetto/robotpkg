3d055ec768bec4caaa1a41ed863b3f6391eda902
Anthony Mallet <anthony.mallet@laas.fr>
2017-06-09 15:10:49 +0200

    [mk/sysdep] Do not require gazebo>=5 by default
    Ubuntu-1204 has gazebo 1.9, which is fine. Let the -config.cmake file be
    optional, it does not exist before version 5.

--- mk/sysdep/gazebo.mk
+++ mk/sysdep/gazebo.mk
@@ -12,13 +12,13 @@ ifeq (+,$(GAZEBO_DEPEND_MK)) # ---------------------------------------------
 
 PREFER.gazebo?=		system
 DEPEND_USE+=		gazebo
-DEPEND_ABI.gazebo?=	gazebo>=5
+DEPEND_ABI.gazebo?=	gazebo>=1
 
 SYSTEM_SEARCH.gazebo=	\
-  'bin/gazebo{-[0-9]*,}:1s/[^.0-9]//gp:% -v | cat'			\
+  'bin/gazebo{-[0-9]*,}:1s/[^.0-9]//gp:echo %'				\
   'include/gazebo{-[0-9]*,}/gazebo/gazebo_config.h:/VERSION_FULL/s/[^.0-9]//gp'	\
   'lib/libgazebo.so'							\
-  'lib/cmake/gazebo/gazebo-config.cmake'				\
+  '{lib/cmake/gazebo/gazebo-config.cmake,}'				\
   'lib/pkgconfig/gazebo.pc:/Version/s/[^.0-9]//gp'
 
 SYSTEM_PKG.Fedora.gazebo=gazebo-devel
