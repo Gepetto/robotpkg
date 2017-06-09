900460c12eb7067de8132847d0324fa9951341e0
Anthony Mallet <anthony.mallet@laas.fr>
2017-06-09 18:25:29 +0200

    [devel/ros-catkin] Manage CATKIN_MARKER_FILE (${PREFIX}/.catkin)
    Some ros shell/python script use a $CATKIN_MARKER_FILE to locate packages.
    It used to be created by this package, since it is a required dependency for
    ros packages. However, this did not work when using a system installation
    of ros-catkin.
    
    This commit adds a INSTALL/DEINSTALL pair of script that manage the
    creation of the marker file for any package depending on catkin.
    The packages using the marker file are tracked within the file itself (since
    its content is otherwise not used) and it is deleted when the last package is
    removed.
    
    Thanks to Olivier Stasse for the deep analysis of why this file is required and
    how it is used by ros scripts.
    
    Bump PKGREVISION.

--- devel/ros-catkin/depend.mk
+++ devel/ros-catkin/depend.mk
@@ -72,6 +72,10 @@ ifneq (,$(filter yes YES Yes,${USE_ROS_CATKIN}))
 	${TEST} ! -h CMakeLists.txt || ${RM} CMakeLists.txt;	\
 	${PREFIX.ros-catkin}/bin/catkin_init_workspace
 
+  # INSTALL script keeping track of ${PREFIX}/.catkin file
+  INSTALL_VARS+=	ECHO CHMOD CP GREP RM TEST
+  INSTALL_SRC+=		${DEPEND_DIR.ros-catkin}/files/catkin
+  DEINSTALL_SRC+=	${DEPEND_DIR.ros-catkin}/files/catkin
 endif
 
 endif # ROS_CATKIN_DEPEND_MK -----------------------------------------------
