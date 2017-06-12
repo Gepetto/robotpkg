a4559159c1452e217f203c6a9e5051710d649596
Anthony Mallet <anthony.mallet@laas.fr>
2017-06-12 17:13:08 +0200

    [devel/ros-catkin] Update script tracking CATKIN_MARKER_FILE
    Use PKG_REFCOUNT_DBDIR to store the packages referencing the catkin marker
    file, instead of the file itself. This way, print-PLIST can automatically
    ignore those files (that are not in any PLIST by construction).

--- devel/ros-catkin/depend.mk
+++ devel/ros-catkin/depend.mk
@@ -73,7 +73,7 @@ ifneq (,$(filter yes YES Yes,${USE_ROS_CATKIN}))
 	${PREFIX.ros-catkin}/bin/catkin_init_workspace
 
   # INSTALL script keeping track of ${PREFIX}/.catkin file
-  INSTALL_VARS+=	ECHO CHMOD CP GREP RM TEST
+  INSTALL_VARS+=	ECHO CHMOD CP FIND GREP MKDIR RM TEST
   INSTALL_SRC+=		${DEPEND_DIR.ros-catkin}/files/catkin
   DEINSTALL_SRC+=	${DEPEND_DIR.ros-catkin}/files/catkin
 endif
