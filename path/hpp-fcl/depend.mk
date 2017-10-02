3217409733608c2fa910c64aff7e1e67e3ee313c
Justin Carpentier <justin.carpentier@laas.fr>
2017-10-02 19:02:03 +0200

    [path/hpp-fcl] Update to 0.5.1
    This version breaks compatibility with 0.4. Use path/hpp-fcl04 for the old
    version.
    
    Changes since 0.4.2:
     * fix numerical issues
     * fully support Eigen for linear algebra.

--- path/hpp-fcl/depend.mk
+++ path/hpp-fcl/depend.mk
@@ -9,20 +9,20 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		hpp-fcl
 endif
 
-ifeq (+,$(HPP_FCL_DEPEND_MK)) # ---------------------------
+ifeq (+,$(HPP_FCL_DEPEND_MK)) # --------------------------------------------
 
 PREFER.hpp-fcl?=	robotpkg
 
 DEPEND_USE+=		hpp-fcl
 
-DEPEND_ABI.hpp-fcl?=	hpp-fcl>=0.4.2
+DEPEND_ABI.hpp-fcl?=	hpp-fcl>=0.5
 DEPEND_DIR.hpp-fcl?=	../../path/hpp-fcl
 
-SYSTEM_SEARCH.hpp-fcl=				  \
-	include/hpp/fcl/narrowphase/narrowphase.h \
-	lib/libhpp-fcl.so			  \
-	'lib/pkgconfig/hpp-fcl.pc:/Version/s/[^0-9.]//gp'
+SYSTEM_SEARCH.hpp-fcl=\
+  'include/hpp/fcl/narrowphase/narrowphase.h'	\
+  'lib/libhpp-fcl.so'				\
+  'lib/pkgconfig/hpp-fcl.pc:/Version/s/[^0-9.]//gp'
 
-endif # HPP_FCL_DEPEND_MK ---------------------------------
+endif # HPP_FCL_DEPEND_MK --------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
