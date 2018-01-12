92219e8773ddca1dd3906f08566e21e964cb3459
Anthony Mallet <anthony.mallet@laas.fr>
2018-01-12 11:59:29 +0100

    [sysutils/ros-rospack] Fix DEPEND_ABI for ros-kinetic
    For some reason, ros-kinetic now ships a rospack-2.4.4, a version greater than
    the one present in ros-lunar. Fix DEPEND_ABI accordingly.
    
    Spotted by Victor Arellano, thanks!

--- sysutils/ros-rospack/depend.mk
+++ sysutils/ros-rospack/depend.mk
@@ -25,7 +25,7 @@ DEPEND_ABI.ros-rospack.groovy?=	ros-rospack>=2.1<2.2
 DEPEND_ABI.ros-rospack.hydro?=	ros-rospack>=2.1<2.2
 DEPEND_ABI.ros-rospack.indigo?=	ros-rospack>=2.2<2.3
 DEPEND_ABI.ros-rospack.jade?=	ros-rospack>=2.3<2.4
-DEPEND_ABI.ros-rospack.kinetic?=ros-rospack>=2.3<2.4
+DEPEND_ABI.ros-rospack.kinetic?=ros-rospack>=2.3<2.5
 DEPEND_ABI.ros-rospack.lunar?=	ros-rospack>=2.4<2.5
 
 SYSTEM_SEARCH.ros-rospack=\
