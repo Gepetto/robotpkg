c4daf2e12528df259a1f8f784249969ea807e5c4
Anthony Mallet <anthony.mallet@laas.fr>
2018-02-09 00:52:23 +0100

    [mk/sysdep] Fix gazebo SYSTEM_SEARCH
    Don't put an empty file in SYSTEM_SEARCH.gazebo to make it optional: it
    actually matches the $prefix directory, which leads to unexpected results when
    building .deb binary package from this (because of dpkg-query -S $prefix).
    
    In this case, the gazebo-config.cmake can actually be found for all versions of
    gazebo, in different locations, though. Explicitly put the possibles locations.

--- mk/sysdep/gazebo.mk
+++ mk/sysdep/gazebo.mk
@@ -18,7 +18,7 @@ SYSTEM_SEARCH.gazebo=	\
   'bin/gazebo{-[0-9]*,}:1s/[^.0-9]//gp:echo %'				\
   'include/gazebo{-[0-9]*,}/gazebo/gazebo_config.h:/VERSION_FULL/s/[^.0-9]//gp'	\
   'lib/libgazebo.so'							\
-  '{lib/cmake/gazebo/gazebo-config.cmake,}'				\
+  '{lib/cmake/gazebo,share/gazebo/cmake}/gazebo-config.cmake'		\
   'lib/pkgconfig/gazebo.pc:/Version/s/[^.0-9]//gp'
 
 SYSTEM_PKG.Fedora.gazebo=gazebo-devel
