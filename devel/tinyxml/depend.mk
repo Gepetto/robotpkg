531ba1f87249acbfd5584535aa8948ecdae1a709
Anthony Mallet <anthony.mallet@laas.fr>
2018-02-21 16:54:01 +0100

    [devel/tinyxml] Move sysdep/tinyxml.mk to devel/tinyxml

--- devel/tinyxml/depend.mk
+++ devel/tinyxml/depend.mk
@@ -1,7 +1,6 @@
-# robotpkg depend.mk for:	devel/tinyxml
-# Created:			Charles Lesire on Fri, 19 Jan 2018
+# robotpkg sysdep/tinyxml.mk
+# Created:			Anthony Mallet on Thu 20 Jun 2013
 #
-
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 TINYXML_DEPEND_MK:=	${TINYXML_DEPEND_MK}+
 
@@ -12,15 +11,20 @@ endif
 ifeq (+,$(TINYXML_DEPEND_MK)) # --------------------------------------------
 
 PREFER.tinyxml?=	system
-
 DEPEND_USE+=		tinyxml
+DEPEND_ABI.tinyxml?=	tinyxml>=2
 
-DEPEND_ABI.tinyxml?=	tinyxml
-DEPEND_DIR.tinyxml?=	../../devel/tinyxml
+_tinyxmlvers=/(MAJOR|MINOR|PATCH)_VERS/ {gsub(/[^0-9]/,"");v=v $$0 "."}
+_tinyxmlvers+=END {gsub(/[.]$$/,"",v); print v}
 
-SYSTEM_SEARCH.tinyxml=\
-  'include/{,tinyxml/}tinyxml.h'	\
-  'lib/libtinyxml.{so,a}'
+SYSTEM_SEARCH.tinyxml=	\
+  'include/{,tinyxml/}tinyxml.h:p:${AWK} '\''${_tinyxmlvers}'\'' %'	\
+  'lib/libtinyxml.so'
+
+SYSTEM_PKG.Fedora.tinyxml=tinyxml-devel
+SYSTEM_PKG.Ubuntu.tinyxml=libtinyxml-dev
+SYSTEM_PKG.Debian.tinyxml=libtinyxml-dev
+SYSTEM_PKG.NetBSD.tinyxml=textproc/tinyxml
 
 endif # TINYXML_DEPEND_MK --------------------------------------------------
 
