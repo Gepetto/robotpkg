1ce9826ed8fde056019e9ca80f9077281768547b
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-03-20 19:11:32 +0100

    [optimization/roboptim-core] Update to 3.1
    Changes since 3.0:
    
    * ColMajor/RowMajor support has been improved (cf. %89). Default is back to
      ColMajor since this is Eigen's default mode, but that can be changed with a
      CMake variable.
    * Allocation checking has been improved (cf. %92).
    * Multiplots are now available with the matplotlib plotting backend (cf. %94).
    * Added vector_t and bool to the solver parameter types. As a consequence,
      std::string parameters should not rely on automatic conversion from const
      char* (cf. 7a0bbb7).
    * Renamed scale[s]* to scaling* (cf. 434559c). Previous methods/typedefs are
      currently kept for backward compatibility, but marked as deprecated.
    * Add new StructuredInput helper (cf. %96).
    * Add support for matrix plotting with matplotlib (cf. 5abd27e / af48e9b).
    
    Changes since 2.0:
    
    * New features:
        * Lots of new functions (cos, sin, etc.), operators (plus, minus, scalar,
          map, etc.) and decorators (cached function etc.),
        * Callback system (logger, callback multiplexer),
        * Simple Matplotlib support for visualization,
        * Function pools,
        * Set argument names (useful for plotting).
    * Improvements:
        * Support Eigen::Ref: now RobOptim functions accept blocks/segments of
          Eigen matrices as input,
        * Improved CachedFunction with LRU cache,
        * Automatic conversion of constraints/cost function types when creating
          problems,
        * Faster forward-difference rule for finite differences.
    * Other changes:
        * Removed exception specifiers (void function(...) throw())
        * Storage order was changed from Eigen's default (column-major) to
          row-major. The storage order is available in the GenericFunctionTraits
          (StorageOrder).
        * Move metaprogramming magic to roboptim/core/detail/utility.hh
        * Merge roboptim/core/visualization/util.hh with roboptim/core/util.hh
    
    Changes since 1.0:
    
    The version 2.0 of roboptim-core now depends on Eigen for matrix computations
    by default.
    Traits allow the user to use its own type. Support for Eigen dense and sparse
    matrices is built-in.

--- optimization/roboptim-core/depend.mk
+++ optimization/roboptim-core/depend.mk
@@ -3,24 +3,25 @@
 #
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH}+
-ROBOPTIM_CORE_DEPEND_MK:=${ROBOPTIM_CORE_DEPEND_MK}+
+ROBOPTIM_CORE_DEPEND_MK:=	${ROBOPTIM_CORE_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=	roboptim-core
 endif
 
-ifeq (+,$(ROBOPTIM_CORE_DEPEND_MK)) # ------------------------------
+ifeq (+,$(ROBOPTIM_CORE_DEPEND_MK)) # --------------------------------------
 
-PREFER.roboptim-core?=	robotpkg
+PREFER.roboptim-core?=		robotpkg
 
 DEPEND_USE+=			roboptim-core
 
-DEPEND_ABI.roboptim-core?=	roboptim-core>=0.4
+DEPEND_ABI.roboptim-core?=	roboptim-core>=3.1
 DEPEND_DIR.roboptim-core?=	../../optimization/roboptim-core
 
 SYSTEM_SEARCH.roboptim-core=\
-	include/roboptim/core/fwd.hh		\
-	'lib/pkgconfig/roboptim-core.pc:/Version/s/[^0-9.]//gp'
+  'include/roboptim/core/fwd.hh'				\
+  'lib/libroboptim-core.so'					\
+  'lib/pkgconfig/roboptim-core.pc:/Version/s/[^0-9.]//gp'
 
 endif # --------------------------------------------------------------------
 
