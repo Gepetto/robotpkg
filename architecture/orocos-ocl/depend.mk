7ee946b2c72714ee1408c48bbb99e730bddefcae
Anthony Mallet <anthony.mallet@laas.fr>
2018-03-28 13:51:49 +0200

    [architecture/orocos-ocl] Update to 2.9.0rc1
    Changes since 2.8.1 not available upstream.
    
    However, visible changes include:
    
     * Fix build with recent g++ and boost

--- architecture/orocos-ocl/depend.mk
+++ architecture/orocos-ocl/depend.mk
@@ -3,27 +3,26 @@
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-OROCOS_OCL_DEPEND_MK:=		${OROCOS_OCL_DEPEND_MK}+
+OROCOS_OCL_DEPEND_MK:=	${OROCOS_OCL_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		orocos-ocl
 endif
 
-ifeq (+,$(OROCOS_OCL_DEPEND_MK)) # ------------------------------------------
+ifeq (+,$(OROCOS_OCL_DEPEND_MK)) # -----------------------------------------
 
-PREFER.orocos-ocl?=		robotpkg
+PREFER.orocos-ocl?=	robotpkg
 
 DEPEND_USE+=		orocos-ocl
 
-DEPEND_ABI.orocos-ocl?=	orocos-ocl>=2.8.0rc1
+DEPEND_ABI.orocos-ocl?=	orocos-ocl>=2.9.0rc1
 DEPEND_DIR.orocos-ocl?=	../../architecture/orocos-ocl
 
 SYSTEM_SEARCH.orocos-ocl=\
-	include/orocos/ocl/OCL.hpp						\
-	lib/liborocos-ocl-deployment-gnulinux.so        \
-	lib/pkgconfig/ocl-gnulinux.pc
+  'include/orocos/ocl/OCL.hpp'			\
+  'lib/liborocos-ocl-deployment-gnulinux.so'	\
+  'lib/pkgconfig/ocl-gnulinux.pc:/Version/s[^0-9.]//gp'
 
-endif # OROCOS_OCL_DEPEND_MK ------------------------------------------------
+endif # OROCOS_OCL_DEPEND_MK -----------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
-
