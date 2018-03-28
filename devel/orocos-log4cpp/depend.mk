294ed56f49f02f9cea1cda7c7ce54cfd6770496d
Anthony Mallet <anthony.mallet@laas.fr>
2018-03-28 13:46:17 +0200

    [devel/orocos-log4cpp] Update to 2.9.0rc1
    Changes since 2.8.1 not available upstream.
    
    However, visible changes include:
    
     * Renamed headers from include/log4cpp to include/orocos/log4cpp
     * Renamed libraires from liblog4cpp.so to liborocos-log4cpp.so
     * Renamed pkg-config file from log4cpp.pc to rocos-log4cpp.pc

--- devel/orocos-log4cpp/depend.mk
+++ devel/orocos-log4cpp/depend.mk
@@ -2,28 +2,27 @@
 # Created:			Arnaud Degroote on Wed, 21 Aug 2013
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-OROCOS_LOG4CPP_DEPEND_MK:=		${OROCOS_LOG4CPP_DEPEND_MK}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+OROCOS_LOG4CPP_DEPEND_MK:=	${OROCOS_LOG4CPP_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		orocos-log4cpp
+DEPEND_PKG+=			orocos-log4cpp
 endif
 
-ifeq (+,$(OROCOS_LOG4CPP_DEPEND_MK)) # ------------------------------------------
+ifeq (+,$(OROCOS_LOG4CPP_DEPEND_MK)) # -------------------------------------
 
 PREFER.orocos-log4cpp?=		robotpkg
 
-DEPEND_USE+=		orocos-log4cpp
+DEPEND_USE+=			orocos-log4cpp
 
-DEPEND_ABI.orocos-log4cpp?=	orocos-log4cpp>=2.8.0rc1
+DEPEND_ABI.orocos-log4cpp?=	orocos-log4cpp>=2.9.0rc1
 DEPEND_DIR.orocos-log4cpp?=	../../devel/orocos-log4cpp
 
 SYSTEM_SEARCH.orocos-log4cpp=\
-	include/log4cpp/config.h \
-	lib/liblog4cpp.so \
-	lib/pkgconfig/log4cpp.pc
+  'include/orocos/log4cpp/config.h'				\
+  'lib/liborocos-log4cpp.so'					\
+  'lib/pkgconfig/orocos-log4cpp.pc:/Version/s/[^0-9.]//gp'
 
-endif # OROCOS_LOG4CPP_DEPEND_MK ------------------------------------------------
+endif # OROCOS_LOG4CPP_DEPEND_MK -------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
-
