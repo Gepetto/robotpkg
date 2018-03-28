c21f81611edb3542d16956b59cdcf73844c78e49
Anthony Mallet <anthony.mallet@laas.fr>
2018-03-28 13:49:24 +0200

    [middleware/orocos-rtt] Update to 2.9.0rc1
    Changes since 2.8.1 not available upstream.
    
    However visible changes include:
    
     * Fix build with recent g++ and boost
     * patch-ac for boost >= 1.61 applied upstream
    
    Still need patch-ab to fix gcc detection for gcc >= 7

--- middleware/orocos-rtt/depend.mk
+++ middleware/orocos-rtt/depend.mk
@@ -3,27 +3,26 @@
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-OROCOS_RTT_DEPEND_MK:=		${OROCOS_RTT_DEPEND_MK}+
+OROCOS_RTT_DEPEND_MK:=	${OROCOS_RTT_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		orocos-rtt
 endif
 
-ifeq (+,$(OROCOS_RTT_DEPEND_MK)) # ------------------------------------------
+ifeq (+,$(OROCOS_RTT_DEPEND_MK)) # -----------------------------------------
 
-PREFER.orocos-rtt?=		robotpkg
+PREFER.orocos-rtt?=	robotpkg
 
 DEPEND_USE+=		orocos-rtt
 
-DEPEND_ABI.orocos-rtt?=	orocos-rtt>=2.8.0rc1
+DEPEND_ABI.orocos-rtt?=	orocos-rtt>=2.9.0rc1
 DEPEND_DIR.orocos-rtt?=	../../middleware/orocos-rtt
 
 SYSTEM_SEARCH.orocos-rtt=\
-	include/rtt/RTT.hpp					\
-	lib/liborocos-rtt-gnulinux.so		\
-	lib/pkgconfig/orocos-rtt-gnulinux.pc
+  'include/rtt/RTT.hpp'					\
+  'lib/liborocos-rtt-gnulinux.so'			\
+  'lib/pkgconfig/orocos-rtt-gnulinux.pc:/Version/s/[^0-9.]//gp'
 
-endif # OROCOS_RTT_DEPEND_MK ------------------------------------------------
+endif # OROCOS_RTT_DEPEND_MK -----------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
-
