8722f859a8e947cf641b39a5b9d8bcf9a50cd495
Anthony Mallet <anthony.mallet@laas.fr>
2018-04-12 10:41:17 +0200

    [optimization/qpoases] Import qpOASES-3.2.1 (from wip/)
    qpOASES is an open-source C++ implementation of the recently proposed
    online active set strategy, which was inspired by important observations
    from the field of parametric quadratic programming (QP). It has several
    theoretical features that make it particularly suited for model predictive
    control (MPC) applications. Further numerical modifications have made
    qpOASES a reliable QP solver, even when tackling semi-definite, ill-posed or
    degenerated QP problems. Moreover, several interfaces to third-party software
    like  Matlab or  Simulink are provided that make qpOASES easy-to-use even for
    users without knowledge of C/C++.

---
+++ optimization/qpoases/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	optimization/qpOASES
+# Created:			Rohan Budhiraja on Fri, 8 Apr 2016
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+QPOASES_DEPEND_MK:=	${QPOASES_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		qpoases
+endif
+
+ifeq (+,$(QPOASES_DEPEND_MK)) # --------------------------------------------
+
+PREFER.qpoases?=	robotpkg
+
+DEPEND_USE+=		qpoases
+
+DEPEND_ABI.qpoases?=	qpoases>=3.2
+DEPEND_DIR.qpoases?=	../../optimization/qpoases
+
+SYSTEM_SEARCH.qpoases=\
+  'include/qpOASES.hpp:/\\version/s/[^0-9.]//gp'
+  'lib/libqpOASES.{so,a}'
+
+endif # qpOASES_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
