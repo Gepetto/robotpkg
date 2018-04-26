021b0f3fa0cab39ec5b1bab418ffec56d76bb7f0
Anthony Mallet <anthony.mallet@laas.fr>
2018-04-26 19:04:27 +0200

    [math/py-pinocchio] Import py-pinocchio
    Pinocchio is a C++ library for dynamic computations focusing on robotic,
    computer animation and biomechanical applications.
    
    This package provides the python bindings for pinocchio.

---
+++ math/py-pinocchio/depend.mk
@@ -0,0 +1,30 @@
+# robotpkg depend.mk for:	math/py-pinocchio
+# Created:			Olivier Stasse on Thu, 4 Feb 2016
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_PINOCCHIO_DEPEND_MK:=	${PY_PINOCCHIO_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			py-pinocchio
+endif
+
+ifeq (+,$(PY_PINOCCHIO_DEPEND_MK)) # ---------------------------------------
+
+PREFER.py-pinocchio?=		robotpkg
+
+SYSTEM_SEARCH.py-pinocchio=\
+  '${PYTHON_SYSLIBSEARCH}/pinocchio/__init__.py'
+
+# no version information is present in any of the installed files: one cannot
+# require a specific version
+DEPEND_USE+=			py-pinocchio
+
+DEPEND_ABI.py-pinocchio?=	${PKGTAG.python-}pinocchio
+DEPEND_DIR.py-pinocchio?=	../../math/py-pinocchio
+
+include ../../mk/sysdep/python.mk
+
+endif # PY_PINOCCHIO_DEPEND_MK ---------------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
