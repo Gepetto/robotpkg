14256ddc3f9e27bb18973299cead377f6ef05b98
Anthony Mallet <anthony.mallet@laas.fr>
2018-04-26 19:03:34 +0200

    [math/pinocchio] Import pinochio-2.7.1r1 (from wip/)
    Pinocchio is a C++ library for dynamic computations focusing on robotic,
    computer animation and biomechanical applications. It is based on the dynamic
    multi-body computations formalized by Featherstone in his book.
    The software comes with a python wrapping and vizualisation tool, available
    separately in the py-pinocchio package.

---
+++ math/pinocchio/depend.mk
@@ -0,0 +1,33 @@
+# robotpkg depend.mk for:	math/pinocchio
+# Created:			Olivier Stasse on Thu, 4 Feb 2016
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PINOCCHIO_DEPEND_MK:=	${PINOCCHIO_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		pinocchio
+endif
+
+ifeq (+,$(PINOCCHIO_DEPEND_MK)) # ------------------------------------------
+
+PREFER.pinocchio?=	robotpkg
+
+SYSTEM_SEARCH.pinocchio=\
+  'include/pinocchio/config.hh:/PINOCCHIO_VERSION/s/[^0-9.]//gp'	\
+  'lib/libpinocchio.so'							\
+  'lib/pkgconfig/pinocchio.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_USE+=		pinocchio
+
+DEPEND_ABI.pinocchio?=	pinocchio>=1.1.2
+DEPEND_DIR.pinocchio?=	../../math/pinocchio
+
+endif # PINOCCHIO_DEPEND_MK ------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
+
+# pulled by the package public headers
+include ../../graphics/urdfdom/depend.mk
+include ../../math/eigen3/depend.mk
+include ../../path/hpp-fcl/depend.mk
