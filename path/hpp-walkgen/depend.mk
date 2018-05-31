9fb719f5e00a76d5a108ca99a4289f3380934b65
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-05-31 18:01:10 +0200

    [path/hpp-walkgen] Initial import
    Walk generator for HPP framework

---
+++ path/hpp-walkgen/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	path/hpp-walkgen
+# Created:			Guilhem Saurel on Wed, 14 Mar 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HPPWALKGEN_DEPEND_MK:=	${HPPWALKGEN_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		hpp-walkgen
+endif
+
+ifeq (+,$(HPPWALKGEN_DEPEND_MK)) # ---------------------------
+
+PREFER.hpp-walkgen?=	robotpkg
+
+DEPEND_USE+=		hpp-walkgen
+
+DEPEND_ABI.hpp-walkgen?=	hpp-walkgen>=4.1
+DEPEND_DIR.hpp-walkgen?=	../../path/hpp-walkgen
+
+SYSTEM_SEARCH.hpp-walkgen=			\
+	include/hpp/walkgen/fwd.hh	\
+	lib/libhpp-walkgen.so		\
+	'lib/pkgconfig/hpp-walkgen.pc:/Version/s/[^0-9.]//gp'
+
+endif # HPPWALKGEN_DEPEND_MK ---------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
