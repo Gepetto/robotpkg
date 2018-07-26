b5e1041abe34ebf5bddee56952217bce374c2d95
Anthony Mallet <anthony.mallet@laas.fr>
2018-07-26 10:55:49 +0200

    [math/hpp-pinocchio]  Export eigen3 dependency in depend.mk
    Since hpp-pinocchio public header require eigen3, make the latter an implicit
    dependency of packages using hpp-pinocchio.

--- math/hpp-pinocchio/depend.mk
+++ math/hpp-pinocchio/depend.mk
@@ -3,7 +3,7 @@
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-HPPPINOCCHIO_DEPEND_MK:=	${HPPPINOCCHIO_DEPEND_MK}+
+HPPPINOCCHIO_DEPEND_MK:=${HPPPINOCCHIO_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		hpp-pinocchio
@@ -18,10 +18,13 @@ DEPEND_USE+=		hpp-pinocchio
 DEPEND_ABI.hpp-pinocchio?=	hpp-pinocchio>=4.1
 DEPEND_DIR.hpp-pinocchio?=	../../math/hpp-pinocchio
 
-SYSTEM_SEARCH.hpp-pinocchio=			\
-	include/hpp/pinocchio/util.hh	\
-	lib/libhpp-pinocchio.so		\
-	'lib/pkgconfig/hpp-pinocchio.pc:/Version/s/[^0-9.]//gp'
+SYSTEM_SEARCH.hpp-pinocchio=		\
+  'include/hpp/pinocchio/util.hh'	\
+  'lib/libhpp-pinocchio.so'		\
+  'lib/pkgconfig/hpp-pinocchio.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_ABI.eigen3 += eigen3>=3.2
+include ../../math/eigen3/depend.mk
 
 endif # HPPPINOCCHIO_DEPEND_MK ---------------------------------
 
