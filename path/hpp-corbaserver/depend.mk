925c8b4ef94fe9d68d4707c39de439a49c1edb4a
Anthony Mallet <anthony.mallet@laas.fr>
2018-07-26 15:51:28 +0200

    [path/hpp-corbaserver] Export dependencies in depend.mk
    The installed .pc file of hpp-corbaserver adds 'Require' on some packages,
    thus making those packages a primary dependency for users of the package.

--- path/hpp-corbaserver/depend.mk
+++ path/hpp-corbaserver/depend.mk
@@ -19,9 +19,14 @@ DEPEND_ABI.hpp-corbaserver?=	hpp-corbaserver>=4.1
 DEPEND_DIR.hpp-corbaserver?=	../../path/hpp-corbaserver
 
 SYSTEM_SEARCH.hpp-corbaserver=\
-	include/hpp/corbaserver/server.hh				\
-	lib/libhpp-corbaserver.so					\
-	'lib/pkgconfig/hpp-corbaserver.pc:/Version/s/[^0-9.]//gp'
+  'include/hpp/corbaserver/server.hh'				\
+  'lib/libhpp-corbaserver.so'					\
+  'lib/pkgconfig/hpp-corbaserver.pc:/Version/s/[^0-9.]//gp'
+
+include ../../middleware/omniORB/depend.mk
+include ../../path/hpp-core/depend.mk
+include ../../path/hpp-constraints/depend.mk
+include ../../math/hpp-pinocchio/depend.mk
 
 endif # --------------------------------------------------------------------
 
