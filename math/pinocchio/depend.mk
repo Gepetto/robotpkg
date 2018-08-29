70a0490ee9e5643f5205ddd1104ec55eaa0557a0
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-08-29 15:27:06 +0200

    [math/pinocchio] Update to 1.3.1
    Changes since 1.2.9:
    - This new release introduces analytical derivatives in the corpus of Pinocchio.
    - This feature is still under development but can already be used both in C++ and Python.
    - This new release also fixes a bunch of bugs related to Eigen and Boost.
    
    Changes since 1.3.0:
    This new release corrects some bugs or bad deprecations
    
    Packaging changes:
    - patch-aa has been updated
    - patch-ab has been applied upstream
    - new patch-{ac,ad} added to fix compilation with gcc-8

--- math/pinocchio/depend.mk
+++ math/pinocchio/depend.mk
@@ -20,7 +20,7 @@ SYSTEM_SEARCH.pinocchio=\
 
 DEPEND_USE+=		pinocchio
 
-DEPEND_ABI.pinocchio?=	pinocchio>=1.2.8
+DEPEND_ABI.pinocchio?=	pinocchio>=1.3.1
 DEPEND_DIR.pinocchio?=	../../math/pinocchio
 
 endif # PINOCCHIO_DEPEND_MK ------------------------------------------------
