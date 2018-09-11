e2c02adc972927b53dff1b9bf93edf72475541b1
Anthony Mallet <anthony.mallet@laas.fr>
2018-09-11 19:14:53 +0200

    [graphics/ros-urdf] Import 1.13.1 "melodic"
    This package contains a C++ parser for the Unified Robot Description
    Format (URDF), which is an XML format for representing a robot model.
    The code API of the parser has been through our review process and will remain
    backwards compatible in future releases.
    
    It is a split of the deprecated ros-robot-model since the melodic release.

---
+++ graphics/ros-urdf/depend.mk
@@ -0,0 +1,44 @@
+# robotpkg depend.mk for:	graphics/ros-urdf
+# Created:			Anthony Mallet on Tue, 11 Sep 2018
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS_URDF_DEPEND_MK:=		${ROS_URDF_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros-urdf
+endif
+
+ifeq (+,$(ROS_URDF_DEPEND_MK)) # -------------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-urdf?=	${PREFER.ros-base}
+SYSTEM_PREFIX.ros-urdf?=	${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=			ros-urdf
+ROS_DEPEND_USE+=		ros-urdf
+
+DEPEND_ABI.ros-urdf?=		ros-urdf>=1.9
+DEPEND_DIR.ros-urdf?=		../../graphics/ros-urdf
+
+DEPEND_ABI.ros-urdf.groovy?=	ros-urdf>=1.9<1.10
+DEPEND_ABI.ros-urdf.hydro?=	ros-urdf>=1.10<1.11
+DEPEND_ABI.ros-urdf.indigo?=	ros-urdf>=1.11<1.12
+DEPEND_ABI.ros-urdf.jade?=	ros-urdf>=1.11<1.12
+DEPEND_ABI.ros-urdf.kinetic?=	ros-urdf>=1.12<1.13
+DEPEND_ABI.ros-urdf.lunar?=	ros-urdf>=1.12<1.13
+DEPEND_ABI.ros-urdf.melodic?=	ros-urdf>=1.13<1.14
+
+SYSTEM_SEARCH.ros-urdf=\
+  'include/urdf/model.h'						\
+  'lib/liburdf.so'							\
+  'lib/pkgconfig/urdf.pc:/Version/s/[^0-9.]//gp'			\
+  'lib/pkgconfig/urdf_parser_plugin.pc:/Version/s/[^0-9.]//gp'		\
+  'share/urdf/cmake/urdfConfig.cmake'					\
+  'share/urdf_parser_plugin/cmake/urdf_parser_pluginConfig.cmake'	\
+  'share/urdf/package.xml:/<version>/s/[^0-9.]//gp'			\
+  'share/urdf_parser_plugin/package.xml:/<version>/s/[^0-9.]//gp'
+
+endif # ROS_URDF_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
