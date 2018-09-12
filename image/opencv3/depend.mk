dc9a27b7611bbc415e6cac5ea8bfbd66ace115c3
Anthony Mallet <anthony.mallet@laas.fr>
2018-09-12 22:58:26 +0200

    [image/opencv3] Import opencv-3.4.3
    The complete ChangeLogs are available online:
    https://github.com/opencv/opencv/releases
    
    This is the package for the version 3, local patches courtersy of pkgsrc.

---
+++ image/opencv3/depend.mk
@@ -0,0 +1,45 @@
+# robotpkg depend.mk for:	image/opencv3
+# Created:			Anthony Mallet on Wed, 12 Sep 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+OPENCV3_DEPEND_MK:=	${OPENCV3_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		opencv3
+endif
+
+ifeq (+,$(OPENCV3_DEPEND_MK)) # --------------------------------------------
+
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 12.04 14.04 16.04,${OS_VERSION}))
+    PREFER.opencv3?=	robotpkg
+  endif
+else ifeq (Debian,${OPSYS})
+  PREFER.opencv3?=	robotpkg
+else ifeq (CentOS,${OPSYS})
+  PREFER.opencv3?=	robotpkg
+endif
+PREFER.opencv3?=	system
+
+DEPEND_USE+=		opencv3
+DEPEND_ABI.opencv3?=	opencv3>=3
+DEPEND_DIR.opencv3?=	../../image/opencv3
+
+SYSTEM_SEARCH.opencv3=\
+  'include{,/opencv-*}/opencv/cv.h'					\
+  'include{,/opencv-*}/opencv/highgui.h'				\
+  'include{,/opencv-*}/opencv2/core/version.hpp'			\
+  'include{,/opencv-*}/opencv2/highgui/highgui.hpp'			\
+  'lib/pkgconfig/opencv{,-[0-9]*}.pc:/Version/s/[^.0-9]//gp'		\
+  '{lib/cmake,share,lib}/{OpenCV,opencv}/OpenCVConfig.cmake'
+
+SYSTEM_PKG.Debian.opencv3=	libopencv-dev libcv-dev libhighgui-dev
+SYSTEM_PKG.Fedora.opencv3=	opencv-devel
+SYSTEM_PKG.Gentoo.opencv3=	media-libs/opencv
+SYSTEM_PKG.NetBSD.opencv3=	graphics/opencv
+
+endif # OPENCV3_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
