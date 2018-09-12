779260c99bafbc0bd355eea39edab7c0affabf1d
Anthony Mallet <anthony.mallet@laas.fr>
2018-09-12 17:21:47 +0200

    [image/ros-vision-opencv] Update to 1.13.0 "melodic"
    This requires opencv >= 3.

--- image/ros-vision-opencv/depend.mk
+++ image/ros-vision-opencv/depend.mk
@@ -30,16 +30,16 @@ DEPEND_ABI.ros-vision-opencv.lunar?=	ros-vision-opencv>=1.12<1.13
 DEPEND_ABI.ros-vision-opencv.melodic?=	ros-vision-opencv>=1.13<1.14
 
 SYSTEM_SEARCH.ros-vision-opencv=\
-	include/cv_bridge/cv_bridge.h					\
-	include/image_geometry/pinhole_camera_model.h			\
-	lib/libcv_bridge.so						\
-	lib/libimage_geometry.so					\
-	'share/cv_bridge/package.xml:/<version>/s/[^0-9.]//gp'		\
-	'share/image_geometry/package.xml:/<version>/s/[^0-9.]//gp'	\
-	'${PYTHON_SYSLIBSEARCH}/cv_bridge/__init__.py'			\
-	'${PYTHON_SYSLIBSEARCH}/image_geometry/__init__.py'		\
-	'lib/pkgconfig/cv_bridge.pc:/Version/s/[^0-9.]//gp'		\
-	'lib/pkgconfig/image_geometry.pc:/Version/s/[^0-9.]//gp'
+  'include/cv_bridge/cv_bridge.h'				\
+  'include/image_geometry/pinhole_camera_model.h'		\
+  'lib/libcv_bridge.so'						\
+  'lib/libimage_geometry.so'					\
+  'share/cv_bridge/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'share/image_geometry/package.xml:/<version>/s/[^0-9.]//gp'	\
+  '${PYTHON_SYSLIBSEARCH}/cv_bridge/__init__.py'		\
+  '${PYTHON_SYSLIBSEARCH}/image_geometry/__init__.py'		\
+  'lib/pkgconfig/cv_bridge.pc:/Version/s/[^0-9.]//gp'		\
+  'lib/pkgconfig/image_geometry.pc:/Version/s/[^0-9.]//gp'
 
 include ../../mk/sysdep/python.mk
 
