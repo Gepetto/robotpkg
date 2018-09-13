41a3046f90069ec72abc65a4cf4d6cc099630bbd
Anthony Mallet <anthony.mallet@laas.fr>
2018-09-13 13:55:00 +0200

    [image/opencv] Rename opencv to opencv2 and provide an opencv2/3 alternative
    This commit creates an alternative for opencv. Available options are opencv2
    and opencv3, for version versions 2 and 3 respectively.
    
    The main point is to allow building opencv3 as a robotpkg package where it is
    not available as a system package (i.e. most distributions), to support
    ros-melodic.

--- image/opencv/depend.mk
+++ image/opencv/depend.mk
@@ -1,35 +1,103 @@
-# robotpkg depend.mk for:	image/opencv
-# Created:			Anthony Mallet on Fri, 14 Mar 2008
+#
+# Copyright (c) 2018 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution  and  use  in  source  and binary  forms,  with  or  without
+# modification, are permitted provided that the following conditions are met:
+#
+#   1. Redistributions of  source  code must retain the  above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice and  this list of  conditions in the  documentation and/or
+#      other materials provided with the distribution.
+#
+# THE SOFTWARE  IS PROVIDED "AS IS"  AND THE AUTHOR  DISCLAIMS ALL WARRANTIES
+# WITH  REGARD   TO  THIS  SOFTWARE  INCLUDING  ALL   IMPLIED  WARRANTIES  OF
+# MERCHANTABILITY AND  FITNESS.  IN NO EVENT  SHALL THE AUTHOR  BE LIABLE FOR
+# ANY  SPECIAL, DIRECT,  INDIRECT, OR  CONSEQUENTIAL DAMAGES  OR  ANY DAMAGES
+# WHATSOEVER  RESULTING FROM  LOSS OF  USE, DATA  OR PROFITS,  WHETHER  IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR  OTHER TORTIOUS ACTION, ARISING OUT OF OR
+# IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+#                                           Anthony Mallet on Wed Sep 12 2018
 #
 
+#
+# OpenCV alternative
+#
+# This file determines which OpenCV version is used as a dependency for
+# a package.
+#
+# === User-settable variables ===
+#
+# PREFER_ALTERNATIVE.opencv
+#	The preferred OpenCV version to use. The order of the entries matters,
+#	since earlier entries are preferred over later ones.
+#
+#	Possible values: opencv2 opencv3
+#	Default: opencv3 opencv2
+#
+# === Package-settable variables ===
+#
+# DEPEND_ABI.opencv
+#	The OpenCV versions that are acceptable for the package.
+#
+#	Possible values: any pattern
+#	Default: opencv>=2.2.0
+#
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 OPENCV_DEPEND_MK:=	${OPENCV_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		opencv
+  DEPEND_PKG+=		${PKG_ALTERNATIVE.opencv}
 endif
 
 ifeq (+,$(OPENCV_DEPEND_MK)) # ---------------------------------------------
 
-PREFER.opencv?=		system
+DEPEND_USE+=		${PKG_ALTERNATIVE.opencv}
 
-DEPEND_USE+=		opencv
+# define an alternative for available opencvs packages
+PKG_ALTERNATIVES+=	opencv
+PKG_ALTERNATIVES.opencv=opencv2 opencv3
+
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+ifeq (Debian,${OPSYS})
+  PREFER_ALTERNATIVE.opencv?=	opencv2 opencv3
+else ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 12.04 14.04 16.04,${OS_VERSION}))
+    PREFER_ALTERNATIVE.opencv?=	opencv2 opencv3
+  endif
+else ifeq (CentOS,${OPSYS})
+  PREFER_ALTERNATIVE.opencv?=	opencv2 opencv3
+else ifeq (Gentoo,${OS_FAMILY})
+  PREFER_ALTERNATIVE.opencv?=	opencv2 opencv3
+endif
+PREFER_ALTERNATIVE.opencv?=	opencv3 opencv2
+
 DEPEND_ABI.opencv?=	opencv>=2.2.0
-DEPEND_DIR.opencv?=	../../image/opencv
 
-SYSTEM_SEARCH.opencv=\
-  'include{,/opencv-*}/opencv/cv.h'					\
-  'include{,/opencv-*}/opencv/highgui.h'				\
-  'include{,/opencv-*}/opencv2/core/version.hpp'			\
-  'include{,/opencv-*}/opencv2/highgui/highgui.hpp'			\
-  'lib/pkgconfig/opencv{,-[0-9]*}.pc:/Version/s/[^.0-9]//gp'		\
-  '{lib/cmake,share,lib}/{OpenCV,opencv}/OpenCVConfig.cmake'
+PKG_ALTERNATIVE_DESCR.opencv2= Use opencv-2
+define PKG_ALTERNATIVE_SELECT.opencv2
+  $(call preduce,${DEPEND_ABI.opencv} opencv>=2<3)
+endef
+define PKG_ALTERNATIVE_SET.opencv2
+  _opencv_abi:=$(subst opencv,opencv2,${PKG_ALTERNATIVE_SELECT.opencv2})
+  DEPEND_ABI.opencv2?=	$(strip ${_opencv_abi})
 
-SYSTEM_PKG.Debian.opencv=	libopencv-dev libcv-dev libhighgui-dev
-SYSTEM_PKG.Fedora.opencv=	opencv-devel
-SYSTEM_PKG.Gentoo.opencv=	media-libs/opencv
-SYSTEM_PKG.NetBSD.opencv=	graphics/opencv
+  include ../../image/opencv2/depend.mk
+endef
 
-endif # OPENCV_DEPEND_MK ---------------------------------------------------
+PKG_ALTERNATIVE_DESCR.opencv3= Use opencv-3
+define PKG_ALTERNATIVE_SELECT.opencv3
+  $(call preduce,${DEPEND_ABI.opencv} opencv>=3<4)
+endef
+define PKG_ALTERNATIVE_SET.opencv3
+  _opencv_abi:=$(subst opencv,opencv3,${PKG_ALTERNATIVE_SELECT.opencv3})
+  DEPEND_ABI.opencv3?=	$(strip ${_opencv_abi})
+
+  include ../../image/opencv3/depend.mk
+endef
+
+endif # OPENCV_DEPEND_MK ------------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
---
+++ image/opencv2/depend.mk
@@ -0,0 +1,46 @@
+# robotpkg depend.mk for:	image/opencv2
+# Created:			Anthony Mallet on Fri, 14 Mar 2008
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+OPENCV2_DEPEND_MK:=	${OPENCV2_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		opencv2
+endif
+
+ifeq (+,$(OPENCV2_DEPEND_MK)) # --------------------------------------------
+
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 12.04 14.04 16.04,${OS_VERSION}))
+    PREFER.opencv2?=	system
+  endif
+  PREFER.opencv2?=	robotpkg
+else ifeq (Fedora,${OPSYS})
+  PREFER.opencv2?=	robotpkg
+else ifeq (NetBSD,${OPSYS})
+  PREFER.opencv2?=	robotpkg
+endif
+PREFER.opencv2?=	system
+
+DEPEND_USE+=		opencv2
+DEPEND_ABI.opencv2?=	opencv2>=2.2.0<3
+DEPEND_DIR.opencv2?=	../../image/opencv2
+
+SYSTEM_SEARCH.opencv2=\
+  'include{,/opencv-*}/opencv/cv.h'					\
+  'include{,/opencv-*}/opencv/highgui.h'				\
+  'include{,/opencv-*}/opencv2/core/version.hpp'			\
+  'include{,/opencv-*}/opencv2/highgui/highgui.hpp'			\
+  'lib/pkgconfig/opencv{,-[0-9]*}.pc:/Version/s/[^.0-9]//gp'		\
+  '{lib/cmake,share,lib}/{OpenCV,opencv}/OpenCVConfig.cmake'
+
+SYSTEM_PKG.Debian.opencv2=	libopencv-dev libcv-dev libhighgui-dev
+SYSTEM_PKG.Fedora.opencv2=	opencv-devel
+SYSTEM_PKG.Gentoo.opencv2=	media-libs/opencv
+SYSTEM_PKG.NetBSD.opencv2=	graphics/opencv
+
+endif # OPENCV2_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
