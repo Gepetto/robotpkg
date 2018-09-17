58542a8f72881716daefdf8f67b93938fa911d6d
Anthony Mallet <anthony.mallet@laas.fr>
2018-09-17 18:58:02 +0200

    [mk/sysdep] Teach robotpkg about python gnupg module
    There is a hack for NetBSD (pkgsrc): it looks for python gnupginterface
    instead. This is temporary until pkgsrc gets a proper python gnupg package.

---
+++ mk/sysdep/py-gnupg.mk
@@ -0,0 +1,31 @@
+# robotpkg sysdep/py-gnupg.mk
+# Created:			Anthony Mallet on Mon, 17 Sep 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PYGNUPG_DEPEND_MK:=	${PYGNUPG_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		py-gnupg
+endif
+
+ifeq (+,$(PYGNUPG_DEPEND_MK)) # --------------------------------------------
+
+PREFER.py-gnupg?=	system
+
+DEPEND_USE+=		py-gnupg
+DEPEND_ABI.py-gnupg?=	${PKGTAG.python-}gnupg>=0
+
+# Also look for python-gnupginterface util pkgsrc gets a real python-gnupg
+SYSTEM_SEARCH.py-gnupg=\
+  '${PYTHON_SYSLIBSEARCH}/{gnupg,GnuPGInterface}.py:/__version__/s/[^0-9.]//gp'
+
+SYSTEM_PKG.RedHat.py-gnupg= python${PYTHON_MAJOR}-gnupg
+SYSTEM_PKG.Debian.py-gnupg= python$(subst 2,,${PYTHON_MAJOR})-gnupg
+SYSTEM_PKG.NetBSD.py-gnupg= pkgsrc/security/${PKGTAG.python-}gnupg
+
+include ../../mk/sysdep/python.mk
+
+endif # PYGNUPG_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
