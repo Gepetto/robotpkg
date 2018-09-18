3d5cc608626132a25a54e8412623b4a13c60a33e
Anthony Mallet <anthony.mallet@laas.fr>
2018-09-18 11:32:58 +0200

    [math/geometry] Don't force C++11 for ros<melodic
    ros<melodic should not pull a C++11 compiler through this depend.mk
    
    Declare it only through the "melodic" alternative selection macro.

--- math/ros-geometry/depend.mk
+++ math/ros-geometry/depend.mk
@@ -47,7 +47,9 @@ SYSTEM_SEARCH.ros-geometry=\
   'lib/pkgconfig/tf_conversions.pc:/Version/s/[^0-9.]//gp'
 
 include ../../math/eigen3/depend.mk
-include ../../mk/language/c++11.mk
+
+# headers use C++11 in ros-melodic
+PKG_ALTERNATIVE_SET.melodic += $(eval include ../../mk/language/c++11.mk)
 
 endif # ROS_GEOMETRY_DEPEND_MK ---------------------------------------------
 
