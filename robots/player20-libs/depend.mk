20cf661f78d08f99ebd380cbc783a754ce987c52
Anthony Mallet <anthony.mallet@laas.fr>
2018-09-20 11:41:14 +0200

    [robots/player20-libs] Headers leak a dependency on RPC, add rpc to depend.mk
    This fixes the build for packages using this package headers when libtirpc must
    be used.
    
    While here, fix SYSTEM_SEARCH.

--- robots/player20-libs/depend.mk
+++ robots/player20-libs/depend.mk
@@ -8,7 +8,7 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=	player20-libs
 endif
 
-ifeq (+,$(PLAYER20_LIBS_DEPEND_MK))
+ifeq (+,$(PLAYER20_LIBS_DEPEND_MK)) # --------------------------------------
 
 PREFER.player20-libs?=		robotpkg
 
@@ -17,8 +17,16 @@ DEPEND_USE+=			player20-libs
 DEPEND_ABI.player20-libs?=	player20-libs>=2.0
 DEPEND_DIR.player20-libs?=	../../robots/player20-libs
 
-SYSTEM_SEARCH.player20-libs=	include/player-2.0/libplayerc/*.h
+SYSTEM_SEARCH.player20-libs=\
+  'include/player-2.0/libplayerc/playerc.h'		\
+  'lib/pkgconfig/playerc.pc:/Version/s/[^0-9.]//gp'
 
-endif
+# headers leak a dependency on rpc
+include ../../mk/language/rpc.mk
+
+# If libtirpc rpc alternative is used
+INCLUDE_DIRS.libtirpc?=	include/tirpc
+
+endif # PLAYER20_LIBS_DEPEND_MK --------------------------------------------
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
