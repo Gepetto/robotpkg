5e24285e407c9922fdd9ebf7f05454fabd5efed1
Anthony Mallet <anthony.mallet@laas.fr>
2018-09-25 11:41:02 +0200

    [motion/ros-orocos-kdl] Rename the package to ros-orocos-kinematics-dynamics
    This is to be consistent with the ros (meta-)package name. Add a conflict on
    the previous package name.

--- motion/ros-orocos-kinematics-dynamics/depend.mk
+++ motion/ros-orocos-kinematics-dynamics/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg depend.mk for:	motion/ros-orocos-kdl
+# robotpkg depend.mk for:	motion/ros-orocos-kinematics-dynamics
 # Created:			Anthony Mallet on Thu,  4 Jul 2013
 #
 
@@ -6,24 +6,26 @@ DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 ROS_OROCOS_KDL_DEPEND_MK:=	${ROS_OROCOS_KDL_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=			ros-orocos-kdl
+DEPEND_PKG+=			ros-orocos-kinematics-dynamics
 endif
 
 ifeq (+,$(ROS_OROCOS_KDL_DEPEND_MK)) # -------------------------------------
 
 include ../../meta-pkgs/ros-base/depend.common
-PREFER.ros-orocos-kdl?=		${PREFER.ros-base}
-SYSTEM_PREFIX.ros-orocos-kdl?=	${SYSTEM_PREFIX.ros-base}
+PREFER.ros-orocos-kinematics-dynamics?=		${PREFER.ros-base}
+SYSTEM_PREFIX.ros-orocos-kinematics-dynamics?=	${SYSTEM_PREFIX.ros-base}
 
-DEPEND_USE+=			ros-orocos-kdl
-ROS_DEPEND_USE+=		ros-orocos-kdl
+DEPEND_USE+=			ros-orocos-kinematics-dynamics
+ROS_DEPEND_USE+=		ros-orocos-kinematics-dynamics
 
 DEPEND_ABI.ros+=		ros>=groovy
 
-DEPEND_ABI.ros-orocos-kdl?=	ros-orocos-kdl>=1.1
-DEPEND_DIR.ros-orocos-kdl?=	../../motion/ros-orocos-kdl
+DEPEND_ABI.ros-orocos-kinematics-dynamics?=\
+  ros-orocos-kinematics-dynamics>=1.1
+DEPEND_DIR.ros-orocos-kinematics-dynamics?=\
+  ../../motion/ros-orocos-kinematics-dynamics
 
-SYSTEM_SEARCH.ros-orocos-kdl=\
+SYSTEM_SEARCH.ros-orocos-kinematics-dynamics=\
   'share/orocos_kinematics_dynamics/package.xml:/<version>/s/[^0-9.]//gp'
 
 include ../../motion/orocos-kdl/depend.mk
