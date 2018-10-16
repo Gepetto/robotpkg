121c9282d6671dd00be815742274131ac5f192f3
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-10-16 17:54:28 +0200

    [graphics/gepetto-viewer-corba] Update to v5.0.0
    Changes since v4.2.0:
    - remove gepetto-viewer-server, fix %74, fix %83
    - using graphics::dynamic_pointer_cast instead of boost::
    - Minor changes
    
    Packaging changes since v4.2.0:
    - removed patch-aa, which has been applied upstream
    - updated to new namespace: gepetto
    - build the package in a standard directory to comply with CI
    - fixed namespace conflict in depend.mk (with gepetto-viewer)

--- graphics/gepetto-viewer-corba/depend.mk
+++ graphics/gepetto-viewer-corba/depend.mk
@@ -2,28 +2,28 @@
 # Created:			Florent Lamiraux on Sun, 8 Mar 2015
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-GEPETTOVIEWER_DEPEND_MK:=	${GEPETTOVIEWER_DEPEND_MK}+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+GEPETTO_VIEWER_CORBA_DEPEND_MK:=	${GEPETTO_VIEWER_CORBA_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		gepetto-viewer-corba
+DEPEND_PKG+=				gepetto-viewer-corba
 endif
 
-ifeq (+,$(GEPETTOVIEWER_DEPEND_MK)) # ---------------------------
+ifeq (+,$(GEPETTO_VIEWER_CORBA_DEPEND_MK)) # -------------------------------
 
-PREFER.gepetto-viewer-corba?=	robotpkg
+PREFER.gepetto-viewer-corba?=		robotpkg
 
-DEPEND_USE+=		gepetto-viewer-corba
+DEPEND_USE+=				gepetto-viewer-corba
 
 DEPEND_ABI.gepetto-viewer-corba?=	gepetto-viewer-corba>=2.2.0
 DEPEND_DIR.gepetto-viewer-corba?=	../../graphics/gepetto-viewer-corba
 
-SYSTEM_SEARCH.gepetto-viewer-corba=			\
-	include/gepetto/viewer/corba/client.hh	\
-	lib/libgepetto-viewer-corba.so		\
-	bin/gepetto-viewer-server		\
-	'lib/pkgconfig/gepetto-viewer-corba.pc:/Version/s/[^0-9.]//gp'
+SYSTEM_SEARCH.gepetto-viewer-corba=\
+  'bin/gepetto-gui'				\
+  'include/gepetto/viewer/corba/client.hh'	\
+  'lib/libgepetto-viewer-corba.so'		\
+  'lib/pkgconfig/gepetto-viewer-corba.pc:/Version/s/[^0-9.]//gp'
 
-endif # GEPETTOVIEWER_DEPEND_MK ---------------------------------
+endif # GEPETTO_VIEWER_CORBA_DEPEND_MK -------------------------------------
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
+DEPEND_DEPTH:=				${DEPEND_DEPTH:+=}
