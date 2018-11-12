f5c08c5a43c02741e0dd01b596af366b5174144d
Anthony Mallet <anthony.mallet@laas.fr>
2018-11-12 14:22:34 +0100

    [mk/sysdep] Let openscenegraph-qt dependency check the Qt version
    openscenegraph-qt is built with Qt: make sure to use the appropriate package
    depending on Qt version selection.
    
    Recent openscenegraph-qt provide a osgQt/Version header which contains the Qt
    version it was compiled with. If this is not present, assume it was Qt4. This
    works well enough in practice on the supported architectures.
    
    The check is done with an obfuscated sed program, that outputs 0 in case of
    version mismatch (and nothing otherwise) to make sure that this dependency does
    not match the required ABI (or that the test does not otherwise influence the
    regular osg-qt ABI check).

--- mk/sysdep/openscenegraph-qt.mk
+++ mk/sysdep/openscenegraph-qt.mk
@@ -14,16 +14,29 @@ PREFER.openscenegraph-qt?=	system
 
 DEPEND_USE+=			openscenegraph-qt
 
-DEPEND_ABI.openscenegraph-qt?=	openscenegraph-qt>=3
+DEPEND_ABI.openscenegraph-qt?=	openscenegraph-qt${QT_SELECT}>=3
 
+# make sure to use the right Qt version: this sed program reports version 0 if
+# there is a Qt version mismatch, nothing otherwise. If there is no
+# OSGQT_QT_VERSION, do the check against Qt4.
+_osgversionpat=\
+  /define OSGQT_QT_VERSION/{/VERSION.*${QT_SELECT}/!{s/.*/0/p;q}};	\
+  $${s/.*/4/;/${QT_SELECT}/!{s/.*/0/p;q}}
+
+# osg-qt>=3.4 defines the QT_VERSION, otherwise assume 4
 SYSTEM_SEARCH.openscenegraph-qt=\
-  'include/osgQt/Export'					\
+  'include/osgQt/{Version,Export}:${_osgversionpat}'		\
   'lib/libosgQt.so'						\
   'lib/pkgconfig/openscenegraph-osgQt.pc:/Version/s/[^0-9.]//gp'
 
-SYSTEM_PKG.Debian.openscenegraph-qt=	libopenscenegraph-dev
-SYSTEM_PKG.Fedora.openscenegraph-qt=	OpenSceneGraph-qt-devel
-SYSTEM_PKG.Arch.openscenegraph-qt=	osgqt (AUR)
+SYSTEM_PKG.Debian.openscenegraph-qt=\
+  libopenscenegraph-dev for ${PKG_ALTERNATIVE.qt}
+SYSTEM_PKG.Fedora.openscenegraph-qt=\
+  OpenSceneGraph-qt-devel for ${PKG_ALTERNATIVE.qt}
+SYSTEM_PKG.Arch.openscenegraph-qt=\
+  osgqt (AUR) for ${PKG_ALTERNATIVE.qt}
+
+include ../../mk/sysdep/qt.mk
 
 endif # OPENSCENEGRAPH_QT_DEPEND_MK -----------------------------------------
 
