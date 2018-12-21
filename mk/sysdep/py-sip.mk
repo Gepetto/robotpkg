6d2d4b1ed4810dddf07e55c9596e32908f5a9452
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-12-21 18:27:46 +0100

    [mk/sysdep/py-sip.mk] Fix SYSTEM_SEARCH on Arch, and add its SYSTEM_PKG
    Fixes #190

--- mk/sysdep/py-sip.mk
+++ mk/sysdep/py-sip.mk
@@ -21,9 +21,10 @@ DEPEND_ABI.py-sip?=	${PKGTAG.python}-sip>=4
 
 SYSTEM_SEARCH.py-sip=\
   'bin/sip{${PYTHON_VERSION},}:p:% -V'					\
-  'include/python${PYTHON_VERSION}/sip.h:/VERSION_STR/s/[^0-9.]//gp'	\
+  'include/{,python${PYTHON_VERSION}/}sip.h:/VERSION_STR/s/[^0-9.]//gp'	\
   '${PYTHON_SYSLIBSEARCH}/sip{,.*${DEB_HOST_MULTIARCH}}.so'
 
+SYSTEM_PKG.Arch.py-sip= 	python-sip sip
 SYSTEM_PKG.Debian.py-sip=	python-sip-dev (python-${PYTHON_VERSION})
 SYSTEM_PKG.Fedora.py-sip=	sip-devel (python-${PYTHON_VERSION})
 SYSTEM_PKG.NetBSD.py-sip=	x11/${PKGTAG.python}-sip
