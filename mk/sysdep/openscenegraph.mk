3bad87ef64662fcca19be98e4cb3166ad05c19f9
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-01-25 16:33:30 +0100

    [mk/sysdep/openscenegraph.mk] fix SYSTEMPKG.Debian

--- mk/sysdep/openscenegraph.mk
+++ mk/sysdep/openscenegraph.mk
@@ -21,7 +21,7 @@ SYSTEM_SEARCH.openscenegraph=\
   'lib/libosg.so'						\
   'lib/pkgconfig/openscenegraph.pc:/Version/s/[^0-9.]//gp'
 
-SYSTEM_PKG.Debian.openscenegraph=	openscenegraph
+SYSTEM_PKG.Debian.openscenegraph=	libopenscenegraph-dev
 SYSTEM_PKG.Arch.openscenegraph=		openscenegraph
 
 endif # OPENSCENEGRAPH_DEPEND_MK -------------------------------------------
