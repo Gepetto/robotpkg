0669f9ed9cb4e3e51b34f1470dc8a1494795561f
Anthony Mallet <anthony.mallet@laas.fr>
2019-02-05 15:35:15 +0100

    [architecture/genom3-pocolibs] Update to 1.13
    Changes since 1.12:
    
     * Fix a race condition in the dovent() client function, causing some events to
       be unseen. Each client now has it's own file descriptor for events.

--- architecture/genom3-pocolibs/depend.mk
+++ architecture/genom3-pocolibs/depend.mk
@@ -18,7 +18,7 @@ SYSTEM_SEARCH.genom3-pocolibs=\
 	lib/libpocolibs-client.so				\
 	'lib/pkgconfig/genom3-pocolibs.pc:/Version/s/[^0-9.]//gp'
 
-DEPEND_ABI.genom3-pocolibs?=	genom3-pocolibs>=1.8
+DEPEND_ABI.genom3-pocolibs?=	genom3-pocolibs>=1.13
 DEPEND_DIR.genom3-pocolibs?=	../../architecture/genom3-pocolibs
 
 endif # GENOM3_POCOLIBS_DEPEND_MK ------------------------------------------
