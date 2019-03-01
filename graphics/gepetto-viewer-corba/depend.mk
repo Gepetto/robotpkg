e37ca0b443d7917b63b52ba8b9675da84bef3fdf
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-03-01 10:27:48 +0100

    [graphics/gepetto-viewer-corba] Update to v5.1.2
    Changes since v5.1.1:
    - Bypass Omniorb name server
    - avoid rendering still scene.
    
    Packaging changes:
    - fix PKGNAME & DEPEND_ABI to properly use PKGTAG

--- graphics/gepetto-viewer-corba/depend.mk
+++ graphics/gepetto-viewer-corba/depend.mk
@@ -15,20 +15,17 @@ PREFER.gepetto-viewer-corba?=		robotpkg
 
 DEPEND_USE+=				gepetto-viewer-corba
 
-# depend on appropriate Qt version when using Qt, all versions otherwise.
-_gvc_qts={qt4,qt5}
-_gvc_qt=$(if $(filter qt,${PKG_ALTERNATIVES}),${PKG_ALTERNATIVE.qt},${_gvc_qts})
-
-DEPEND_ABI.gepetto-viewer-corba?=	${_gvc_qt}-gepetto-viewer-corba>=2.2.0
+DEPEND_ABI.gepetto-viewer-corba?=	${PKGTAG.python}-${PKGTAG.qt}-gepetto-viewer-corba>=2.2.0
 DEPEND_DIR.gepetto-viewer-corba?=	../../graphics/gepetto-viewer-corba
 
 SYSTEM_SEARCH.gepetto-viewer-corba=\
   'bin/gepetto-gui'							\
   'include/gepetto/viewer/corba/client.hh'				\
   'lib/libgepetto-viewer-corba.so'					\
-  $(foreach n,4 5,$(foreach _,'lib/pkgconfig/gepetto-viewer-corba.pc',	\
-    '$_:/qtversion=$n/p::qt$n-gepetto-viewer-corba'))			\
   'lib/pkgconfig/gepetto-viewer-corba.pc:/Version/s/[^0-9.]//gp'
+
+include ../../mk/sysdep/python.mk
+include ../../mk/sysdep/qt.mk
 
 endif # GEPETTO_VIEWER_CORBA_DEPEND_MK -------------------------------------
 
