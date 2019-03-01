82209a00eaa1f993f427052067f4fdab356443b3
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-03-01 10:45:28 +0100

    [graphics/hpp-gepetto-viewer] Update to v4.4.0
    Changes since v4.3.1:
    - Bind method displayRoadmap in ViewerFactory class.
    - Fix roadmap display.
    
    Packaging changes:
    - fix PKGNAME

--- graphics/hpp-gepetto-viewer/depend.mk
+++ graphics/hpp-gepetto-viewer/depend.mk
@@ -3,24 +3,28 @@
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-HPPGEPETTOVIEWER_DEPEND_MK:=	${HPPGEPETTOVIEWER_DEPEND_MK}+
+HPP_GEPETTO_VIEWER_DEPEND_MK:=	${HPP_GEPETTO_VIEWER_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		hpp-gepetto-viewer
 endif
 
-ifeq (+,$(HPPGEPETTOVIEWER_DEPEND_MK)) # ---------------------------
+ifeq (+,$(HPP_GEPETTO_VIEWER_DEPEND_MK)) # ---------------------------
 
 PREFER.hpp-gepetto-viewer?=	robotpkg
 
 DEPEND_USE+=		hpp-gepetto-viewer
 
-DEPEND_ABI.hpp-gepetto-viewer?=	hpp-gepetto-viewer>=4.2.0
+# depend on appropriate Qt version when using Qt, all versions otherwise.
+_hgv_qts={qt4,qt5}
+_hgv_qt=$(if $(filter qt,${PKG_ALTERNATIVES}),${PKG_ALTERNATIVE.qt},${_hgv_qts})
+
+DEPEND_ABI.hpp-gepetto-viewer?=	${PKGTAG.python}-${_hgv_qt}-hpp-gepetto-viewer>=4.2.0
 DEPEND_DIR.hpp-gepetto-viewer?=	../../graphics/hpp-gepetto-viewer
 
 SYSTEM_SEARCH.hpp-gepetto-viewer=			\
 	'lib/pkgconfig/hpp-gepetto-viewer.pc:/Version/s/[^0-9.]//gp'
 
-endif # HPPGEPETTOVIEWER_DEPEND_MK ---------------------------------
+endif # HPP_GEPETTO_VIEWER_DEPEND_MK ---------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
