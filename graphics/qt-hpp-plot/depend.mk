2f6cbbe0066267bfce8a0f68ee45d66d1fae063d
Florent Lamiraux <florent@laas.fr>
2019-05-29 13:40:56 +0200

    [graphics/qt-hpp-plot] Import hpp-plot-4.6.0 from wip/
    Graphical utilities for constraint graphs in hpp-manipulation

---
+++ graphics/qt-hpp-plot/depend.mk
@@ -0,0 +1,33 @@
+# robotpkg depend.mk for:	graphics/qt-hpp-plot
+# Created:			Guilhem Saurel on Tue, 26 Feb 2019
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+QT_HPP_PLOT_DEPEND_MK:=	${QT_HPP_PLOT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		qt-hpp-plot
+endif
+
+ifeq (+,$(QT_HPP_PLOT_DEPEND_MK)) # ----------------------------------------
+
+PREFER.qt-hpp-plot?=	robotpkg
+
+DEPEND_USE+=		qt-hpp-plot
+
+# depend on appropriate Qt version when using Qt, all versions otherwise.
+_hppp_qts={qt4,qt5}
+_hppp_qt=\
+  $(if $(filter qt,${PKG_ALTERNATIVES}),${PKG_ALTERNATIVE.qt},${_hppp_qts})
+
+DEPEND_ABI.qt-hpp-plot?=	${_hppp_qt}-hpp-plot>=4.6.0
+DEPEND_DIR.qt-hpp-plot?=	../../graphics/qt-hpp-plot
+
+SYSTEM_SEARCH.qt-hpp-plot=\
+  'include/hpp/plot/config.hh'	\
+  'lib/libhpp-plot.so'	\
+  'lib/pkgconfig/hpp-plot.pc:/Version/s/[^0-9.]//gp'
+
+endif # QT_HPP_PLOT_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
