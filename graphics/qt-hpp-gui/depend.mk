26ced399c2677ce6d7aeb554cdfdfbaa769d53f7
Florent Lamiraux <florent@laas.fr>
2019-05-29 13:48:55 +0200

    [graphics/qt-hpp-gui] Import qt-hpp-gui 4.6.1 from wip/
    Qt based GUI for HPP project.

---
+++ graphics/qt-hpp-gui/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	graphics/qt-hpp-gui
+# Created:			Guilhem Saurel on Tue, 26 Feb 2019
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+QT_HPP_GUI_DEPEND_MK:=	${QT_HPP_GUI_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		qt-hpp-gui
+endif
+
+ifeq (+,$(QT_HPP_GUI_DEPEND_MK)) # -----------------------------------------
+
+PREFER.qt-hpp-gui?=	robotpkg
+
+DEPEND_USE+=		qt-hpp-gui
+
+# depend on appropriate Qt version when using Qt, all versions otherwise.
+_hppg_qts={qt4,qt5}
+_hppg_qt=$(if $(filter qt,${PKG_ALTERNATIVES}),${PKG_ALTERNATIVE.qt},${_hppg_qts})
+
+DEPEND_ABI.qt-hpp-gui?=	${_hppg_qt}-hpp-gui>=4.6.1
+DEPEND_DIR.qt-hpp-gui?=	../../graphics/qt-hpp-gui
+
+SYSTEM_SEARCH.qt-hpp-gui=\
+ 'include/hpp/gui/config.hh'				\
+ 'lib/pkgconfig/hpp-gui.pc:/Version/s/[^0-9.]//gp'
+
+endif # QT_HPP_GUI_DEPEND_MK -----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
