ffe253f470780bb3605177ea211e16286174907f
Anthony Mallet <anthony.mallet@laas.fr>
2019-06-04 15:49:06 +0200

    [mk/sysdep] Provide a USE_QT5_COMPONENTS variable for Qt5 packages
    When requiring qt5-qtbase, one can specify a list of additional Qt5
    dependencies. This is useful for packages that whant to provide an alternative
    between Qt4 and Qt5 and cannot include required Qt5 dependencies directly.
    
    While here, add the cmake *Config.cmake files in SYSTEM_SEARCH.qt5-qtbase, so
    that robotpkg defines a *_DIR cmake variable. Keep the CMAKE_PREFIX_PATH in the
    meantime, until all Qt5 sysdep are fixed.
    
    Also add a C++11 dependency: Qt5 wants this.

--- mk/sysdep/qt5-qtbase.mk
+++ mk/sysdep/qt5-qtbase.mk
@@ -17,7 +17,8 @@ DEPEND_ABI.qt5-qtbase?=	qt5-qtbase>=5<6
 
 SYSTEM_SEARCH.qt5-qtbase=\
   'include/{,qt{,5}/}QtCore/qtcoreversion.h:/VERSION_STR/s/[^0-9.]//gp' \
-  'lib/libQt5Core.so'						    \
+  'lib/libQt5Core.so'							\
+  '{,lib}/cmake/Qt5Core/Qt5CoreConfig.cmake'				\
   'lib/pkgconfig/Qt5Core.pc:/Version/s/[^0-9.]//gp'
 
 SYSTEM_PREFIX.qt5-qtbase?=	${SYSTEM_PREFIX:=/qt5} ${SYSTEM_PREFIX}
@@ -27,11 +28,27 @@ SYSTEM_PKG.Debian.qt5-qtbase=	qtbase5-dev
 SYSTEM_PKG.NetBSD.qt5-qtbase=	x11/qt5-qtbase
 
 # this is required for cmake to locate the Config.cmake files
-CMAKE_PREFIX_PATH+=${PREFIX.qt5-qtbase}
+CMAKE_PREFIX_PATH.qt5-qtbase=	${PREFIX.qt5-qtbase}
 
 # define Qt version for qmake et al.
 export QT_SELECT=	5
 
+# USE_QT5_COMPONENTS selection
+USE_QT5_COMPONENTS?=
+override define _use_qt5_components
+  ifneq (,$(filter-out core,$1))
+    PREFER.qt5-$1?=	$${PREFER.qt5-qtbase}
+    DEPEND_ABI.qt5-$1?=	$$(subst qt5-qtbase,qt5-$1,$${DEPEND_ABI.qt5-qtbase})
+    include ../../mk/sysdep/qt5-$1.mk
+  endif
+endef
+
+# Qt wants C++11
+include ../../mk/language/c++11.mk
+
 endif # QT5_QTBASE_DEPEND_MK -----------------------------------------------
 
 DEPEND_DEPTH:= ${DEPEND_DEPTH:+=}
+
+# apply USE_QT5_COMPONENTS
+$(foreach _,${USE_QT5_COMPONENTS},$(eval $(call _use_qt5_components,$_)))
