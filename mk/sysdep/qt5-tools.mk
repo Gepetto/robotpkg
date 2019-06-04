94336127ebff554146d21fa6172461371eb205f5
Anthony Mallet <anthony.mallet@laas.fr>
2019-06-04 15:51:39 +0200

    [mk/sysdep] Teach robotpkg about qt5-qttools

---
+++ mk/sysdep/qt5-tools.mk
@@ -0,0 +1,30 @@
+# robotpkg sysdep/qt5-tools
+# Created: Anthony Mallet on 04 Jun 2019
+#
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+QT5_TOOLS_DEPEND_MK:=	${QT5_TOOLS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		qt5-tools
+endif
+
+ifeq (+,$(QT5_TOOLS_DEPEND_MK)) # ------------------------------------------
+
+PREFER.qt5-tools?=	system
+
+DEPEND_USE+=		qt5-tools
+DEPEND_ABI.qt5-tools?=	qt5-tools>=5.0.0
+
+SYSTEM_SEARCH.qt5-tools=\
+  '{,qt5/}include/{,qt5/{,include/}}Qt{,Designer}/QtDesigner'		\
+  '{lib,share/qt5/lib,qt5/lib,lib/qt5}/libQt5Designer.{so,a}'		\
+  '{,lib,qt5/lib}/cmake/Qt5Designer/Qt5DesignerConfig.cmake'		\
+  'lib/pkgconfig/Qt5Designer.pc:/Version/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Debian.qt5-tools=	qttools5-dev-tools
+SYSTEM_PKG.Fedora.qt5-tools=	qt5-qttools-devel
+SYSTEM_PKG.NetBSD.qt5-tools=	x11/qt5-qttools
+
+endif # QT5_TOOLS_DEPEND_MK ------------------------------------------
+
+DEPEND_DEPTH:= ${DEPEND_DEPTH:+=}
