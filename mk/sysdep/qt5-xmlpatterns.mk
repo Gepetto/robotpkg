d6c2ec6fd2cb369c2f310223cbc6b28ef3e9d38d
Anthony Mallet <anthony.mallet@laas.fr>
2019-06-04 15:51:55 +0200

    [mk/sysdep] Teach robotpkg about qt5-xmlpatterns package

---
+++ mk/sysdep/qt5-xmlpatterns.mk
@@ -0,0 +1,30 @@
+# robotpkg sysdep/qt5-xmlpatterns
+# Created: Anthony Mallet on 04 Jun 2019
+#
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+QT5_XMLPATTERNS_DEPEND_MK:=	${QT5_XMLPATTERNS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		qt5-xmlpatterns
+endif
+
+ifeq (+,$(QT5_XMLPATTERNS_DEPEND_MK)) # ------------------------------------
+
+PREFER.qt5-xmlpatterns?=	system
+
+DEPEND_USE+=			qt5-xmlpatterns
+DEPEND_ABI.qt5-xmlpatterns?=	qt5-xmlpatterns>=5.0.0
+
+SYSTEM_SEARCH.qt5-xmlpatterns=\
+  '{,qt5/}include/{,qt5/{,include/}}Qt{,XmlPatterns}/QtXmlPatternsVersion' \
+  '{lib,share/qt5/lib,qt5/lib,lib/qt5}/libQt5XmlPatterns.{so,a}'	\
+  '{,lib}/cmake/Qt5XmlPatterns/Qt5XmlPatternsConfig.cmake'		\
+  'lib/pkgconfig/Qt5XmlPatterns.pc:/Version/s/[^0-9.]//gp'
+
+SYSTEM_PKG.Debian.qt5-xmlpatterns=	libqt5xmlpatterns5-dev
+SYSTEM_PKG.Fedora.qt5-xmlpatterns=	qt5-qtxmlpatterns-devel
+SYSTEM_PKG.NetBSD.qt5-xmlpatterns=	x11/qt5-qtxmlpatterns
+
+endif # QT5_XMLPATTERNS_DEPEND_MK ------------------------------------------
+
+DEPEND_DEPTH:= ${DEPEND_DEPTH:+=}
