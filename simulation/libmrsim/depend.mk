9abbe428d040fa5fa9d915b655e0b66e2aa73caa
Anthony Mallet <anthony.mallet@laas.fr>
2019-06-11 15:41:27 +0200

    [simulation/libmrsim] Add libmrsim-1.0
    The library provides a pty emulating the tk3-mikrokopter communication protocol
    and implements all the tk3-mikrokopter features so that the hardware can be
    fully emulated.
    
    The rotor dynamics are simulated using a realistic motor model and a simple
    thrust/drag propeller model. The same hardware velocity controller as
    implemented in the tk3-mikrokopter software controls the propellers
    velocities. Then, a simple free-flying 6D rigid body dynamics simulation can
    also be used, although it is not meant as a generic physics engine (e.g. no
    collision, no atmosphere, ...).

---
+++ simulation/libmrsim/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	simulation/libmrsim
+# Created:			Anthony Mallet on Tue, 11 Jun 2019
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+LIBMRSIM_DEPEND_MK:=	${LIBMRSIM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libmrsim
+endif
+
+ifeq (+,$(LIBMRSIM_DEPEND_MK)) # -------------------------------------------
+
+PREFER.libmrsim?=	robotpkg
+
+SYSTEM_SEARCH.libmrsim=\
+  'include/libmrsim.h'					\
+  'lib/libmrsim.so'					\
+  'lib/pkgconfig/libmrsim.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_USE+=		libmrsim
+
+DEPEND_ABI.libmrsim?=	libmrsim>=1.0
+DEPEND_DIR.libmrsim?=	../../simulation/libmrsim
+
+endif # LIBMRSIM_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
