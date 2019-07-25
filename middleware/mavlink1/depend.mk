8a7d4587a3a78e442e39d7e3af4468835ed47f64
Anthony Mallet <anthony.mallet@laas.fr>
2019-07-25 14:10:47 +0200

    [middleware/mavlink1] Import mavlink-1.0.12 (from wip/)
    This is imported as mavlink1, because the original mavlink package in wip was
    versionned as 2015, so it would always be preferred over the regular 1.0.12
    version.
    
    mavlink is a library for lightweight communication between Micro Air Vehicles
    (swarm) and/or ground control stations. It serializes C-structs for serial
    channels and can be used with any type of radio modem.
    
    Messages definitions are created in XML, and then converted into C header
    files.

---
+++ middleware/mavlink1/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	middleware/mavlink1
+# Created:			Arnaud Degroote on Mon, 16 Dec 2013
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+MAVLINK_DEPEND_MK:=	${MAVLINK_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		mavlink1
+endif
+
+ifeq (+,$(MAVLINK_DEPEND_MK)) # --------------------------------------------
+
+PREFER.mavlink?=	robotpkg
+
+DEPEND_USE+=		mavlink1
+
+DEPEND_ABI.mavlink?=	mavlink1>=1.0.9
+DEPEND_DIR.mavlink?=	../../wip/mavlink
+
+SYSTEM_SEARCH.mavlink=\
+  'include/mavlink/config.h'	\
+  'lib/pkgconfig/mavlink.pc:/Version/s/[^0-9.]//gp'
+
+endif # MAVLINK_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
