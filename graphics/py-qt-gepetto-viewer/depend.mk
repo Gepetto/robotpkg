14372be7b24a234e9f1b721dd3df9419fbb80131
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-25 18:04:28 +0200

    [graphics/{ → py-qt-}gepetto-viewer]

---
+++ graphics/py-qt-gepetto-viewer/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	graphics/py-qt-gepetto-viewer
+# Created:			Florent Lamiraux on Sun, 8 Mar 2015
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_QT_GEPETTO_VIEWER_DEPEND_MK:=	${PY_QT_GEPETTO_VIEWER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			py-qt-gepetto-viewer
+endif
+
+ifeq (+,$(PY_QT_GEPETTO_VIEWER_DEPEND_MK)) # ---------------------------
+
+PREFER.py-qt-gepetto-viewer?=		robotpkg
+
+DEPEND_USE+=			py-qt-gepetto-viewer
+
+DEPEND_ABI.py-qt-gepetto-viewer?=	${PKGTAG.python}-${PKGTAG.qt}-gepetto-viewer>=4.6.0
+DEPEND_DIR.py-qt-gepetto-viewer?=	../../graphics/py-qt-gepetto-viewer
+
+SYSTEM_SEARCH.py-qt-gepetto-viewer=\
+  'bin/gepetto-gui'						\
+  'include/gepetto/viewer/group-node.h'				\
+  'lib/libgepetto-viewer.so'					\
+  'lib/pkgconfig/gepetto-viewer.pc:/Version/s/[^0-9.]//gp'
+
+include ../../mk/sysdep/python.mk
+include ../../mk/sysdep/qt.mk
+
+endif # PY_QT_GEPETTO_VIEWER_DEPEND_MK ---------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
