5351470eb42c96fb311e0675698043538ac4ea6a
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-25 18:06:09 +0200

    [graphics/{ → py-qt-}gepetto-viewer-corba]

---
+++ graphics/py-qt-gepetto-viewer-corba/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	graphics/py-qt-gepetto-viewer-corba
+# Created:			Florent Lamiraux on Sun, 8 Mar 2015
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+PY_QT_GEPETTO_VIEWER_CORBA_DEPEND_MK:=	${PY_QT_GEPETTO_VIEWER_CORBA_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=				py-qt-gepetto-viewer-corba
+endif
+
+ifeq (+,$(PY_QT_GEPETTO_VIEWER_CORBA_DEPEND_MK)) # -------------------------------
+
+PREFER.py-qt-gepetto-viewer-corba?=		robotpkg
+
+DEPEND_USE+=				py-qt-gepetto-viewer-corba
+
+DEPEND_ABI.py-qt-gepetto-viewer-corba?=	${PKGTAG.python}-${PKGTAG.qt}-gepetto-viewer-corba>=5.3.0
+DEPEND_DIR.py-qt-gepetto-viewer-corba?=	../../graphics/py-qt-gepetto-viewer-corba
+
+SYSTEM_SEARCH.py-qt-gepetto-viewer-corba=\
+  'include/gepetto/viewer/corba/server.hh'				\
+  'lib/libgepetto-viewer-corba.so'					\
+  'lib/pkgconfig/gepetto-viewer-corba.pc:/Version/s/[^0-9.]//gp'
+
+include ../../mk/sysdep/python.mk
+include ../../mk/sysdep/qt.mk
+
+endif # PY_QT_GEPETTO_VIEWER_CORBA_DEPEND_MK -------------------------------------
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH:+=}
