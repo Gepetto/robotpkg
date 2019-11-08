e69cf06cd232f40b797c666e0e154e60dadada94
Anthony Mallet <anthony.mallet@laas.fr>
2019-11-08 11:41:09 +0100

    [interfaces/humanoid-nav-msgs] Rename ros-humanoid-msgs-0.3.0 and drop ROS
    This package has been provided by ROS until kinetic version. It is now not part
    of ROS anymore, but still available.
    
    In order to satisfy the few packages that depend on it and enable those package
    on recent ROS version, this package is imported as a standalone pacakge (not
    pretending to be ROS). However, on distributions that provide system ROS
    packages <= kinetic, this dependency is set to be a ros system package.
    
    This addresses #227.

---
+++ interfaces/humanoid-nav-msgs/depend.mk
@@ -0,0 +1,45 @@
+# robotpkg depend.mk for:	interfaces/humanoid-nav-msgs
+# Created:			Séverin Lemaignan on Wed, 07 Aug 2013
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+HUMANOID_NAV_MSGS_DEPEND_MK:=	${HUMANOID_NAV_MSGS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			humanoid-nav-msgs
+endif
+
+ifeq (+,$(HUMANOID_NAV_MSGS_DEPEND_MK)) # ----------------------------------
+
+# On distributions that provide ros<=kinetic, use the system ROS package
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 12.04 14.04 16.04,${OS_VERSION}))
+    include ../../meta-pkgs/ros-base/depend.common
+    PREFER.humanoid-nav-msgs?=		${PREFER.ros-base}
+    SYSTEM_PREFIX.humanoid-nav-msgs?=	${SYSTEM_PREFIX.ros-base}
+  endif
+else ifeq (Debian,${OPSYS})
+  ifneq (,$(filter 9,${OS_VERSION}))
+    include ../../meta-pkgs/ros-base/depend.common
+    PREFER.humanoid-nav-msgs?=		${PREFER.ros-base}
+    SYSTEM_PREFIX.humanoid-nav-msgs?=	${SYSTEM_PREFIX.ros-base}
+  endif
+endif
+
+PREFER.humanoid-nav-msgs?=		robotpkg
+
+DEPEND_USE+=			humanoid-nav-msgs
+
+DEPEND_ABI.humanoid-nav-msgs?=	humanoid-nav-msgs>=0.1
+DEPEND_DIR.humanoid-nav-msgs?=	../../interfaces/humanoid-nav-msgs
+
+SYSTEM_SEARCH.humanoid-nav-msgs=\
+  'include/humanoid_nav_msgs/ClipFootstep.h'				\
+  'share/humanoid_nav_msgs/msg/StepTarget.msg'				\
+  'share/humanoid_nav_msgs/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/humanoid_nav_msgs.pc:/Version/s/[^0-9.]//gp'
+
+endif # HUMANOID_NAV_MSGS_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
