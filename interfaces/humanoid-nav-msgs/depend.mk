a7a94f690cfa80a1cb6bdfdd090ca377ebca6b14
Anthony Mallet <anthony.mallet@laas.fr>
2019-11-12 10:48:24 +0100

    [interfaces/humanoid-nav-msgs] Export a CMAKE_PREFIX_PATH for catkin
    catkin is unable to use cmake's <pkg>_DIR variable to locate its *-paths.cmake
    files, so one has to append the <pkg> installation prefix to the global
    CMAKE_PREFIX_PATH.
    
    While here, add *Config.cmake file to the SYSTEM_SEARCH, so that <pkg>_DIR is
    automatically defined and cmake can locate this dependency.

--- interfaces/humanoid-nav-msgs/depend.mk
+++ interfaces/humanoid-nav-msgs/depend.mk
@@ -31,8 +31,12 @@ DEPEND_DIR.humanoid-nav-msgs?=	../../interfaces/humanoid-nav-msgs
 SYSTEM_SEARCH.humanoid-nav-msgs=\
   'include/humanoid_nav_msgs/ClipFootstep.h'				\
   'share/humanoid_nav_msgs/msg/StepTarget.msg'				\
+  'share/humanoid_nav_msgs/cmake/humanoid_nav_msgsConfig.cmake'		\
   'share/humanoid_nav_msgs/package.xml:/<version>/s/[^0-9.]//gp'	\
   'lib/pkgconfig/humanoid_nav_msgs.pc:/Version/s/[^0-9.]//gp'
+
+# for humanoid_nav_msgs-msg-paths.cmake
+CMAKE_PREFIX_PATH.humanoid-nav-msgs=	${PREFIX.humanoid-nav-msgs}
 
 endif # HUMANOID_NAV_MSGS_DEPEND_MK ----------------------------------------
 
