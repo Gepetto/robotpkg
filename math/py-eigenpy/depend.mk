a9913d2246c72fb3275e79bb7b9b6a757f817f94
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-12-12 11:15:39 +0100

    [math/py-eigenpy] Update to v1.6.12
    Changes since v1.6.11:
    - fix the compatibility on Win32 systems
    - fix a bug introduced in 1.6.10
    
    Changes since v1.6.10:
    - fix a bug introduced in 1.6.10
    
    Changes since v1.6.9:
    - make the project fully compatible with CMake package policy
    - improve the documentation of the Python bindings
    - fix the convertibility of Eigen base classes
    
    Packaging changes:
    - use eigenpyConfigVersion.cmake

--- math/py-eigenpy/depend.mk
+++ math/py-eigenpy/depend.mk
@@ -14,14 +14,16 @@ ifeq (+,$(PY_EIGENPY_DEPEND_MK)) # -----------------------------------------
 PREFER.py-eigenpy?=	robotpkg
 
 SYSTEM_SEARCH.py-eigenpy=\
-  'include/eigenpy/config.hh:/EIGENPY_VERSION /s/[^0-9.]//gp'	\
-  'lib/libeigenpy.so'						\
-  '${PYTHON_SYSLIBSEARCH}/eigenpy*.so'				\
-  'lib/pkgconfig/eigenpy.pc:/Version/s/[^0-9.]//gp'		\
+  'include/eigenpy/config.hpp:/EIGENPY_VERSION /s/[^0-9.]//gp'			\
+  'lib/libeigenpy.so'								\
+  '${PYTHON_SYSLIBSEARCH}/eigenpy/*.so'						\
+  '${PYTHON_SYSLIBSEARCH}/eigenpy/__init__.py'					\
+  'lib/cmake/eigenpy/eigenpyConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/eigenpy.pc:/Version/s/[^0-9.]//gp'				\
   'share/eigenpy/package.xml:/<version>/s/[^0-9.]//gp'
 
 DEPEND_USE+=		py-eigenpy
-DEPEND_ABI.py-eigenpy?=	${PKGTAG.python-}eigenpy>=1.6.8
+DEPEND_ABI.py-eigenpy?=	${PKGTAG.python-}eigenpy>=1.6.12
 DEPEND_DIR.py-eigenpy?=	../../math/py-eigenpy
 
 include ../../mk/sysdep/python.mk
