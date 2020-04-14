073ca531b9f23b056e039bc5e9ca4221f60ffd5c
Justin Carpentier <jcarpent@laas.fr>
2020-04-14 19:06:28 +0200

    [optimization/tsid] Import 1.2.3
    This package provides an efficient library to compute rigid multibody dynamics.

---
+++ optimization/tsid/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	optimization/tsid
+# Created:			Justin Carpentier on Tue, 14 Apr 2020
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+TSID_DEPEND_MK:=	${TSID_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		tsid
+endif
+
+ifeq (+,$(TSID_DEPEND_MK)) # -----------------------------------------------
+
+PREFER.tsid?=		robotpkg
+
+DEPEND_USE+=		tsid
+
+DEPEND_ABI.tsid?=	tsid>=1.0.0
+DEPEND_DIR.tsid?=	../../optimization/tsid
+
+SYSTEM_SEARCH.tsid=\
+  'lib/pkgconfig/tsid.pc:/Version/s/[^0-9.]//gp'
+
+endif # TSID_DEPEND_MK -----------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
