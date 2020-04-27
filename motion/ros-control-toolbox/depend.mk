96613a3ccb3cfd494a2deb3158bdf8c1605b47f2
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-04-27 13:24:46 +0200

    [motion/ros-control-toolbox] Upadte DEPEND_ABI for melodic to <1.19
    ref https://github.com/ros/rosdistro/pull/24246

--- motion/ros-control-toolbox/depend.mk
+++ motion/ros-control-toolbox/depend.mk
@@ -28,7 +28,7 @@ DEPEND_ABI.ros-control-toolbox.indigo?=	ros-control-toolbox>=1.13<1.15
 DEPEND_ABI.ros-control-toolbox.jade?=	ros-control-toolbox>=1.13<1.15
 DEPEND_ABI.ros-control-toolbox.kinetic?=ros-control-toolbox>=1.15<1.18
 DEPEND_ABI.ros-control-toolbox.lunar?=	ros-control-toolbox>=1.15<1.18
-DEPEND_ABI.ros-control-toolbox.melodic?=ros-control-toolbox>=1.16<1.18
+DEPEND_ABI.ros-control-toolbox.melodic?=ros-control-toolbox>=1.16<1.19
 
 SYSTEM_SEARCH.ros-control-toolbox=\
   'include/control_toolbox/ParametersConfig.h'				\
