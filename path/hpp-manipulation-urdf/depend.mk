57ea03e9567d974f01eb6d0b07f11e0e7dc1ea4f
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-04 17:23:28 +0200

    [path/hpp-manipulation-urdf] update SYSTEM_SEARCH

--- path/hpp-manipulation-urdf/depend.mk
+++ path/hpp-manipulation-urdf/depend.mk
@@ -2,26 +2,27 @@
 # Created:			Florent Lamiraux on Sat, 7 Mar 2015
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-HPPMANIPULATIONURDF_DEPEND_MK:=	${HPPMANIPULATIONURDF_DEPEND_MK}+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+HPPMANIPULATIONURDF_DEPEND_MK:=		${HPPMANIPULATIONURDF_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		hpp-manipulation-urdf
+DEPEND_PKG+=				hpp-manipulation-urdf
 endif
 
 ifeq (+,$(HPPMANIPULATIONURDF_DEPEND_MK)) # ---------------------------
 
-PREFER.hpp-manipulation-urdf?=	robotpkg
+PREFER.hpp-manipulation-urdf?=		robotpkg
 
-DEPEND_USE+=		hpp-manipulation-urdf
+DEPEND_USE+=				hpp-manipulation-urdf
 
-DEPEND_ABI.hpp-manipulation-urdf?=	hpp-manipulation-urdf>=4.5.0
+DEPEND_ABI.hpp-manipulation-urdf?=	hpp-manipulation-urdf>=4.9.0
 DEPEND_DIR.hpp-manipulation-urdf?=	../../path/hpp-manipulation-urdf
 
-SYSTEM_SEARCH.hpp-manipulation-urdf=			\
-	include/hpp/manipulation/parser/parser.hh	\
-	include/hpp/manipulation/parser/factories/sequence.hh \
-	lib/libhpp-manipulation-urdf.so		\
+SYSTEM_SEARCH.hpp-manipulation-urdf=											\
+	'include/hpp/manipulation/parser/parser.hh'									\
+	'include/hpp/manipulation/parser/factories/sequence.hh'								\
+	'lib/cmake/hpp-manipulation-urdf/hpp-manipulation-urdfConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+	'lib/libhpp-manipulation-urdf.so'										\
 	'lib/pkgconfig/hpp-manipulation-urdf.pc:/Version/s/[^0-9.]//gp'
 
 endif # HPPMANIPULATIONURDF_DEPEND_MK ---------------------------------
