0f6dffbebdfa78f01348a70e79d031738b8158ba
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-04 17:24:54 +0200

    [path/py-hpp-manipulation-corba] update SYSTEM_SEARCH

--- path/py-hpp-manipulation-corba/depend.mk
+++ path/py-hpp-manipulation-corba/depend.mk
@@ -2,11 +2,11 @@
 # Created:			Florent Lamiraux on Sat, 7 Mar 2015
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
 PY_HPP_MANIPULATION_CORBA_DEPEND_MK:=	${PY_HPP_MANIPULATION_CORBA_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		py-hpp-manipulation-corba
+DEPEND_PKG+=				py-hpp-manipulation-corba
 endif
 
 ifeq (+,$(PY_HPP_MANIPULATION_CORBA_DEPEND_MK)) # ---------------------------
@@ -15,14 +15,15 @@ include ../../mk/sysdep/python.mk
 
 PREFER.py-hpp-manipulation-corba?=	robotpkg
 
-DEPEND_USE+=		py-hpp-manipulation-corba
+DEPEND_USE+=				py-hpp-manipulation-corba
 
-DEPEND_ABI.py-hpp-manipulation-corba?=	${PKGTAG.python-}hpp-manipulation-corba>=4.7.0
+DEPEND_ABI.py-hpp-manipulation-corba?=	${PKGTAG.python-}hpp-manipulation-corba>=4.9.0
 DEPEND_DIR.py-hpp-manipulation-corba?=	../../path/py-hpp-manipulation-corba
 
-SYSTEM_SEARCH.py-hpp-manipulation-corba=		\
-	include/hpp/corbaserver/manipulation/server.hh  \
-	lib/libhpp-manipulation-corba.so		\
+SYSTEM_SEARCH.py-hpp-manipulation-corba=										\
+	'include/hpp/corbaserver/manipulation/server.hh'								\
+	'lib/cmake/hpp-manipulation-corba/hpp-manipulation-corbaConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+	'lib/libhpp-manipulation-corba.so'										\
 	'lib/pkgconfig/hpp-manipulation-corba.pc:/Version/s/[^0-9.]//gp'
 
 endif # PY_HPP_MANIPULATION_CORBA_DEPEND_MK ---------------------------------
