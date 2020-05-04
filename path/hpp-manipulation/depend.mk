162a71c9bda44712e23934a72abe7dce5f248fe1
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-04 17:22:08 +0200

    [path/hpp-manipulation] update SYSTEM_SEARCH

--- path/hpp-manipulation/depend.mk
+++ path/hpp-manipulation/depend.mk
@@ -2,26 +2,27 @@
 # Created:			Florent Lamiraux on Sat, 7 Mar 2015
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 HPPMANIPULATION_DEPEND_MK:=	${HPPMANIPULATION_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		hpp-manipulation
+DEPEND_PKG+=			hpp-manipulation
 endif
 
 ifeq (+,$(HPPMANIPULATION_DEPEND_MK)) # ---------------------------
 
 PREFER.hpp-manipulation?=	robotpkg
 
-DEPEND_USE+=		hpp-manipulation
+DEPEND_USE+=			hpp-manipulation
 
-DEPEND_ABI.hpp-manipulation?=	hpp-manipulation>=4.6.0
+DEPEND_ABI.hpp-manipulation?=	hpp-manipulation>=4.9.0
 DEPEND_DIR.hpp-manipulation?=	../../path/hpp-manipulation
 
-SYSTEM_SEARCH.hpp-manipulation=			\
-	include/hpp/manipulation/device.hh	\
-	include/hpp/manipulation/problem-solver.hh \
-	lib/libhpp-manipulation.so		\
+SYSTEM_SEARCH.hpp-manipulation=										\
+	'include/hpp/manipulation/device.hh'								\
+	'include/hpp/manipulation/problem-solver.hh'							\
+	'lib/cmake/hpp-manipulation/hpp-manipulationConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+	'lib/libhpp-manipulation.so'									\
 	'lib/pkgconfig/hpp-manipulation.pc:/Version/s/[^0-9.]//gp'
 
 endif # HPPMANIPULATION_DEPEND_MK ---------------------------------
