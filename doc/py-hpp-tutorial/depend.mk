10a0186eaaafefad3eecf3ad88847707c15bb187
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-04 17:35:45 +0200

    [doc/py-hpp-tutorial] update SYSTEM_SEARCH

--- doc/py-hpp-tutorial/depend.mk
+++ doc/py-hpp-tutorial/depend.mk
@@ -2,7 +2,7 @@
 # Created:			Florent Lamiraux on Thu, 12 Mar 2015
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 PY_HPP_TUTORIAL_DEPEND_MK:=	${PY_HPP_TUTORIAL_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
@@ -17,11 +17,12 @@ PREFER.py-hpp-tutorial?=	robotpkg
 
 DEPEND_USE+=			py-hpp-tutorial
 
-DEPEND_ABI.py-hpp-tutorial?=	${PKGTAG.python-}hpp_tutorial>=4.7.0
+DEPEND_ABI.py-hpp-tutorial?=	${PKGTAG.python-}hpp_tutorial>=4.9.0
 DEPEND_DIR.py-hpp-tutorial?=	../../doc/py-hpp-tutorial
 
-SYSTEM_SEARCH.py-hpp-tutorial=\
-	bin/hpp-tutorial-server		\
+SYSTEM_SEARCH.py-hpp-tutorial=									\
+	'bin/hpp-tutorial-server'								\
+	'lib/cmake/hpp-tutorial/hpp-tutorialConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/hpp_tutorial.pc:/Version/s/[^0-9.]//gp'
 
 endif # PY_HPP_TUTORIAL_DEPEND_MK --------------------------------------------
