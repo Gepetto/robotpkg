998d3bb6753636a95ef99d9cb7760edb50c4eccd
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-04 16:02:03 +0200

    [path/hpp-util] update SYSTEM_SEARCH

--- path/hpp-util/depend.mk
+++ path/hpp-util/depend.mk
@@ -15,12 +15,13 @@ PREFER.hpp-util?=	robotpkg
 
 DEPEND_USE+=		hpp-util
 
-DEPEND_ABI.hpp-util?=	hpp-util>=4.5.0
+DEPEND_ABI.hpp-util?=	hpp-util>=4.9.0
 DEPEND_DIR.hpp-util?=	../../path/hpp-util
 
-SYSTEM_SEARCH.hpp-util=			\
-	include/hpp/util/version.hh	\
-	lib/libhpp-util.so		\
+SYSTEM_SEARCH.hpp-util=									\
+	'include/hpp/util/version.hh'							\
+	'lib/cmake/hpp-util/hpp-utilConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+	'lib/libhpp-util.so'								\
 	'lib/pkgconfig/hpp-util.pc:/Version/s/[^0-9.]//gp'
 
 endif # HPPUTIL_DEPEND_MK ---------------------------------
