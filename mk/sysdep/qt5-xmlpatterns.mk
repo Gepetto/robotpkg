d78d72dc6c83c15d4e9ff6caa0941ef30de57613
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-30 15:35:33 +0200

    [mk/sysdep/qt5-xmlpatterns] fix SYSTEM_SEARCH for Arch
    for /usr/include/qt/QtXmlPatterns/QtXmlPatternsVersion
    
    While here, set SYSTEM_PKG.Arch

--- mk/sysdep/qt5-xmlpatterns.mk
+++ mk/sysdep/qt5-xmlpatterns.mk
@@ -16,11 +16,12 @@ DEPEND_USE+=			qt5-xmlpatterns
 DEPEND_ABI.qt5-xmlpatterns?=	qt5-xmlpatterns>=5.0.0
 
 SYSTEM_SEARCH.qt5-xmlpatterns=\
-  '{,qt5/}include/{,qt5/{,include/}}Qt{,XmlPatterns}/QtXmlPatternsVersion' \
-  '{lib,share/qt5/lib,qt5/lib,lib/qt5}/libQt5XmlPatterns.{so,a}'	\
-  '{,lib}/cmake/Qt5XmlPatterns/Qt5XmlPatternsConfig.cmake'		\
+  '{,qt5/}include/{,qt{,5}/{,include/}}Qt{,XmlPatterns}/QtXmlPatternsVersion'	\
+  '{lib,share/qt5/lib,qt5/lib,lib/qt5}/libQt5XmlPatterns.{so,a}'		\
+  '{,lib}/cmake/Qt5XmlPatterns/Qt5XmlPatternsConfig.cmake'			\
   'lib/pkgconfig/Qt5XmlPatterns.pc:/Version/s/[^0-9.]//gp'
 
+SYSTEM_PKG.Arch.qt5-xmlpatterns=	qt5-xmlpatterns
 SYSTEM_PKG.Debian.qt5-xmlpatterns=	libqt5xmlpatterns5-dev
 SYSTEM_PKG.Fedora.qt5-xmlpatterns=	qt5-qtxmlpatterns-devel
 SYSTEM_PKG.NetBSD.qt5-xmlpatterns=	x11/qt5-qtxmlpatterns
