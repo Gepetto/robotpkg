eac443fe3e098777fb1cfc974ead85b205831720
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-30 15:25:38 +0200

    [mk/sysdep/qt5-multimedia] fix SYSTEM_SEARCH for Arch
    for /usr/include/qt/QtMultimedia/qcamera.h
    
    While here, set SYSTEM_PKG.Arch.qt5-multimedia

--- mk/sysdep/qt5-multimedia.mk
+++ mk/sysdep/qt5-multimedia.mk
@@ -16,13 +16,14 @@ DEPEND_USE+=		qt5-multimedia
 DEPEND_ABI.qt5-multimedia?=	qt5-multimedia>=5.0.0
 
 SYSTEM_SEARCH.qt5-multimedia=\
-  '{,qt5/}include/{,qt5/{,include/}}Qt{,Multimedia}/qcamera.h'		\
-  '{lib,share/qt5/lib,qt5/lib,lib/qt5}/libQt5Multimedia.{so,a}'	\
+  '{,qt5/}include/{,qt{,5}/{,include/}}Qt{,Multimedia}/qcamera.h'	\
+  '{lib,share/qt5/lib,qt5/lib,lib/qt5}/libQt5Multimedia.{so,a}'		\
   'lib/pkgconfig/Qt5Multimedia.pc:/Version/s/[^0-9.]//gp'
 
+SYSTEM_PKG.Arch.qt5-multimedia=		qt5-multimedia
 SYSTEM_PKG.Debian.qt5-multimedia=	qtmultimedia5-dev
 SYSTEM_PKG.Fedora.qt5-multimedia=	qt5-qtmultimedia-devel
-SYSTEM_PKG.NetBSD.qt5-multimedia=	x11/qt5-qtmultimedia 
+SYSTEM_PKG.NetBSD.qt5-multimedia=	x11/qt5-qtmultimedia
 
 endif # QT5_MULTIMEDIA_DEPEND_MK -------------------------------------------------
 
