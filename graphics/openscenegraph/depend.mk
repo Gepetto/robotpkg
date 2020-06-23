7636c708be390bb9494df7f1ca15d6feee26df22
Anthony Mallet <anthony.mallet@laas.fr>
2020-06-23 17:17:37 +0200

    [graphics/openscenegraph] PREFER the robotpkg version by default
    Depending packages require the collada plugin. Unfortunately, no distribution
    provides this in their system packages, so use a PREFER robotpkg by default.
    
    It might be better to provide a separate package just for the collada plugin,
    but it has to be assessed if a single plugin source would be compatible with
    diffent system osg versions. #264.

--- graphics/openscenegraph/depend.mk
+++ graphics/openscenegraph/depend.mk
@@ -11,21 +11,25 @@ endif
 
 ifeq (+,$(OPENSCENEGRAPH_DEPEND_MK)) # -------------------------------------
 
-include ../../mk/robotpkg.prefs.mk # for OPSYS
-ifeq (CentOS,${OPSYS})
-  PREFER.openscenegraph?=	robotpkg
-else ifeq (NetBSD,${OPSYS})
-  PREFER.openscenegraph?=	robotpkg
-endif
-PREFER.openscenegraph?=		system
+# Depending packages require the collada plugin. Unfortunately, no distribution
+# provides this in their system packages, so use a PREFER robotpkg by default.
+#
+# It might be better to provide a separate package just for the collada plugin,
+# but it has to be assessed if a single plugin source would be compatible with
+# diffent system osg versions. #264.
+#
+PREFER.openscenegraph?=		robotpkg
 
 DEPEND_USE+=			openscenegraph
 DEPEND_ABI.openscenegraph?=	openscenegraph>=3
 DEPEND_DIR.openscenegraph?=	../../graphics/openscenegraph
 
+# Make sure the collada plugin is found - this may be turned into a package
+# option if needed.
 SYSTEM_SEARCH.openscenegraph=\
-  'include/osg/Object'						\
-  'lib/libosg.so'						\
+  'include/osg/Object'							\
+  'lib/libosg.so'							\
+  'lib/osgPlugins-*/osgdb_dae.so:s@.*Plugins-\([0-9.]*\)/.*@\1@p:echo %'\
   'lib/pkgconfig/openscenegraph.pc:/Version/s/[^0-9.]//gp'
 
 SYSTEM_PKG.Debian.openscenegraph=	libopenscenegraph-dev
