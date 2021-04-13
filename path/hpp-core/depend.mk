d4d19e539bc518e11c913b71f613e28df59ed2a9
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-13 19:07:24 +0200

    [path/hpp-core] Update to v4.11.0
    Changes in v4.11.0:
    - Serialize several classes for save and loading roadmaps
    - use shared pointers to class Problem only. Classes previously storing
      const references to Problem now store const weak pointers. Classes
      that use to take a const reference to problem in the constructor now
      take a const shared pointer.
    - Make Path::operator() deprecated: use method Path::eval instead.
    - Improve Vmax computation for solid-solid collision
    - [ConfigProjector] Add method isSatisfied with input error threshold.

--- path/hpp-core/depend.mk
+++ path/hpp-core/depend.mk
@@ -2,27 +2,28 @@
 # Created:			Anthony Mallet on Wed, 17 Sep 2008
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-HPPCORE_DEPEND_MK:=	${HPPCORE_DEPEND_MK}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+HPPCORE_DEPEND_MK:=		${HPPCORE_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		hpp-core
+DEPEND_PKG+=			hpp-core
 endif
 
 ifeq (+,$(HPPCORE_DEPEND_MK)) # --------------------------------------
 
-PREFER.hpp-core?=	robotpkg
+PREFER.hpp-core?=		robotpkg
 
-DEPEND_USE+=		hpp-core
+DEPEND_USE+=			hpp-core
 
-DEPEND_ABI.hpp-core?=	hpp-core>=4.9.0
-DEPEND_DIR.hpp-core?=	../../path/hpp-core
+DEPEND_ABI.hpp-core?=		hpp-core>=4.9.0
+DEPEND_DIR.hpp-core?=		../../path/hpp-core
 
 SYSTEM_SEARCH.hpp-core=\
 	'include/hpp/core/problem.hh'							\
 	'lib/cmake/hpp-core/hpp-coreConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
 	'lib/libhpp-core.so'								\
-	'lib/pkgconfig/hpp-core.pc:/Version/s/[^0-9.]//gp'
+	'lib/pkgconfig/hpp-core.pc:/Version/s/[^0-9.]//gp'				\
+	'share/hpp-core/package.xml:/<version>/s/[^0-9.]//gp'
 
 endif # HPPCORE_DEPEND_MK --------------------------------------------
 
