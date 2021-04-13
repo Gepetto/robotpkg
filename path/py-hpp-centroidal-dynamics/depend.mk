94dc88e954d06a0627b80f18067cd7f7a68b10df
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-13 12:08:49 +0200

    [path/py-hpp-centroidal-dynamics] import from wip/

---
+++ path/py-hpp-centroidal-dynamics/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	path/py39-hpp-centroidal-dynamics
+# Created:			Guilhem Saurel on Tue, 13 Apr 2021
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+PY_HPP_CENTROIDAL_DYNAMICS_DEPEND_MK:=	${PY_HPP_CENTROIDAL_DYNAMICS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=				py-hpp-centroidal-dynamics
+endif
+
+ifeq (+,$(PY_HPP_CENTROIDAL_DYNAMICS_DEPEND_MK)) # --------------------------------------
+
+include ../../mk/sysdep/python.mk
+
+PREFER.py-hpp-centroidal-dynamics?=	robotpkg
+
+DEPEND_USE+=				py-hpp-centroidal-dynamics
+
+DEPEND_ABI.py-hpp-centroidal-dynamics?=	${PKGTAG.python-}hpp-centroidal-dynamics>=4.9.0
+DEPEND_DIR.py-hpp-centroidal-dynamics?=	../../path/py-hpp-centroidal-dynamics
+
+SYSTEM_SEARCH.py-hpp-centroidal-dynamics=										\
+	'include/hpp/centroidal-dynamics/config.hh'									\
+	'lib/cmake/hpp-centroidal-dynamics/hpp-centroidal-dynamicsConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+	'lib/libhpp-centroidal-dynamics.so'										\
+	'lib/pkgconfig/hpp-centroidal-dynamics.pc:/Version/s/[^0-9.]//gp'						\
+	'share/hpp-centroidal-dynamics/package.xml:/<version>/s/[^0-9.]//gp'
+
+endif # PY_HPP_CENTROIDAL_DYNAMICS_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
