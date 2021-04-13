a194e6a37f1740ff4b4c82ba6bdd1ef7aae6e847
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-13 16:55:49 +0200

    [path/hpp-constraints] Update to v4.11.0
    Changes in v4.11.0:
    - Solvers now handle constraints with right hand sides in Lie groups.
    - A mask has been added to class Implicit to select which lines of the
      constraint should be taken into account (active rows).
    - Use hpp::shared_ptr instead of boost::shared_ptr
    - Remove usage of boost list_of
    - [solver] Add methods isSatisfied with input error threshold.

--- path/hpp-constraints/depend.mk
+++ path/hpp-constraints/depend.mk
@@ -2,18 +2,18 @@
 # Created:			Antonio El Khoury on Thu, 26 Sep 2013
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 HPP_CONSTRAINTS_DEPEND_MK:=	${HPP_CONSTRAINTS_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		hpp-constraints
+DEPEND_PKG+=			hpp-constraints
 endif
 
 ifeq (+,$(HPP_CONSTRAINTS_DEPEND_MK)) # --------------------------------------
 
 PREFER.hpp-constraints?=	robotpkg
 
-DEPEND_USE+=		hpp-constraints
+DEPEND_USE+=			hpp-constraints
 
 DEPEND_ABI.hpp-constraints?=	hpp-constraints>=4.9.0
 DEPEND_DIR.hpp-constraints?=	../../path/hpp-constraints
@@ -22,7 +22,8 @@ SYSTEM_SEARCH.hpp-constraints=									\
   'include/hpp/constraints/differentiable-function.hh'						\
   'lib/libhpp-constraints.so'									\
   'lib/cmake/hpp-constraints/hpp-constraintsConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/hpp-constraints.pc:/Version/s/[^0-9.]//gp'
+  'lib/pkgconfig/hpp-constraints.pc:/Version/s/[^0-9.]//gp'					\
+  'share/hpp-constraints/package.xml:/<version>/s/[^0-9.]//gp'
 
 DEPEND_ABI.eigen3 += eigen3>=3.2.4
 include ../../math/eigen3/depend.mk
