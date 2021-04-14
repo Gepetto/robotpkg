de099dc03d2ec8c1bda86f2e0bfe6c3464d92010
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-14 13:02:12 +0200

    [robots/py-talos-rbprm] import from wip/

---
+++ robots/py-talos-rbprm/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	robots/py39-talos-rbprm
+# Created:			Guilhem Saurel on Wed, 14 Apr 2021
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_TALOS_RBPRM_DEPEND_MK:=	${PY_TALOS_RBPRM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			py-talos-rbprm
+endif
+
+ifeq (+,$(PY_TALOS_RBPRM_DEPEND_MK)) # --------------------------------------
+
+include ../../mk/sysdep/python.mk
+
+PREFER.py-talos-rbprm?=		robotpkg
+
+DEPEND_USE+=			py-talos-rbprm
+
+DEPEND_ABI.py-talos-rbprm?=	${PKGTAG.python-}talos-rbprm>=4.7.0
+DEPEND_DIR.py-talos-rbprm?=	../../robots/py-talos-rbprm
+
+SYSTEM_SEARCH.py-talos-rbprm=									\
+	'include/hpp/talos-rbprm/config.hh:/HPP_TALOS_RBPRM_VERSION /s/[^0-9.]//gp'		\
+	'lib/cmake/talos-rbprm/talos-rbprmConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+	'lib/pkgconfig/talos-rbprm.pc:/Version/s/[^0-9.]//gp'					\
+	'share/talos-rbprm/package.xml:/<version>/s/[^0-9.]//gp'				\
+	'${PYTHON_SYSLIBSEARCH}/talos_rbprm/__init__.py'
+
+endif # PY_TALOS_RBPRM_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
