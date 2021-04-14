dafa8470849c939d1e88c6ae82b28a21f3ebadae
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-14 11:37:45 +0200

    [middleware/hpp-template-corba] Update to v4.11.0

--- middleware/hpp-template-corba/depend.mk
+++ middleware/hpp-template-corba/depend.mk
@@ -18,10 +18,12 @@ DEPEND_USE+=			hpp-template-corba
 DEPEND_ABI.hpp-template-corba?=	hpp-template-corba>=4.9.0
 DEPEND_DIR.hpp-template-corba?=	../../middleware/hpp-template-corba
 
-SYSTEM_SEARCH.hpp-template-corba=\
+SYSTEM_SEARCH.hpp-template-corba=										\
 	'include/hpp/corba/template/server.hh'									\
 	'lib/cmake/hpp-template-corba/hpp-template-corbaConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
-	'lib/pkgconfig/hpp-template-corba.pc:/Version/s/[^0-9.]//gp'
+	'lib/libhpp-template-corba.so'										\
+	'lib/pkgconfig/hpp-template-corba.pc:/Version/s/[^0-9.]//gp'						\
+	'share/hpp-template-corba/package.xml:/<version>/s/[^0-9.]//gp'
 
 endif # --------------------------------------------------------------------
 
