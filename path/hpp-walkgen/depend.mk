01a16b82ef973dccbb438923c92ce081a63d973e
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-14 11:49:40 +0200

    [path/hpp-walkgen] Update to v4.11.0
    Changes in v4.11.0:
    - Update to modifications in hpp-core
    - use std::shared_ptr
    - install package.xml

--- path/hpp-walkgen/depend.mk
+++ path/hpp-walkgen/depend.mk
@@ -2,27 +2,28 @@
 # Created:			Guilhem Saurel on Wed, 14 Mar 2018
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-HPPWALKGEN_DEPEND_MK:=	${HPPWALKGEN_DEPEND_MK}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+HPPWALKGEN_DEPEND_MK:=		${HPPWALKGEN_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		hpp-walkgen
+DEPEND_PKG+=			hpp-walkgen
 endif
 
 ifeq (+,$(HPPWALKGEN_DEPEND_MK)) # ---------------------------
 
-PREFER.hpp-walkgen?=	robotpkg
+PREFER.hpp-walkgen?=		robotpkg
 
-DEPEND_USE+=		hpp-walkgen
+DEPEND_USE+=			hpp-walkgen
 
 DEPEND_ABI.hpp-walkgen?=	hpp-walkgen>=4.9.0
 DEPEND_DIR.hpp-walkgen?=	../../path/hpp-walkgen
 
 SYSTEM_SEARCH.hpp-walkgen=									\
-	'include/hpp/walkgen/fwd.hh'								\
+	'include/hpp/walkgen/config.hh:/HPP_WALKGEN_VERSION /s/[^0-9.]//gp'			\
 	'lib/cmake/hpp-walkgen/hpp-walkgenConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
 	'lib/libhpp-walkgen.so'									\
-	'lib/pkgconfig/hpp-walkgen.pc:/Version/s/[^0-9.]//gp'
+	'lib/pkgconfig/hpp-walkgen.pc:/Version/s/[^0-9.]//gp'					\
+	'share/hpp-walkgen/package.xml:/<version>/s/[^0-9.]//gp'
 
 endif # HPPWALKGEN_DEPEND_MK ---------------------------------
 
