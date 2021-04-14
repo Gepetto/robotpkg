198bb6944319105cefee016e6002f9af124b1943
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-14 12:28:11 +0200

    [robots/py-hyq-rbprm] import from wip/

---
+++ robots/py-hyq-rbprm/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	robots/py39-hyq-rbprm
+# Created:			Guilhem Saurel on Wed, 14 Apr 2021
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_HYQ_RBPRM_DEPEND_MK:=	${PY_HYQ_RBPRM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			py-hyq-rbprm
+endif
+
+ifeq (+,$(PY_HYQ_RBPRM_DEPEND_MK)) # --------------------------------------
+
+include ../../mk/sysdep/python.mk
+
+PREFER.py-hyq-rbprm?=		robotpkg
+
+DEPEND_USE+=			py-hyq-rbprm
+
+DEPEND_ABI.py-hyq-rbprm?=	${PKGTAG.python-}hyq-rbprm>=4.10.0
+DEPEND_DIR.py-hyq-rbprm?=	../../robots/py-hyq-rbprm
+
+SYSTEM_SEARCH.py-hyq-rbprm=									\
+	'include/hpp/hyq-rbprm/config.hh:/HPP_HYQ_RBPRM_VERSION /s/[^0-9.]//gp'			\
+	'lib/cmake/hyq-rbprm/hyq-rbprmConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+	'lib/pkgconfig/hyq-rbprm.pc:/Version/s/[^0-9.]//gp'					\
+	'share/hyq-rbprm/package.xml:/<version>/s/[^0-9.]//gp'					\
+	'${PYTHON_SYSLIBSEARCH}/hyq_rbprm/__init__.py'
+
+endif # PY_HYQ_RBPRM_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
