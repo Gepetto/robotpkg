9cf987313830fa25d09dce2142105f09f8f9702f
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-14 22:30:09 +0200

    [path/hpp-wholebody-step] Update to v4.11.0
    Changes in v4.11.0:
    - update to changes in hpp-core
    - use std::shared_ptr

--- path/hpp-wholebody-step/depend.mk
+++ path/hpp-wholebody-step/depend.mk
@@ -2,27 +2,28 @@
 # Created:			Antonio El Khoury on Thu, 26 Sep 2013
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 HPP_WHOLEBODY_STEP_DEPEND_MK:=	${HPP_WHOLEBODY_STEP_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		hpp-wholebody-step
+DEPEND_PKG+=			hpp-wholebody-step
 endif
 
 ifeq (+,$(HPP_WHOLEBODY_STEP_DEPEND_MK)) # ----------------------------------
 
 PREFER.hpp-wholebody-step?=	robotpkg
 
-DEPEND_USE+=		hpp-wholebody-step
+DEPEND_USE+=			hpp-wholebody-step
 
 DEPEND_ABI.hpp-wholebody-step?=	hpp-wholebody-step>=4.9.0
 DEPEND_DIR.hpp-wholebody-step?=	../../path/hpp-wholebody-step
 
 SYSTEM_SEARCH.hpp-wholebody-step=										\
-	'include/hpp/wholebody-step/static-stability-constraint.hh'						\
+	'include/hpp/wholebody-step/config.hh:/HPP_WHOLEBODY_STEP_VERSION /s/[^0-9.]//gp'			\
 	'lib/cmake/hpp-wholebody-step/hpp-wholebody-stepConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
 	'lib/libhpp-wholebody-step.so'										\
-	'lib/pkgconfig/hpp-wholebody-step.pc:/Version/s/[^0-9.]//gp'
+	'lib/pkgconfig/hpp-wholebody-step.pc:/Version/s/[^0-9.]//gp'						\
+	'share/hpp-wholebody-step/package.xml:/<version>/s/[^0-9.]//gp'
 
 endif # HPP_WHOLEBODY_STEP_DEPEND_MK ----------------------------------------
 
