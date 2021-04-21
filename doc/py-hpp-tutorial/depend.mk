f485d535591c604611328dfd9e2e41c55551e170
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-21 10:32:18 +0200

    [doc/py-hpp-tutorial] Update to v4.11.0
    Changes in v4.11.0:
    - fix tutorial 3
    - Update to modifications in hpp-core
    - Remove dependency to iai_maps and hence to xacro.

--- doc/py-hpp-tutorial/depend.mk
+++ doc/py-hpp-tutorial/depend.mk
@@ -21,9 +21,11 @@ DEPEND_ABI.py-hpp-tutorial?=	${PKGTAG.python-}hpp_tutorial>=4.9.0
 DEPEND_DIR.py-hpp-tutorial?=	../../doc/py-hpp-tutorial
 
 SYSTEM_SEARCH.py-hpp-tutorial=									\
+	'include/hpp/tutorial/config.hh:/HPP_TUTORIAL_VERSION /s/[^0-9.]//gp'			\
 	'bin/hpp-tutorial-server'								\
 	'lib/cmake/hpp-tutorial/hpp-tutorialConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
-	'lib/pkgconfig/hpp_tutorial.pc:/Version/s/[^0-9.]//gp'
+	'lib/pkgconfig/hpp_tutorial.pc:/Version/s/[^0-9.]//gp'					\
+	'share/hpp_tutorial/package.xml:/<version>/s/[^0-9.]//gp'
 
 endif # PY_HPP_TUTORIAL_DEPEND_MK --------------------------------------------
 
