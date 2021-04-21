94884656bb8960aff63eeacc501dd0a3afd42925
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-21 08:39:03 +0200

    [graphics/py-qt-gepetto-viewer] Update to v4.12.0
    Changes in v4.12.0:
    - export OSG include directories for gepetto-viewer-corba
    - add extension to fix CMP0115
    
    Changes in v4.11.0:
    - add sleep mode
    - add properties to floor node
    - workaround large scale issue in LeafNodeCollada
    - remove explicit dependency to X11
    - add test on OpenGL
    
    Packaging changes:
    - removed patches, applied upstream

--- graphics/py-qt-gepetto-viewer/depend.mk
+++ graphics/py-qt-gepetto-viewer/depend.mk
@@ -2,27 +2,29 @@
 # Created:			Florent Lamiraux on Sun, 8 Mar 2015
 #
 
-DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
 PY_QT_GEPETTO_VIEWER_DEPEND_MK:=	${PY_QT_GEPETTO_VIEWER_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=			py-qt-gepetto-viewer
+DEPEND_PKG+=				py-qt-gepetto-viewer
 endif
 
 ifeq (+,$(PY_QT_GEPETTO_VIEWER_DEPEND_MK)) # ---------------------------
 
 PREFER.py-qt-gepetto-viewer?=		robotpkg
 
-DEPEND_USE+=			py-qt-gepetto-viewer
+DEPEND_USE+=				py-qt-gepetto-viewer
 
 DEPEND_ABI.py-qt-gepetto-viewer?=	${PKGTAG.python}-${PKGTAG.qt}-gepetto-viewer>=4.6.0
 DEPEND_DIR.py-qt-gepetto-viewer?=	../../graphics/py-qt-gepetto-viewer
 
-SYSTEM_SEARCH.py-qt-gepetto-viewer=\
-  'bin/gepetto-gui'						\
-  'include/gepetto/viewer/group-node.h'				\
-  'lib/libgepetto-viewer.so'					\
-  'lib/pkgconfig/gepetto-viewer.pc:/Version/s/[^0-9.]//gp'
+SYSTEM_SEARCH.py-qt-gepetto-viewer=								\
+  'bin/gepetto-gui'										\
+  'include/gepetto/viewer/config.hh:/GEPETTO_VIEWER_VERSION /s/[^0-9.]//gp'			\
+  'lib/cmake/gepetto-viewer/gepetto-viewerConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+  'lib/libgepetto-viewer.so'									\
+  'lib/pkgconfig/gepetto-viewer.pc:/Version/s/[^0-9.]//gp'					\
+  'share/gepetto-viewer/package.xml:/<version>/s/[^0-9.]//gp'
 
 include ../../mk/sysdep/python.mk
 include ../../mk/sysdep/qt.mk
