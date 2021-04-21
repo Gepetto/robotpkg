f6b602461a7b5f12f112dddb687860adb6784c74
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-21 08:58:17 +0200

    [graphics/py-qt-hpp-gepetto-viewer] Update to v4.11.0
    Changes in v4.11.0:
    - Raise error when using old API
    - Add PathGraphWidget
    - Add loadEnvironmentModelFromString
    - Update callback API

--- graphics/py-qt-hpp-gepetto-viewer/depend.mk
+++ graphics/py-qt-hpp-gepetto-viewer/depend.mk
@@ -2,29 +2,30 @@
 # Created:			Florent Lamiraux on Sun, 8 Mar 2015
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
 PY_QT_HPP_GEPETTO_VIEWER_DEPEND_MK:=	${PY_QT_HPP_GEPETTO_VIEWER_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		py-qt-hpp-gepetto-viewer
+DEPEND_PKG+=				py-qt-hpp-gepetto-viewer
 endif
 
 ifeq (+,$(PY_QT_HPP_GEPETTO_VIEWER_DEPEND_MK)) # ---------------------------
 
 PREFER.py-qt-hpp-gepetto-viewer?=	robotpkg
 
-DEPEND_USE+=		py-qt-hpp-gepetto-viewer
+DEPEND_USE+=				py-qt-hpp-gepetto-viewer
 
 # depend on appropriate Qt version when using Qt, all versions otherwise.
 _hgv_qts={qt4,qt5}
 _hgv_qt=$(if $(filter qt,${PKG_ALTERNATIVES}),${PKG_ALTERNATIVE.qt},${_hgv_qts})
-
 DEPEND_ABI.py-qt-hpp-gepetto-viewer?=	${PKGTAG.python}-${_hgv_qt}-hpp-gepetto-viewer>=4.9.0
 DEPEND_DIR.py-qt-hpp-gepetto-viewer?=	../../graphics/py-qt-hpp-gepetto-viewer
 
 SYSTEM_SEARCH.py-qt-hpp-gepetto-viewer=										\
-	'lib/cmake/hpp-gepetto-viewer/hpp-gepetto-viewerConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
-	'lib/pkgconfig/hpp-gepetto-viewer.pc:/Version/s/[^0-9.]//gp'
+  'include/hpp/gepetto/viewer/config.hh:/HPP_GEPETTO_VIEWER_VERSION /s/[^0-9.]//gp'			\
+  'lib/cmake/hpp-gepetto-viewer/hpp-gepetto-viewerConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/hpp-gepetto-viewer.pc:/Version/s/[^0-9.]//gp'						\
+  'share/hpp-gepetto-viewer/package.xml:/<version>/s/[^0-9.]//gp'
 
 endif # PY_QT_HPP_GEPETTO_VIEWER_DEPEND_MK ---------------------------------
 
