ca433e3b552622bb4cfd54f672e2675e0d7f5f60
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-21 10:15:02 +0200

    [path/py-hpp-manipulation-corba] Update to v4.11.0
    Changes in v4.11.0:
    - [Problem] Fix security margins for environment contact surfaces.
    - Update serialization
    - Remove passive dofs

--- path/py-hpp-manipulation-corba/depend.mk
+++ path/py-hpp-manipulation-corba/depend.mk
@@ -21,10 +21,11 @@ DEPEND_ABI.py-hpp-manipulation-corba?=	${PKGTAG.python-}hpp-manipulation-corba>=
 DEPEND_DIR.py-hpp-manipulation-corba?=	../../path/py-hpp-manipulation-corba
 
 SYSTEM_SEARCH.py-hpp-manipulation-corba=										\
-	'include/hpp/corbaserver/manipulation/server.hh'								\
+	'include/hpp/corbaserver/manipulation/config.hh:/HPP_MANIPULATION_VERSION /s/[^0-9.]//gp'			\
 	'lib/cmake/hpp-manipulation-corba/hpp-manipulation-corbaConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
 	'lib/libhpp-manipulation-corba.so'										\
-	'lib/pkgconfig/hpp-manipulation-corba.pc:/Version/s/[^0-9.]//gp'
+	'lib/pkgconfig/hpp-manipulation-corba.pc:/Version/s/[^0-9.]//gp'						\
+	'share/hpp-manipulation-corba/package.xml:/<version>/s/[^0-9.]//gp'
 
 endif # PY_HPP_MANIPULATION_CORBA_DEPEND_MK ---------------------------------
 
