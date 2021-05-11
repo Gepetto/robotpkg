45b00006b80e749bc7d88d6a8947463f512530e2
Anthony Mallet <anthony.mallet@laas.fr>
2021-05-11 15:27:17 +0200

    [graphics/py-qt-geptto-viewer-corba] Require 5.6.0 in depend.mk
    While here, shorten long lines.

--- graphics/py-qt-gepetto-viewer-corba/depend.mk
+++ graphics/py-qt-gepetto-viewer-corba/depend.mk
@@ -9,25 +9,27 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=				py-qt-gepetto-viewer-corba
 endif
 
-ifeq (+,$(PY_QT_GEPETTO_VIEWER_CORBA_DEPEND_MK)) # -------------------------------
+ifeq (+,$(PY_QT_GEPETTO_VIEWER_CORBA_DEPEND_MK)) # -------------------------
 
 PREFER.py-qt-gepetto-viewer-corba?=	robotpkg
 
 DEPEND_USE+=				py-qt-gepetto-viewer-corba
 
-DEPEND_ABI.py-qt-gepetto-viewer-corba?=	${PKGTAG.python}-${PKGTAG.qt}-gepetto-viewer-corba>=5.3.0
-DEPEND_DIR.py-qt-gepetto-viewer-corba?=	../../graphics/py-qt-gepetto-viewer-corba
+DEPEND_ABI.py-qt-gepetto-viewer-corba?=\
+  ${PKGTAG.python}-${PKGTAG.qt}-gepetto-viewer-corba>=5.6.0
+DEPEND_DIR.py-qt-gepetto-viewer-corba?=\
+  ../../graphics/py-qt-gepetto-viewer-corba
 
-SYSTEM_SEARCH.py-qt-gepetto-viewer-corba=									\
-  'include/gepetto/viewer/corba/config.hh:/GEPETTO_VIEWER_CORBA_VERSION /s/[^0-9.]//gp'				\
-  'lib/cmake/gepetto-viewer-corba/gepetto-viewer-corbaConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
-  'lib/libgepetto-viewer-corba.so'										\
-  'lib/pkgconfig/gepetto-viewer-corba.pc:/Version/s/[^0-9.]//gp'						\
+SYSTEM_SEARCH.py-qt-gepetto-viewer-corba=\
+  'include/gepetto/viewer/corba/config.hh:/GEPETTO_VIEWER_CORBA_VERSION /s/[^0-9.]//gp' \
+  'lib/cmake/gepetto-viewer-corba/gepetto-viewer-corbaConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp' \
+  'lib/libgepetto-viewer-corba.so'					\
+  'lib/pkgconfig/gepetto-viewer-corba.pc:/Version/s/[^0-9.]//gp'	\
   'share/gepetto-viewer-corba/package.xml:/<version>/s/[^0-9.]//gp'
 
 include ../../mk/sysdep/python.mk
 include ../../mk/sysdep/qt.mk
 
-endif # PY_QT_GEPETTO_VIEWER_CORBA_DEPEND_MK -------------------------------------
+endif # PY_QT_GEPETTO_VIEWER_CORBA_DEPEND_MK -------------------------------
 
 DEPEND_DEPTH:=				${DEPEND_DEPTH:+=}
