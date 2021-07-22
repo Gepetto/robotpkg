36d5f4ffdcb61cf4b92017900771f2faa515d0cf
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-07-22 13:10:53 +0200

    [graphics/simple-humanoid-description] Update to v1.1.0
    Changes in v1.1.0:
    
    - setup CI
    - Add visual information in URDF
    - Add simple launch to play with the robot model.
    - update license

--- graphics/simple-humanoid-description/depend.mk
+++ graphics/simple-humanoid-description/depend.mk
@@ -16,11 +16,12 @@ PREFER.simple-humanoid-description?=	robotpkg
 SYSTEM_SEARCH.simple-humanoid-description=\
   'share/simple_humanoid_description/urdf/simple_humanoid.urdf' \
   'share/simple_humanoid_description/srdf/simple_humanoid.srdf' \
+  'lib/cmake/simple_humanoid_description/simple_humanoid_descriptionConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
   'lib/pkgconfig/simple_humanoid_description.pc:/Version/s/[^0-9.]//gp'
 
 DEPEND_USE+=		simple-humanoid-description
 
-DEPEND_ABI.simple-humanoid-description?= simple-humanoid-description>=1.0.1
+DEPEND_ABI.simple-humanoid-description?= simple-humanoid-description>=1.1.0
 DEPEND_DIR.simple-humanoid-description?= ../../graphics/simple-humanoid-description
 
 endif # SIMPLE_HUMANOID_DESCRIPTION_DEPEND_MK ------------------------------
