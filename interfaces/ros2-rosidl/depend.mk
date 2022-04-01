6d4719d3c8efe722ef5b2ef9d5c010c6d2ba6398
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-01 11:30:28 +0200

    [interfaces/ros2-rosidl] Import ros2-rosidl-3.1.1
    The rosidl API consists of a few message related static functions and types
    along with a definition of what code should be generated by messages in
    different languages. The generated message code specified in the API will be
    language specific, but may or may not reuse generated code for other
    languages. The generated message code specified in the API contains things like
    the message data structure, functions for construction, destruction, etc. The
    API will also implement a way to get the type support structure for the message
    type, which is used when publishing or subscribing to a topic of that message
    type.

---
+++ interfaces/ros2-rosidl/depend.mk
@@ -0,0 +1,40 @@
+# robotpkg depend.mk for:	interfaces/ros2-rosidl
+# Created:			Anthony Mallet on Fri, 1 Apr 2022
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ROS2_ROSIDL_DEPEND_MK:=	${ROS2_ROSIDL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		ros2-rosidl
+endif
+
+ifeq (+,$(ROS2_ROSIDL_DEPEND_MK)) # ----------------------------------------
+
+include ../../meta-pkgs/ros2-core/depend.common
+
+ROS2_DEPEND_USE+=	ros2-rosidl
+
+DEPEND_DIR.ros2-rosidl?=../../interfaces/ros2-rosidl
+DEPEND_ABI.ros2-rosidl?=ros2-rosidl>=0
+
+SYSTEM_SEARCH.ros2-rosidl=\
+  $(call ros2_system_search,			\
+    rosidl_adapter				\
+    rosidl_cmake				\
+    rosidl_generator_c				\
+    rosidl_generator_cpp			\
+    rosidl_parser				\
+    rosidl_runtime_c				\
+    rosidl_runtime_cpp				\
+    rosidl_typesupport_interface		\
+    rosidl_typesupport_introspection_c		\
+    rosidl_typesupport_introspection_cpp	\
+    rosidl_typesupport_introspection_tests)	\
+  '${PYTHON_SITELIB}/rosidl_cli/__init__.py'
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS2_ROSIDL_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
