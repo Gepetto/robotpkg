1250ccd4a0f334b8398d32c36e028496194a280d
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-05 14:20:08 +0200

    [middleware/fastcdr] Import Fast-CDR-1.0.24
    eProsima Fast CDR is a C++ library that provides two serialization
    mechanisms. One is the standard CDR serialization mechanism, while the other
    is a faster implementation that modifies the standard.

---
+++ middleware/fastcdr/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	middleware/fastcdr
+# Created:			Anthony Mallet on Tue,  5 Apr 2022
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+FASTCDR_DEPEND_MK:=	${FASTCDR_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		fastcdr
+endif
+
+ifeq (+,$(FASTCDR_DEPEND_MK)) # --------------------------------------------
+
+include ../../meta-pkgs/ros2-core/depend.common
+
+ROS2_DEPEND_USE+=	fastcdr
+
+DEPEND_ABI.fastcdr?=	fastcdr>=1
+DEPEND_DIR.fastcdr?=	../../middleware/fastcdr
+
+SYSTEM_SEARCH.fastcdr=\
+  'include/fastcdr/Cdr.h'						\
+  'lib/libfastcdr.so'							\
+  'lib/cmake/fastcdr/fastcdr-config.cmake:/fastcdr_VERSION/s/[^0-9.]//gp'
+
+endif # FASTCDR_DEPEND_MK --------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
