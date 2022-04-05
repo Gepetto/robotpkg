d608bfb0f7d20885e305d57d098bde1705b67f31
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-05 18:53:47 +0200

    [interfaces/ros2-rcl-interfaces] Import ros2-rcl-interfaces-1.2.0
    This package contains ROS2 interface files (.msg and .srv) which are used both
    to implement client library concepts and for testing.
    
    * action_msgs: Messages and services for ROS 2 actions.
    * builtin_interfaces: Message definitions for types in the OMG IDL Platform
      Specific Model.
    * composition_interfaces: Services for managing composeable nodes.
    * lifecycle_msgs: Message and service definitions for managing lifecycle nodes.
    * rcl_interfaces: Message and service definitions for ROS client libraries.
    * rosgraph_msgs: Message definitions relating the ROS Computation Graph.
    * test_msgs: Used exclusively for testing purposes.

---
+++ interfaces/ros2-rcl-interfaces/depend.mk
@@ -0,0 +1,34 @@
+# robotpkg depend.mk for:	interfaces/ros2-rcl-interfaces
+# Created:			Anthony Mallet on Wed,  6 Apr 2022
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS2_RCL_INTERFACESDEPEND_MK:=	${ROS2_RCL_INTERFACESDEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros2-rcl-interfaces
+endif
+
+ifeq (+,$(ROS2_RCL_INTERFACESDEPEND_MK)) # ---------------------------------
+
+include ../../meta-pkgs/ros2-core/depend.common
+
+ROS2_DEPEND_USE+=		ros2-rcl-interfaces
+
+DEPEND_ABI.ros2-rcl-interfaces?=ros2-rcl-interfaces>=0
+DEPEND_DIR.ros2-rcl-interfaces?=../../interfaces/ros2-rcl-interfaces
+
+SYSTEM_SEARCH.ros2-rcl-interfaces=\
+  $(call ros2_system_search,	\
+    action_msgs			\
+    builtin_interfaces		\
+    composition_interfaces	\
+    lifecycle_msgs		\
+    rcl_interfaces		\
+    rosgraph_msgs		\
+    statistics_msgs		\
+    test_msgs)
+
+endif # ROS2_RCL_INTERFACESDEPEND_MK ---------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
