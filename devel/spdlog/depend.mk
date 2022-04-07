a7f17065a283a678f5232fec8f83a14b40111e6f
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-07 12:11:05 +0200

    [devel/spdlog] Import spdlog-1.10.0
    Very fast, header-only/compiled, C++ logging library.

---
+++ devel/spdlog/depend.mk
@@ -0,0 +1,39 @@
+# robotpkg depend.mk for:	devel/ros2-rcutils
+# Created:			Anthony Mallet on Wed,  6 Apr 2022
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+SPDLOG_DEPEND_MK:=	${SPDLOG_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		spdlog
+endif
+
+ifeq (+,$(SPDLOG_DEPEND_MK)) # ---------------------------------------------
+
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+ifeq (Debian,${OPSYS})
+  ifneq (,$(filter 9 10,${OS_VERSION}))
+    PREFER.spdlog?=	robotpkg
+  endif
+else ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 16.04% 18.04%,${OS_VERSION}))
+    PREFER.spdlog?=	robotpkg
+  endif
+endif
+PREFER.spdlog?=		system
+
+DEPEND_USE+=		spdlog
+
+DEPEND_METHOD.spdlog?=	build
+DEPEND_ABI.spdlog?=	spdlog>=1.5.0
+DEPEND_DIR.spdlog?=	../../devel/spdlog
+
+SYSTEM_SEARCH.spdlog=\
+  'include/spdlog/logger.h'				\
+  'lib/cmake/spdlog/spdlogConfig.cmake'			\
+  'lib/pkgconfig/spdlog.pc:/Version/s/[^0-9.]//gp'
+
+endif # SPDLOG_DEPEND_MK ---------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
