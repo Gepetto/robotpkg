d4b11adf80c508e42c8737580977242fb9b7979d
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-09 16:10:43 +0200

    [middleware/fastrtps] Import Fast-DDS-2.6.0
    eprosima Fast DDS (formerly Fast RTPS) is a C++ implementation of the DDS (Data
    Distribution Service) standard of the OMG (Object Management Group). eProsima
    Fast DDS implements the RTPS (Real Time Publish Subscribe) protocol, which
    provides publisher-subscriber communications over unreliable transports such as
    UDP, as defined and maintained by the Object Management Group (OMG)
    consortium. RTPS is also the wire interoperability protocol defined for the
    Data Distribution Service (DDS) standard. eProsima Fast DDS expose an API to
    access directly the RTPS protocol, giving the user full access to the protocol
    internals.
    
    Some of the main features of this library are:
    
    * Configurable best-effort and reliable publish-subscribe communication
      policies for real-time applications.
    * Plug and play connectivity so that any new applications are automatically
      discovered by any other members of the network.
    * Modularity and scalability to allow continuous growth with complex and simple
      devices in the network.
    * Configurable network behavior and interchangeable transport layer: Choose the
      best protocol and system input/output channel combination for each
      deployment.
    * Two API Layers: a high-level Publisher-Subscriber one focused on usability
      (DDS) and a lower-level Writer-Reader one that provides finer access to the
      inner workings of the RTPS protocol.

---
+++ middleware/fastrtps/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	middleware/fastrtps
+# Created:			Anthony Mallet on Mon, 11 Apr 2022
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+FASTRTPS_DEPEND_MK:=	${FASTRTPS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		fastrtps
+endif
+
+ifeq (+,$(FASTRTPS_DEPEND_MK)) # -------------------------------------------
+
+include ../../meta-pkgs/ros2-core/depend.common
+
+ROS2_DEPEND_USE+=	fastrtps
+
+DEPEND_ABI.fastrtps?=	fastrtps>=1
+DEPEND_DIR.fastrtps?=	../../middleware/fastrtps
+
+SYSTEM_SEARCH.fastrtps=\
+  'include/fastrtps/config.h:/FASTRTPS_VERSION_STR/s/[^0-9.]//gp'	\
+  'lib/libfastrtps.so'							\
+  'share/fastrtps/cmake/fastrtps-config.cmake'
+
+endif # FASTRTPS_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
