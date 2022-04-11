2ba523e9a2287b1ea6927bc85f01e1e409d5eabb
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-11 17:19:41 +0200

    [middleware/ros2-rmw-dds-common] Import rmw-dds-common-1.6.0
    rmw_dds_common is a collection of C++ APIs to support DDS based, C++ ROS2
    middleware implementations.

---
+++ middleware/ros2-rmw-dds-common/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	middleware/ros2-rmw-dds-common
+# Created:			Anthony Mallet on Mon, 11 Apr 2022
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+ROS2_RMW_DDS_COMMON_DEPEND_MK:=	${ROS2_RMW_DDS_COMMON_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros2-rmw-dds-common
+endif
+
+ifeq (+,$(ROS2_RMW_DDS_COMMON_DEPEND_MK)) # --------------------------------
+
+include ../../meta-pkgs/ros2-core/depend.common
+
+ROS2_DEPEND_USE+=		ros2-rmw-dds-common
+
+DEPEND_ABI.ros2-rmw-dds-common?=ros2-rmw-dds-common>=0
+DEPEND_DIR.ros2-rmw-dds-common?=../../middleware/ros2-rmw-dds-common
+
+SYSTEM_SEARCH.ros2-rmw-dds-common=\
+  $(call ros2_system_search, rmw_dds_common)
+
+endif # ROS2_RMW_DDS_COMMON_DEPEND_MK --------------------------------------
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
