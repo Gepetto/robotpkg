e2ade715d1cb48b11e78c8f2eabd67c5b4251021
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-11 18:51:25 +0200

    [interfaces/ros2-rosidl-typesupport-fastrtps] Import 2.2.0
    rosidl typesupport for eProsima's FastRTPS.

---
+++ interfaces/ros2-rosidl-typesupport-fastrtps/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	interfaces/ros2-rosidl-typesupport-fastrtps
+# Created:			Anthony Mallet on Mon, 11 Apr 2022
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+ROS2_ROSIDL_TS_FASTRTPS_DEPEND_MK:=	${ROS2_ROSIDL_TS_FASTRTPS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			ros2-rosidl-typesupport-fastrtps
+endif
+
+ifeq (+,$(ROS2_ROSIDL_TS_FASTRTPS_DEPEND_MK)) # ----------------------------
+
+include ../../meta-pkgs/ros2-core/depend.common
+
+ROS2_DEPEND_USE+=		ros2-rosidl-typesupport-fastrtps
+
+DEPEND_DIR.ros2-rosidl-typesupport-fastrtps?=\
+  ../../interfaces/ros2-rosidl-typesupport-fastrtps
+DEPEND_ABI.ros2-rosidl-typesupport-fastrtps?=\
+  ros2-rosidl-typesupport-fastrtps>=0
+
+SYSTEM_SEARCH.ros2-rosidl-typesupport-fastrtps=\
+  $(call ros2_system_search,			\
+    fastrtps_cmake_module			\
+    rosidl_typesupport_fastrtps_c		\
+    rosidl_typesupport_fastrtps_cpp)
+
+endif # ROS2_ROSIDL_TS_FASTRTPS_DEPEND_MK ----------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
