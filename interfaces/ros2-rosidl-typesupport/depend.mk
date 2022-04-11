63d35fa91d1b86c94ff82217356abc57d8976f2f
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-11 17:27:36 +0200

    [interfaces/ros2-rosidl-typesupport] Import ros2-rosidl-typesupport-2.0.0
    rosidl_typesupport_c and rosidl_typesupport_cpp provides functionality for
    getting the associated message or service C and C++ typesupport handler
    functions.

---
+++ interfaces/ros2-rosidl-typesupport/depend.mk
@@ -0,0 +1,32 @@
+# robotpkg depend.mk for:	interfaces/ros2-rosidl-typesupport
+# Created:			Anthony Mallet on Mon, 11 Apr 2022
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+ROS2_ROSIDL_TYPESUPPORT_DEPEND_MK:=	${ROS2_ROSIDL_TYPESUPPORT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=				ros2-rosidl-typesupport
+endif
+
+ifeq (+,$(ROS2_ROSIDL_TYPESUPPORT_DEPEND_MK)) # ----------------------------
+
+include ../../meta-pkgs/ros2-core/depend.common
+
+ROS2_DEPEND_USE+=			ros2-rosidl-typesupport
+
+DEPEND_DIR.ros2-rosidl-typesupport?=	../../interfaces/ros2-rosidl-typesupport
+DEPEND_ABI.ros2-rosidl-typesupport?=	ros2-rosidl-typesupport>=0
+
+SYSTEM_SEARCH.ros2-rosidl-typesupport=\
+  $(call ros2_system_search,			\
+    rosidl_typesupport_c					\
+    rosidl_typesupport_cpp)					\
+  '${PYTHON_SITELIB}/rosidl_typesupport_c/__init__.py'		\
+  '${PYTHON_SITELIB}/rosidl_typesupport_cpp/__init__.py'
+
+include ../../mk/sysdep/python.mk
+
+endif # ROS2_ROSIDL_TYPESUPPORT_DEPEND_MK ----------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
