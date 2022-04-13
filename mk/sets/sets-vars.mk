Gepetto/robotpkg

commit 745317e11afa845ea42e6ed91baf9dcb510c5349

[mk/sets] Handles pacakge sets targets from toplevel directory
The sets targets can run from any directory. But if run form a directory that
defines some alternatives on itself or for some dependencies, those will be
implicitly inherited for the whole set. In order to avoid that, change to
toplevel directory and reset any such settings before doing anything.

mk/sets/sets.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2010-2011,2013 LAAS/CNRS
+# Copyright (c) 2010-2011,2013,2021 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use  in  source  and binary  forms,  with  or  without
@@ -64,8 +64,9 @@ set-print-var-%: .FORCE
 
 
 # --- recursion ------------------------------------------------------------
-
-override define _pkgset_recursive
+#
+ifneq (2,${_ROBOTPKG_DEPTH})
+  override define _pkgset_recursive
 	${PHASE_MSG} $(if $(filter -n,$2),'Scanning','Sorting')		\
 	  'packages for ${PKGSET_DESCR.$*}';				\
 	${TEST} -t 1 && i="-i";						\
@@ -99,4 +100,13 @@ override define _pkgset_recursive
 	done;								\
 	${PHASE_MSG} 'Done $(patsubst set-%-$*,%,$@) for'		\
 	  '${PKGSET_DESCR.$*}'
-endef
+  endef
+else
+  # Restart from toplevel directory, to avoid inheritance of alternatives
+  # defined by the package in the current directory
+  override define _pkgset_recursive
+	unset ROBOTPKG_TRUSTED_ENV;					\
+	cd ${ROBOTPKG_DIR};						\
+	${MAKE} ${MFLAGS} ${MAKEOVERRIDES} ${MAKECMDGOALS}
+  endef
+endif

commit 60135b6bd81a6f7ffe3df07ecbecb58c2765e561

[mk/sets] Fix %-depends targets (clean-depends, update-depends, ...)
A bug was introduced by commit:745317e (Handles pacakge sets targets from
toplevel directory). The %-depends set targets must run from a package
directory, as the contents of the set if computed from there.

To fix, add a new PKGSET_TOPLEVEL.<set> variable. It specifies if the set
targets need to be run at robotpkg toplevel directory. Normally, set
targets must run at the toplevel directory to avoid inheritance of
alternatives defined by a package (purpose of commit:745317e). The only
exception is the the special %-depends set, which sets the variable
accordingly.

mk/robotpkg.default.conf
@@ -29,6 +29,15 @@ PKGSET_NULLGLOB?=	no
 # least one package (useful to prevent typo).
 # Default: no
 
+PKGSET_TOPLEVEL=	yes
+# Specify if the set targets need to be run at robotpkg toplevel directory.
+# Normally, set targets must run at the toplevel directory to avoid inheritance
+# of alternatives defined by a package in the current directory. The only
+# exception is the special %-depends set, that needs to run at the package
+# level.
+# Don't change this unless you know what you are doing.
+# Default: yes
+
 
 # --- permissions for installed files --------------------------------------
 

mk/sets/sets-vars.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2010-2011,2013,2018 LAAS/CNRS
+# Copyright (c) 2010-2011,2013,2018,2022 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use  in  source  and binary  forms,  with  or  without
@@ -33,6 +33,7 @@ PKGSET_PATTERN?=	PKGSET.%
 PKGSET_FAILSAFE?=	no
 PKGSET_STRICT?=		no
 PKGSET_NULLGLOB?=	no
+PKGSET_TOPLEVEL?=	yes
 
 # names of existing sets in robotpkg.conf, plus special 'installed' and
 # 'depends' set, sorted for unicity
@@ -56,7 +57,8 @@ $(foreach _set_,${_pkgset_names},					\
   $(eval PKGSET_DESCR.${_set_}?=${_set_})				\
   $(eval PKGSET_FAILSAFE.${_set_}?=${PKGSET_FAILSAFE})			\
   $(eval PKGSET_STRICT.${_set_}?=${PKGSET_STRICT})			\
-  $(eval PKGSET_NULLGLOB.${_set_}?=${PKGSET_NULLGLOB}))
+  $(eval PKGSET_NULLGLOB.${_set_}?=${PKGSET_NULLGLOB})			\
+  $(eval PKGSET_TOPLEVEL.${_set_}?=${PKGSET_TOPLEVEL}))
 
 
 # compute PKGSET.installed if needed
@@ -73,6 +75,7 @@ ifdef PKGNAME
     PKGSET.depends=		${PKGPATH}
     PKGSET_DESCR.depends=	dependencies of ${PKGNAME}
     PKGSET_STRICT.depends=	no
+    PKGSET_TOPLEVEL.depends=	no
   endif
 endif
 

mk/sets/sets.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2010-2011,2013,2021 LAAS/CNRS
+# Copyright (c) 2010-2011,2013,2021-2022 LAAS/CNRS
 # All rights reserved.
 #
 # Redistribution  and  use  in  source  and binary  forms,  with  or  without
@@ -27,46 +27,55 @@
 # Recursive targets for a set.
 #
 set-clean-%: .FORCE
-	${RUN}$(call _pkgset_recursive,clean,-n -p)
+	${RUN}$(call _pkgset_do,clean,-n -p)
 
 set-fetch-%: .FORCE
-	${RUN}$(call _pkgset_recursive,fetch cleaner,-n -e)
+	${RUN}$(call _pkgset_do,fetch cleaner,-n -e)
 
 set-extract-%: .FORCE
-	${RUN}$(call _pkgset_recursive,extract,-1 -n -e)
+	${RUN}$(call _pkgset_do,extract,-1 -n -e)
 
 set-install-%: .FORCE
-	${RUN}$(call _pkgset_recursive,install,-1 -e)
+	${RUN}$(call _pkgset_do,install,-1 -e)
 
 set-replace-%: .FORCE
-	${RUN}$(call _pkgset_recursive,replace cleaner,-1 -e)
+	${RUN}$(call _pkgset_do,replace cleaner,-1 -e)
 
 set-update-%: .FORCE
-	${RUN}$(call _pkgset_recursive,update,-1 -e)
+	${RUN}$(call _pkgset_do,update,-1 -e)
 
 set-bulk-%: .FORCE
-	${RUN}$(call _pkgset_recursive,bulk,-e				\
-	  -t 'LOCALBASE=${BULKBASE}'					\
+	${RUN}$(call _pkgset_do,bulk,-e				\
+	  -t 'LOCALBASE=${BULKBASE}'				\
 	  -t 'EXPECT_TARGETS=fetch install package')
 
 set-deinstall-%: .FORCE
-	${RUN}$(call _pkgset_recursive,deinstall,-e -r)
+	${RUN}$(call _pkgset_do,deinstall,-e -r)
 
 set-mirror-distfiles-%: .FORCE
-	${RUN}$(call _pkgset_recursive,mirror-distfiles,-e		\
+	${RUN}$(call _pkgset_do,mirror-distfiles,-e		\
 	  -t 'EXPECT_TARGETS=mirror-distfiles')
 
 set-show-var-%: .FORCE
-	${RUN}$(call _pkgset_recursive,show-var)
+	${RUN}$(call _pkgset_do,show-var)
 
 set-print-var-%: .FORCE
-	${RUN}$(call _pkgset_recursive,print-var,-n)
+	${RUN}$(call _pkgset_do,print-var,-n)
 
 
 # --- recursion ------------------------------------------------------------
 #
-ifneq (2,${_ROBOTPKG_DEPTH})
-  override define _pkgset_recursive
+override define _pkgset_do
+	$(if $(call isyes,${PKGSET_TOPLEVEL.$*}),			\
+	  if ${TEST} "${_ROBOTPKG_DEPTH}" -ne 0; then			\
+	    ${_pkgset_restart};						\
+	    exit 0;							\
+	  fi;)								\
+	${_pkgset_recursive}
+endef
+
+# Expand and sort set and perform action for each package
+override define _pkgset_recursive
 	${PHASE_MSG} $(if $(filter -n,$2),'Scanning','Sorting')		\
 	  'packages for ${PKGSET_DESCR.$*}';				\
 	${TEST} -t 1 && i="-i";						\
@@ -100,13 +109,12 @@ ifneq (2,${_ROBOTPKG_DEPTH})
 	done;								\
 	${PHASE_MSG} 'Done $(patsubst set-%-$*,%,$@) for'		\
 	  '${PKGSET_DESCR.$*}'
-  endef
-else
-  # Restart from toplevel directory, to avoid inheritance of alternatives
-  # defined by the package in the current directory
-  override define _pkgset_recursive
+endef
+
+# Restart from toplevel directory to avoid inheritance of alternatives
+# defined by the package in the current directory.
+override define _pkgset_restart
 	unset ROBOTPKG_TRUSTED_ENV;					\
 	cd ${ROBOTPKG_DIR};						\
 	${MAKE} ${MFLAGS} ${MAKEOVERRIDES} ${MAKECMDGOALS}
-  endef
-endif
+endef