f73879c9321c25e87a6ff2a361bf6c67373ab89e
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-15 13:58:18 +0200

    [sysutils/py-rosdistro] Update to 0.8.3
    py-rosdistro is no longer a ros package (go figure...) and is provided as
    a system package on most distributions.

--- sysutils/py-rosdistro/depend.mk
+++ sysutils/py-rosdistro/depend.mk
@@ -1,5 +1,5 @@
 # robotpkg depend.mk for:	sysutils/py-rosdistro
-# Created:			Anthony Mallet on Thu, 20 Jun 2013
+# Created:			Anthony Mallet on Tue, 25 Jun 2013
 #
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH}+
@@ -11,9 +11,12 @@ endif
 
 ifeq (+,$(PY_ROSDISTRO_DEPEND_MK)) # ---------------------------------------
 
-include ../../meta-pkgs/ros-base/depend.common
-PREFER.py-rosdistro?=		${PREFER.ros-base}
-SYSTEM_PREFIX.py-rosdistro?=	${SYSTEM_PREFIX.ros-base}
+# select default preferences depending on OS/VERSION
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+ifeq (NetBSD,${OPSYS})
+  PREFER.py-rosdistro?=		robotpkg
+endif
+PREFER.py-rosdistro?=		system
 
 DEPEND_USE+=			py-rosdistro
 
@@ -22,10 +25,9 @@ DEPEND_DIR.py-rosdistro?=	../../sysutils/py-rosdistro
 
 SYSTEM_SEARCH.py-rosdistro=\
   'bin/rosdistro_build_cache'				\
-  '${PYTHON_SYSLIBSEARCH}/rosdistro/__init__.py'
-  '${PYTHON_SYSLIBSEARCH}/rosdistro/_version.py:/__version__/s/[^0-9.]//gp'
+  '${PYTHON_SYSLIBSEARCH}/rosdistro/__init__.py:/__version__/s/[^0-9.]//gp'
 
-SYSTEM_PKG.Ubuntu.py-rosdistro=python-rosdistro
+SYSTEM_PKG.py-rosdistro=python${PYTHON_MAJOR}-rosdistro
 
 include ../../mk/sysdep/python.mk
 
