2051c9af847222da2cfa7deb1ce116da95a4c2d0
Anthony Mallet <anthony.mallet@laas.fr>
2022-04-21 14:38:49 +0200

    [sysutils/py-ament-package] Add runtime dependencies
    Runtime needs:
    * importlib.resource for python>=3.7, but fallbacks to importlib_resources
    * importlib.metadata for python>=3.8, but fallbacks to importlib_metadata
    
    Include the appropriate sysdep when needed, by appending the directive to
    the python alternative selection macro.

--- sysutils/py-ament-package/depend.mk
+++ sysutils/py-ament-package/depend.mk
@@ -22,8 +22,35 @@ SYSTEM_SEARCH.py-ament-package=\
   'share/ament_package/package.xml:/<version>/s/[^0-9.]//gp'
 
 DEPEND_ABI.python+= python>=3.6
-
 include ../../mk/sysdep/python.mk
+
+# runtime needs:
+# importlib.resource for python>=3.7, but fallbacks to importlib_resources
+# importlib.metadata for python>=3.8, but fallbacks to importlib_metadata
+define PKG_ALTERNATIVE_SET.python27+=
+
+  include ../../mk/sysdep/py-importlib-metadata.mk
+  include ../../mk/sysdep/py-importlib-resources.mk
+endef
+define PKG_ALTERNATIVE_SET.python34+=
+
+  include ../../mk/sysdep/py-importlib-metadata.mk
+  include ../../mk/sysdep/py-importlib-resources.mk
+endef
+define PKG_ALTERNATIVE_SET.python35+=
+
+  include ../../mk/sysdep/py-importlib-metadata.mk
+  include ../../mk/sysdep/py-importlib-resources.mk
+endef
+define PKG_ALTERNATIVE_SET.python36+=
+
+  include ../../mk/sysdep/py-importlib-metadata.mk
+  include ../../mk/sysdep/py-importlib-resources.mk
+endef
+define PKG_ALTERNATIVE_SET.python37+=
+
+  include ../../mk/sysdep/py-importlib-metadata.mk
+endef
 
 endif # PY_AMENT_PACKAGE_DEPEND_MK -----------------------------------------
 
