a6e276c7e2a497e0079c45b065ddc814e070ec00
Anthony Mallet <anthony.mallet@laas.fr>
2022-05-13 18:05:14 +0200

    [sysutils/py-rosdistro] Fix SYSTEM_SEARCH on ubuntu2204
    There is a mess with ros2 and python3-rosdistro on ubuntu2204.
    Both a system python3-rosdistro and a ros2 python3-rosdistro-modules exist and
    are conflicting. It happens that ros2 is built with the latter, that must thus
    be installed, but lacks the various script normally provided by this package.
    
    So to overcome all this, don't check for the scripts in SYSTEM_SEARCH...

--- sysutils/py-rosdistro/depend.mk
+++ sysutils/py-rosdistro/depend.mk
@@ -24,7 +24,6 @@ DEPEND_ABI.py-rosdistro?=	${PKGTAG.python}-rosdistro>=0.2.9
 DEPEND_DIR.py-rosdistro?=	../../sysutils/py-rosdistro
 
 SYSTEM_SEARCH.py-rosdistro=\
-  'bin/rosdistro_build_cache'				\
   '${PYTHON_SYSLIBSEARCH}/rosdistro/__init__.py:/__version__/s/[^0-9.]//gp'
 
 SYSTEM_PKG.py-rosdistro=python${PYTHON_MAJOR}-rosdistro
