71eafdc76cc48e9da8ce479ab7d59f5677eb440d
Anthony Mallet <anthony.mallet@laas.fr>
2023-08-23 11:31:01 +0200

    [devel/ros2-python-cmake-module] Set PYTHON_EXT_SUFFIX consistently
    Make sure that the package variable PythonExtra_EXTENSION_SUFFIX is set
    to robotpkg PYTHON_EXT_SUFFIX, according to the system in use. Update patch-aa
    to reflect this.
    
    When using a vanilla system pacakge, an empty PYTHON_EXT_SUFFIX will be
    rejected (see patch-aa). But on systems where there is a python-cmake-module
    system package, the definition is not supposed to be empty.

--- devel/ros2-python-cmake-module/depend.mk
+++ devel/ros2-python-cmake-module/depend.mk
@@ -22,6 +22,14 @@ DEPEND_DIR.ros2-python-cmake-module?=	../../devel/ros2-python-cmake-module
 SYSTEM_SEARCH.ros2-python-cmake-module=\
   $(call ros2_system_search, python_cmake_module)
 
+include ../../mk/sysdep/python.mk
+
+# Set EXT_SUFFIX consistently.
+# When using a vanilla system package, an empty PYTHON_EXT_SUFFIX will be
+# rejected (see patch-aa). But on systems where there is a python-cmake-module
+# system package, the definition is not supposed to be empty.
+CMAKE_ARGS+=-DPythonExtra_EXTENSION_SUFFIX=${PYTHON_EXT_SUFFIX:.so=}
+
 endif # ROS2_PYTHON_CMAKE_DEPEND_MK ----------------------------------------
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
