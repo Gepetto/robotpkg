b11faebd105c59ebd486fe8b37241c532c560744
Anthony Mallet <anthony.mallet@laas.fr>
2023-09-05 00:03:22 +0200

    [interfaces/ros2-rcl-interfaces] Update to 1.7.0
    This packages in split in two (see ros2-rcl-core-interfaces) since the
    addition of rosidl-core. Only the packages using rosidl-default remains here.
    
    Changes since 1.2.0:
    * Add interfaces for logging service. (%154)

--- interfaces/ros2-rcl-interfaces/depend.mk
+++ interfaces/ros2-rcl-interfaces/depend.mk
@@ -1,5 +1,5 @@
 # robotpkg depend.mk for:	interfaces/ros2-rcl-interfaces
-# Created:			Anthony Mallet on Wed,  6 Apr 2022
+# Created:			Anthony Mallet on Tue, 5 Apr 2022
 #
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH}+
@@ -15,13 +15,11 @@ include ../../meta-pkgs/ros2-core/depend.common
 
 ROS2_DEPEND_USE+=		ros2-rcl-interfaces
 
-DEPEND_ABI.ros2-rcl-interfaces?=ros2-rcl-interfaces>=0
+DEPEND_ABI.ros2-rcl-interfaces?=ros2-rcl-interfaces>=1.5.0
 DEPEND_DIR.ros2-rcl-interfaces?=../../interfaces/ros2-rcl-interfaces
 
 SYSTEM_SEARCH.ros2-rcl-interfaces=\
   $(call ros2_system_search,	\
-    action_msgs			\
-    builtin_interfaces		\
     composition_interfaces	\
     lifecycle_msgs		\
     rcl_interfaces		\
