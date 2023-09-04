df98f9ecf76b88a2d64451e7eabe2a2052e9b1ff
Anthony Mallet <anthony.mallet@laas.fr>
2023-09-05 00:11:45 +0200

    [middleware/ros2-rmw] Update to 7.2.1
    This is required to handle new dependency on dynamic-typesupport
    
    Changes since 6.1.0:
    * Add 'best available' QoS policies (%320)
    * Add rmw count clients, services (%334)
    * Reduce GID storage to 16 bytes. (%345)
    * Add matched event support (%331)
    * Bugfixes

--- middleware/ros2-rmw/depend.mk
+++ middleware/ros2-rmw/depend.mk
@@ -15,13 +15,16 @@ include ../../meta-pkgs/ros2-core/depend.common
 
 ROS2_DEPEND_USE+=	ros2-rmw
 
-DEPEND_ABI.ros2-rmw?=	ros2-rmw>=0
+# version 7.1 introduced dynamic-typesupport dependency
+DEPEND_ABI.ros2-rmw?=	ros2-rmw>=7.1
 DEPEND_DIR.ros2-rmw?=	../../middleware/ros2-rmw
 
 SYSTEM_SEARCH.ros2-rmw=\
   $(call ros2_system_search,	\
     rmw				\
     rmw_implementation_cmake)
+
+include ../../interfaces/ros2-rosidl-dynamic-typesupport/depend.mk
 
 endif # ROS2_RMW_DEPEND_MK -------------------------------------------------
 
