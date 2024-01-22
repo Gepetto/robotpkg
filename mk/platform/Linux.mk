7da6f4acbc102aa2691cb5e0dd4ba1c707d27d6b
Anthony Mallet <anthony.mallet@laas.fr>
2024-01-22 21:38:17 +0100

    [mk/platform] Use pathsearch from macro.mk instead of re-coding it

--- mk/platform/Linux.mk
+++ mk/platform/Linux.mk
@@ -24,8 +24,7 @@ $(call setdefault, EXPR,	/usr/bin/expr)
 $(call setdefault, GREP,	/bin/grep)
 $(call setdefault, EGREP,	/bin/egrep)
 $(call setdefault, SED,		/bin/sed)
-$(call setdefault, SORT,\
-	$(firstword $(realpath $(addsuffix /sort,/bin /usr/bin))))
+$(call setdefault, SORT,	$(call pathsearch sort,/bin:/usr/bin))
 $(call setdefault, TOUCH,	/bin/touch)
 $(call setdefault, XARGS,	/usr/bin/xargs -r)
 
