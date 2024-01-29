8051911c17d35f14772af9ff4c3ff426019faa2b
Anthony Mallet <anthony.mallet@laas.fr>
2024-01-29 12:23:56 +0100

    [graphics/assimp] Default to system package everywhere
    NetBSD/pkgsrc now has recent assimp, no need to build it from robotpkg.

--- graphics/assimp/depend.mk
+++ graphics/assimp/depend.mk
@@ -11,27 +11,21 @@ endif
 
 ifeq (+,$(ASSIMP_DEPEND_MK)) # ---------------------------------------------
 
-include ../../mk/robotpkg.prefs.mk # for OPSYS
-ifeq (Gentoo,${OS_FAMILY})
-  PREFER.assimp?=	robotpkg
-else ifeq (NetBSD,${OPSYS})
-  PREFER.assimp?=	robotpkg
-endif
 PREFER.assimp?=		system
 
 DEPEND_USE+=		assimp
 
-DEPEND_ABI.assimp?=	assimp>=2.0
+DEPEND_ABI.assimp?=	assimp>=3.0
 DEPEND_DIR.assimp?=	../../graphics/assimp
 
 SYSTEM_SEARCH.assimp=\
-	bin/assimp						\
-	'include/assimp/{aiV,v}ersion.h'			\
-	'lib/pkgconfig/assimp.pc:/Version/s/[^0-9.]//gp'	\
-	lib/libassimp.so
+  'include/assimp/{aiV,v}ersion.h'			\
+  'lib/pkgconfig/assimp.pc:/Version/s/[^0-9.]//gp'	\
+  'lib/libassimp.so'
 
 SYSTEM_PKG.RedHat.assimp=assimp assimp-devel
 SYSTEM_PKG.Debian.assimp=assimp-utils libassimp-dev
+SYSTEM_PKG.NetBSD.assimp=multimedia/assimp
 
 endif # ASSIMP_DEPEND_MK ---------------------------------------------------
 
