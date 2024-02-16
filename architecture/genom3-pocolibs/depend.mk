6b51653e70816c69107d419d677f930c13fc85f5
Anthony Mallet <anthony.mallet@laas.fr>
2024-02-16 13:22:36 +0100

    [architecture/genom3-pocolibs] Update to 1.23
    Changes since 1.22:
    * Optimize serialization of unbounded sequences of basic types
    * Retrieve CFLAGS/LDFLAGS from the component "requires" list
    * Fix handling of events from activities imported from interfaces
    * Constify the exception detail in function raising exceptions

--- architecture/genom3-pocolibs/depend.mk
+++ architecture/genom3-pocolibs/depend.mk
@@ -18,7 +18,7 @@ SYSTEM_SEARCH.genom3-pocolibs=\
 	lib/libpocolibs-client.so				\
 	'lib/pkgconfig/genom3-pocolibs.pc:/Version/s/[^0-9.]//gp'
 
-DEPEND_ABI.genom3-pocolibs?=	genom3-pocolibs>=1.13
+DEPEND_ABI.genom3-pocolibs?=	genom3-pocolibs>=1.23
 DEPEND_DIR.genom3-pocolibs?=	../../architecture/genom3-pocolibs
 
 include ../../mk/language/c11.mk
