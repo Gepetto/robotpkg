1a823eb2992369d8f555f63db1e3da7fd57cbb08
Anthony Mallet <anthony.mallet@laas.fr>
2024-05-07 19:19:41 +0200

    [interfaces/ros2-rosidl-python] Update to 0.21.1
    Changes since 0.20:
    * Install compiled libraries only to 'lib' (#195)
    
    This version installs one file less, so require it as a minimum version to
    avoid dealing with PLIST complexity. This corresponds to the packaged version
    in ubuntu-22.04.
    
    Also, deal with python-cmake-module<0.12 when used as a system package (only
    ubuntu-22.04) that will compile python libraries with a PYTHON_EXT_SUFFIX
    extension. In order to avoid too much PLIST complexity, create a PLIST filter
    for this case, adding the required PYTHON_EXT_SUFFIX.

--- interfaces/ros2-rosidl-python/depend.mk
+++ interfaces/ros2-rosidl-python/depend.mk
@@ -16,7 +16,7 @@ include ../../meta-pkgs/ros2-core/depend.common
 ROS2_DEPEND_USE+=		ros2-rosidl-python
 
 DEPEND_DIR.ros2-rosidl-python?=	../../interfaces/ros2-rosidl-python
-DEPEND_ABI.ros2-rosidl-python?=	ros2-rosidl-python>=0
+DEPEND_ABI.ros2-rosidl-python?=	ros2-rosidl-python>=0.21
 
 SYSTEM_SEARCH.ros2-rosidl-python=\
   $(call ros2_system_search, rosidl_generator_py)	\
@@ -27,9 +27,23 @@ include ../../sysutils/py-ros2-rpyutils/depend.mk
 include ../../mk/sysdep/py-numpy.mk
 include ../../mk/sysdep/python.mk
 
-# For some reason, those files are compiled as bare .so
+# For python-cmake-module<0.12 as a system package, PYTHON_EXT_SUFFIX is
+# needed.
+#
+PLIST_FILTER+=\
+  $(if $(and								\
+    $(filter system,${PREFER.ros2-python-cmake-module}),		\
+    $(call isyes,$(call pmatch,						\
+      ros2-python-cmake-module<0.12,					\
+      ${PKGVERSION.ros2-python-cmake-module}))),			\
+    | ${AWK} '								\
+        /$(subst /,\/,${PYTHON_SITELIB})/ && /[.]so$$/ {		\
+          gsub(/[.]so$$/, "${PYTHON_EXT_SUFFIX}");			\
+        }								\
+        {print}')
+
 PRINT_PLIST_AWK_SUBST+=\
-  gsub(/generator_py[$$]{PYTHON_EXT_SUFFIX}$$/, "generator_py.so")
+  gsub(/[$$]{PYTHON_EXT_SUFFIX}$$/, ".so")
 
 endif # ROS2_ROSIDL_PYTHON_DEPEND_MK ---------------------------------------
 
