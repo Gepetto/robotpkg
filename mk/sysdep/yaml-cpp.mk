4ab2e8202be78e738a01bf713b62fced159ec2b7
Anthony Mallet <anthony.mallet@laas.fr>
2024-06-19 11:50:45 +0200

    [devel/yaml-cpp] Remove package available on all platforms
    Turn this package into a sysdep-only as a recent enough version is provided by
    all supported distributions.

--- mk/sysdep/yaml-cpp.mk
+++ mk/sysdep/yaml-cpp.mk
@@ -11,31 +11,20 @@ endif
 
 ifeq (+,$(YAML_CPP_DEPEND_MK)) # -------------------------------------------
 
-include ../../mk/robotpkg.prefs.mk # for OPSYS
-
-ifeq (Ubuntu,${OPSYS})
-  ifneq (,$(filter 10.04 12.04,${OS_VERSION}))
-    PREFER.yaml-cpp?=	robotpkg
-  endif
-else ifeq (NetBSD,${OPSYS})
-  PREFER.yaml-cpp?=	robotpkg
-endif
 PREFER.yaml-cpp?=	system
-
 DEPEND_USE+=		yaml-cpp
-
 DEPEND_ABI.yaml-cpp?=	yaml-cpp>=0.5
-DEPEND_DIR.yaml-cpp?=	../../devel/yaml-cpp
 
 SYSTEM_SEARCH.yaml-cpp=\
-	include/yaml-cpp/yaml.h					\
-	'lib/libyaml-cpp.{so,a}'				\
-	'{lib,share}/pkgconfig/yaml-cpp.pc:/Version/s/[^0-9.]//gp'
+  'include/yaml-cpp/yaml.h'					\
+  'lib/libyaml-cpp.{so,a}'					\
+  '{lib,share}/pkgconfig/yaml-cpp.pc:/Version/s/[^0-9.]//gp'
 
 SYSTEM_PKG.Arch.yaml-cpp=	yaml-cpp
 SYSTEM_PKG.RedHat.yaml-cpp=	yaml-cpp-devel
 SYSTEM_PKG.Debian.yaml-cpp=	libyaml-cpp-dev
 SYSTEM_PKG.Gentoo.yaml-cpp=	dev-cpp/yaml-cpp
+SYSTEM_PKG.NetBSD.yaml-cpp=	textproc/yaml-cpp
 
 endif # YAML_CPP_DEPEND_MK -------------------------------------------------
 
